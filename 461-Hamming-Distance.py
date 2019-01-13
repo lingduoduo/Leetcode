e09166850612e5a09095b8fc25f7607a1a1a4140
linghduoduo <linghypshen@gmail.com>
2019-01-13 11:02:33 -0500

    add 461-Hamming-Distance.py

---
+++ 461-Hamming-Distance.py
@@ -0,0 +1,15 @@
+def Hamming_distince(x, y):
+	result = 0
+	nLoop = max(x,y)
+	while nLoop>0:
+		bx = x%2
+		by = y%2
+		x=x/2
+		y=y/2
+		nLoop = max(x, y)
+		if bx!=by:
+			result +=1
+	return result
+
+if __name__== "__main__":
+	print(Hamming_distince(1, 15))
