0dbd1d389cf1f37838d0a283110de1b2c02a1abd
linghduoduo <linghypshen@gmail.com>
2019-01-21 15:06:13 -0500

    add other methods

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -1,3 +1,4 @@
+# Method 1 
 def twoSum(rawList, target):
 	for k, v in enumerate(rawList):
 		i = k+1
@@ -12,3 +13,21 @@ if __name__=="__main__":
 	numbers=[2, 7, 11, 15]
 	results = twoSum(numbers, 9)
 	print(results)
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        hash_map = {}
+        for i,v in enumerate(nums):
+            hash_map[v]=i
+
+        for index1, v in enumerate(nums):
+            if target-v in hash_map.keys():
+                index2 = hash_map[target-v]
+                return [index1+1, index2+1]
+
+if __name__ == "__main__":
+    numbers=[2,7,11,15]
+    result = Solution().twoSum(numbers, 9)
+    print result
+    print 'Done'
