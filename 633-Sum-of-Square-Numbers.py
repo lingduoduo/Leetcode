24bc98e1e13f1896acc047e2335e8833177b3581
linghduoduo <linghypshen@gmail.com>
2019-01-27 23:03:27 -0500

    633-Sum-of-Square-Numbers.py

---
+++ 633-Sum-of-Square-Numbers.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        n = int(c**(1/2.0)+1)
+        for i in range(n):
+        	a = int((c-i*i)**(1/2.0))
+        	if i*i + a*a == c:
+        		return True
+        return False
