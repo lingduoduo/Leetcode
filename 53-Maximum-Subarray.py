06b9441cdc1d11876d76fb9852e4a4c00398d932
linghduoduo <linghypshen@gmail.com>
2019-01-28 22:17:41 -0500

    53-Maximum-Subarray.py

---
+++ 53-Maximum-Subarray.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        f = [0]*len(nums)
+        f[0] = nums[0]
+        for i in range(1,len(nums)):
+        	f[i] = max(f[i-1]+nums[i], nums[i])
+        return max(f)
+        
+
