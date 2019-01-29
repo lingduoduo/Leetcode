04559b211dde819cae31ac945d6908e45bb293d7
linghduoduo <linghypshen@gmail.com>
2019-01-28 22:03:16 -0500

    70-Climbing-Stairs.py

---
+++ 70-Climbing-Stairs.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        f=dict()
+        f[0]=1
+        f[1]=1
+        for i in range(2, n+1):
+        	f[i] = f[i-1] + f[i-2]
+        return f[n]
+        
