9983c5d9adffe853b68002f17317a4b225ab7e15
linghduoduo <linghypshen@gmail.com>
2019-01-29 19:17:12 -0500

    add 033-Search-in-Rotated-Sorted-Array.py

--- 033-Search-in-Rotated-Sorted-Array.py
+++ 033-Search-in-Rotated-Sorted-Array.py
@@ -5,11 +5,25 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        if nums is None:
-        	return -1
+        start = 0
+        end = len(nums)-1
 
-        for i in len(range(nums)):
-        	if target == nums[i]:
-        		return i
+        while start <= end:
+        	mid = (start+end)//2
 
+        	if nums[mid]==target:
+        		return mid
+
+        	if nums[mid] >= nums[start]:
+        		if nums[start]<= target and target<=nums[mid]:
+        			end = mid -1
+        		else:
+        			start = mid+1
+
+        	if nums[mid] >= nums[end]:
+        		if nums[mid]<= target and target <= nums[end]:
+        			start = mid+1
+        		else:
+        			end = mid -1
         return -1
+
