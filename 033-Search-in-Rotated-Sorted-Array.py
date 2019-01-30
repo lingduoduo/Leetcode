b5a792ecf1ff4f22ffb33e8c0713e6e460ab230c
linghduoduo <linghypshen@gmail.com>
2019-01-30 17:05:16 -0500

    033-Search-in-Rotated-Sorted-Array.py

--- 033-Search-in-Rotated-Sorted-Array.py
+++ 033-Search-in-Rotated-Sorted-Array.py
@@ -5,25 +5,28 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        start = 0
-        end = len(nums)-1
-
-        while start <= end:
-        	mid = (start+end)//2
-
-        	if nums[mid]==target:
-        		return mid
-
-        	if nums[mid] >= nums[start]:
-        		if nums[start]<= target and target<=nums[mid]:
-        			end = mid -1
-        		else:
-        			start = mid+1
-
-        	if nums[mid] >= nums[end]:
-        		if nums[mid]<= target and target <= nums[end]:
-        			start = mid+1
-        		else:
-        			end = mid -1
+        start=0
+        end=len(nums)-1
+        
+        while start<=end:
+        
+            mid=(start+end)/2
+        
+            if nums[mid]==target:
+                return mid
+                
+            if nums[mid]>=nums[start]:                              #当nums[mid]属于左边升序序列时
+        
+                if target>=nums[start] and target<nums[mid]:
+                    end=mid-1
+                else:
+                    start=mid+1
+            
+            if nums[mid]<nums[end]:                                 #当nums[mid]属于右边升序序列时
+        
+                if target>nums[mid] and target<=nums[end]:
+                    start=mid+1
+                else:
+                    end=mid-1
+                
         return -1
-
