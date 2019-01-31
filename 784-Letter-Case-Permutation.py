7d43f880259535e8377819d068a398b4c658b58f
linghduoduo <linghypshen@gmail.com>
2019-01-30 19:08:33 -0500

    784-Letter-Case-Permutation.py

---
+++ 784-Letter-Case-Permutation.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def letterCasePermutation(self, S):
+        """
+        :type S: str
+        :rtype: List[str]
+        """
+        result = list()
+
+        def dfs(S, i):
+          if i == len(S):
+            result.append("".join(S))
+            return
+          
+          S[i] = S[i].lower()
+          dfs(S, i + 1)      
+          if not S[i].isalpha(): return      
+          S[i] = S[i].upper()
+          dfs(S, i + 1)
+          
+
+        dfs(list(S), 0)
+        return result
+
