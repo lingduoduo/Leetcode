f204f7545db26f4fd9f7b7344fd0684a062cfbc5
linghduoduo <linghypshen@gmail.com>
2019-01-30 20:11:05 -0500

    69-Sqrt(x).py

---
+++ 69-Sqrt(x).py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        # i = 0
+        # while i*i < x:
+        # 	i+=1
+        # if i*i == x:
+        #     return i
+        # else:
+        #     return i-1
+
+        L = 0
+        R = x
+        while L<=R:
+        	mid = (L+R)//2
+        	if mid**2 == x:
+        		return mid
+        	elif mid**2 > x:
+        		R = mid-1
+        	else:
+        		L = mid+1
+        return R
+
