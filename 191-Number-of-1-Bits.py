c02d200970d6cd6a580a4a19194ac16cb0140d2c
linghduoduo <linghypshen@gmail.com>
2019-01-31 22:45:16 -0500

    191-Number-of-1-Bits.py

---
+++ 191-Number-of-1-Bits.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        result = 0
+
+        while n>0:
+        	if n%2 == 1:
+        		result += 1
+        	n=n//2
+
+        return result
+
+
+
+        
