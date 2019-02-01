fd86c4dd2dad54d1f0ffb3b1d34b14bb969a3227
linghduoduo <linghypshen@gmail.com>
2019-01-31 19:35:20 -0500

    951-Flip-Equivalent-Binary-Trees.py

---
+++ 951-Flip-Equivalent-Binary-Trees.py
@@ -0,0 +1,59 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def flipEquiv(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: bool
+        """
+        if root1 is None and root2 is None:
+        	return True
+        if root1 is None and root2 is not None:
+        	return False
+        if root1 is not None and root2 is None:
+        	return False
+        if root1.val != root2.val:
+        	return False
+        flag1 = self.flipEquiv(root1.left, root2.right)
+        flag2 = self.flipEquiv(root1.right, root2.left)
+
+        flag3 = self.flipEquiv(root1.left, root2.left)
+        flag4 = self.flipEquiv(root1.right, root2.right)
+
+         and ((flag1 and flag2) or (flag1 and flag2)):
+        	return True
+        else:
+        	return False
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def flipEquiv(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: bool
+        """
+        if not root1 and not root2: return True
+        if not root1 and root2: return False
+        if root1 and not root2: return False
+        if root1.val != root2.val: return False
+        return (self.flipEquiv(root1.left, root2.right) 
+        	and self.flipEquiv(root1.right, root2.left)) 
+        or (self.flipEquiv(root1.left, root2.left) and 
+        	self.flipEquiv(root1.right, root2.right))
+
+
+
