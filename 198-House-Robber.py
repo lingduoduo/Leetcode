07a47ce5ea6195d4a0d7b73c3898c29904b88920
linghduoduo <linghypshen@gmail.com>
2019-02-01 21:18:49 -0500

    198-House-Robber.py

---
+++ 198-House-Robber.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if not nums :
+            return 0
+        if len(nums)==1:
+        	return nums[0]
+        
+        dp = [0]*(len(nums)+1)
+        dp[0] = 0
+        dp[1] = nums[0]
+        for i in range(1, len(nums)):
+        	dp[1+i] = max(dp[i-1]+nums[i], dp[i])
+        
+        return dp[len(nums)]
