2184885abaccfc390fa69b48417ac5c367076d2d
linghduoduo <linghypshen@gmail.com>
2019-02-02 21:31:01 -0500

    409-Longest-Palindrome.py

---
+++ 409-Longest-Palindrome.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        freqs = dict()
+        for i in s:
+        	try:
+        		freqs[i] += 1
+        	except:
+        		freqs[i] = 1
+
+        result = 0
+        odd = 0
+        for v in freqs.values():
+        	result += v//2*2
+        	if v%2 == 1:
+        		odd = 1
+        result = result + odd
+        return result
+
+if __name__ == "__main__":
+    numbers="abccccdd"
+    result = Solution().longestPalindrome(numbers)
+    print(result)
+    print('Done')
