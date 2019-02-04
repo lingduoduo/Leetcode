0eb2d6c1044a70788c71dc33c2cc9638c6b1c329
linghduoduo <linghypshen@gmail.com>
2019-02-03 22:37:31 -0500

    167-Two-Sum-II-Input-array-is-sorted.py

---
+++ 167-Two-Sum-II-Input-array-is-sorted.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """     
+        d=dict()
+        
+        for i in range(len(numbers)):
+            g = d.setdefault(numbers[i], list())
+            g.append(i)
+        
+        for num in numbers:
+            if target-num in d.keys():
+                idx1 = d[num].pop(0)+1
+                idx2 = d[target-num].pop(0)+1
+                return [idx1, idx2]
+        return [0]
