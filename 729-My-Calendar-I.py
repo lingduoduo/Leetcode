71f920be94b7f9fbb01f262665df68f97978a0ab
linghduoduo <linghypshen@gmail.com>
2019-02-04 20:16:35 -0500

    731-My-Calendar-II.py

--- 729-My-Calendar-I.py
+++ 729-My-Calendar-I.py
@@ -10,6 +10,16 @@ class MyCalendar(object):
         :type end: int
         :rtype: bool
         """
+
+        if start_lists is None and end_lists is None:
+        	start_lists.append(start)
+        	end_lists.append(end)
+
+        for i in range(len(start_lists)):
+        	if max(start_lists[i], start) < min(end_lists[i], end):
+        		return False
+        return True
+
         
 
 
