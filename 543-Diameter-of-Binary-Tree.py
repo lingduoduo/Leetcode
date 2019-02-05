9b916379bb9c872e35d16b24ca1b04dc7870cc1f
linghduoduo <linghypshen@gmail.com>
2019-02-05 17:59:22 -0500

    543-Diameter-of-Binary-Tree.py

---
+++ 543-Diameter-of-Binary-Tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        result = 0
+
+        def depth(root):
+        	if root is None:
+        		return 0
+        	if root.left is None and root.right is None:
+        		return 1
+        	l = depth(root.left)
+        	r = depth(root.right)
+        	result = max(result, l+r)
+        	return max(l, r)
+
+        return result
