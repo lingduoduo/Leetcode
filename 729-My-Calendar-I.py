8ef4f84e8fdc56d072dd890eb67a5da5fafc94bc
linghduoduo <linghypshen@gmail.com>
2019-02-04 19:15:34 -0500

    729-My-Calendar-I.py

---
+++ 729-My-Calendar-I.py
@@ -0,0 +1,18 @@
+class MyCalendar(object):
+
+    def __init__(self):
+    	self.start_lists = list()
+    	self.end_lists = list()
+        
+    def book(self, start, end):
+        """
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
