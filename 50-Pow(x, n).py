78b99fb85dc7b35c133c12e62d640517a3c94df3
linghduoduo <linghypshen@gmail.com>
2019-02-07 00:05:06 -0500

    50-Pow(x, n).py

---
+++ 50-Pow(x, n).py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        ## 二分求幂
+        if n==0:
+        	return 1
+
+        if n<0:
+        	x = 1/x
+        	n = -n
+        if n%2:
+        	return x*self.myPow(x, n-1)
+        retrun self.myPow(x*x, n/2)
+
