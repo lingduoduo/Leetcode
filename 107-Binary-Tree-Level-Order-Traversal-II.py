cf0fca84b8e2936077a9fe92262cb2286ae926cd
linghduoduo <linghypshen@gmail.com>
2019-02-06 22:54:19 -0500

    107-Binary-Tree-Level-Order-Traversal-II.py

---
+++ 107-Binary-Tree-Level-Order-Traversal-II.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.DFS(root, result, 0)
+        return result[::-1]
+
+    def DFS(self, root, result, level):
+    	if not root: return
+    	if level>= len(result):
+    		result.append([])
+    	result[level].append(root.val)
+    	self.DFS(root.left, res, level+1)
+    	self.DFS(root.right, res, level+1)
+
+        
+
+
