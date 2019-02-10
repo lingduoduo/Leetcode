ba84e91328898414a1883b3e75744e719eb4a4de
linghduoduo <linghypshen@gmail.com>
2019-02-09 19:51:32 -0500

    006-ZigZag-Conversion.py

--- 006-ZigZag-Conversion.py
+++ 006-ZigZag-Conversion.py
@@ -5,4 +5,12 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
-        
+        if numRows == 1:
+            return s
+        step, zigzag = 2 * numRows - 2, ""
+        for i in xrange(numRows):
+            for j in xrange(i, len(s), step):
+                zigzag += s[j]
+                if 0 < i < numRows - 1 and j + step - 2 * i < len(s):
+                    zigzag += s[j + step - 2 * i]
+        return zigzag
