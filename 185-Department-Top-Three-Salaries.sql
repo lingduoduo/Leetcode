cf85a5279fdb6511969c2ae591d69fac76c178ee
linghduoduo <linghypshen@gmail.com>
2019-02-10 19:30:07 -0500

    185-Department-Top-Three-Salaries.sql

---
+++ 185-Department-Top-Three-Salaries.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT D.Name as Department,
+E.Name as Employee,
+E.Salary
+FROM Department D,
+Employee E,
+Employee E2
+WHERE D.ID = E.DepartmentId
+AND E.DepartmentId = E2.DepartmentId 
+AND E.Salary <= E2.Salary
+GROUP by D.ID, E.Name 
+HAVING count(distinct E2.Salary) <= 3
+ORDER by D.Name, E.Salary desc
