ef511ceab7ec23535c78a3092784dde35e9300db
linghduoduo <linghypshen@gmail.com>
2019-02-10 19:50:50 -0500

    196-Delete-Duplicate-Emails.sql

---
+++ 196-Delete-Duplicate-Emails.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+DELETE FROM Person
+WHERE Person.Id not in (
+    select Person_derived.minId from(
+        select min(Id) as minId from Person group by Email
+    )Person_derived
+)
