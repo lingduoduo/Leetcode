d11a5d0fe2ce0be6693555285fc6269e0671bdc2
linghduoduo <linghypshen@gmail.com>
2019-02-12 18:22:04 -0500

    189-Rotate-Array.py

---
+++ 189-Rotate-Array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        changed = list()
+        while k>len(nums):
+            k-=len(nums)
+        changed = nums[-k:] + nums[:-k]
+        for i in range(len(nums)):
+            nums[i] = changed[i]
