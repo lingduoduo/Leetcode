797cda55d3ec37e21374fd0812005503c74ae357
linghduoduo <linghypshen@gmail.com>
2019-02-12 21:34:02 -0500

    400-Nth-Digit.py

---
+++ 400-Nth-Digit.py
@@ -0,0 +1,23 @@
+# 1   1-9
+# 2   10-99
+# 3   100-999
+# 4   1000-9999
+# 5   10000-99999
+# 6   100000-999999
+# 7   1000000-9999999
+# 8   10000000-99999999
+# 9   100000000-99999999
+
+class Solution(object):
+    def findNthDigit(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        for i in range(9):
+            d = 9 * pow(10, i)
+            if n <= d * (i + 1): break
+            n -= d * (i+1)
+        n -= 1
+        return int(str(pow(10, i) + n/(i+1))[n % (i+1)])
