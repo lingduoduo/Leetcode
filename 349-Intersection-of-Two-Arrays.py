b98df5aa38321c05ac247a02582551eb18f4a50a
linghduoduo <linghypshen@gmail.com>
2019-02-13 22:09:30 -0500

    349-Intersection-of-Two-Arrays.py

---
+++ 349-Intersection-of-Two-Arrays.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        result = set()
+        
+        l1 = len(nums1)
+        l2 = len(nums2)
+
+        if l1<l2:
+        	for i in range(l1):
+        		if nums1[i] in nums2:
+        			result.add(nums1[i])
+        else:
+        	for i in range(l2):
+        		if nums2[i] in nums1:
+        			result.add(nums2[i])
+        return list(result)
+
+
+        #return list(set(nums1) & set(nums2))
