f2522774211ed9a7cc24e7d149c8adf6ecfe471c
linghduoduo <linghypshen@gmail.com>
2019-02-15 22:31:28 -0500

    changes

--- 283-Move-Zeroes.py
+++ 283-Move-Zeroes.py
@@ -4,10 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        pos = list()
         for i in range(len(nums)-1, -1, -1):
-            if nums[i] == 0:
-                pos.append(i)
-        for i in pos:
-            nums.pop(i)
-            nums.append(0)
+            if nums[i]==0:
+                nums.pop(i)
+                nums.append(0)
