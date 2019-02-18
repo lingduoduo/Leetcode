a153befee008456a93ecfb59706f0ad8502bcece
linghduoduo <linghypshen@gmail.com>
2019-02-17 23:46:58 -0500

    017-Letter-Combinations-of-a-Phone-Number.py

---
+++ 017-Letter-Combinations-of-a-Phone-Number.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        d = dict()
+        d['0'] = ''
+        d['1'] = ''
+        d['2'] = 'abc'
+        d['3'] = 'def'
+        d['4'] = 'ghi'
+        d['5'] = 'jkl'
+        d['6'] = 'mno'
+        d['7'] = 'pqrs'
+        d['8'] = 'tuv'
+        d['9'] = 'wxyz'
+
+        result = []
+
+        def dfs(num, path):
+            if num == len(digits):
+                result.append(path)
+                return
+            for letter in d[digits[num]]:
+            	dfs(num+1, path+letter)
+
+        dfs(0, '')
+        return result
+
+if __name__=="__main__":
+	input = '23'
+	results = Solution().letterCombinations(input)
+	print(results)
+
