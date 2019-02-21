e4047d66b12dc609a60bfd10dd3d4be3ac530fc0
linghduoduo <linghypshen@gmail.com>
2019-02-20 21:28:14 -0500

    647-Palindromic-Substrings.py

---
+++ 647-Palindromic-Substrings.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+## brute force
+        result = list()
+
+        for i in range(len(s)):
+        	for j in range(i+1, len(s)+1):
+        		sub = s[i:j]
+        		if sub == sub[::-1]:
+        			result.append(sub)
+        return len(result)
+
+## dp
+        n = len(s)
+        count = 0
+        start, end, maxL = 0, 0, 0
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(i):
+                dp[j][i] = (s[j] == s[i]) & ((i - j < 2) | dp[j + 1][i - 1])
+                if dp[j][i]:
+                    count += 1
+            dp[i][i] = 1
+            count += 1
+        return count
+   
+
+if __name__=='__main__':
+	s = "abc" 
+	results = Solution().countSubstrings(s)  
+	print(results)
+
