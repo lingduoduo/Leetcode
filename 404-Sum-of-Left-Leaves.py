fd5df1afe6f93e16c10f16cfc55fb8121a45a693
linghduoduo <linghypshen@gmail.com>
2019-02-23 20:58:38 -0500

    add 404

--- 404-Sum-of-Left-Leaves.py
+++ 404-Sum-of-Left-Leaves.py
@@ -1,19 +1,23 @@
-class TreeNode(object):
-	def __init__(self, x):
-		self.val = x
-		self.left = None
-		self.right = None
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-def sumofLeftLeaves(tree):
-	if tree == None:
-		return 0
-	sum = 0
-	if tree.left != None and tree.right != None:
-		sum += tree.left.val
-	return(sum + sumofLeftLeaves(tree.left) + sumofLeftLeaves(tree.right))
+	def sumofLeftLeaves(tree):
+	  	"""
+        :type root: TreeNode
+        :rtype: int
+        """
+		if tree == None:
+			return 0
+		sum = 0
+		if tree.left != None and tree.right != None:
+			sum += tree.left.val
+		return(sum + sumofLeftLeaves(tree.left) + sumofLeftLeaves(tree.right))
 
-
-	    def sumOfLeftLeaves(self, root):
+	def sumOfLeftLeaves(self, root):
         """
         :type root: TreeNode
         :rtype: int
@@ -26,4 +30,25 @@ def sumofLeftLeaves(tree):
             sum += root.left.val
 
         return sum + self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
+        self.result = 0
+        def trav(root):
+            if not root:
+                return
+            
+            if root.left and not root.left.left and not root.left.right:
+                self.result += root.left.val
+            
+            trav(root.left)
+            trav(root.right)
+        
+        trav(root)
+        return self.result
+            
         
