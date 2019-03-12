db84ed7aa59c73d4a08081f72a692c4c5adedba2
linghduoduo <linghypshen@gmail.com>
2019-03-11 22:19:24 -0400

    add 009-Palindrome-Number.py

--- 009-Palindrome-Number.py
+++ 009-Palindrome-Number.py
@@ -4,22 +4,30 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x<0 or (x%10 == 0 and x!=0): 
+        ## First try
+        # if x<0 or (x%10 == 0 and x!=0): 
+        #     return False
+        # if x in range(10):
+        #     return True
+
+        # revx = 0
+        # i = x
+
+        # while i>0:
+        #     revx = revx*10 + i%10
+        #     i=int(i/10)
+
+        # if revx == x:
+        #     return True
+        # else:
+        #     return False
+
+        ## Second try
+        if x<0:
             return False
-        if x in range(10):
-            return True
-
-        revx = 0
-        i = x
-
-        while i>0:
-            revx = revx*10 + i%10
-            i=int(i/10)
-
-        if revx == x:
-            return True
         else:
-            return False
+            num = str(x)
+            return num[::-1]==num
 
 if __name__=="__main__":
 	numbers = 88888
