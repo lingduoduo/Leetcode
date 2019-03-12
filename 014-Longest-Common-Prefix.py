b0cc7ce01fddb155ac06c17a2eaad6c96120eb6b
linghduoduo <linghypshen@gmail.com>
2019-03-11 23:31:52 -0400

    add 014

--- 014-Longest-Common-Prefix.py
+++ 014-Longest-Common-Prefix.py
@@ -4,11 +4,33 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        if not strs: return false
-        pre = min(strs, key=len)
+        ## First Try
+        # if not strs: return false
+        # pre = min(strs, key=len)
 
-        for i in range(len(pre)):
-        	for s in strs:
-        		if s[i] != pre[i]:
-        			return pre[:i]
-        return pre
+        # for i in range(len(pre)):
+        # 	for s in strs:
+        # 		if s[i] != pre[i]:
+        # 			return pre[:i]
+        # return pre
+
+        ## Second Try
+        if not strs: return ""
+        d = []
+        for s in strs:
+            d.append(len(s))
+        d=sorted(d)
+        base=strs[0][:d[0]]
+
+        for i in range(len(base)):
+            for j in range(len(strs)):
+                if base[i]!=strs[j][i]:
+                    return base[:i]
+        return base
+
+if __name__=="__main__":
+    strs=["flower","flow","flight"]
+    strs = ["aa","a"]
+    strs = ["dog","racecar","car"]
+    results = Solution().longestCommonPrefix(strs)
+    print(results)
