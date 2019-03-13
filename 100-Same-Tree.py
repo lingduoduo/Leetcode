e18618bb8289084d15ec03202f4f482c5f761968
linghduoduo <linghypshen@gmail.com>
2019-03-12 21:25:11 -0400

    add 100-Same-Tree.py

--- 100-Same-Tree.py
+++ 100-Same-Tree.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def isSameTree(self, p, q):
@@ -12,14 +12,37 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        if p is None and q is None:
-        	return True
-        if p is None and q is not None:
-        	return False
-        if p is not None and q is None:
-        	return False
-        if p.val != q.val:
-        	return False
-        if isSameTree(p.left, q.left) and isSameTree(p.right, q.right):
-        	return True
-        return False
+        ## First Try
+        # if p is None and q is None:
+        # 	return True
+        # if p is None and q is not None:
+        # 	return False
+        # if p is not None and q is None:
+        # 	return False
+        # if p.val != q.val:
+        # 	return False
+        # if isSameTree(p.left, q.left) and isSameTree(p.right, q.right):
+        # 	return True
+        # return False
+
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right):
+            return True
+        else:
+            return False
+
+if __name__ == "__main__":
+    p = TreeNode(1)
+    p.left = TreeNode(2)
+    p.right = TreeNode(3)
+
+    q = TreeNode(1)
+    q.left = TreeNode(2)
+    q.right = TreeNode(3)
+
+    result = Solution().isSameTree(p, q)
+    print(result)      
+    
