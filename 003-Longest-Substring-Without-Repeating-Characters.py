a3c1dc0775bcd9b6492b5d444842fe37a0943ae2
linghduoduo <linghypshen@gmail.com>
2019-03-15 17:56:40 -0400

    003-Longest-Substring-Without-Repeating-Characters.py

--- 003-Longest-Substring-Without-Repeating-Characters.py
+++ 003-Longest-Substring-Without-Repeating-Characters.py
@@ -9,16 +9,37 @@ class Solution(object):
         if len(s) <=1:
         	return 1
 
-        longest = 0
-        start = -1
-        location = [-1 for i in range(256)]
+        ## First try
+        # longest = 0
+        # start = -1
+        # location = [-1 for i in range(256)]
         
-        for i,v in enumerate(s):
-            if location[ord(v)] > start:
-                start = location[ord(v)]
-            longest = max(longest, i-start)
-            location[ord(v)] = i
-        return longest
+        # for i,v in enumerate(s):
+        #     if location[ord(v)] > start:
+        #         start = location[ord(v)]
+        #     longest = max(longest, i-start)
+        #     location[ord(v)] = i
+        # return longest
+
+        ## Second try
+        if not s:
+            return 0
+        if len(s) <=1:
+            return 1
+
+        result = 0
+        left = -1
+        n = len(s)
+        d = {}
+
+        for i in range(len(s)):
+            if s[i] in d and d[s[i]]>left:
+                left = d[s[i]]
+            d[s[i]]=i
+            result = max(result, i-left)
+        return result
+
+
 
 if __name__ == "__main__":    
 
@@ -26,3 +47,4 @@ if __name__ == "__main__":
     result = Solution().lengthOfLongestSubstring("au")
     result = Solution().lengthOfLongestSubstring("abba")
     print(result)
+
