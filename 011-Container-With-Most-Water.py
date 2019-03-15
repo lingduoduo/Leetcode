afc60ecdedd0a5b05631812f5a763cea4c2586b6
linghduoduo <linghypshen@gmail.com>
2019-03-14 23:08:06 -0400

    add 011

---
+++ 011-Container-With-Most-Water.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        i=0
+        j=len(height)-1
+        result = 0
+        while i<j:
+        	result = max(result, (j-i)*min(height[i], height[j]))
+        	if height[i]<height[j]:
+        		i+=1
+        	else:
+        		j-=1
+        return result
