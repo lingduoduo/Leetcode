8087fcca484c9e3af2438776cb0ecbb8a4713b1c
linghduoduo <linghypshen@gmail.com>
2019-03-14 22:46:43 -0400

    add 035

--- 035-Search-Insert-Position.py
+++ 035-Search-Insert-Position.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        ## First try
         if target < nums[0]:
         	return 0
 
@@ -12,3 +13,24 @@ class Solution(object):
         	if target <= nums[i]:
         		return i
         return len(nums)
+
+        ## Second try
+        left=0
+        right=len(nums)-1
+        
+        while left<=right:
+            mid=left+(right-left)/2
+            
+            if nums[left]==target:
+                return left
+            elif nums[right]==target:
+                return right
+            elif nums[mid]==target:
+                return mid
+
+            if nums[mid]<target:
+                left=mid+1
+            else:
+                right=mid-1
+
+        return left
