27380a5f46632068bd8ae3f9017e63963372ad82
linghduoduo <linghypshen@gmail.com>
2019-03-26 12:20:43 -0400

    add 069

--- 069-Sqrt(x).py
+++ 069-Sqrt(x).py
@@ -12,15 +12,25 @@ class Solution(object):
         # else:
         #     return i-1
 
-        L = 0
-        R = x
-        while L<=R:
-        	mid = (L+R)//2
-        	if mid**2 == x:
-        		return mid
-        	elif mid**2 > x:
-        		R = mid-1
-        	else:
-        		L = mid+1
-        return R
+        # L = 0
+        # R = x
+        # while L<=R:
+        # 	mid = (L+R)//2
+        # 	if mid**2 == x:
+        # 		return mid
+        # 	elif mid**2 > x:
+        # 		R = mid-1
+        # 	else:
+        # 		L = mid+1
+        # return R
 
+## second Try
+        L = 0
+        R = x+1
+        while L<R:
+            mid = L + (R-L)//2
+            if mid**2 <= x:
+                L = mid+1
+            else:
+                R = mid
+        return L-1
