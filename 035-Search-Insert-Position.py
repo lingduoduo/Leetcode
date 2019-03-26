8745e0d62d295c6ecaaa2dcbf0c4a18d886cf2d0
linghduoduo <linghypshen@gmail.com>
2019-03-26 12:29:57 -0400

    update 035

--- 035-Search-Insert-Position.py
+++ 035-Search-Insert-Position.py
@@ -6,31 +6,57 @@ class Solution(object):
         :rtype: int
         """
         ## First try
-        if target < nums[0]:
-        	return 0
+        # if target < nums[0]:
+        #     return 0
 
-        for i in range(len(nums)):
-        	if target <= nums[i]:
-        		return i
-        return len(nums)
 
-        ## Second try
-        left=0
-        right=len(nums)-1
+        # for i in range(len(nums)):
+        #   if target <= nums[i]:
+        #       return i
+        # return len(nums)
+
+        # ## Second try
+        # left=0
+        # right=len(nums)-1
         
-        while left<=right:
-            mid=left+(right-left)/2
+        # while left<=right:
+        #     mid=left+(right-left)/2
             
-            if nums[left]==target:
-                return left
-            elif nums[right]==target:
-                return right
-            elif nums[mid]==target:
+        #     if nums[left]==target:
+        #         return left
+        #     elif nums[right]==target:
+        #         return right
+        #     elif nums[mid]==target:
+        #         return mid
+
+        #     if nums[mid]<target:
+        #         left=mid+1
+        #     else:
+        #         right=mid-1
+
+        # return left
+        
+        ## Second Try
+        if not nums:
+            return 0
+
+        left = 0
+        right = len(nums)
+
+        while left<right:
+            mid = left + (right-left)//2
+            if nums[mid]==target:
                 return mid
-
-            if nums[mid]<target:
-                left=mid+1
+            elif nums[mid]<target:
+                left = mid + 1
             else:
-                right=mid-1
-
+                right = mid
         return left
+
+if __name__=="__main__":
+    nums=[1, 3, 5, 6]
+    result = Solution().searchInsert(nums, 0)
+    print(result)
+
+
+            
