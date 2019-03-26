a2c551ad9679a36b682283bd0e2e04452baa337f
linghduoduo <linghypshen@gmail.com>
2019-03-26 12:36:15 -0400

    add 278

---
+++ 278-First-Bad-Version.py
@@ -0,0 +1,22 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 0
+        right = n
+
+        while left<right:
+        	mid = left + (right-left)//2
+        	if not isBadVersion(mid):
+        		left = mid +1
+        	else:
+        		right = mid
+        return left
+
