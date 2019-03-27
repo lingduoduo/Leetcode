88de29d1cf6c40a30fdc60ac3b86f3174d1a7e67
linghduoduo <linghypshen@gmail.com>
2019-03-27 17:40:45 -0400

    add 202

---
+++ 202-Happy-Number.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        result = n
+        while result>=0:
+            l=list(str(result))
+            result=0
+
+            while l:
+                result+=int(l.pop())**2
+            if result==1 or result==7:
+                return True
+            elif result<10:
+                return False
+
+if __name__ == "__main__":
+    n=1111111
+    result = Solution().isHappy(n)
+    print(result)
