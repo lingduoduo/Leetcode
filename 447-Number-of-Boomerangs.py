c7f4c516f8853208bfee27f153a096683b41463f
linghduoduo <linghypshen@gmail.com>
2019-04-01 17:45:46 -0400

    add 447

---
+++ 447-Number-of-Boomerangs.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def numberOfBoomerangs(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        res = 0
+        for i in range(len(points)):
+            d = dict()
+            for j in range(len(points)):
+                dist = (points[i][0] - points[j][0])**2 + \
+                    (points[i][1] - points[j][1])**2
+                if dist in d:
+                    d[dist] += 1
+                else:
+                    d[dist] = 1
+            res += sum(v * (v - 1)for v in d.values())
+        return res
+
+
+if __name__ == "__main__":
+    numbers = [[0, 0], [1, 0], [2, 0]]
+    numbers = [[0, 0], [1, 0], [-1, 0], [0, 1], [0, -1]]
+    result = Solution().numberOfBoomerangs(numbers)
+    print(result)
