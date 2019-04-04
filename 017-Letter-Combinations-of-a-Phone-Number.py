39accd8a7495a72cf214cb5d25ffca39276136f7
linghduoduo <linghypshen@gmail.com>
2019-04-04 18:12:32 -0400

    update 017-Letter-Combinations-of-a-Phone-Number.py

--- 017-Letter-Combinations-of-a-Phone-Number.py
+++ 017-Letter-Combinations-of-a-Phone-Number.py
@@ -7,26 +7,31 @@ class Solution(object):
         if digits=="":
             return []
         
-        d = dict()
-        d['0'] = ''
-        d['1'] = ''
-        d['2'] = 'abc'
-        d['3'] = 'def'
-        d['4'] = 'ghi'
-        d['5'] = 'jkl'
-        d['6'] = 'mno'
-        d['7'] = 'pqrs'
-        d['8'] = 'tuv'
-        d['9'] = 'wxyz'
-        
-        result = []
+        self.d = dict()
+        self.d['0'] = ''
+        self.d['1'] = ''
+        self.d['2'] = 'abc'
+        self.d['3'] = 'def'
+        self.d['4'] = 'ghi'
+        self.d['5'] = 'jkl'
+        self.d['6'] = 'mno'
+        self.d['7'] = 'pqrs'
+        self.d['8'] = 'tuv'
+        self.d['9'] = 'wxyz'
 
-        def dfs(num, path):
-            if num == len(digits):
-                result.append(path)
-                return
-            for letter in d[digits[num]]:
-            	dfs(num+1, path+letter)
+        self.result = []
+        self.dfs(0, '', digits)
+        return self.result
 
-        dfs(0, '')
-        return result
+
+    def dfs(self, num, path, digits):
+        if num == len(digits):
+            self.result.append(path)
+            return
+        for letter in self.d[digits[num]]:
+            self.dfs(num+1, path+letter, digits)
+
+if __name__ == "__main__":
+    digits = '23'
+    result = Solution().letterCombinations(digits)
+    print(result)
