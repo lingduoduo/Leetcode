005d70c9eb1f122c698bb61b9083ff9bb76f51cf
linghduoduo <linghypshen@gmail.com>
2019-04-04 13:49:25 -0400

    131-Palindrome-Partitioning.py

---
+++ 131-Palindrome-Partitioning.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        self.res=[]
+        self.dfs(s, [])
+        return self.res
+
+    def dfs(self, s, temp):
+    	## return condition
+    	if not s:
+    		self.res.append(temp[:])
+    		return
+
+    	## backtracking
+    	for i in range(1, len(s)+1):
+    		ss = s[:i]
+    		if ss == ss[::-1]:
+    			temp.append(ss)
+    			self.dfs(s[i:], temp)
+    			temp.pop()
+
