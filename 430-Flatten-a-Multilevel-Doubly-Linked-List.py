376031a2197a1ef16f573100cc3de0f276fbffb0
linghduoduo <linghypshen@gmail.com>
2019-04-06 15:53:24 -0400

    430-Flatten-a-Multilevel-Doubly-Linked-List.py

---
+++ 430-Flatten-a-Multilevel-Doubly-Linked-List.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+   if not head:
+        return
+
+    dummy = Node(0, None, head, None)
+    stack = []
+    stack.append(head)
+    prev = dummy
+
+    while stack:
+        curr = stack.pop()
+        curr.prev = prev
+        prev.next = curr
+
+        if curr.next:
+            stack.append(curr.next)
+            curr.next = None
+        if curr.child:
+            stack.append(curr.child)
+            curr.child = None
+        prev = curr
+
+    dummy.next.prev = None
+    return dummy.next
