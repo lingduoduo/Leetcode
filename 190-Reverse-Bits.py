97ff9bc6c347d66723e0dfb554c2b1cae52d135d
linghduoduo <linghypshen@gmail.com>
2019-04-07 00:12:58 -0400

    190-Reverse-Bits.py

---
+++ 190-Reverse-Bits.py
@@ -0,0 +1,33 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        if n == 0:
+            return 0
+        bits = self.intToBit(n)
+        return self.bitToInt(bits[::-1])
+        # return bits
+
+    def intToBit(self, n):
+        bits = ''
+        while(n > 0):
+            bits = str(n % 2) + bits
+            n = n // 2
+        while len(bits) < 32:
+            bits = '0' + bits
+        return bits
+
+    def bitToInt(self, bits):
+        res = 0
+        for i in range(len(bits)):
+            if bits[i] == '1':
+                res = res * 2 + 1
+            else:
+                res = res * 2
+        return res
+
+
+if __name__ == '__main__':
+    n = 43261596
+    result = Solution().reverseBits(n)
+    print(result)
