99993f9da1a1156512aeabb90abd6a7d293caa8e
linghduoduo <linghypshen@gmail.com>
2019-04-08 16:10:43 -0400

    add 114-Flatten-Binary-Tree-to-Linked-List.py

---
+++ 114-Flatten-Binary-Tree-to-Linked-List.py
@@ -0,0 +1,66 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+    #     if not root:
+    #         return root
+    #
+    #     dummy = TreeNode(-1)
+    #     self.last = dummy
+    #     self.dfs(root)
+    #     return dummy.right
+    #
+    # def dfs(self, curr):
+    #     if not curr:
+    #         return
+    #
+    #     self.last.right = TreeNode(curr.val)
+    #     self.last = self.last.right
+    #
+    #     self.dfs(curr.left)
+    #     self.dfs(curr.right)
+
+        self.nodes = list()
+        self.dfs(root)
+
+        dummy = TreeNode(-1)
+        dummy.right = root
+        curr = dummy
+        while self.nodes:
+            curr.right = self.nodes.pop(0)
+            curr = curr.right
+        return dummy.right
+
+    def dfs(self, root):
+        if not root:
+            return
+        self.nodes.append(root)
+        left = root.left
+        right = root.right
+        root.left = None
+        root.right = None
+        self.dfs(left)
+        self.dfs(right)
+
+
+if __name__ == "__main__":
+    p = TreeNode(1)
+    p.left = TreeNode(2)
+    p.right = TreeNode(5)
+    p.left.left = TreeNode(3)
+    p.left.right = TreeNode(4)
+    p.right.right = TreeNode(6)
+
+    # Solution().printTree(p)
+
+    result = Solution().flatten(p)
+    Solution().printTree(result)
