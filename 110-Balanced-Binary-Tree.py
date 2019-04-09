7fc054460239e66ac23f6e27886a4640332f8587
linghduoduo <linghypshen@gmail.com>
2019-04-08 21:27:09 -0400

    PEP8 updates

--- 110-Balanced-Binary-Tree.py
+++ 110-Balanced-Binary-Tree.py
@@ -5,15 +5,16 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution(object):
 	def getHeight(self, root):
-    	if root is None:
+        if root is None:
     		return 0
     	if root.left is None and root.right is None:
     		return 1
-    	left = self.getHeight(root.left)
-    	right = self.getHeight(root.right)
-    	return max(left, right)+1
+        left = self.getHeight(root.left)
+        right = self.getHeight(root.right)
+        return max(left, right) + 1
 
     def isBalanced(self, root):
         """
@@ -21,10 +22,10 @@ class Solution(object):
         :rtype: bool
         """
         if root is None:
-        	return True
+            return True
         leftHeight = getHeight(root.left)
         rightHeight = getHeight(root.right)
         diff = abs(leftHeight-rightHeight)
         if diff<=1 and isBalanced(root.left) and isBalanced(root.right):
-        	return True
+            return True
         return False
--- 387-First-Unique-Character-in-a-String.py
+++ 387-First-Unique-Character-in-a-String.py
@@ -4,21 +4,30 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        d = dict()
+        # d = dict()
         
-        for i in range(len(s)):
-            try:
-                d[s[i]]+=1
-            except:
-                d[s[i]]=1
-        for i in range(len(s)):
-            if d[s[i]] == 1:
-                return i
+        # for i in range(len(s)):
+        #     try:
+        #         d[s[i]]+=1
+        #     except:
+        #         d[s[i]]=1
+        # for i in range(len(s)):
+        #     if d[s[i]] == 1:
+        #         return i
+        # return -1
+        print(s)
+        l = list(s)
+        res = 0
+        while l:
+            curr = l.pop(0)
+            if curr not in s[:res] and curr not in s[res+1:]:
+                return res
+            res += 1
         return -1
 
-
 if __name__=="__main__":
-    s = "leetcode"
+    # s = "leetcode"
     s = "loveleetcode"
+    # s = "cc"
     results = Solution().firstUniqChar(s)
     print(results)
