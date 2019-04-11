97c696a33c36e2ffb0a8c5ca17bbd35c6e43e398
linghduoduo <linghypshen@gmail.com>
2019-04-11 16:50:13 -0400

    PEP8 updates

--- 094-Binary-Tree-Inorder-Traversal.py
+++ 094-Binary-Tree-Inorder-Traversal.py
@@ -5,34 +5,36 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
+
 class Solution(object):
     def inorderTraversal(self, root):
         """
         :type root: TreeNode
         :rtype: List[int]
         """
-        ## Firs Recursive solution
-        if root is None: return []
+        # Firs Recursive solution
+        if root is None:
+            return []
         result = list()
 
         def dfs(root):
-            if root is None: 
+            if root is None:
                 return
             dfs(root.left)
             result.append(root.val)
             dfs(root.right)
-        
+
         dfs(root)
         return result
 
         # ## Second recursive solution
         res = []
         if root:
-            res = self.inorderTraversal(root.left) 
+            res = self.inorderTraversal(root.left)
             res.append(root.val)
-            res += self.inorderTraversal(root.right) 
+            res += self.inorderTraversal(root.right)
         return res
-        
+
         # iterative solution
         stack = []
         result = []
@@ -47,6 +49,24 @@ class Solution(object):
             result.append(curr.val)
             curr = curr.right
 
+        # Third try
+        if not root:
+            return root
+        visited = []
+        res = []
+        curr = root
+
+        while True:
+            while curr:
+                visited.append(curr)
+                curr = curr.left
+            if not visited:
+                return res
+            curr = visited.pop()
+            res.append(curr.val)
+            curr = curr.right
+        return res
+
 
 if __name__ == "__main__":
     root = TreeNode(1)
@@ -60,4 +80,4 @@ if __name__ == "__main__":
     root.right.right = TreeNode(3)
 
     result = Solution().inorderTraversal(root)
-    print(result)    
+    print(result)
