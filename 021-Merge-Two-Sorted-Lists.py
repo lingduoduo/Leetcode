2153df1cfe88a3b4dc3bdf6c64937b3e2b369613
linghduoduo <linghypshen@gmail.com>
2019-04-12 19:14:49 -0400

    PEP8 updates

--- 021-Merge-Two-Sorted-Lists.py
+++ 021-Merge-Two-Sorted-Lists.py
@@ -4,6 +4,7 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
         """
@@ -11,19 +12,41 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        # first try
         if l1 is None:
-        	return l2
+            return l2
         if l2 is None:
-        	return l1
+            return l1
 
-    	if l1.val<l2.val:
-    		root = ListNode(l1.val)
-    		l1 = l1.next
-    		root.next = self.mergeTwoLists(l1, l2)
-    	else:
-    		root = ListNode(l2.val)
-    		l2 = l2.next
-    		root.next = self.mergeTwoLists(l1, l2)
+        if l1.val < l2.val:
+            root = ListNode(l1.val)
+            l1 = l1.next
+            root.next = self.mergeTwoLists(l1, l2)
+        else:
+            root = ListNode(l2.val)
+            l2 = l2.next
+            root.next = self.mergeTwoLists(l1, l2)
 
-    	return root
+        return root
 
+        # second try
+        dummy = ListNode(-1)
+        dummy.next = head
+        curr = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+                curr = curr.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+                curr = curr.next
+
+        if l1:
+            curr.next = l1
+        elif l2:
+            curr.next = l2
+
+        return dummy.next
