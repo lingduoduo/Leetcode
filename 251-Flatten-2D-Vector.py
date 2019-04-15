9ee273e6172de0e0e7e2a6675b8fea5529566820
linghduoduo <linghypshen@gmail.com>
2019-04-14 22:29:16 -0400

    251-Flatten-2D-Vector.py

---
+++ 251-Flatten-2D-Vector.py
@@ -0,0 +1,50 @@
+# Implement an iterator to flatten a 2d vector.
+
+# For example,
+# Given 2d vector =
+
+# [
+#   [1,2],
+#   [3],
+#   [4,5,6]
+# ]
+# By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,2,3,4,5,6].
+
+class Vector2D(object):
+ 
+    # @param vec2d {List[List[int]]}
+    def __init__(self, vec2d):
+        # Initialize your data structure here
+        self.vec2d = vec2d
+        self.row = 0
+        self.col = 0
+ 
+    # @return {int} a next element
+    def next(self):
+        # Write your code here
+        val = self.vec2d[self.row][self.col]
+
+        if self.col < len(self.vec2d[self.row])-1:
+            self.col += 1
+        elif self.row < len(self.vec2d)-1:
+            self.row += 1
+            self.col = 0
+
+        return val
+
+    # @return {boolean} true if it has next element
+    # or false
+    def hasNext(self):
+        # Write your code here
+        if self.col < len(self.vec2d[self.row])-1:
+            return True
+        elif self.row < len(self.vec2d)-1:
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    vec2d = [[1,2],[3],[4,5,6]]
+    i, v = Vector2D(vec2d), []
+    while i.hasNext(): v.append(i.next())
+    print(v)
