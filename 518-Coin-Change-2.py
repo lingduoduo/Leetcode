73016bba8ea4dbcb0f8d2d09eb31f156a2226e60
linghduoduo <linghypshen@gmail.com>
2019-04-15 21:39:51 -0400

    518-Coin-Change-2.py

---
+++ 518-Coin-Change-2.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        dp = [1] + [0] * amount
+
+        for i in range(len(coins)):
+            for j in range(1, amount + 1):
+                if coins[i] <= j:
+                    dp[j] += dp[j - coins[i]]
+        return dp[-1]
