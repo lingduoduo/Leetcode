8aef29ff38576c38548827d3c23453c709eebe8b
linghduoduo <linghypshen@gmail.com>
2019-04-20 07:51:14 -0400

    PEP8 updates

--- 692-Top-K-Frequent-Words.py
+++ 692-Top-K-Frequent-Words.py
@@ -1,5 +1,7 @@
 
 import collections
+
+
 class Solution(object):
     def topKFrequent(self, words, k):
         """
@@ -9,31 +11,31 @@ class Solution(object):
         ["i", "love", "leetcode", "i", "love", "coding"]
         k = 2
         """
-        ## first try
+        # first try
         count = collections.Counter(words)
         candidates = count.keys()
-        candidates.sort(key= lambda w: (-count[w], w))
+        candidates.sort(key=lambda w: (-count[w], w))
         return candidates[:k]
 
-        ## second try
+        # second try
         d = dict()
         for i in range(len(words)):
             d[words[i]] = d.get(words[i], 0) + 1
 
         d2 = dict()
-        for key,v in d.items():
+        for key, v in d.items():
             if v not in d2:
                 d2[v] = [key]
             else:
                 d2[v] += [key]
             d2[v] = sorted(d2[v])
-        
+
         res = []
         for key in sorted(d2.keys(), reverse=True):
             res += d2[key]
         return res[:k]
 
-        ## third try
+        # third try
         d = dict()
         for i in range(len(words)):
             d[words[i]] = d.get(words[i], 0) + 1
@@ -45,30 +47,37 @@ class Solution(object):
                 return -cmp(x[1], y[1])
         return [x[0] for x in sorted(d.items(), cmp=comp)[:k]]
 
-        ## Fouth try
+        # Fouth try
         d = dict()
         for i in range(len(words)):
             d[words[i]] = d.get(words[i], 0) + 1
         heap = [(-freq, word) for word, freq in d.items()]
         heapq.heapify(heap)
         return [heapq.heappop(heap)[1] for _ in range(k)]
-                   
-
-
 
 
 if __name__ == '__main__':
     words = ["i", "love", "leetcode", "i", "love", "coding"]
-    k=2
+    k = 2
     result = Solution().topKFrequent(words, k)
     print(result)
     #
-    words = ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"]
-    k=4
+    words = [
+        "the",
+        "day",
+        "is",
+        "sunny",
+        "the",
+        "the",
+        "the",
+        "sunny",
+        "is",
+        "is"]
+    k = 4
     result = Solution().topKFrequent(words, k)
     print(result)
 
-    words = ["aaa","aa","a"]
-    k=1
+    words = ["aaa", "aa", "a"]
+    k = 1
     result = Solution().topKFrequent(words, k)
     print(result)
