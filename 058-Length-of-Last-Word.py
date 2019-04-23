04a4b5df2e0e4f5d9a7c91e9dadc00c70f22e452
linghduoduo <linghypshen@gmail.com>
2019-04-23 18:32:51 -0400

    058-Length-of-Last-Word.py

--- 058-Length-of-Last-Word.py
+++ 058-Length-of-Last-Word.py
@@ -4,4 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        if s=='': return 0
         
+        words = s.split(" ")
+
+        for i in range(len(words)):
+        	if words[len(words)-i-1] != '':
+        		return len(words[len(words)-i-1])
+        return 0
