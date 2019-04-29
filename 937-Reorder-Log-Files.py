e2386e80edeaa6a6e955e02ae57855dad8264592
linghduoduo <linghypshen@gmail.com>
2019-04-29 09:56:06 -0400

    PEP8 updates

--- 937-Reorder-Log-Files.py
+++ 937-Reorder-Log-Files.py
@@ -8,15 +8,21 @@ class Solution(object):
         digits = []
 
         for log in logs:
-        	items = log.split(" ")
-        	if items[1][0].isalpha():
-        		letters[" ".join(items[1:])] = log
-        	else:
-        		digits.append(log)
+            items = log.split(" ")
+            if items[1][0].isalpha():
+                letters[" ".join(items[1:])] = log
+            else:
+                digits.append(log)
         letterKeys = sorted(letters.keys())
-        return [letters[key] for key in letterKeys]+digits
+        return [letters[key] for key in letterKeys] + digits
+
 
 if __name__ == "__main__":
-	logs = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
-	result = Solution().reorderLogFiles(logs)
-	print(result)   
+    logs = [
+        "a1 9 2 3 1",
+        "g1 act car",
+        "zo4 4 7",
+        "ab1 off key dog",
+        "a8 act zoo"]
+    result = Solution().reorderLogFiles(logs)
+    print(result)
