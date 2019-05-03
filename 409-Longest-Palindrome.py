35c2d4e82654fd3030ddc738fa092f339049d53c
linghduoduo <linghypshen@gmail.com>
2019-05-02 21:59:23 -0400

    PEP8 updates

--- 409-Longest-Palindrome.py
+++ 409-Longest-Palindrome.py
@@ -6,22 +6,23 @@ class Solution(object):
         """
         freqs = dict()
         for i in s:
-        	try:
-        		freqs[i] += 1
-        	except:
-        		freqs[i] = 1
+            try:
+                freqs[i] += 1
+            except BaseException:
+                freqs[i] = 1
 
         result = 0
         odd = 0
         for v in freqs.values():
-        	result += v//2*2
-        	if v%2 == 1:
-        		odd = 1
+            result += v // 2 * 2
+            if v % 2 == 1:
+                odd = 1
         result = result + odd
         return result
 
+
 if __name__ == "__main__":
-    numbers="abccccdd"
+    numbers = "abccccdd"
     result = Solution().longestPalindrome(numbers)
     print(result)
     print('Done')
