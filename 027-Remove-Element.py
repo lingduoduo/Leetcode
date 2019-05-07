22d459037054b26cb03f46712f9748d9c9f4acef
linghduoduo <linghypshen@gmail.com>
2019-05-07 09:21:44 -0400

    PEP8 updates

--- 027-Remove-Element.py
+++ 027-Remove-Element.py
@@ -6,18 +6,19 @@ class Solution(object):
         :rtype: int
         """
         if nums is None:
-        	return -1
+            return -1
 
         n = len(nums)
         i = 0
         j = 0
         for j in range(0, n):
-            if nums[j] <> val:
+            if nums[j] != val:
                 nums[i] = nums[j]
                 i = i + 1
         return i
 
-if __name__=="__main__":
-	numbers=[3,2,2,3]
-	results = Solution().removeElement(numbers, 3)
-	print(results)
+
+if __name__ == "__main__":
+    numbers = [3, 2, 2, 3]
+    results = Solution().removeElement(numbers, 3)
+    print(results)
