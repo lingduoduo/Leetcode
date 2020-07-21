9ebea1ac780aef7a1b1f84e505a9f80988764e79
linghduoduo <linghypshen@gmail.com>
2020-07-21 13:29:02 -0400

    update code

--- 007-Reverse-Integer.py
+++ 007-Reverse-Integer.py
@@ -4,20 +4,35 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if x < -2**31 or x > 2**31 - 1:
+        # first try
+        # if x < -2**31 or x > 2**31 - 1:
+        #     return 0
+        # ind = 1 if x > 0 else -1
+        # x = abs(x)
+        # result = 0
+        # while x > 0:
+        #     result = result * 10 + x % 10
+        #     x = x / 10
+        # if result < -2**31 or result > 2**31 - 1:
+        #     return 0
+        # return ind * result
+        if x<=-2**31 or x>=2**31-1:
             return 0
-        ind = 1 if x > 0 else -1
-        x = abs(x)
-        result = 0
-        while x > 0:
-            result = result * 10 + x % 10
-            x = x / 10
-        if result < -2**31 or result > 2**31 - 1:
+        if x<0:
+            ind = -1
+        else:
+            ind = 1
+        x=abs(x)
+        res = 0
+        while x>0:
+            res = res*10 + x % 10
+            x = x//10
+        if res < -2**31 or res > 2**31 - 1:
             return 0
-        return ind * result
+        return ind*res
 
 
 if __name__ == "__main__":
-    numbers = 1534236469
+    numbers = -123
     results = Solution().reverse(numbers)
     print(results)
