b9fd4e50ffff2fd40d34b763f9cefda1038742f2
linghduoduo <linghypshen@gmail.com>
2020-07-21 11:50:57 -0400

    update code

--- 004-Median-of-Two-Sorted-Arrays.py
+++ 004-Median-of-Two-Sorted-Arrays.py
@@ -1,34 +1,74 @@
+# class Solution(object):
+# first try
+#     def findMedianSortedArrays(self, nums1, nums2):
+    #     """
+    #     :type nums1: List[int]
+    #     :type nums2: List[int]
+    #     :rtype: float
+    #     """
+    #     lenA, lenB = len(nums1), len(nums2)
+    #     if (lenA + lenB) % 2 == 1:
+    #         return self.getKth(nums1, nums2, (lenA + lenB) / 2 + 1)
+    #     else:
+    #         return (self.getKth(nums1, nums2, (lenA + lenB) / 2) +
+    #                 self.getKth(nums1, nums2, (lenA + lenB) / 2 + 1)) * 0.5
+    #
+    # def getKth(self, A, B, k):
+    #     lenA, lenB = len(A), len(B)
+    #     if lenA > lenB:
+    #         return self.getKth(B, A, k)
+    #
+    #     if lenA == 0:
+    #         return B[k - 1]
+    #
+    #     if k == 1:
+    #         return min(A[0], B[0])
+    #
+    #     pa = min(k / 2, lenA)
+    #     pb = k - pa
+    #
+    #     if A[pa - 1] < B[pb - 1]:
+    #         return self.getKth(A[pa:], B, k - pa)
+    #     elif A[pa - 1] > B[pb - 1]:
+    #         return self.getKth(A, B[pb:], k - pb)
+    #     else:
+    #         return A[pa - 1]
+
+# second try
 class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
-        lenA, lenB = len(nums1), len(nums2)
-        if (lenA + lenB) % 2 == 1:
-            return self.getKth(nums1, nums2, (lenA + lenB) / 2 + 1)
-        else:
-            return (self.getKth(nums1, nums2, (lenA + lenB) / 2) +
-                    self.getKth(nums1, nums2, (lenA + lenB) / 2 + 1)) * 0.5
+def findMedianSortedArrays(self, nums1, nums2):
+    lst = nums1 + nums2
+    lst.sort()
+    median = 0
+    if len(lst) % 2 == 1:
+        median = lst[(len(lst) // 2)]
+    else:
+        median = (float(lst[len(lst) // 2] + lst[(len(lst) // 2) - 1]) / 2)
+    return median
 
-    def getKth(self, A, B, k):
-        lenA, lenB = len(A), len(B)
-        if lenA > lenB:
-            return self.getKth(B, A, k)
+# class Solution:
+#     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+#         if len(nums1)>len(nums2):
+#             nums1, nums2 = nums2, nums1
+#         n1 = len(nums1)
+#         n2 = len(nums2)
+#
+#         k = (n1+n2+1)//2
+#
+#         left = 0
+#         right = n1
+#
+#         while (left<right):
+#             m1 = left + (right-left)//2
+#             m2 = k - m1
+#             if (nums1[m1]<nums2[m2-1]):
+#                 left = m1+1
+#             else:
+#                 right = m1
+#
+#         m1 = left
+#         m2 = k-m1
+#
+#         c1 = max(if m1<0)
+# ...
 
-        if lenA == 0:
-            return B[k - 1]
-
-        if k == 1:
-            return min(A[0], B[0])
-
-        pa = min(k / 2, lenA)
-        pb = k - pa
-
-        if A[pa - 1] < B[pb - 1]:
-            return self.getKth(A[pa:], B, k - pa)
-        elif A[pa - 1] > B[pb - 1]:
-            return self.getKth(A, B[pb:], k - pb)
-        else:
-            return A[pa - 1]
--- 005-Longest-Palindromic-Substring.py
+++ 005-Longest-Palindromic-Substring.py
@@ -5,26 +5,46 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        print(s)
-        if len(s) <= 1:
-            return s
+        # print(s)
+        # if len(s) <= 1:
+        #     return s
+        #
+        # result = ""
+        # size = 0
+        #
+        # for i in range(len(s)):
+        #     indices = [k for k, x in enumerate(s) if x == s[i] and k > i]
+        #
+        #     for j in indices:
+        #         if s[i:(j + 1)] == s[i:(j + 1)][::-1] and (j - i + 1) > size:
+        #             size = j - i + 1
+        #             result = s[i:(j + 1)]
+        #
+        # if size < 2:
+        #     return s[1]
+        # else:
+        #     return result
+        
+        n = len(s)
+        
+        def getlen(l, r):
+            while l>=0 and r<n and s[l]==s[r]:
+                l-=1
+                r+=1
+            return r-l-1
+        
+        start = 0
+        length = 0
+        for i in range(n):
+            cur = max(getlen(i,i), getlen(i, i+1))
+            if cur <= length:
+                continue
+            length = cur
+            start = i - (cur -1)//2
+        return s[start: start+length]
+        
 
-        result = ""
-        size = 0
-
-        for i in range(len(s)):
-            indices = [k for k, x in enumerate(s) if x == s[i] and k > i]
-
-            for j in indices:
-                if s[i:(j + 1)] == s[i:(j + 1)][::-1] and (j - i + 1) > size:
-                    size = j - i + 1
-                    result = s[i:(j + 1)]
-
-        if size < 2:
-            return s[1]
-        else:
-            return result
-
+        
 if __name__ == "__main__":
     print(Solution().longestPalindrome("abbae"))
     print(Solution().longestPalindrome("bb"))
