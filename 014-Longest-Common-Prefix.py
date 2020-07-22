787d863d7db629325424f7ce247e13d7a31a29f3
linghduoduo <linghypshen@gmail.com>
2020-07-21 20:56:21 -0400

    update code

--- 014-Longest-Common-Prefix.py
+++ 014-Longest-Common-Prefix.py
@@ -15,22 +15,33 @@ class Solution(object):
         # return pre
 
         ## Second Try
-        if not strs: return ""
-        d = []
-        for s in strs:
-            d.append(len(s))
-        d=sorted(d)
-        base=strs[0][:d[0]]
+        # if not strs: return ""
+        # d = []
+        # for s in strs:
+        #     d.append(len(s))
+        # d=sorted(d)
+        # base=strs[0][:d[0]]
+        #
+        # for i in range(len(base)):
+        #     for j in range(len(strs)):
+        #         if base[i]!=strs[j][i]:
+        #             return base[:i]
+        # return base
+        
+        # third try
+        if not strs:
+            return ""
 
-        for i in range(len(base)):
-            for j in range(len(strs)):
-                if base[i]!=strs[j][i]:
-                    return base[:i]
-        return base
+        for i in range(len(strs[0])):
+            for string in strs:
+                if i >= len(string) or string[i] != strs[0][i]:
+                    return strs[0][:i]
+
+        return strs[0]
 
 if __name__=="__main__":
     strs=["flower","flow","flight"]
-    strs = ["aa","a"]
-    strs = ["dog","racecar","car"]
+    # strs = ["aa","a"]
+    # strs = ["dog","racecar","car"]
     results = Solution().longestCommonPrefix(strs)
     print(results)
