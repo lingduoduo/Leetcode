304f74b052495c7c40e41ada6b0afa8d71e1405b
linghduoduo <linghypshen@gmail.com>
2020-07-21 20:09:02 -0400

    update code

--- 011-Container-With-Most-Water.py
+++ 011-Container-With-Most-Water.py
@@ -1,16 +1,33 @@
-class Solution(object):
-    def maxArea(self, height):
+class Solution:
+    def maxArea(self, height) -> int:
         """
         :type height: List[int]
         :rtype: int
         """
-        i=0
-        j=len(height)-1
-        result = 0
-        while i<j:
-            result = max(result, (j-i)*min(height[i], height[j]))
-            if height[i]<height[j]:
-                i+=1
+        # first try
+        # i=0
+        # j=len(height)-1
+        # result = 0
+        # while i<j:
+        #     result = max(result, (j-i)*min(height[i], height[j]))
+        #     if height[i]<height[j]:
+        #         i+=1
+        #     else:
+        #         j-=1
+        # return result
+        
+        res = 0
+        left = 0
+        right = len(height)-1
+        while left<right:
+            h = min(height[left], height[right])
+            res = max(res, h*(right-left))
+            if height[left]<height[right]:
+                left += 1
             else:
-                j-=1
-        return result
+                right -= 1
+        return res
+
+if __name__=="__main__":
+    height = [1,8,6,2,5,4,8,3,7]
+    print(Solution().maxArea(height))
--- 013-Roman-to-Integer.py
+++ 013-Roman-to-Integer.py
@@ -1,15 +1,31 @@
 class Solution(object):
-    def romanToInt(self, s):
+    def intToRoman(self, num: int) -> str:
         """
         :type s: str
         :rtype: int
         """
-        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
-        result = d[s[0]]
-        for i in range(1, len(s)):
-            if d[s[i - 1]] >= d[s[i]]:
-                result += d[s[i]]
-            else:
-                result = result + d[s[i]] - 2 * d[s[i - 1]]
-        return result
+        # first try
+        # d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        #
+        # result = d[s[0]]
+        # for i in range(1, len(s)):
+        #     if d[s[i - 1]] >= d[s[i]]:
+        #         result += d[s[i]]
+        #     else:
+        #         result = result + d[s[i]] - 2 * d[s[i - 1]]
+        # return result
+        
+        # second try
+        k = ['M', 'CM', 'D', "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        v = [1000, 900, 500, 400, 100,   90,   50,  40,   10,  9,  5,  4, 1]
+        
+        res = ""
+        for i in range(len(v)):
+            while v[i] <= num:
+                res += k[i]
+                num -= v[i]
+        return res
+       
+        
+if __name__=="__main__":
+    print(Solution().intToRoman(3))
