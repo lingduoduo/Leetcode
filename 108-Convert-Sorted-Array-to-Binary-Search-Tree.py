29ea26c1c0145524273127c4189e50c7d95f7d64
linghduoduo <linghypshen@gmail.com>
2020-07-23 16:36:32 -0400

    update code

--- 108-Convert-Sorted-Array-to-Binary-Search-Tree.py
+++ 108-Convert-Sorted-Array-to-Binary-Search-Tree.py
@@ -11,15 +11,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: TreeNode
         """
-        if not nums: return None
+        # if not nums: return None
+        #
+        # # if len(nums)==1: return TreeNode(nums[0])
+        # mid = len(nums) // 2
+        # root = TreeNode(nums[mid])
+        # left = nums[:mid]
+        # right = nums[mid + 1:]
+        # if len(left) >= 1:
+        #     root.left = self.sortedArrayToBST(left)
+        # if len(right) >= 1:
+        #     root.right = self.sortedArrayToBST(right)
+        # return root
         
-        # if len(nums)==1: return TreeNode(nums[0])
-        mid = len(nums) // 2
-        root = TreeNode(nums[mid])
-        left = nums[:mid]
-        right = nums[mid + 1:]
-        if len(left) >= 1:
-            root.left = self.sortedArrayToBST(left)
-        if len(right) >= 1:
-            root.right = self.sortedArrayToBST(right)
+        if not nums:
+            return None
+        
+        return self.build(nums, 0, len(nums)-1)
+        
+    def build(self, arr, left, right):
+        if left>right:
+            return None
+        mid = left + (right - left)//2
+        print(mid)
+        root = TreeNode(arr[mid])
+        root.left = self.build(arr, left, mid-1)
+        root.right = self.build(arr, mid+1, right)
         return root
