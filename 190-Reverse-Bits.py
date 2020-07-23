84018337d1cba4dd6a64d7eaa18ff0ff9070cd71
linghduoduo <linghypshen@gmail.com>
2020-07-23 17:45:54 -0400

    update code

--- 190-Reverse-Bits.py
+++ 190-Reverse-Bits.py
@@ -1,31 +1,39 @@
-class Solution:
+# class Solution:
     # @param n, an integer
     # @return an integer
-    def reverseBits(self, n):
-        if n == 0:
-            return 0
-        bits = self.intToBit(n)
-        return self.bitToInt(bits[::-1])
-        # return bits
-    
-    def intToBit(self, n):
-        bits = ''
-        while (n > 0):
-            bits = str(n % 2) + bits
-            n = n // 2
-        while len(bits) < 32:
-            bits = '0' + bits
-        return bits
-    
-    def bitToInt(self, bits):
-        res = 0
-        for i in range(len(bits)):
-            if bits[i] == '1':
-                res = res * 2 + 1
-            else:
-                res = res * 2
-        return res
+    # def reverseBits(self, n):
+    #     if n == 0:
+    #         return 0
+    #     bits = self.intToBit(n)
+    #     return self.bitToInt(bits[::-1])
+    #     # return bits
+    #
+    # def intToBit(self, n):
+    #     bits = ''
+    #     while (n > 0):
+    #         bits = str(n % 2) + bits
+    #         n = n // 2
+    #     while len(bits) < 32:
+    #         bits = '0' + bits
+    #     return bits
+    #
+    # def bitToInt(self, bits):
+    #     res = 0
+    #     for i in range(len(bits)):
+    #         if bits[i] == '1':
+    #             res = res * 2 + 1
+    #         else:
+    #             res = res * 2
+    #     return res
 
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            res = (res << 1) + (n & 1)
+            n = n >> 1
+        return res
+        
 
 if __name__ == '__main__':
     n = 43261596
--- 973-K-Closest-Points-to-Origin.py
+++ 973-K-Closest-Points-to-Origin.py
@@ -5,19 +5,30 @@ class Solution(object):
         :type K: int
         :rtype: List[List[int]]
         """
-        d = dict()
-        i = 0
-        for point in points:
-            d[i] = point[0] ** 2 + point[1] ** 2
-            i += 1
-        k = sorted(d.values())
+        # d = dict()
+        # i = 0
+        # for point in points:
+        #     d[i] = point[0] ** 2 + point[1] ** 2
+        #     i += 1
+        # k = sorted(d.values())
+        #
+        # result = list()
+        # for j in range(len(points)):
+        #     if d[j] <= k[K - 1]:
+        #         result.append(points[j])
+        # return result
         
-        result = list()
-        for j in range(len(points)):
-            if d[j] <= k[K - 1]:
-                result.append(points[j])
-        return result
-
+        import heapq
+        heap = []
+        for p in points:
+            d = p[0]**2 + p[1]**2
+            heap.append([d, p])
+        
+        heapq.heapify(heap)
+        res = []
+        for d in heapq.nsmallest(K, heap):
+            res.append(d[1])
+        return res
 
 if __name__ == "__main__":
     points = [[3, 3], [5, -1], [-2, 4]]
