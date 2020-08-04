a8e3112d1242cea7c913e9ae5c3fa25465195cd0
linghduoduo <linghypshen@gmail.com>
2020-08-04 08:59:22 -0400

    add code

---
+++ 406-Queue-Reconstruction-by-Height.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        d = {}
+        for p in people:
+            height, position = p[0], p[1]
+            if height in d:
+                d[height].append(position)
+            else:
+                d[height] = [position]
+        
+        res = []
+        
+        for height in sorted(d.keys(), reversed = True):
+            for position in sorted(d[height]):
+                res.insert(position, [height, position])
+        return res
+        
+        
--- 409-Longest-Palindrome.py
+++ 409-Longest-Palindrome.py
@@ -4,25 +4,45 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        freqs = dict()
-        for i in s:
-            try:
-                freqs[i] += 1
-            except BaseException:
-                freqs[i] = 1
+        # freqs = dict()
+        # for i in s:
+        #     try:
+        #         freqs[i] += 1
+        #     except BaseException:
+        #         freqs[i] = 1
+        #
+        # result = 0
+        # odd = 0
+        # for v in freqs.values():
+        #     result += v // 2 * 2
+        #     if v % 2 == 1:
+        #         odd = 1
+        # result = result + odd
+        # return result
+
+        d = {}
+        for cha in s:
+            if cha in d:
+                d[cha] += 1
+            else:
+                d[cha] = 1
+
+        even = odd = 0
+        for v in d.values():
+            if v % 2 == 0:
+                even += v
+            else:
+                odd = max(odd, v)
+                even += v - 1
+        if odd > 0:
+            return even + 1
+        else:
+            return even
         
-        result = 0
-        odd = 0
-        for v in freqs.values():
-            result += v // 2 * 2
-            if v % 2 == 1:
-                odd = 1
-        result = result + odd
-        return result
-
-
 if __name__ == "__main__":
-    numbers = "abccccdd"
+    # numbers = "abccccdd"
+    # numbers = "ccc"
+    numbers = "bananas"
     result = Solution().longestPalindrome(numbers)
     print(result)
     print('Done')
