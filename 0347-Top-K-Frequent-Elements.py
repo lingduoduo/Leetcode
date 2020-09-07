83180241fc5e31cbc41f8f811cef22422eb43aeb
linghduoduo <linghypshen@gmail.com>
2020-09-07 14:12:02 -0400

    add code

--- 0347-Top-K-Frequent-Elements.py
+++ 0347-Top-K-Frequent-Elements.py
@@ -6,20 +6,24 @@ class Solution(object):
         :rtype: List[int]
         """
         import collections
-        d = collections.Counter(nums)
-        l = sorted(d.values())[-k:]
+        # d = collections.Counter(nums)
+        # l = sorted(d.values())[-k:]
 
-        res = list()
-        for k, v in d.items():
-            if v in l:
-                res.append(k)
-        return res
+        # res = list()
+        # for k, v in d.items():
+        #     if v in l:
+        #         res.append(k)
+        # return res
+
+        counter = collections.Counter(nums).most_common()
+        print(counter)
+        return [counter[i][0] for i in range(k)]
 
 
 if __name__ == "__main__":
     nums = [1, 1, 1, 2, 2, 3]
     result = Solution().topKFrequent(nums, 2)
     print(result)
-    nums = [1]
-    result = Solution().topKFrequent(nums, 1)
-    print(result)
+    # nums = [1]
+    # result = Solution().topKFrequent(nums, 1)
+    # print(result)
--- 0692-Top-K-Frequent-Words.py
+++ 0692-Top-K-Frequent-Words.py
@@ -11,50 +11,56 @@ class Solution(object):
         k = 2
         """
         # first try
-        count = collections.Counter(words)
-        candidates = count.keys()
-        candidates.sort(key=lambda w: (-count[w], w))
-        return candidates[:k]
+        # count = collections.Counter(words)
+        # candidates = count.keys()
+        # candidates.sort(key=lambda w: (-count[w], w))
+        # return candidates[:k]
         
-        # second try
-        d = dict()
-        for i in range(len(words)):
-            d[words[i]] = d.get(words[i], 0) + 1
+        # # second try
+        # d = dict()
+        # for i in range(len(words)):
+        #     d[words[i]] = d.get(words[i], 0) + 1
         
-        d2 = dict()
-        for key, v in d.items():
-            if v not in d2:
-                d2[v] = [key]
-            else:
-                d2[v] += [key]
-            d2[v] = sorted(d2[v])
+        # d2 = dict()
+        # for key, v in d.items():
+        #     if v not in d2:
+        #         d2[v] = [key]
+        #     else:
+        #         d2[v] += [key]
+        #     d2[v] = sorted(d2[v])
         
-        res = []
-        for key in sorted(d2.keys(), reverse=True):
-            res += d2[key]
-        return res[:k]
+        # res = []
+        # for key in sorted(d2.keys(), reverse=True):
+        #     res += d2[key]
+        # return res[:k]
         
-        # third try
-        d = dict()
-        for i in range(len(words)):
-            d[words[i]] = d.get(words[i], 0) + 1
+        # # third try
+        # d = dict()
+        # for i in range(len(words)):
+        #     d[words[i]] = d.get(words[i], 0) + 1
         
-        def comp(x, y):
-            if x[1] == y[1]:
-                return cmp(x[0], y[0])
-            else:
-                return -cmp(x[1], y[1])
+        # def comp(x, y):
+        #     if x[1] == y[1]:
+        #         return cmp(x[0], y[0])
+        #     else:
+        #         return -cmp(x[1], y[1])
         
-        return [x[0] for x in sorted(d.items(), cmp=comp)[:k]]
+        # return [x[0] for x in sorted(d.items(), cmp=comp)[:k]]
         
-        # Fouth try
-        d = dict()
-        for i in range(len(words)):
-            d[words[i]] = d.get(words[i], 0) + 1
-        heap = [(-freq, word) for word, freq in d.items()]
-        heapq.heapify(heap)
-        return [heapq.heappop(heap)[1] for _ in range(k)]
+        # # Fouth try
+        # d = dict()
+        # for i in range(len(words)):
+        #     d[words[i]] = d.get(words[i], 0) + 1
+        # heap = [(-freq, word) for word, freq in d.items()]
+        # heapq.heapify(heap)
+        # return [heapq.heappop(heap)[1] for _ in range(k)]
 
+
+        # fifth try
+        count = collections.Counter(words)
+        count = {k: v for k, v in sorted(count.items(), key=lambda x: (-x[1], x[0]))}
+        candidates = list(count.keys())
+        return candidates[:k]
 
 if __name__ == '__main__':
     words = ["i", "love", "leetcode", "i", "love", "coding"]
