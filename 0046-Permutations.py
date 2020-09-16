4daba773e64dc10aa0a6deb92fcf5f74493ca990
linghduoduo <linghypshen@gmail.com>
2020-09-15 23:18:13 -0400

    add code

--- 0046-Permutations.py
+++ 0046-Permutations.py
@@ -4,18 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        ###first try
-        ###    res = []
-        ###    self.dfs(nums, res, [])
-        ###    return res
-        
-        ###def dfs(self, nums, res, path):
-        ###    if not nums:
-        ###        res.append(path)
-        ###    else:
-        ###        for i in range(len(nums)):
-        ###            self.dfs(nums[:i] + nums[i + 1:], res, path + [nums[i]])
-        
+    #     res = []
+    #     self.dfs(nums, res, [])
+    #     return res
+
+    # def dfs(self, nums, res, path):
+    #     if not nums:
+    #         return
+    #     else:
+    #         for i in range(len(nums)):
+    #             self.dfs(nums[:i] + nums[:i+1], res, path+[nums[i]])
+
     ###second try
     ###    res = []
     ###    self.tot = len(nums)
@@ -31,17 +30,34 @@ class Solution(object):
     ###                     depth + 1, path + [nums[i]])
 
     ###third try
-        if len(nums) <= 1:
-            return [nums]
+        # if len(nums) <= 1:
+        #     return [nums]
         
+        # res = []
+        # for i, num in enumerate(nums):
+        #     n = nums[:i] + nums[i+1:]
+        #     for y in self.permute(n):
+        #         res.append([num]+y)
+        # return res
+        
+
+        visited = [0]*len(nums)
         res = []
-        for i, num in enumerate(nums):
-            n = nums[:i] + nums[i+1:]
-            for y in self.permute(n):
-                res.append([num]+y)
+
+        def dfs(path):
+            if len(path) == len(nums):
+                res.append(path)
+            else:
+                for i in range(len(nums)):
+                    if not visited[i]:
+                        visited[i]=1
+                        dfs(path+nums[i])
+                        visited[i]=0
+        dfs([])
         return res
-        
-        
+
+
+
 
 if __name__ == "__main__":
     nums = [1, 2, 3]
