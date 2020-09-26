01ed653cc51a9df8ce84e1e97b12890d57297cba
linghduoduo <linghypshen@gmail.com>
2020-09-25 22:18:51 -0400

    add code

---
+++ 0324-Wiggle-Sort-II.py
@@ -0,0 +1,29 @@
+class Solution:
+    def wiggleSort(self, nums) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # if not nums: return None
+
+        nums.sort()
+        mid = (len(nums)-1)//2 
+
+        left = nums[mid::-1]
+        right = nums[:mid:-1]
+
+        for i in range(len(nums)):
+            if i%2==0 and left:
+                nums[i] = left.pop(0)
+            elif i%2==1 and right:
+                nums[i] = right.pop(0)
+        return nums
+
+        # mid = (len(nums)-1)//2
+        # nums.sort()
+        # nums[::2], nums[1::2] = nums[mid::-1], nums[:mid:-1]
+
+if __name__ == '__main__':
+    # nums = [5,1,1,6,2]
+    nums = [1, 3, 2, 2, 3, 1]
+    results = Solution().wiggleSort(nums)
+    print(results)
