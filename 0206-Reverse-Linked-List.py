8c9abadfa93f306336a7cc11b6dc99acc4cfd2b2
linghduoduo <linghypshen@gmail.com>
2020-10-06 23:00:55 -0400

    add code

--- 0206-Reverse-Linked-List.py
+++ 0206-Reverse-Linked-List.py
@@ -4,28 +4,52 @@
 ###        self.val = x
 ###        self.next = None
 
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        ###if head is None:
-        ###    return None
-        #
-        ###curr1 = head
-        ###dummy = ListNode(-1)
-        ###while curr1:
-        ###    curr2 = curr1.next
-        ###    curr1.next = dummy.next
-        ###    dummy.next = curr1
-        ###    curr1 = curr2
-        ###return dummy.next
+# class Solution(object):
+#     def reverseList(self, head):
+#         """
+#         :type head: ListNode
+#         :rtype: ListNode
+#         """
+#         ###if head is None:
+#         ###    return None
+#         #
+#         ###curr1 = head
+#         ###dummy = ListNode(-1)
+#         ###while curr1:
+#         ###    curr2 = curr1.next
+#         ###    curr1.next = dummy.next
+#         ###    dummy.next = curr1
+#         ###    curr1 = curr2
+#         ###return dummy.next
+        
+#         dummy = ListNode(0)
+
+#         while head:
+#             dummy.next, head.next, head = head, dummy.next, head.next
+#         return dummy.next
+
+        # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head: return None
         
         dummy = ListNode(0)
-
-        while head:
-            dummy.next, head.next, head = head, dummy.next, head.next
+        dummy.next = head
+        
+        curr = head
+        head = head.next
+        curr.next = None
+        curr = head
+        
+        while curr:
+            tmp = curr.next
+            curr.next = dummy.next
+            dummy.next = curr
+            curr = tmp
         return dummy.next
         
         
---
+++ 0503-Next-Greater-Element-II.py
@@ -0,0 +1,34 @@
+class Solution:
+    def nextGreaterElements(self, nums):
+        stack = []
+        padding = nums * 2
+        res = [-1] * len(padding)
+
+        # for i in range(len(padding)-1):
+        #     j = i+1
+        #     while j < len(padding) and padding[j] <= padding[i]:
+        #         j += 1
+        #     if j < len(padding):
+        #         res[i] = padding[j]
+        # return res[:len(nums)]
+
+
+        for i in reversed(range(len(padding))): 
+            if stack and padding[i] < stack[-1]:
+                res[i] = stack[-1]
+            elif stack and padding[i] >= stack[-1]:
+                while stack and padding[i] >= stack[-1]:
+                    stack.pop()
+                if stack:
+                    res[i] = stack[-1]
+            stack.append(padding[i])
+        return [res[len(nums)]] + res[1:len(nums)]
+
+
+
+if __name__ == '__main__':
+    # nums = [1,2,1]
+    nums = [1, 2, 3, 4, 5]
+    # nums = [5, 4, 3, 2, 1]
+    results = Solution().nextGreaterElements(nums)
+    print(results)
