a224dbd99e55a82eb6c532b302a14562855e884a
linghduoduo <linghypshen@gmail.com>
2020-10-16 21:12:41 -0400

    add codes

--- 0297-Serialize-and-Deserialize-Binary-Tree.py
+++ 0297-Serialize-and-Deserialize-Binary-Tree.py
@@ -7,23 +7,42 @@
 
 class Codec:
 
+    def preOrder(self, root):
+        if not root:
+            self.vals.append('#')
+        else:
+            self.vals.append(str(root.val))
+            self.preOrder(root.left)
+            self.preOrder(root.right)
+
     def serialize(self, root):
         """Encodes a tree to a single string.
         
         :type root: TreeNode
         :rtype: str
         """
+        self.vals = []
+        self.preOrder(root)
+        return ' '.join(self.vals)
+
+    def build(self, vals):
+        if not vals:
+            return None
         
+        val = vals.popleft()
+        if val == '#':
+            return None
+        root = TreeNode(int(val))
+        root.left = self.build(vals)
+        root.right = self.build(vals)
+        return root
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
-        
+
         :type data: str
         :rtype: TreeNode
         """
+        vals = collections.deque(val for val in data.split())
+        return self.build(vals)
         
-
-# Your Codec object will be instantiated and called as such:
-# ser = Codec()
-# deser = Codec()
-# ans = deser.deserialize(ser.serialize(root))
