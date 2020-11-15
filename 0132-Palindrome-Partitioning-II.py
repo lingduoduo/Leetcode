342ed676b12f734b93e77dde40140276422fcbdc
linghduoduo <linghypshen@gmail.com>
2020-11-15 13:19:34 -0500

    add code

--- 0132-Palindrome-Partitioning-II.py
+++ 0132-Palindrome-Partitioning-II.py
@@ -12,6 +12,23 @@ class Solution:
                     dp[i] = min(1+dp[j+1], dp[i])
         return dp[0]-1
 
+
+class Solution:
+    def minCut(self, s: str) -> int:
+        dp=[0]*(len(s)+1)
+        dp[0]=-1
+        for i in range(len(s)):
+            dp[i+1]=i
+        
+        p=[[False]*len(s) for i in range(len(s))]
+            
+        for i in range(len(s)):
+            for j in range(i+1):
+                if s[j]==s[i] and ((i-j<2) or p[j+1][i-1]):
+                    p[j][i]=True 
+                    dp[i+1]=min(1+dp[j],dp[i+1])
+        return dp[-1] 
+        
 if __name__ == '__main__':
     result = Solution().minCut('abb')
     print(result)
---
+++ 0458-Poor-Pigs.py
@@ -0,0 +1,4 @@
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        tmp = minutesToTest //  minutesToDie + 1
+        return math.ceil(math.log(buckets)/math.log(tmp))
