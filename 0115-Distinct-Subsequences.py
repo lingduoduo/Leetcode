9112784eefdcb9acb56ae9290ba9a278662b12f6
linghduoduo <linghypshen@gmail.com>
2020-11-16 11:59:40 -0500

    add code

---
+++ 0115-Distinct-Subsequences.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        for j in range(m + 1):
+            dp[0][j] = 1
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s[j - 1] == t[i - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1]
+                else:
+                    dp[i][j] = dp[i][j - 1]
+        return dp[-1][-1]
--- 0123-Best-Time-to-Buy-and-Sell-Stock-III.py
+++ 0123-Best-Time-to-Buy-and-Sell-Stock-III.py
@@ -1,12 +1,31 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:  
+#         if not prices: return 0
+#         n = len(prices)
+#         g = [[0] * 3 for _ in range(N)]
+#         l = [[0] * 3 for _ in range(N)]
+#         for i in range(1, n):
+#             diff = prices[i] - prices[i - 1]
+#             for j in range(1, 3):
+#                 l[i][j] = max(g[i - 1][j - 1] + max(diff, 0), l[i - 1][j] + diff)
+#                 g[i][j] = max(l[i][j], g[i - 1][j])
+#         return g[-1][-1]
+
+
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:  
+    def maxProfit(self, prices: List[int]) -> int:
         if not prices: return 0
-        n = len(prices)
-        g = [[0] * 3 for _ in range(N)]
-        l = [[0] * 3 for _ in range(N)]
-        for i in range(1, n):
-            diff = prices[i] - prices[i - 1]
-            for j in range(1, 3):
-                l[i][j] = max(g[i - 1][j - 1] + max(diff, 0), l[i - 1][j] + diff)
-                g[i][j] = max(l[i][j], g[i - 1][j])
-        return g[-1][-1]
+        
+# buy1[i] = max(buy1[i - 1], -prices[i])
+# sell1[i] = max(sell1[i - 1], buy1[i] + prices[i])
+# buy2[i] = max(buy2[i - 1], sell1[i - 1] - prices[i])
+# sell2[i] = max(sell2[i - 1], buy2[i] + prices[i])
+        
+        sell1 = sell2 = 0
+        buy1 = buy2 = -float('inf')
+        for p in prices:
+            buy1 = max(buy1, -p);
+            sell1 = max(sell1, buy1 + p)
+            buy2 = max(buy2, sell1 - p)
+            sell2 = max(sell2, buy2 + p)
+        return sell2
