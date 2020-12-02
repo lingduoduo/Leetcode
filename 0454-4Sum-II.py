61654c9500998f9f2e6b24ebe021002bb9ff1885
linghduoduo <linghypshen@gmail.com>
2020-12-02 00:14:55 -0500

    add code

---
+++ 0454-4Sum-II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        d = collections.defaultdict(int)
+        for num1 in A:
+            for num2 in B:
+                d[num1+num2] += 1
+                
+        res = 0
+        for num3 in C:
+            for num4 in D:
+                if -(num3+num4) in d:
+                    res += d[-(num3+num4)]
+        return res
