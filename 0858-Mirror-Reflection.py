5580fc038b2c5396da68e530b1bcce237e38d6b1
linghduoduo <linghypshen@gmail.com>
2020-12-06 19:54:49 -0500

    add code

---
+++ 0858-Mirror-Reflection.py
@@ -0,0 +1,10 @@
+import fractions
+class Solution:
+    def mirrorReflection(self, p: int, q: int) -> int:
+        g = gcd(p, q)
+        
+        if (p//g) % 2 == 0:
+            return 2
+        
+        return (q//g) % 2
+        
