4c7839bc2610031a304e9cdda0818c48ab94b3a8
linghduoduo <linghypshen@gmail.com>
2020-12-09 09:08:31 -0500

    add code

--- 0993-Cousins-in-Binary-Tree.py
+++ 0993-Cousins-in-Binary-Tree.py
@@ -1,28 +1,19 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
-        xparent, xdepth = self.findParent(root, x, 0)
-        yparent, ydepth = self.findParent(root, y, 0)
-        if xparent != yparent and xdepth == ydepth:
-            return True
-        else:
-            return False
-    
-    
-    def findParent(self, root, node, depth):
-        if not root or (not root.left and not root.right):
-            return 
-
-        if root.left.val==node or root.right.val==node:
-            return root, depth+1
-
-        if root.left:
-            result = self.findParent(root.left, node, depth+1)
-        if root.right:
-            result = self.findParent(root.right, node, depth+1)
-        return result
+        d = {root.val: (0, None)}
+        stack = [root]
+        depth = 0
+        while stack:
+            for i in range(len(stack)):
+                node = stack.pop(0)
+                if node.left:
+                    stack.append(node.left)
+                    d[node.left.val] = (depth + 1, node)
+                if node.right:
+                    stack.append(node.right)
+                    d[node.right.val] = (depth + 1, node)
+            depth += 1
+            
+        dx, px = d[x]
+        dy, py = d[y]
+        return dx == dy and px != py
