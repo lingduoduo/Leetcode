06f880a72a0b256786662a3772d2c0f577848053
linghduoduo <linghypshen@gmail.com>
2020-12-10 09:58:10 -0500

    add code

--- 0208-Implement-Trie-Prefix-Tree.py
+++ 0208-Implement-Trie-Prefix-Tree.py
@@ -1,68 +1,56 @@
-'''
-Trie trie = new Trie();
-
-trie.insert("apple");
-trie.search("apple");   // returns true
-trie.search("app");     // returns false
-trie.startsWith("app"); // returns true
-trie.insert("app");
-trie.search("app");     // returns true
-'''
-
-
-class Node:
+# class Node:
     
-    def __init__(self):
-        self.d = dict()
-        self.isword = False
+#     def __init__(self):
+#         self.d = dict()
+#         self.isword = False
 
 
-class Trie:
+# class Trie:
     
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.root = Node()
+#     def __init__(self):
+#         """
+#         Initialize your data structure here.
+#         """
+#         self.root = Node()
     
-    def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
-        current = self.root
+#     def insert(self, word: str) -> None:
+#         """
+#         Inserts a word into the trie.
+#         """
+#         current = self.root
         
-        for i in range(len(word)):
-            node = Node()
-            current.d[word[i]] = current.d.get(word[i], node)
-            current = current.d[word[i]]
-        current.isword = True
+#         for i in range(len(word)):
+#             node = Node()
+#             current.d[word[i]] = current.d.get(word[i], node)
+#             current = current.d[word[i]]
+#         current.isword = True
     
-    def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
-        current = self.root
+#     def search(self, word: str) -> bool:
+#         """
+#         Returns if the word is in the trie.
+#         """
+#         current = self.root
         
-        for i in range(len(word)):
-            if word[i] in current.d:
-                current = current.d[word[i]]
-            else:
-                return False
+#         for i in range(len(word)):
+#             if word[i] in current.d:
+#                 current = current.d[word[i]]
+#             else:
+#                 return False
         
-        return current.isword
+#         return current.isword
     
-    def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
-        current = self.root
+#     def startsWith(self, prefix: str) -> bool:
+#         """
+#         Returns if there is any word in the trie that starts with the given prefix.
+#         """
+#         current = self.root
         
-        for i in range(len(prefix)):
-            if prefix[i] in current.d:
-                current = current.d[prefix[i]]
-            else:
-                return False
-        return True
+#         for i in range(len(prefix)):
+#             if prefix[i] in current.d:
+#                 current = current.d[prefix[i]]
+#             else:
+#                 return False
+#         return True
 
 
 class Trie:
@@ -102,16 +90,15 @@ class Trie:
 if __name__ == '__main__':
     obj = Trie()
     obj.insert("apple")
+    obj.search("apple")
     obj.insert("apps")
-    # result = obj.search("apple")
-    # result = obj.startsWith("app")
-    # print(result)
-    # result = obj.startsWith("tpp")
+    obj.startsWith("app")
+    obj.insert("app")
+    obj.startsWith("app")
     # print(result)
     # obj.insert("app");
     # result = obj.search("app")
     # print(result)
-
 ###Your Trie object will be instantiated and called as such:
 
 ###obj.insert(word)
