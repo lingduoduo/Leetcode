d9a20dbf84159dafd80471b71e74112b29871c23
linghduoduo <linghypshen@gmail.com>
2020-12-13 14:25:39 -0500

    add code

---
+++ 1217-Minimum-Cost-to-Move-Chips-to-The-Same-Position.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        even = 0
+        odd = 0
+        for p in position:
+        	if p % 2 == 1:
+        		even += 1
+        	else:
+        		odd += 1
+        return min(even, odd)
+                
