8f9a88ce64d68fc09a864676b02bb407a1448203
linghduoduo <linghypshen@gmail.com>
2020-12-14 09:13:52 -0500

    add code

--- 0300-Longest-Increasing-Subsequence.py
+++ 0300-Longest-Increasing-Subsequence.py
@@ -1,18 +1,37 @@
+# class Solution:
+#     def lengthOfLIS(self, nums) -> int:
+#         if not nums:
+#             return 0
+
+#         dp = [1]*len(nums)
+#         for i in range(1, len(nums)):
+#             for j in range(i):
+#                 if nums[j] < nums[i]:
+#                     dp[i] = max(dp[i], dp[j]+1)
+#         return max(dp)
+
+import bisect
 class Solution:
     def lengthOfLIS(self, nums) -> int:
         if not nums:
             return 0
 
-        dp = [1]*len(nums)
-        for i in range(1, len(nums)):
-            for j in range(i):
-                if nums[i]>nums[j]:
-                    dp[i] = max(dp[i], dp[j]+1)
-        return max(dp)
-
+        res = []
+        for i in range(len(nums)):
+            print(res)
+            index = bisect.bisect_left(res, nums[i])
+            if len(res) == index:
+                res.append(nums[i])
+            else:
+                res[index] = nums[i]
+        return len(res)
 
 if __name__=='__main__':
-    nums = [10,9,2,5,3,7,101,18]
+    # nums = [10,9,2,5,3,7,101,18]
+    # result = Solution().lengthOfLIS(nums)
+    # print(result)
+
+    nums = [0,1,0,3,2,3]
     result = Solution().lengthOfLIS(nums)
     print(result)
 
