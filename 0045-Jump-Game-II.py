b14f29343af69f67b7c0c1ea228e7deb1cf06632
linghduoduo <linghypshen@gmail.com>
2020-12-22 00:42:51 -0500

    add code

--- 0045-Jump-Game-II.py
+++ 0045-Jump-Game-II.py
@@ -1,17 +1,33 @@
+# class Solution:
+#     def jump(self, nums) -> int:
+#         n = len(nums)
+#         res = 0
+
+#         reach = 0
+#         cur = 0
+#         i = 0
+#         while cur < n - 1:
+#             res += 1
+#             potential = cur
+#             while i <= potential:
+#                 cur = max(cur, i + nums[i])
+#                 i += 1
+#         return res
+
 class Solution:
     def jump(self, nums) -> int:
-        n = len(nums)
-        res = 0
+        if not nums or len(nums) < 2:
+            return 0
 
-        reach = 0
+        res = 0
         cur = 0
-        i = 0
-        while cur < n - 1:
-            res += 1
-            potential = cur
-            while i <= potential:
-                cur = max(cur, i + nums[i])
-                i += 1
+        maxreach = 0
+
+        for i in range(len(nums)-1):
+            maxreach = max(maxreach, i + nums[i])
+            if i == cur:
+                res += 1
+                cur = maxreach
         return res
 
 if __name__ == '__main__':
--- 0054-Spiral-Matrix.py
+++ 0054-Spiral-Matrix.py
@@ -70,6 +70,9 @@ if __name__ == '__main__':
         [4, 5, 6],
         [7, 8, 9]
     ]
+    result = Solution().spiralOrder(matrix)
+    print(result)
+
     matrix = [
     [1, 2, 3, 4], 
     [5, 6, 7, 8], 
