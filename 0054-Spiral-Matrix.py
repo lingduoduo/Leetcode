10bc74c437cbb8bbd3e059ccc97ddecce37c3950
linghduoduo <linghypshen@gmail.com>
2020-12-22 08:27:53 -0500

    add code

--- 0054-Spiral-Matrix.py
+++ 0054-Spiral-Matrix.py
@@ -73,9 +73,9 @@ if __name__ == '__main__':
     result = Solution().spiralOrder(matrix)
     print(result)
 
-    matrix = [
-    [1, 2, 3, 4], 
-    [5, 6, 7, 8], 
-    [9, 10, 11, 12]]
-    result = Solution().spiralOrder(matrix)
-    print(result)
+    # matrix = [
+    # [1, 2, 3, 4], 
+    # [5, 6, 7, 8], 
+    # [9, 10, 11, 12]]
+    # result = Solution().spiralOrder(matrix)
+    # print(result)
