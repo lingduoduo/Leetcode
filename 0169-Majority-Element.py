5f5ea45cc4e24f1a29e8d63300890ea7a7415c31
linghduoduo <linghypshen@gmail.com>
2020-12-23 13:34:13 -0500

    add code

--- 0169-Majority-Element.py
+++ 0169-Majority-Element.py
@@ -1,3 +1,4 @@
+import collections
 class Solution(object):
     def majorityElement(self, nums):
         """
@@ -10,3 +11,8 @@ class Solution(object):
         d = collections.Counter(nums)
         f = d.most_common()
         return f[0][0]
+
+if __name__ == '__main__':
+    nums = [2,2,1,1,1,2,2]
+    res = Solution().majorityElement(nums)
+    print(res)
---
+++ 0424-Longest-Repeating-Character-Replacement.py
@@ -0,0 +1,19 @@
+import collections
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        d = collections.Counter()
+        start = 0
+        res = 0
+        for idx, cha in enumerate(s):
+            d[cha] += 1
+            maxCnt = d.most_common(1)[0][1]
+            while idx - start + 1 - maxCnt > k:
+                d[s[start]] -= 1
+                start += 1
+            res = max(res, idx - start + 1)
+        return res
+
+if __name__ == '__main__':
+    s = "ABAB"
+    res = Solution().characterReplacement(s, 1)
+    print(res)
