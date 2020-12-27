98d55f98e24c91e044f920a97a9116b5b9bceae3
linghduoduo <linghypshen@gmail.com>
2020-12-26 19:03:35 -0500

    add code

--- 0494-Target-Sum.py
+++ 0494-Target-Sum.py
@@ -1,3 +1,12 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], S: int) -> int:
-    	
+    	n = len(nums)
+    	tot = sum(nums)
+    	dp = [[0] * (2 * tot + 1) for _ in (n+1)]
+
+    	dp[0][tot] = 1
+    	for i in range(n):
+    		for j in range(nums[i], tot-nums[i]):
+    			if dp[i][j]:
+    				dp[i + 1][nums[i] + j] += dp[i][j]
+    				dp[i + 1][nums[i] - j] += dp[i][j]
