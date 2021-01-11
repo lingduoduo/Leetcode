4bfeaf6df35e749fa135d5c7c81697ecedab13b5
linghduoduo <linghypshen@gmail.com>
2021-01-11 08:36:44 -0500

    add code

--- 0021-Merge-Two-Sorted-Lists.py
+++ 0021-Merge-Two-Sorted-Lists.py
@@ -12,50 +12,52 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        ###first try
-        ###if l1 is None:
-        ###    return l2
-        ###if l2 is None:
-        ###    return l1
-        #
-        ###if l1.val < l2.val:
-        ###    root = ListNode(l1.val)
-        ###    l1 = l1.next
-        ###    root.next = self.mergeTwoLists(l1, l2)
-        ###else:
-        ###    root = ListNode(l2.val)
-        ###    l2 = l2.next
-        ###    root.next = self.mergeTwoLists(l1, l2)
-        #
-        ###return root
+        if l1 is None:
+           return l2
+        if l2 is None:
+           return l1
+        
+        if l1.val < l2.val:
+           root = ListNode(l1.val)
+           l1 = l1.next
+           root.next = self.mergeTwoLists(l1, l2)
+        else:
+           root = ListNode(l2.val)
+           l2 = l2.next
+           root.next = self.mergeTwoLists(l1, l2)
+        
+        return root
 
-        ###second try
-        ###dummy = ListNode(-1)
-        ###dummy.next = head
-        ###curr = dummy
-        #
-        ###while l1 and l2:
-        ###    if l1.val < l2.val:
-        ###        curr.next = l1
-        ###        l1 = l1.next
-        ###        curr = curr.next
-        ###    else:
-        ###        curr.next = l2
-        ###        l2 = l2.next
-        ###        curr = curr.next
-        #
-        ###if l1:
-        ###    curr.next = l1
-        ###elif l2:
-        ###    curr.next = l2
-        #
-        ###return dummy.next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        dummy = ListNode(-1)
+        dummy.next = head
+        curr = dummy
+        
+        while l1 and l2:
+           if l1.val < l2.val:
+               curr.next = l1
+               l1 = l1.next
+               curr = curr.next
+           else:
+               curr.next = l2
+               l2 = l2.next
+               curr = curr.next
+        
+        if l1:
+           curr.next = l1
+        elif l2:
+           curr.next = l2
+        
+        return dummy.next
         
         if not l1:
             return l2
         if not l2:
             return l1
-        
+            
+ class Solution(object):
+    def mergeTwoLists(self, l1, l2):       
         dummy = ListNode(0)
         curr = dummy
         while l1 and l2 :
