3efccd80227e9a13af5b96407704b4186a1f8aa2
linghduoduo <linghypshen@gmail.com>
2021-01-17 10:40:11 -0500

    add code

--- 1221-Split-a-String-in-Balanced-Strings.py
+++ 1221-Split-a-String-in-Balanced-Strings.py
@@ -1,36 +1,41 @@
+# class Solution:
+#     def balancedStringSplit(self, s: str) -> int:
+#         r = s.split("L")
+#         curr = 0
+#         for i in range(len(r)):
+#             if r[i] != '':
+#                 r[curr] = r[i]
+#                 curr += 1
+#         r = r[:curr]
+#         l = s.split("R")
+#         curr = 0
+#         for i in range(len(l)):
+#             if l[i] != '':
+#                 l[curr] = l[i]
+#                 curr += 1
+#         l = l[:curr]
+#         return min(len(l), len(r))
+        
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
-        r = s.split("L")
-        curr = 0
-        for i in range(len(r)):
-            if r[i] != '':
-                r[curr] = r[i]
-                curr += 1
-        r = r[:curr]
-        l = s.split("R")
-        curr = 0
-        for i in range(len(l)):
-            if l[i] != '':
-                l[curr] = l[i]
-                curr += 1
-        l = l[:curr]
-        return min(len(l), len(r))
+        res = 0
+        l, r = 0, 0
+        for i in list(s):
+            print([i, l, r, res])
+            if i == 'L':
+                l += 1
+            else:
+                r += 1
+            if l == r:
+                res += 1
+                l = r = 0
+        return res
         
-        # while l and r:
-        #     while l[0] == '': l.pop(0)
-        #     while r[0] == '': r.pop(0)
-        #     left = l.pop(0)
-        #     right = r.pop(0)
-        #     size = min(len(left), len(right))
-        #     if s[0] == 'L':
-        #         res.append(left[:size]+right[:size])
-        #     else:
-        #         res.append(right[:size]+left[:size])
-        # return len(res)
 
 if __name__ == '__main__':
-    # s = "RLRRLLRLRRRL"
-    s = "RLLLLRRRLR"
+    # s = "RLRRLLRLRL"
+    # s = "RLLLLRRRLR"
+    s = "LLLLRRRR"
     results = Solution().balancedStringSplit(s)
     print(results)
 
