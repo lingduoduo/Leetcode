c9e0560e495d51282046926fef0d5719aad261b6
linghduoduo <linghypshen@gmail.com>
2021-01-19 09:47:01 -0500

    add code

--- 0832-Flipping-an-Image.py
+++ 0832-Flipping-an-Image.py
@@ -4,4 +4,10 @@ class Solution:
         for i in range(len(A)):
             res[i] = [1-num for num in A[i][::-1]]
         return res
-                
+
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        for row in A:
+            for i in range((len(row) + 1) // 2):
+                row[i], row[~i] = row[~i] ^ 1, row[i] ^ 1
+        return A
