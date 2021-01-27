64083d40f3d40908a49a243993bf35dc2121f884
linghduoduo <linghypshen@gmail.com>
2021-01-26 23:25:33 -0500

    add code

--- 1657-Determine-if-Two-Strings-Are-Close.py
+++ 1657-Determine-if-Two-Strings-Are-Close.py
@@ -1,3 +1,4 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-    	return set(word1) == set(word2) and Counter(Counter(word1).values()) == Counter(Counter(word2).values())
+    	return set(word1) == set(word2) 
+    	and collections.Counter(collections.Counter(word1).values()) == collections.Counter(collections.Counter(word2).values())
