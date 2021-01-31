df7d368dcd6f568bbaf4f9d581965cfe80ba007b
linghduoduo <linghypshen@gmail.com>
2021-01-30 19:02:24 -0500

    add codes

--- 0150-Evaluate-Reverse-Polish-Notation.py
+++ 0150-Evaluate-Reverse-Polish-Notation.py
@@ -8,14 +8,16 @@ class Solution:
             else:
                 n2 = res.pop()
                 n1 = res.pop()
+                res.append(int(eval(f'{n1}{token}{n2}')))
                 
-            if token == '+':
-                res.append(n1+n2)
-            elif token == '-':
-                res.append(n1-n2)
-            elif token == '*':
-                res.append(n1*n2)
-            elif token == '/':
-                res.append(int(n1/n2))
+            # if token == '+':
+            #     res.append(n1+n2)
+            # elif token == '-':
+            #     res.append(n1-n2)
+            # elif token == '*':
+            #     res.append(n1*n2)
+            # elif token == '/':
+            #     res.append(int(n1/n2))
+
 
         return res[0]
