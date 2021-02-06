01d4cda34a4d9dac12313eb50413db5783adc47a
linghduoduo <linghypshen@gmail.com>
2021-02-06 12:52:07 -0500

    add code

---
+++ 1035-Uncrossed-Lines.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxUncrossedLines(self, A: List[int], B: List[int]) -> int:
+        dp = [[0 for _ in range(len(B))] for _ in range(len(A))]
+        for i, a in enumerate(A):
+            for j, b in enumerate(B):
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                if a == b:
+                    if i >= 1 and j >=1:
+                        dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1)
+                    else:
+                        dp[i][j] = 1
+        return dp[-1][-1]
