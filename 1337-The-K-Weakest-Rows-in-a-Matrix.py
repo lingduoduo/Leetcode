0be0ecac09f80846c17596242826771df563ebf5
linghduoduo <linghypshen@gmail.com>
2021-02-15 09:30:37 -0500

    add code

---
+++ 1337-The-K-Weakest-Rows-in-a-Matrix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        stack = []
+        res = []
+        for idx, row in enumerate(mat):
+            heapq.heappush(stack, (sum(row), idx))
+        for i in range(k):
+            tot, idx = heapq.heappop(stack)
+            res.append(idx)
+        return res
