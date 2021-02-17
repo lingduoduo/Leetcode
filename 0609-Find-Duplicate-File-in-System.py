9def150423f6f0c817c99333871b8f86dc115d34
linghduoduo <linghypshen@gmail.com>
2021-02-17 10:30:08 -0500

    add codes

---
+++ 0609-Find-Duplicate-File-in-System.py
@@ -0,0 +1,21 @@
+from typing import List
+import collections
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        d = collections.defaultdict(list)
+        for path in paths:
+            parts = path.split()
+            for i in range(1, len(parts)):
+                k, v = parts[i].split("(")
+                v = v.replace(")", "")
+                d[v].append(parts[0] + "/" + k)
+        res = []
+        for k, v in d.items():
+            if len(v) > 1:
+                res.append(v)
+        return res
+
+if __name__ == '__main__':
+    paths = ["root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"]
+    res = Solution().findDuplicate(paths)
+    print(res)
---
+++ 0640-Solve-the-Equation.py
@@ -0,0 +1,46 @@
+import re
+class Solution:
+    def solveEquation(self, equation: str) -> str:
+        left, right = equation.split('=')
+        if left[0] == '-':
+            left = '0' + left
+        if right[0] == '-':
+            right = '0' + right
+        left = left.replace('-', '+-')
+        right = right.replace('-', '+-')
+        left_x, left_val, right_x, right_val = 0, 0, 0, 0
+        for num in left.split('+'):
+            if 'x' in num:
+                if num == 'x':
+                    left_x += 1
+                elif num == '-x':
+                    left_x -= 1
+                else:
+                    left_x += int(num[:-1])
+            else:
+                left_val += int(num)
+        for num in right.split('+'):
+            if 'x' in num:
+                if num == 'x':
+                    right_x += 1
+                elif num == '-x':
+                    right_x -= 1
+                else:
+                    right_x += int(num[:-1])
+            else:
+                right_val += int(num)
+        left_x -= right_x
+        right_val -= left_val
+        if left_x != 0 and right_val == 0:
+            return "x=0"
+        elif left_x != 0 and right_val != 0:
+            return 'x=' + str(right_val // left_x)
+        elif left_x == 0 and right_val == 0:
+            return "Infinite solutions"
+        elif left_x == 0 and right_val != 0:
+            return "No solution"
+
+if __name__ == '__main__':
+    equation = "x+5-3+x=6+x-2"
+    res = Solution().solveEquation(equation)
+    print(res)
