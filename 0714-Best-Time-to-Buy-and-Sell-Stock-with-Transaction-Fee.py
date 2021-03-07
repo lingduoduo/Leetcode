bade117b23bda1726a3eee1a220c103177c3d44d
linghduoduo <linghypshen@gmail.com>
2021-03-07 11:45:34 -0500

    add code

---
+++ 0714-Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        cash, hold = 0, -price[0]
+
+        for i in range(1, len(prices)):
+            cash = max(cash, hold + prices[i] - fee)
+            hold = max(hold, cash - prices[i])
+
+        retirm cash
