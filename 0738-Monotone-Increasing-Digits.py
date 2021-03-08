5bbae56cc340b9bd4c5e1801aebaa977907cadab
linghduoduo <linghypshen@gmail.com>
2021-03-08 09:34:44 -0500

    add code

---
+++ 0738-Monotone-Increasing-Digits.py
@@ -0,0 +1,26 @@
+class Solution:
+    def monotoneIncreasingDigits(self, N: int) -> int:
+        if N < 10:
+            return N
+
+        nums = list(map(int, list(str(N))))
+        
+        idx = len(nums) - 1
+        for i in range(len(nums)- 2, -1, -1):
+            if nums[i] > nums[i + 1] or (idx != len(nums) - 1 and nums[i] == nums[idx]):
+                idx = i
+
+        if idx == len(nums)-1:
+            return N
+
+        res = nums[:idx]
+        res.append(nums[idx] - 1)
+        for i in range(idx+1, len(nums)):
+            res.append(9)
+        
+        return str(int(''.join(map(str, res))))
+
+
+if __name__ == '__main__':
+    res = Solution().monotoneIncreasingDigits(668841)
+    print(res)
