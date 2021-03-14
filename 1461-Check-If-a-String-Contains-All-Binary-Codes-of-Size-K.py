574e3972ff1e8b906209e495246afbf57eb9f296
linghduoduo <linghypshen@gmail.com>
2021-03-14 12:53:48 -0400

    add code

---
+++ 1461-Check-If-a-String-Contains-All-Binary-Codes-of-Size-K.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        d = set()
+        for i in range(len(s) - k + 1):
+            d.add(s[i : (i + k)])
+        return len(d) == 2 ** k        
+
+
+if __name__ == '__main__':
+    # s = "00110110"
+    s = "00110"
+    k = 2
+    res = Solution().hasAllCodes(s, k)
+    print(res)
