f2298d0c7f24bf58d553032905adbafc5145665f
linghduoduo <linghypshen@gmail.com>
2021-03-21 16:50:51 -0400

    add code

---
+++ 0869-Reordered-Power-of-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reorderedPowerOf2(self, N: int) -> bool:
+    	pow2 = []
+    	for i in range(31):
+    		pow2.append(''.join(sorted(list(str(2 ** i)))))
+    	return ''.join(sorted(list(str(N)))) in pow2
+
+if __name__ == '__main__':
+	res = Solution().reorderedPowerOf2(5)
+	print(res)
