cb42ca8be2cb01d735a777ee4a419197e7d4dee5
linghduoduo <linghypshen@gmail.com>
2021-03-26 13:43:32 -0400

    add code

---
+++ 0777-Swap-Adjacent-in-LR-String.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canTransform(self, start: str, end: str) -> bool:
+        N = len(start)
+        sR = eR = sL = eL = 0
+        for x in range(N):
+            if start[x] == 'R': sR += 1
+            if end[x] == 'R': eR += 1
+            if sR < eR: return False
+        if sR != eR: return False
+
+        for x in range(N - 1, -1, -1):
+            if start[x] == 'L': sL += 1
+            if end[x] == 'L': eL += 1
+            if sL < eL: return False
+        if sL != eL: return False
+        
+        start, end = start.replace('X', ''), end.replace('X', '')
+        return start.split('R') == end.split('R')
