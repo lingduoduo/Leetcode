a51f5bb81741bc3cef00c5256d10e9af31520d2b
linghduoduo <linghypshen@gmail.com>
2021-03-27 12:57:37 -0400

    add code

---
+++ 0921-Minimum-Add-to-Make-Parentheses-Valid.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minAddToMakeValid(self, S: str) -> int:
+    	left = 0
+
+    	res = 0
+
+    	stack = []
+
+    	for s in S:
+    		if s == "(":
+    			stack.append(s)
+    			left += 1
+    		elif s == ")" and left > 0:
+    			stack.pop()
+    			left -= 1
+    		else:
+    			res += 1
+
+    	return res + len(stack)
+
+if __name__ == '__main__':
+	S = "())"
+	res = Solution().minAddToMakeValid(S)
+	print(res)
+
+	S = "((("
+	res = Solution().minAddToMakeValid(S)
+	print(res)
+
+	S = "()"
+	res = Solution().minAddToMakeValid(S)
+	print(res)
+
+	S = "()))(("
+	res = Solution().minAddToMakeValid(S)
+	print(res)
+		
