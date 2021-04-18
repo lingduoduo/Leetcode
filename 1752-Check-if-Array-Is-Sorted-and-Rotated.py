176351a119734a85f93c31b6f8be29bc9d7d4feb
linghduoduo <linghypshen@gmail.com>
2021-04-18 18:20:29 -0400

    add code

--- 1752-Check-if-Array-Is-Sorted-and-Rotated.py
+++ 1752-Check-if-Array-Is-Sorted-and-Rotated.py
@@ -1,36 +1,36 @@
 from typing import List
 class Solution:
     def check(self, nums: List[int]) -> bool:
-    	if len(nums) <= 1:
-    		return True
+        if len(nums) <= 1:
+            return True
 
-    	j = 1
-    	while j < len(nums):
-    		if nums[0] <= nums[1]:		
-	    		if nums[j-1] <= nums[j]:
-	    			j += 1
-	    		elif nums[j:] + nums[:j] == sorted(nums):
-	    			return True
-	    		else:
-	    			break
-	    	else:
-	    		if nums[j:] + nums[:j] == sorted(nums):
-	    			return True
-	    		else:
-	    			break
-    	if j == len(nums):
-    		return True
-    	return False
+        j = 1
+        while j < len(nums):
+            if nums[0] <= nums[1]:      
+                if nums[j-1] <= nums[j]:
+                    j += 1
+                elif nums[j:] + nums[:j] == sorted(nums):
+                    return True
+                else:
+                    break
+            else:
+                if nums[j:] + nums[:j] == sorted(nums):
+                    return True
+                else:
+                    break
+        if j == len(nums):
+            return True
+        return False
 
 if __name__ == '__main__':
-	nums = [1, 1, 1]
-	res = Solution().check(nums)
-	print(res)
+    nums = [1, 1, 1]
+    res = Solution().check(nums)
+    print(res)
 
-	nums = [3, 4, 5, 1, 2]
-	res = Solution().check(nums)
-	print(res)
+    nums = [3, 4, 5, 1, 2]
+    res = Solution().check(nums)
+    print(res)
 
-	nums = [2,1, 3, 4]
-	res = Solution().check(nums)
-	print(res)
+    nums = [2,1, 3, 4]
+    res = Solution().check(nums)
+    print(res)
