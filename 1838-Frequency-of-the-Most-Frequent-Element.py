a3e5dd422dc662fc18f62e6cf92a7a34e0fb4852
linghduoduo <linghypshen@gmail.com>
2021-04-26 10:01:20 -0400

    add code

---
+++ 1838-Frequency-of-the-Most-Frequent-Element.py
@@ -0,0 +1,22 @@
+from typing import List
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort()
+
+        count = 0
+        j = 0
+        res = 1
+        for i in range(len(nums)):
+        	count += (i-j) * nums[i] - nums[i-1]
+        	while count > k:
+        		count -= nums[i] - nums[j]
+        		j += 1
+        	res = max(res,  i - j + 1)
+        return res
+
+if __name__ == '__main__':
+	nums = [1,2,4]
+	k = 5
+	res = Solution().maxFrequency(nums, k)
+	print(res)
+
