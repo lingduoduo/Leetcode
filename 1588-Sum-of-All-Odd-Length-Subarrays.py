ce9ef19a5a56ddf51b091ec077af7d0e96c2b24f
linghduoduo <linghypshen@gmail.com>
2021-04-27 09:43:39 -0400

    add code

---
+++ 1588-Sum-of-All-Odd-Length-Subarrays.py
@@ -0,0 +1,26 @@
+from typing import List
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        lens = [i for i in range(1 , len(arr)+1) if i % 2 == 1]
+        res = 0
+        for l in lens:
+        	j = 0
+        	while j + l - 1 < len(arr):
+        		for i in range(j, j + l):
+        			res += arr[i]
+        		j += 1
+        return res
+
+
+if __name__ == '__main__':
+	arr = [1,4,2,5,3]
+	res = Solution().sumOddLengthSubarrays(arr)
+	print(res)
+
+	arr = [1,2]
+	res = Solution().sumOddLengthSubarrays(arr)
+	print(res)
+
+	arr = [10,11,12]
+	res = Solution().sumOddLengthSubarrays(arr)
+	print(res)
