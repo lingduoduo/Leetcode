566855e335f6b6cdc15cd315f947613844d9e16e
linghduoduo <linghypshen@gmail.com>
2021-05-02 20:38:31 -0400

    add code

--- 0888-Fair-Candy-Swap.py
+++ 0888-Fair-Candy-Swap.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def fairCandySwap(self, A: List[int], B: List[int]) -> List[int]:
         sumA = sum(A)
@@ -8,12 +9,22 @@ class Solution:
         res = []
 
         for a in A:
-        	if target - (sumA - a) in setB:
-        		res.append([a, target - (sumA - a)])
-        return res
+            if target - (sumA - a) in setB:
+                res.extend([a, target - (sumA - a)])
+                return res
 
 if __name__ == '__main__':
-	A = [1,1]
-	B = [2,2]
-	res = Solution().fairCandySwap(A, B)
-	print(res)
+    A = [1,1]
+    B = [2,2]
+    res = Solution().fairCandySwap(A, B)
+    print(res)
+
+    A = [1,2]
+    B = [2,3]
+    res = Solution().fairCandySwap(A, B)
+    print(res)
+
+    A = [2]
+    B = [1,3]
+    res = Solution().fairCandySwap(A, B)
+    print(res)
