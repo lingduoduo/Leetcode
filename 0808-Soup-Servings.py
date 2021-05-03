e9c0d393be14577ed1fcb0b180b7a61428717f5c
linghduoduo <linghypshen@gmail.com>
2021-05-03 09:36:49 -0400

    add code

---
+++ 0808-Soup-Servings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def soupServings(self, N: int) -> float:
+        self.d = dict()
+        if N > 5000: return 1.0
+        return self.solve(N, N)
+
+
+    def solve(self, A, B):
+        if (A, B) in self.d:
+            return self.d[(A, B)]
+        if A <= 0 and B > 0: return 1
+        if A <= 0 and B <= 0: return 0.5
+        if A > 0 and B <= 0: return 0
+        prob = 0.25 * (self.solve(A - 100, B) + self.solve(A - 75, B - 25) + self.solve(A - 50, B - 50) + self.solve(A - 25, B - 75))
+        self.d[(A, B)] = prob
+        return prob
