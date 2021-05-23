102ee4dabbb2076f27e424e0183619f042ae83a1
linghduoduo <linghypshen@gmail.com>
2021-05-23 16:08:31 -0400

    add code

---
+++ Leetcode/1048-Longest-String-Chain.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        words.sort(key=lambda x: len(x))
+        dic = {}
+        res = 0
+
+        for word in words:
+            cur = 1
+            for i in range(len(word)):
+                tmp = word[:i] + word[i+1:]
+                if tmp in dic:
+                    cur = max(cur, dic[tmp] + 1)
+                dic[word] = cur
+                res = max(res, dic[word])
+        return res
--- Leetcode/1576-Replace-All-?'s–to–Avoid-Consecutive-Repeating-Characters.py
+++ Leetcode/1576-Replace-All-?'s–to–Avoid-Consecutive-Repeating-Characters.py
@@ -3,33 +3,33 @@ class Solution:
         d = set('abcdefghijklmnopqrstuvwxyz')
 
         for cha in s:
-        	if cha != "?" and cha in d:
-        		d.remove(cha)
+            if cha != "?" and cha in d:
+                d.remove(cha)
 
 
         res = list(s)
         d = list(d)
         for idx, cha in enumerate(res):
-        	j = 0
-        	if cha == "?":
-        		while j < len(d) and ((idx >= 1 and d[j] == res[idx-1]) or (idx <= len(s)-2 and d[j] == res[idx+1])):
-        			j += 1
-        		res[idx] = d[j]
+            j = 0
+            if cha == "?":
+                while j < len(d) and ((idx >= 1 and d[j] == res[idx-1]) or (idx <= len(s)-2 and d[j] == res[idx+1])):
+                    j += 1
+                res[idx] = d[j]
         return ''.join(res)
 
 if __name__ == '__main__':
-	s = "?zs"
-	res = Solution().modifyString(s)
-	print(res)
+    s = "?zs"
+    res = Solution().modifyString(s)
+    print(res)
 
-	s = "ubv?w"
-	res = Solution().modifyString(s)
-	print(res)
+    s = "ubv?w"
+    res = Solution().modifyString(s)
+    print(res)
 
-	s = "j?qg??b"
-	res = Solution().modifyString(s)
-	print(res)
+    s = "j?qg??b"
+    res = Solution().modifyString(s)
+    print(res)
 
-	s = "??yw?ipkj?"
-	res = Solution().modifyString(s)
-	print(res)
+    s = "??yw?ipkj?"
+    res = Solution().modifyString(s)
+    print(res)
