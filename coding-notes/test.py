5915bd1bb5adc364221884c3b5e4f2ea505674e3
linghduoduo <linghypshen@gmail.com>
2021-07-27 14:43:34 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -5,28 +5,23 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    def reConstructBinaryTree(self, preorder, intorder):
-        if len(preorder) == 0 or len(intorder) == 0:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if len(preorder) == 0 or len(inorder) == 0:
             return None
 
-        root_idx = intorder.index(preorder[0])
+        root_idx = inorder.index(preorder[0])
         preorder_left = preorder[1:root_idx+1]
-        inorder_left = intorder[:root_idx]
+        inorder_left = inorder[:root_idx]
 
         preorder_right = preorder[root_idx+1:]
-        inorder_right = intorder[root_idx+1:]
+        inorder_right = inorder[root_idx+1:]
 
-        left = self.reConstructBinaryTree(preorder_left, inorder_left)
-        right = self.reConstructBinaryTree(preorder_right, inorder_right)
+        left = self.buildTree(preorder_left, inorder_left)
+        right = self.buildTree(preorder_right, inorder_right)
 
         return TreeNode(preorder[0], left, right)
 
 if __name__ == '__main__':
-    res = Solution().reConstructBinaryTree(preorder=[3, 9, 20, 15, 7], intorder=[9, 3, 15, 20, 7])
-    # l1 = ListNode(0)
-    # l1.next = ListNode(1)
-    # l1.next.next = ListNode(2)
-    # res = Solution().printListFromTailToHead(l1)
-    # res.printList()
+    res = Solution().buildTree(preorder=[3, 9, 20, 15, 7], intorder=[9, 3, 15, 20, 7])
 
 
