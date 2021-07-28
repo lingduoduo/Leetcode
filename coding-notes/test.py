0fa22551b822d6882ef64186ee6e2961a33456e0
linghduoduo <linghypshen@gmail.com>
2021-07-28 15:35:15 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,14 +1,18 @@
+from typing import List
 class Solution:
-    def Fibonacci(self, n) -> int:
-        res = []
-        res.append(0)
-        res.append(1)
-        for i in range(n-1):
-            res.append(res[-1] + res[-2])
-        print(res)
-        return res[-1]
-
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return None
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = left + (right - left)//2
+            if nums[mid] <= nums[right]:
+                right = mid
+            else:
+                left = mid + 1
+        return nums[left]
 
 if __name__ == '__main__':
-    res = Solution().Fibonacci(3)
+    res = Solution().findMin(nums=[3,4,5,1,2])
     print(res)
