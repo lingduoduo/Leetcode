aa9deb21ef236dfbc6da4a3c80cf360313a230f2
linghduoduo <linghypshen@gmail.com>
2021-07-28 11:29:03 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,27 +1,34 @@
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val=0, left=None, right=None, next=None):
         self.val = val
         self.left = None
         self.right = None
+        self.next = None
 
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        if len(preorder) == 0 or len(inorder) == 0:
-            return None
+    def GetNext(self, pNode) -> TreeNode:
+        if pNode.right != None:
+            pt = pNode.right
+            while pt.left:
+                pt = pt.left
+            return pt
+        else:
+            pt = pNode
+            while pt:
+                parent = pt.next
+                if parent.left == pt:
+                    return parent
+                pt = pt.next
+        return None
 
-        root_idx = inorder.index(preorder[0])
-        preorder_left = preorder[1:root_idx+1]
-        inorder_left = inorder[:root_idx]
-
-        preorder_right = preorder[root_idx+1:]
-        inorder_right = inorder[root_idx+1:]
-
-        left = self.buildTree(preorder_left, inorder_left)
-        right = self.buildTree(preorder_right, inorder_right)
-
-        return TreeNode(preorder[0], left, right)
 
 if __name__ == '__main__':
-    res = Solution().buildTree(preorder=[3, 9, 20, 15, 7], intorder=[9, 3, 15, 20, 7])
+    root = TreeNode(1, None, None, None)
+    root.left = TreeNode(2, None, None, root)
+    root.right = TreeNode(3, None, None, root)
+    root.left.left = TreeNode(4, None, None, root.left)
+    root.left.right = TreeNode(5, None, None, root.left)
+    res = Solution().GetNext(root.left)
+    print(res.val)
 
 
