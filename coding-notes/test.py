c7ab32da13ad757ba4c17f1977df39e8b92ac69f
linghduoduo <linghypshen@gmail.com>
2021-07-30 12:19:17 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,12 +1,12 @@
 class Solution:
-    def cutRope(self, n: int) -> int:
-        dp = [0] * (n + 1)
-        dp[1] = 1
-        for i in range(2, n+1):
-            for j in range(1, i):
-                dp[i] = max(dp[i], dp[j] * (i-j), j * (i - j))
-        return dp[n]
+    def NumberOf1(self, n: int) -> int:
+        res = 0
+        while n:
+            res += 1
+            n = n & (n - 1)
+        return res
+
 
 if __name__ == '__main__':
-    res = Solution().cutRope(n=10)
+    res = Solution().NumberOf1(15)
     print(res)
