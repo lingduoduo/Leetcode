b78630047b15ecf55510779df61cd8a2a9d881d4
linghduoduo <linghypshen@gmail.com>
2021-07-30 13:05:41 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,12 +1,21 @@
 class Solution:
-    def NumberOf1(self, n: int) -> int:
-        res = 0
-        while n:
-            res += 1
-            n = n & (n - 1)
-        return res
+    def power(self, x:int, n: int) -> int:
+        print([x, n])
+        if n == 0:
+            return 1
+        if n == 1:
+            return x 
 
+        self.res = 1
+        if x < 0:
+            self.res = -1
+            x = - x
+        if n % 2 == 0:
+            self.res *= self.power(x, n//2) ** 2
+        else:
+            self.res *= self.power(x, (n-1)//2) ** 2 
+        return self.res
 
 if __name__ == '__main__':
-    res = Solution().NumberOf1(15)
+    res = Solution().power(x=-2, n=5)
     print(res)
