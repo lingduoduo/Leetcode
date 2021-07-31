ce7ca9ed819b5f1aff4437fbb0f829dd2a162e45
linghduoduo <linghypshen@gmail.com>
2021-07-30 21:36:00 -0400

    add code

--- Leetcode/1071- Greatest-Common–Divisor-of-Strings.py
+++ Leetcode/1071- Greatest-Common–Divisor-of-Strings.py
@@ -1,5 +1,3 @@
-from typing import List
-from collections import Counter
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         if len(str1) > len(str2):
@@ -12,7 +10,6 @@ class Solution:
 
         i = len(str1)
         while i > 0:
-            print([str2, str1[:i]])
             m1 = len(str2)//len(str1[:i])
             m2 = len(str1)//len(str1[:i])
             if str2 == str1[:i] * m1 and str1 == str1[:i] * m2:
@@ -20,16 +17,3 @@ class Solution:
             else:
                 i -= 1
         return ""
-
-if __name__ == '__main__':
-    res = Solution().gcdOfStrings(str1 = "ABABAB", str2 = "AB")
-    print(res)
-
-    res = Solution().gcdOfStrings(str1 = "ABCABC", str2 = "ABC")
-    print(res)
-
-    res = Solution().gcdOfStrings(str1 = "LEET", str2 = "CODE")
-    print(res)
-
-    res = Solution().gcdOfStrings(str1 = "AA", str2 = "A")
-    print(res)
