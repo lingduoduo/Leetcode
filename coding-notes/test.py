62e98fdeb55d6d029fb3895b1595b9afe2b1d427
linghduoduo <linghypshen@gmail.com>
2021-08-02 18:05:11 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,39 +1,15 @@
-import re
 class Solution:
-    def isNumeric(self, s):
-        if len(s) == 0:
-            return None
-
-        return re.search(r"[+-]?\d*(\.\d+)?([eE][+-]?\d+)?", s).group() == s
-
+    def reOrderArray(self, nums):
+        n = len(nums)
+        for i in reversed(range(len(nums))):
+            for j in range(i):
+                if nums[i] % 2 == 1 and nums[j] % 2 == 0:
+                    nums[i], nums[j] = nums[j], nums[i]
+        return nums
 
 if __name__ == '__main__':
-
-    res = Solution().isNumeric(s="+100")
+    res = Solution().reOrderArray(nums=[1, 2, 3, 4, 5])
     print(res)
 
-    res = Solution().isNumeric(s="5e2")
+    res = Solution().reOrderArray(nums=[1, 2, 3, 4, 5, 6])
     print(res)
-
-    res = Solution().isNumeric(s="-123")
-    print(res)
-
-    res = Solution().isNumeric(s="+100")
-    print(res)
-
-    res = Solution().isNumeric(s="3.1416")
-    print(res)
-
-    res = Solution().isNumeric(s="-1E-16")
-    print(res)
-
-
-    res = Solution().isNumeric(s="12e")
-    print(res)
-
-    res = Solution().isNumeric(s="1.2.3")
-    print(res)
-
-    res = Solution().isNumeric(s="12e+4.3")
-    print(res)
-
