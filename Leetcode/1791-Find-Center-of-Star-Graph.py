46dfca51a39ece8ba37621c58cd9ef6cf505f874
linghduoduo <linghypshen@gmail.com>
2021-08-02 18:55:21 -0400

    add code

---
+++ Leetcode/1791-Find-Center-of-Star-Graph.py
@@ -0,0 +1,13 @@
+import collections
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        d = collections.defaultdict(list)
+        for x, y in edges:
+            d[x].append(y)
+            d[y].append(x)
+        
+        n = len(d.keys())
+        for k in d:
+            if len(d[k]) == n - 1:
+                return k
+                
