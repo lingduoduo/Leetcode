a96b3c8f1c333a8e4f4c64e573f29fa27d418306
linghduoduo <linghypshen@gmail.com>
2021-08-02 19:05:00 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,15 +1,31 @@
+class ListNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.next = None
+
 class Solution:
-    def reOrderArray(self, nums):
-        n = len(nums)
-        for i in reversed(range(len(nums))):
-            for j in range(i):
-                if nums[i] % 2 == 1 and nums[j] % 2 == 0:
-                    nums[i], nums[j] = nums[j], nums[i]
-        return nums
+    def FindKthToTail(self, head, k):
+        if not head:
+            return None
+
+        p1 = head
+        while p1 and k > 0:
+            p1 = p1.next
+            k -= 1
+
+        if k > 0:
+            return None
+
+        p2 = head
+        while p1:
+            p2 = p2.next
+            p1 = p1.next
+        return p2
+
 
 if __name__ == '__main__':
-    res = Solution().reOrderArray(nums=[1, 2, 3, 4, 5])
-    print(res)
-
-    res = Solution().reOrderArray(nums=[1, 2, 3, 4, 5, 6])
-    print(res)
+    root = ListNode(1)
+    root.next = ListNode(2)
+    root.next.next = ListNode(3)
+    res = Solution().FindKthToTail(root, 1)
+    print(res.val)
