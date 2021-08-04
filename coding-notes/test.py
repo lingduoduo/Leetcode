aa5178631f73ab7acdddadc9b0ee24d8f1fa95a8
linghduoduo <linghypshen@gmail.com>
2021-08-04 14:36:53 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,31 +1,36 @@
 class Solution:
-    def printMatrix(self, matrix):
-        r1 = 0
-        r2 = len(matrix) - 1
-        c1 = 0
-        c2 = len(matrix[0]) - 1
 
-        res = []
-        while r1 <= r2 and c1 <= c2:
-            for j in range(c1, c2 + 1 ):
-                res.append(matrix[r1][j])
-            r1 += 1
+    def IsPopOrder(self, pushSequence, popSequence):
+        stack = []
+        for i in range(len(pushSequence)):
+            stack.append(pushSequence[i])
+            while stack and popSequence and stack[-1] == popSequence[0]:
+                print(stack)
+                popSequence.pop(0)
+                stack.pop()
+        return len(stack) == 0
+                
 
-            for i in range(r1, r2 + 1):
-                res.append(matrix[i][c2])
-            c2 -= 1
-
-            for j in reversed(range(c1, c2 + 1)):
-                res.append(matrix[r2][j])
-            r2 -= 1
-            
-
-            for i in reversed(range(r1, r2 + 1)):
-                res.append(matrix[i][c1])
-            c1 += 1
-        return res          
 
 if __name__ == '__main__':
-    matrix = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]
-    res = Solution().printMatrix(matrix)
+    res = Solution().IsPopOrder(pushSequence=[1,2,3,4,5], popSequence=[4,5,3,2,1])
     print(res)
+
+    res = Solution().IsPopOrder(pushSequence=[1,2,3,4,5], popSequence=[4,3,5,1,2])
+    print(res)
+
+
+        
+# public boolean IsPopOrder(int[] pushSequence, int[] popSequence) {
+#     int n = pushSequence.length;
+#     Stack<Integer> stack = new Stack<>();
+#     for (int pushIndex = 0, popIndex = 0; pushIndex < n; pushIndex++) {
+#         stack.push(pushSequence[pushIndex]);
+#         while (popIndex < n && !stack.isEmpty() 
+#                 && stack.peek() == popSequence[popIndex]) {
+#             stack.pop();
+#             popIndex++;
+#         }
+#     }
+#     return stack.isEmpty();
+# }
