c58b6e6bd8179408e22ebd41520a69a6df819b75
linghduoduo <linghypshen@gmail.com>
2021-08-04 15:29:01 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,36 +1,61 @@
+class TreeNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
 class Solution:
+    def PrintFromTopToBottom(self, root):
+        queue = []
+        res = []
+        queue.append(root)
+        level = 0
 
-    def IsPopOrder(self, pushSequence, popSequence):
-        stack = []
-        for i in range(len(pushSequence)):
-            stack.append(pushSequence[i])
-            while stack and popSequence and stack[-1] == popSequence[0]:
-                print(stack)
-                popSequence.pop(0)
-                stack.pop()
-        return len(stack) == 0
+        while queue:
+            tmp = []
+            for i in range(len(queue)):
+                node = queue.pop(0)
+                tmp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            if level % 2 == 0:
+                res.append(tmp)
+            else:
+                res.append(tmp[::-1])
+            level += 1
+        return res
                 
-
-
 if __name__ == '__main__':
-    res = Solution().IsPopOrder(pushSequence=[1,2,3,4,5], popSequence=[4,5,3,2,1])
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.left.left = TreeNode(4)
+    root.right = TreeNode(3)
+    root.right.right = TreeNode(9)
+
+    res = Solution().PrintFromTopToBottom(root)
     print(res)
 
-    res = Solution().IsPopOrder(pushSequence=[1,2,3,4,5], popSequence=[4,3,5,1,2])
-    print(res)
 
 
-        
-# public boolean IsPopOrder(int[] pushSequence, int[] popSequence) {
-#     int n = pushSequence.length;
-#     Stack<Integer> stack = new Stack<>();
-#     for (int pushIndex = 0, popIndex = 0; pushIndex < n; pushIndex++) {
-#         stack.push(pushSequence[pushIndex]);
-#         while (popIndex < n && !stack.isEmpty() 
-#                 && stack.peek() == popSequence[popIndex]) {
-#             stack.pop();
-#             popIndex++;
+# ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+#     ArrayList<ArrayList<Integer>> ret = new ArrayList<>();
+#     Queue<TreeNode> queue = new LinkedList<>();
+#     queue.add(pRoot);
+#     while (!queue.isEmpty()) {
+#         ArrayList<Integer> list = new ArrayList<>();
+#         int cnt = queue.size();
+#         while (cnt-- > 0) {
+#             TreeNode node = queue.poll();
+#             if (node == null)
+#                 continue;
+#             list.add(node.val);
+#             queue.add(node.left);
+#             queue.add(node.right);
 #         }
+#         if (list.size() != 0)
+#             ret.add(list);
 #     }
-#     return stack.isEmpty();
-# }
+#     return ret;
