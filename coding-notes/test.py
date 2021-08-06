c6c26028ef729dd38a9176e72e68aa01e591f694
linghduoduo <linghypshen@gmail.com>
2021-08-06 17:16:10 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,41 +1,17 @@
+import heapq
+
 class Solution():
-    def getLeastNumbers_Solution(self, nums, k):
-        res = []
-        if k > len(nums) or k <= 0:
-            return res
+    def getMedian(self, nums):
+        minnums = nums[:]
+        heapq.heapify(minnums)
+        maxnums = [-num for num in nums]
+        heapq.heapify(maxnums)
 
-        self.findKthSmallest(nums, k - 1);
-        for i in range(k):
-            res.append(nums[i])
-        return res
-
-    def findKthSmallest(self, nums, k):
-        l = 0
-        r = len(nums) - 1
-        while l < r:
-            j = self.partition(nums, l, r)
-            print(j)
-            if j == k:
-                break
-            if j > k:
-                r = j - 1
-            else:
-                l = j + 1
-
-    def partition(self, nums, l, r):
-        p = nums[l]
-        i = l 
-        j = r 
-        while i < j:
-            while i < r and nums[i] < p:
-                i += 1
-            while j > l and nums[j] > p:
-                j -= 1
-            if i == j:
-                break
-            nums[i], nums[j] = nums[j], nums[i]
-        return j 
+        for i in range(len(nums)//2):
+            min_num = heapq.heappop(minnums)
+            max_num = - heapq.heappop(maxnums)
+        return (min_num + max_num)/2 if len(nums) % 2 == 0 else heapq.heappop(minnums)
 
 if __name__ == '__main__':
-    res = Solution().getLeastNumbers_Solution([4, 5, 1, 6, 2, 7, 3, 8], k=4)
+    res = Solution().getMedian([4, 5, 1, 6, 2, 7, 3, 8])
     print(res)
