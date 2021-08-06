1ce4cf1d6698317192d5f71d16b175c6a6a060cc
linghduoduo <linghypshen@gmail.com>
2021-08-05 20:55:01 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,49 +1,33 @@
-import collections
-
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-
-
 class Solution:
-    def serialize(self, root):
-        if root is None:
-            return "#"
-        return str(root.val) + " " + self.serialize(root.left) + " " + self.serialize(root.right)
+    def permutation(self, strs: str):
+        self.res = []
 
-    def deserialize(self, strs):
-        vals = collections.deque(val for val in strs.split())
-        return self.build(vals)
+        chars = list(strs)
+        chars.sort()
 
-    def build(self, vals):
-        if not vals:
-            return None
-        
-        val = vals.popleft()
-        if val == '#':
-            return None
-        root = TreeNode(int(val))
-        root.left = self.build(vals)
-        root.right = self.build(vals)
-        return root
+        visited = [False] * len(strs)
+
+        self.backtracking(chars, visited, path=[])
+        return self.res
+
+    def backtracking(self, chars, visited, path):
+        print(path, visited)
+        if len(chars) == len(path):
+            self.res.append("".join(path[:]))
+            return
+
+        for i in range(len(chars)):
+            if visited[i]:
+                continue
+            if (i != 0 and chars[i] == chars[i-1]) and not visited[i-1]:
+                continue
+            visited[i] = True
+            path.append(chars[i])
+            self.backtracking(chars, visited, path)
+            path.pop()
+            visited[i] = False
 
 if __name__ == '__main__':
-    root = TreeNode(10)
-    root.left = TreeNode(6)
-    root.left.left = TreeNode(4)
-    root.left.right = TreeNode(8)
-
-    root.right = TreeNode(14)
-    root.right.left = TreeNode(12)
-    root.right.right = TreeNode(16)
-
-    res = Solution().serialize(root)
-    print(res)
-
-    root = Solution().deserialize(res)
-    
-    res = Solution().serialize(root)
+    res = Solution().permutation(strs="aabc")
     print(res)
 
