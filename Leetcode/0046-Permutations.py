e74478507defec60f0a8d56c7d71fbba7f301d01
linghduoduo <linghypshen@gmail.com>
2021-08-05 21:07:38 -0400

    add code

--- Leetcode/0046-Permutations.py
+++ Leetcode/0046-Permutations.py
@@ -1,59 +1,33 @@
-# class Solution(object):
-#     def permute(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: List[List[int]]
-#         """
-#         self.res = []
-#         self.dfs(nums, [])
-#         return self.res
+from itertools import permutations
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return list(permutations(nums))
 
-#     def dfs(self, nums, path):
-#         if nums == []:
-#             return self.res.append(path)
-#         else:
-#             for i in range(len(nums)):
-#                 self.dfs(nums[:i] + nums[i+1:], path+[nums[i]])
-                
-    ###second try
-    ###    res = []
-    ###    self.tot = len(nums)
-    ###    self.dfs(nums, res, 0, [])
-    ###    return res
-    #
-    ###def dfs(self, nums, res, depth, path):
-    ###    if depth == self.tot:
-    ###        res.append(path)
-    ###    else:
-    ###        for i in range(len(nums)):
-    ###            self.dfs(nums[:i] + nums[i + 1:], res, depth + 1, path + [nums[i]])
-
-    ###third try
-        # if len(nums) <= 1:
-        #     return [nums]
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        visited = [0] * len(nums)
+        res = []
         
-        # res = []
-        # for i, num in enumerate(nums):
-        #     n = nums[:i] + nums[i+1:]
-        #     for y in self.permute(n):
-        #         res.append([num]+y)
-        # return res
+        def dfs(path):
+            if len(path) == len(nums):
+                res.append(path)
+            else:
+                for i in range(len(nums)):
+                    if not visited[i]:
+                        visited[i] = 1
+                        dfs(path + [nums[i]])
+                        visited[i] = 0
         
-
-        # visited = [0]*len(nums)
-        # res = []
-
-        # def dfs(path):
-        #     if len(path) == len(nums):
-        #         res.append(path)
-        #     else:
-        #         for i in range(len(nums)):
-        #             if not visited[i]:
-        #                 visited[i]=1
-        #                 dfs(path+nums[i])
-        #                 visited[i]=0
-        # dfs([])
-        # return res
+        dfs([])
+        return res
 
 class Solution(object):
     def permute(self, nums):
