3c0f9902b58c28ca9d392a6d6a4fa4cc20db5ceb
linghduoduo <linghypshen@gmail.com>
2021-08-06 15:52:20 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,21 +1,26 @@
+# from typing import List
+# import heapq
+# class Solution:
+#     def getLeastNumbers_Solution(self, nums, k):
+#         res = []
+#         heapq.heapify(nums)
+#         for i in range(k):
+#             res.append(heapq.heappop(nums))
+#         return res
+
+
 from typing import List
+import heapq
 class Solution:
-    def moreThanHalfNum_Solution(self, numbers):
-        # return sorted(numbers)[len(numbers)//2]
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        nums = [matrix[i][j] for i in range(len(matrix)) for j in range(len(matrix[0]))]
+        print(nums)
 
-        majority = numbers[0]
-        cnt = 0
-        for num in numbers:
-            if num == majority:
-                cnt += 1
-            else:
-                cnt -= 1
-            if cnt == 0:
-                majority = num
-                cnt = 1
-        return majority
-
+        heapq.heapify(nums)
+        for i in range(k):
+            res = heapq.heappop(nums)
+        return res
 
 if __name__ == '__main__':
-    res = Solution().moreThanHalfNum_Solution(numbers = [1, 2, 3, 2, 2, 2, 5, 4, 2])
+    res = Solution().kthSmallest(matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8)
     print(res)
