eb7aeacbf9faca563627cd87cedf0f92d773f1e2
linghduoduo <linghypshen@gmail.com>
2021-08-06 16:28:52 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,26 +1,41 @@
-# from typing import List
-# import heapq
-# class Solution:
-#     def getLeastNumbers_Solution(self, nums, k):
-#         res = []
-#         heapq.heapify(nums)
-#         for i in range(k):
-#             res.append(heapq.heappop(nums))
-#         return res
+class Solution():
+    def getLeastNumbers_Solution(self, nums, k):
+        res = []
+        if k > len(nums) or k <= 0:
+            return res
 
-
-from typing import List
-import heapq
-class Solution:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        nums = [matrix[i][j] for i in range(len(matrix)) for j in range(len(matrix[0]))]
-        print(nums)
-
-        heapq.heapify(nums)
+        self.findKthSmallest(nums, k - 1);
         for i in range(k):
-            res = heapq.heappop(nums)
+            res.append(nums[i])
         return res
 
+    def findKthSmallest(self, nums, k):
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            j = self.partition(nums, l, r)
+            print(j)
+            if j == k:
+                break
+            if j > k:
+                r = j - 1
+            else:
+                l = j + 1
+
+    def partition(self, nums, l, r):
+        p = nums[l]
+        i = l 
+        j = r 
+        while i < j:
+            while i < r and nums[i] < p:
+                i += 1
+            while j > l and nums[j] > p:
+                j -= 1
+            if i == j:
+                break
+            nums[i], nums[j] = nums[j], nums[i]
+        return j 
+
 if __name__ == '__main__':
-    res = Solution().kthSmallest(matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8)
+    res = Solution().getLeastNumbers_Solution([4, 5, 1, 6, 2, 7, 3, 8], k=4)
     print(res)
