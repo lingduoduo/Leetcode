eb2989fb95f0f316862fa9436b2ade43be511196
linghduoduo <linghypshen@gmail.com>
2021-08-08 15:34:46 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,47 +1,32 @@
 class Solution:
-    def firstNotRepeatingChar(self, s: str) -> int:
-        cnts = [0] * 128
-        for i in range(len(s)):
-            cnts[ord(s[i]) - ord('a')] += 1
-            
-        for i in range(len(s)):
-            if cnts[ord(s[i]) - ord('a')] == 1:
-                return s[i]
-        return -1
+    def getMost(self, matrix) -> int:
+        if not matrix:
+            return 0 
+
+        n = len(matrix)
+        dp = [0] * n
+        for i in range(n):
+            dp[0] += matrix[i][0]
+            for j in range(1, n):
+                dp[j] = max(dp[j], dp[j-1]) + matrix[i][j]
+        return dp[n-1]
 
 if __name__ == '__main__':
-    res = Solution().firstNotRepeatingChar('abacc')
+    matrix = [[1, 10, 3, 8], [12, 2, 9, 6], [5, 7, 4, 11], [3, 7, 16, 5]]
+    res = Solution().getMost(matrix=matrix)
     print(res)
 
-
 # ```java
-# public int FirstNotRepeatingChar(String str) {
-#     int[] cnts = new int[128];
-#     for (int i = 0; i < str.length(); i++)
-#         cnts[str.charAt(i)]++;
-#     for (int i = 0; i < str.length(); i++)
-#         if (cnts[str.charAt(i)] == 1)
-#             return i;
-#     return -1;
-# }
-# ```
-
-# 以上实现的空间复杂度还不是最优的。考虑到只需要找到只出现一次的字符，那么需要统计的次数信息只有 0,1,更大，使用两个比特位就能存储这些信息。
-# ```java
-# public int FirstNotRepeatingChar2(String str) {
-#     BitSet bs1 = new BitSet(128);
-#     BitSet bs2 = new BitSet(128);
-#     for (char c : str.toCharArray()) {
-#         if (!bs1.get(c) && !bs2.get(c))
-#             bs1.set(c);     // 0 0 -> 0 1
-#         else if (bs1.get(c) && !bs2.get(c))
-#             bs2.set(c);     // 0 1 -> 1 1
+# public int getMost(int[][] values) {
+#     if (values == null || values.length == 0 || values[0].length == 0)
+#         return 0;
+#     int n = values[0].length;
+#     int[] dp = new int[n];
+#     for (int[] value : values) {
+#         dp[0] += value[0];
+#         for (int i = 1; i < n; i++)
+#             dp[i] = Math.max(dp[i], dp[i - 1]) + value[i];
 #     }
-#     for (int i = 0; i < str.length(); i++) {
-#         char c = str.charAt(i);
-#         if (bs1.get(c) && !bs2.get(c))  // 0 1
-#             return i;
-#     }
-#     return -1;
+#     return dp[n - 1];
 # }
 # ```
