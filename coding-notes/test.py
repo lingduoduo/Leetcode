4aff1c7a3b00a3f0d866f7db175dfd5a30419105
linghduoduo <linghypshen@gmail.com>
2021-08-08 16:20:15 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,6 +1,18 @@
 class Solution:
     def getMost(self, strs) -> int:
-        
+        cur = 0
+        res = 0
+        preindex = [-1] * 26
+
+        for i, cha in enumerate(strs):
+            idx = ord(cha) - ord("a")
+            if preindex[idx] == -1 or i - preindex[idx] > cur:
+                cur += 1
+            else:
+                res = max(res, cur)
+                cur = i - preindex[idx]
+            preindex[idx] = i
+        return res
 
 if __name__ == '__main__':
     res = Solution().getMost(strs="arabcacfr")
