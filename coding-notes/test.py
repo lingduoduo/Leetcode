0a22d6ae618897303a1d7248c4ecaefb6b762eb0
linghduoduo <linghypshen@gmail.com>
2021-08-09 18:43:19 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,34 +1,28 @@
-class listNode:
-    def __init__(self, val):
-        self.val = val
-        self.next = None 
-
 class Solution:
-    def findFirstCommonNode(self, l1, l2):
-        p1, p2 = l1, l2
-        while p1 != p2:
-            p1 = l2 if p1 is None else p1.next
-            p2 = l1 if p2 is None else p2.next
-        return p1
+    def binarySearch(self, nums, k):
+        l = 0
+        r = len(nums)
+        
+        while l < r:
+            m = l + (r - l) // 2
+            if nums[m] >= k:
+                r = m 
+            else:
+                l = m + 1
+        return l
 
-
+    def getNumberofK(self, nums, k):
+        first = self.binarySearch(nums, k)
+        print(first)
+        last = self.binarySearch(nums, k + 1)
+        print(last)
+        return 0 if first == len(nums) - 1 or nums[first] != k else last - first
+    
 if __name__ == '__main__':
-    l1 = listNode(1)
-    l1.next = listNode(2)
-    l1.next.next = listNode(3)
+    # nums = [1, 2, 3, 3, 3, 3, 4, 5]
+    # res = Solution().getNumberofK(nums, 3)
+    # print(res)
 
-    l2 = listNode(1)
-    l2.next = listNode(2)
-    l2.next.next = l1.next.next
-
-    res = Solution().findFirstCommonNode(l1, l2)
-    print(res.val)
-
-
-#     ListNode l1 = pHead1, l2 = pHead2;
-#     while (l1 != l2) {
-#         l1 = (l1 == null) ? pHead2 : l1.next;
-#         l2 = (l2 == null) ? pHead1 : l2.next;
-#     }
-#     return l1;
-# }
+    nums = [2,2]
+    res = Solution().getNumberofK(nums, 2)
+    print(res)
