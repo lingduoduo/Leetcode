e9c4d769846bf17e823cfb69ab4f47b6dd9ba843
linghduoduo <linghypshen@gmail.com>
2021-08-09 15:38:41 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,25 +1,42 @@
 class Solution:
-    def getUglyNumber_Solution(self, n) -> int:
-        if n <= 6:
-            return n 
-        i, j, k = 0, 0, 0 
-        dp = [0] * n 
-        dp[0] = 1
-        for idx in range(1, n):
-            next2 = dp[i] * 2
-            next3 = dp[j] * 3
-            next5 = dp[k] * 5
-            dp[idx] = min(next2, next3, next5)
-            if next2 == dp[idx]:
-                i += 1
-            elif next3 == dp[idx]:
+    def inversePairs(self, nums):
+        self.res = [0] * len(nums)
+        return self.mergeSort(nums, 0, len(nums)-1)
+
+    def mergeSort(self, A, l, r):
+        if l >= r:
+            return 0
+        
+        m = (l + r) >> 1
+        ans = self.mergeSort(A, l, m) + self.mergeSort(A, m + 1, r)
+
+        i, j, k = l, m + 1, l
+        while i <= m and j <= r:
+            if A[i] > A[j]:
+                self.res[k] = A[j]
                 j += 1
+                ans += m - i + 1
             else:
-                k += 1
-        return dp[-1]
+                self.res[k] = A[i]
+                i += 1
+            k += 1
+    
+        while i <= m:
+            self.res[k] = A[i]
+            k += 1
+            i += 1
+        while j <= r:
+            self.res[k] = A[j]
+            k += 1
+            j += 1
+        for i in range(l, r + 1):
+            A[i] = self.res[i]
+        print(A)
+
+        return ans
 
 if __name__ == '__main__':
-    res = Solution().getUglyNumber_Solution(n=7)
+    res = Solution().inversePairs(nums=[2, 4, 1, 3, 5])
     print(res)
 
 
