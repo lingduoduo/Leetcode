696cacb417b55564e0a45002aaf2400570cc2633
linghduoduo <linghypshen@gmail.com>
2021-08-10 10:43:45 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,57 +1,38 @@
-class treeNode:
-    def __init__(self, val):
-        self.val = val 
-        self.left = None
-        self.right = None
-
-
 class Solution:
-    def isBalanced(self, root):
-        self.res  = True
-        self.height
-        return self.res
+    def findNumsAppearOnce(self, nums):
+        return nums
 
-    def height(self, root):
-        if not root or not self.res:
-            return 0
 
-        left = self.height(root.left)
-        right = self.height(root.right)
-        if abs(left - right) <= 1:
-            self.res = True
-        else:
-            self.res = False
-        return 1 + max(left, right) 
 
     
 if __name__ == '__main__':
-    root = treeNode(5)
-    root.left = treeNode(3)
-    root.right = treeNode(7)
-    root.left.left = treeNode(2)
-    root.left.right = treeNode(4)
-    root.right.left = treeNode(6)
-    root.right.right = treeNode(8)
-
-    res = Solution().isBalanced(root)
+    nums = [2, 4, 3, 6, 3, 2, 5, 5]
+    res = Solution().findNumsAppearOnce(nums)
     print(res)
   
 
 # ```java
-# private boolean isBalanced = true;
-
-# public boolean IsBalanced_Solution(TreeNode root) {
-#     height(root);
-#     return isBalanced;
+# public int[] FindNumsAppearOnce (int[] nums) {
+#     int[] res = new int[2];
+#     int diff = 0;
+#     for (int num : nums)
+#         diff ^= num;
+#     diff &= -diff;
+#     for (int num : nums) {
+#         if ((num & diff) == 0)
+#             res[0] ^= num;
+#         else
+#             res[1] ^= num;
+#     }
+#     if (res[0] > res[1]) {
+#         swap(res);
+#     }
+#     return res;
 # }
 
-# private int height(TreeNode root) {
-#     if (root == null || !isBalanced)
-#         return 0;
-#     int left = height(root.left);
-#     int right = height(root.right);
-#     if (Math.abs(left - right) > 1)
-#         isBalanced = false;
-#     return 1 + Math.max(left, right);
-# }
+# private void swap(int[] nums) {
+#     int t = nums[0];
+#     nums[0] = nums[1];
+#     nums[1] = t;
+
 # ```
