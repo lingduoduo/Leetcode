54da5e152a7800b96703b483cb48d47eec62b64c
linghduoduo <linghypshen@gmail.com>
2021-08-09 20:41:26 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -6,11 +6,22 @@ class treeNode:
 
 
 class Solution:
-    def treeDepth(self, root):
-        if root is None:
+    def isBalanced(self, root):
+        self.res  = True
+        self.height
+        return self.res
+
+    def height(self, root):
+        if not root or not self.res:
             return 0
+
+        left = self.height(root.left)
+        right = self.height(root.right)
+        if abs(left - right) <= 1:
+            self.res = True
         else:
-            return 1 + max(self.treeDepth(root.left), self.treeDepth(root.right)) 
+            self.res = False
+        return 1 + max(left, right) 
 
     
 if __name__ == '__main__':
@@ -22,6 +33,25 @@ if __name__ == '__main__':
     root.right.left = treeNode(6)
     root.right.right = treeNode(8)
 
-    res = Solution().treeDepth(root)
+    res = Solution().isBalanced(root)
     print(res)
   
+
+# ```java
+# private boolean isBalanced = true;
+
+# public boolean IsBalanced_Solution(TreeNode root) {
+#     height(root);
+#     return isBalanced;
+# }
+
+# private int height(TreeNode root) {
+#     if (root == null || !isBalanced)
+#         return 0;
+#     int left = height(root.left);
+#     int right = height(root.right);
+#     if (Math.abs(left - right) > 1)
+#         isBalanced = false;
+#     return 1 + Math.max(left, right);
+# }
+# ```
