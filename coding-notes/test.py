e1eee05ee43954a0e3cbc48631c2b5ded2d3fcaf
linghduoduo <linghypshen@gmail.com>
2021-08-12 11:51:19 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,21 +1,14 @@
-import collections
 class Solution(object):
-    def isContinuous(self, nums):
-        nums.sort()
+    def lastRemaining_Solution(self, n, m):
+        if n == 0:
+            return -1 
+        if n == 1:
+            return 0 
+        return (self.lastRemaining_Solution(n - 1, m) + m) % n
 
-        cnt = sum([1 for num in nums if num == 0])
-        
-        for i in range(len(nums) - 1):
-            if nums[i + 1] == nums[i]: 
-                return False
-            else:
-                cnt -= nums[i + 1] - nums[i] - 1 
-        return cnt >= 0
 
 
 if __name__ == '__main__':
-    res = Solution().isContinuous(nums=[1, 0, 3, 4, 5])
+    res = Solution().lastRemaining_Solution(n = 5, m = 2)
     print(res)
 
-    res = Solution().isContinuous(nums=[1, 2, 4, 6, 7])
-    print(res)
