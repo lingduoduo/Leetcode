6f395e059b05843bc21feafd4b520fb2c84c44bd
linghduoduo <linghypshen@gmail.com>
2021-08-13 13:51:14 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,7 +1,20 @@
 class Solution:
-    def sum_Solution(self, n : int) -> int:
-        return n and (n + self.sum_Solution(n - 1))    
+    def multiply(self, nums) -> int:
+        res = [1] * len(nums)
+        prod = 1
+        for i in range(1, len(nums)):
+            prod *=  nums[i - 1]
+            res[i] = prod
 
+        prod = 1
+        for i in reversed(range(len(nums) - 1)):
+            prod *=  nums[i + 1]
+            res[i] *= prod
+
+        return res
+
+        
 if __name__ == '__main__':
-    res = Solution().sum_Solution(n = 100)
+    res = Solution().multiply(nums = [1,2,3,4,5])
     print(res)
+
