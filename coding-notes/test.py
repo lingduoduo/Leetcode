d26e09814f5e0ffa106bd61c358d7fe815e13150
linghduoduo <linghypshen@gmail.com>
2021-08-13 14:35:42 -0400

    add code

--- coding-notes/test.py
+++ coding-notes/test.py
@@ -1,20 +1,9 @@
 class Solution:
-    def multiply(self, nums) -> int:
-        res = [1] * len(nums)
-        prod = 1
-        for i in range(1, len(nums)):
-            prod *=  nums[i - 1]
-            res[i] = prod
+    def add(self, a, b) -> int:
+        return a if b == 0 else self.add(a ^ b, (a & b) << 1)
 
-        prod = 1
-        for i in reversed(range(len(nums) - 1)):
-            prod *=  nums[i + 1]
-            res[i] *= prod
 
-        return res
-
-        
 if __name__ == '__main__':
-    res = Solution().multiply(nums = [1,2,3,4,5])
+    res = Solution().add(a = 2, b = 4)
     print(res)
 
