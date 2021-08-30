628544e0d9ac68b320b7ad77a1dbe2dbf3db10bb
Ling Huang <linghypshenl@gmail.com>
2021-08-30 09:16:45 -0400

    add code

--- Leetcode/0019-Remove-Nth-Node-From-End-of-List.py
+++ Leetcode/0019-Remove-Nth-Node-From-End-of-List.py
@@ -4,13 +4,13 @@
 ###        self.val = x
 ###        self.next = None
 
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
+# class Solution(object):
+#     def removeNthFromEnd(self, head, n):
+#         """
+#         :type head: ListNode
+#         :type n: int
+#         :rtype: ListNode
+#         """
         ###first try
         ###dummy = ListNode(0)
         ###dummy.next = head
@@ -28,17 +28,46 @@ class Solution(object):
         ###d[pos].next = d[pos].next.next
         ###return dummy.next
 
-        dummy = ListNode(0)
-        slow = fast = dummy
-        dummy.next = head
+        # dummy = ListNode(0)
+        # slow = fast = dummy
+        # dummy.next = head
+        #
+        # for i in range(n):
+        #     fast = fast.next
+        #
+        # while fast.next != None:
+        #     fast = fast.next
+        #     slow = slow.next
+        #
+        # slow.next = slow.next.next
+        # return dummy.next
 
+# Definition for singly-linked list.
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        fast = head
         for i in range(n):
             fast = fast.next
 
-        while fast.next != None:
+        if fast is None:
+            return head.next
+
+        slow = head
+        while fast.next:
             fast = fast.next
             slow = slow.next
-
         slow.next = slow.next.next
-        return dummy.next
-    
+        return head
+
+
+
+
+
+
--- data-structures/1-linked-list.md
+++ data-structures/1-linked-list.md
@@ -193,3 +193,51 @@ class Solution:
         return head.next if head.val == head.next.val else head
 ```
 
+5. 删除链表的倒数第 n 个节点
+
+19 Remove Nth Node From End of List (Medium)
+
+Given linked list: 1->2->3->4->5, and n = 2.
+
+After removing the second node from the end, the linked list becomes 1->2->3->5.
+```java
+public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode fast = head;
+    while (n-- > 0) {
+        fast = fast.next;
+    }
+    if (fast == null) return head.next;
+    ListNode slow = head;
+    while (fast.next != null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    return head;
+}
+```
+
+```python
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        fast = head
+        for i in range(n):
+            fast = fast.next
+
+        if fast is None:
+            return head.next
+
+        slow = head
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head
+```
