942652c11a73d094c041b4de554eb15f137b28c6
Ling Huang <linghypshen@gmail.com>
2021-09-06 10:11:03 -0400

    add code

--- Leetcode/0543-Diameter-of-Binary-Tree.py
+++ Leetcode/0543-Diameter-of-Binary-Tree.py
@@ -5,27 +5,27 @@
 ###        self.left = None
 ###        self.right = None
 
-class Solution(object):
-    def diameterOfBinaryTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        if root is None: return 0
-        result = 0
-        
-        def depth(root):
-            if root is None:
-                return 0
-            if root.left is None and root.right is None:
-                return 1
-            l = depth(root.left)
-            r = depth(root.right)
-            result = max(result, l + r)
-            return max(l, r) + 1
-        
-        depth(root)
-        return result
+# class Solution(object):
+#     def diameterOfBinaryTree(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: int
+#         """
+#         if root is None: return 0
+#         result = 0
+#
+#         def depth(root):
+#             if root is None:
+#                 return 0
+#             if root.left is None and root.right is None:
+#                 return 1
+#             l = depth(root.left)
+#             r = depth(root.right)
+#             result = max(result, l + r)
+#             return max(l, r) + 1
+#
+#         depth(root)
+#         return result
 
 
 # Definition for a binary tree node.
@@ -34,6 +34,33 @@ class Solution(object):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        self.depth(root)
+        return self.res
+
+    def depth(self, root):
+        if not root:
+            return 0
+
+        l = self.depth(root.left)
+        r = self.depth(root.right)
+
+        self.res = max(self.res, l + r)
+
+        return 1 + max(l, r)
+
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
         if not root: return 0
--- data-structures/2-tree.md
+++ data-structures/2-tree.md
@@ -50,5 +50,82 @@ public int maxDepth(TreeNode root) {
 ```
 
 ```python
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.res = True
+        self.maxDepth(root)
+        return self.res
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        l = self.maxDepth(root.left)
+        r = self.maxDepth(root.right)
+        if abs(l - r) > 1:
+            self.res = False
+        return 1 + max(l, r)
+```
+
+3. 两节点的最长路径
+543 Diameter of Binary Tree (Easy)
+
+Input:
+
+         1
+        / \
+       2  3
+      / \
+     4   5
+
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+
+```java
+private int max = 0;
+
+public int diameterOfBinaryTree(TreeNode root) {
+    depth(root);
+    return max;
+}
+
+private int depth(TreeNode root) {
+    if (root == null) return 0;
+    int leftDepth = depth(root.left);
+    int rightDepth = depth(root.right);
+    max = Math.max(max, leftDepth + rightDepth);
+    return Math.max(leftDepth, rightDepth) + 1;
+}
+```
+
+```python
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        self.depth(root)
+        return self.res
+
+    def depth(self, root):
+        if not root:
+            return 0
+
+        l = self.depth(root.left)
+        r = self.depth(root.right)
+
+        self.res = max(self.res, l + r)
+
+        return 1 + max(l, r)
 ```
