acbdd8ab8b122414088da3b21c2e2725c82fb3ea
Ling Huang <linghypshen@gmail.com>
2021-09-06 10:09:23 -0400

    add code

--- Leetcode/0110-Balanced-Binary-Tree.py
+++ Leetcode/0110-Balanced-Binary-Tree.py
@@ -6,7 +6,7 @@
 ###        self.right = None
 
 
-class Solution(object):
+# class Solution(object):
 ###    def getHeight(self, root):
 #
 ###        if root is None:
@@ -18,11 +18,11 @@ class Solution(object):
 ###    return max(left, right) + 1
 #
 #
-def isBalanced(self, root):
-    """
-    :type root: TreeNode
-    :rtype: bool
-    """
+# def isBalanced(self, root):
+#     """
+#     :type root: TreeNode
+#     :rtype: bool
+#     """
 ###    if root is None:
 ###        return True
 ###    leftHeight = getHeight(root.left)
@@ -32,29 +32,51 @@ def isBalanced(self, root):
 ###        return True
 ###    return False
     
-    if not root:
-        return True
-    if not root.left and not root.right:
-        return True
-    left = self.get_height(root.left)
-    right = self.get_height(root.right)
-    if abs(left-right)<= and isBalanced(root.left) and isinstance((root.right)):
-        return True
-    else:
-        return False
+#     if not root:
+#         return True
+#     if not root.left and not root.right:
+#         return True
+#     left = self.get_height(root.left)
+#     right = self.get_height(root.right)
+#     if abs(left-right)<= and isBalanced(root.left) and isinstance((root.right)):
+#         return True
+#     else:
+#         return False
+#
+#
+# def get_height(self, root):
+#     if not root:
+#         return 0
+#     if not root.left and not root.right:
+#         return 1
+#     if not root.left:
+#         return 1+self.get_height(root.right)
+#     if not root.right:
+#         return 1+self.get_height(root.left)
+#
+#     return 1+max(self.get_height(root.left), self.get_height(root.right))
+    
+from typing import Optional
+import math
+    
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.res = True
+        self.maxDepth(root)
+        return self.res
 
-def get_height(self, root):
-    if not root:
-        return 0
-    if not root.left and not root.right:
-        return 1
-    if not root.left:
-        return 1+self.get_height(root.right)
-    if not root.right:
-        return 1+self.get_height(root.left)
-    
-    return 1+max(self.get_height(root.left), self.get_height(root.right))
-    
-    
-    
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        l = self.maxDepth(root.left)
+        r = self.maxDepth(root.right)
+        if abs(l - r) > 1:
+            self.res = False
+        return 1 + max(l, r)
