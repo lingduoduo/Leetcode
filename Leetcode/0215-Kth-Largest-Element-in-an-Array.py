b90f60321e4eafe4f3f2f42ce2c47989a0602377
Ling Huang <linghypshen@gmail.com>
2022-06-12 00:28:50 -0400

    add code

--- Leetcode/0215-Kth-Largest-Element-in-an-Array.py
+++ Leetcode/0215-Kth-Largest-Element-in-an-Array.py
@@ -34,3 +34,30 @@ class Solution(object):
     def findKthLargest(self, nums, k):
         tmp = sorted(nums)
         return tmp[-k]
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+        def findKthLargestHelper(left, right, numberK):
+            '''
+                left will always be less than or equal right and 
+                numberK will not exceed the value of right-left+1
+            '''                
+            pivot = nums[right]
+            divider = left #all elements before divider are less than pivot
+            for i in range(left, right):
+                if nums[i] < pivot:
+                    nums[divider], nums[i] = nums[i], nums[divider]
+                    divider += 1    
+            nums[divider], nums[right] = nums[right], nums[divider]
+            pivotIndex=divider
+
+            if right - pivotIndex == numberK - 1:
+                return nums[pivotIndex]
+            elif right - pivotIndex < numberK - 1:
+                return findKthLargestHelper(0, pivotIndex-1, numberK - (right - pivotIndex) - 1 )
+            else:
+                return findKthLargestHelper(pivotIndex+1, right, numberK)
+            
+        
+        return findKthLargestHelper(0, len(nums)-1, k)
