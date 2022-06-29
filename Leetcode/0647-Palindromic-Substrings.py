aa6c5b6a62c4250fbbb6ca27d8ea08b9533be412
Ling Huang <linghypshen@gmail.com>
2022-06-29 09:45:37 -0400

    add code

--- Leetcode/0647-Palindromic-Substrings.py
+++ Leetcode/0647-Palindromic-Substrings.py
@@ -2,18 +2,18 @@ class Solution(object):
     def countSubstrings(self, s):
         """
         :type s: str
-        :rtype: int
+
         """
         ####brute force
         result = list()
-        
+
         for i in range(len(s)):
             for j in range(i + 1, len(s) + 1):
                 sub = s[i:j]
                 if sub == sub[::-1]:
                     result.append(sub)
         return len(result)
-        
+
         ####dp
         n = len(s)
         count = 0
@@ -27,6 +27,20 @@ class Solution(object):
             dp[i][i] = 1
             count += 1
         return count
+
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        n = len(s)
+        for mid in range(2 * n - 1):
+            left = mid // 2
+            right = left + mid % 2
+            while left >= 0 and right < n and s[left] == s[right]:
+                res += 1
+                left -= 1
+                right += 1
+        return res
 
 
 if __name__ == '__main__':
