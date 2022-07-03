bf216aadd07613d8fb7b09ab47ae0d8e907421ca
Ling Huang <linghypshen@gmail.com>
2022-07-03 17:27:01 -0400

    add code

--- Leetcode/0271-Encoder-and-Decoder-Strings.py
+++ Leetcode/0271-Encoder-and-Decoder-Strings.py
@@ -1,52 +1,23 @@
-"""
-@param: strs: a list of strings
-@return: encodes a list of strings to a single string.
-"""
-
-
-def encode(self, strs):
-    res = ''
-    for s in strs:
-        encoded = str(len(s)) + '/' + s
-        res += encoded
-    return res
-
-
-"""
-@param: str: A string
-@return: dcodes a single string to a list of strings
-"""
-
-
-def decode(self, str):
-    res, i = [], 0
-    while i < len(str):
-        # For example, 12/abc
-        e = i
-        while e < len(str) and str[e] != '/':
-            e += 1
-        size = int(str[i:e])
-        word = str[e + 1, e + 1 + size]
-        i = e + 1 + size
-        res.append(word)
-    return res
-
-
-from typing import List
-
-
 class Solution:
-    def __init__(self):
-        self._s = ''
 
-    def encoder(self, strList: List[str]) -> str:
-        for s in strList:
-            self._s += s + "|"
-            print(self._s)
+    def encode(self, strs):
+        """Encodes a list of strings to a single string.
 
-    def decoder(self) -> List[str]:
-        if len(self._s) > 0:
-            res = self._s.split("|")
-            return res[:-1]
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return ""
         else:
-            return self._s
+            return "//".join([s.replace("/", "#/#") for s in strs]) + "//"
+
+
+    def decode(self, s):
+        """Decodes a single string to a list of strings.
+
+        :type s: str
+        :rtype: List[str]
+        """
+        if len(s) == 0:
+            return []
+        return [seg.replace("#/#", "/") for seg in s.split("//")][:-1]
