da7bdbefcef0aa4e67598943eed04e61f9226786
Ling Huang <linghypshen@gmail.com>
2022-07-05 08:40:18 -0400

    add code

--- Leetcode/0023-Merge-k-Sorted-Lists.py
+++ Leetcode/0023-Merge-k-Sorted-Lists.py
@@ -1,62 +1,62 @@
-###Definition for singly-linked list.
+# Definition for singly-linked list.
 class ListNode(object):
-   def __init__(self, x):
-       self.val = x
-       self.next = None
-
-
-# class Solution(object):
-#     def mergeKLists(self, lists):
-        ###first method
-        ###vals = []
-        ###for i in range(len(lists)):
-        ###    node = lists[i]
-        ###    while node:
-        ###        vals.append(node.val)
-        ###        node = node.next
-        ###vals = sorted(vals)
-        #
-        ###dummy = ListNode(-1)
-        ###curr = dummy
-        ###while vals:
-        ###    curr.next = ListNode(vals.pop(0))
-        ###    curr = curr.next
-        ###return dummy.next
-
-# class Solution(object):
-#     def mergeKLists(self, lists):        
-#         import heapq
-        
-#         ###heapq.heappush(q, 10)
-#         ###heapq.heappush(q, 1)
-#         #
-#         ###while q:
-#         ###    print(heapq.heappop(q))
-        
-#         head = ListNode(0)
-#         curr = head
-#         heap = []
-#         heapq.heapify(heap)
-#         [heapq.heappush(heap, (l.val, i)) for i, l in enumerate(lists) if l]
-        
-#         while heap:
-#             curVal, curIdx = heapq.heappop(heap)
-#             curHead = lists[curIdx]
-#             curNext = curHead.next
-            
-#             curr.next = curHead
-#             curr.next.next = None
-#             curr = curr.next
-            
-#             if curNext:
-#                 lists[curIdx]=curNext
-#                 heapq.heappush(heap, (curNext.val, curIdx))
-
-#         return head.next
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution(object):
-    def mergeKLists(self, lists):  
+    def mergeKLists(self, lists):
+        vals = []
+        for i in range(len(lists)):
+            node = lists[i]
+            while node:
+                vals.append(node.val)
+                node = node.next
+        vals = sorted(vals)
+
+        dummy = ListNode(-1)
+        curr = dummy
+        while vals:
+            curr.next = ListNode(vals.pop(0))
+            curr = curr.next
+        return dummy.next
+
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        import heapq
+
+        ###heapq.heappush(q, 10)
+        ###heapq.heappush(q, 1)
+        #
+        ###while q:
+        ###    print(heapq.heappop(q))
+
+        head = ListNode(0)
+        curr = head
+        heap = []
+        heapq.heapify(heap)
+        [heapq.heappush(heap, (l.val, i)) for i, l in enumerate(lists) if l]
+
+        while heap:
+            curVal, curIdx = heapq.heappop(heap)
+            curHead = lists[curIdx]
+            curNext = curHead.next
+
+            curr.next = curHead
+            curr.next.next = None
+            curr = curr.next
+
+            if curNext:
+                lists[curIdx] = curNext
+                heapq.heappush(heap, (curNext.val, curIdx))
+
+        return head.next
+
+
+class Solution(object):
+    def mergeKLists(self, lists):
         import heapq
 
         # init heap
@@ -73,7 +73,7 @@ class Solution(object):
         while heap:
             # pop the min element (i.e. least node val)
             value, idx, node = heapq.heappop(heap)
-            
+
             # append the popped value to cursor
             curr.next = ListNode(value)
             # increment cursor and increment node
@@ -92,17 +92,34 @@ class Solution(object):
         return dummy.next
 
 
-        dummy = curr = ListNode(None)
-        q = PriorityQueue()
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-        while not q.empty():
-            _,idx,curr.next = q.get()
-            curr=curr.next
-            if curr.next: q.put((curr.next.val, idx, curr.next))
-        return dummy.next     
 
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        q = []
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(q, (lists[i].val, i))
+                lists[i] = lists[i].next
+
+        dummy = ListNode()
+        cur = dummy
+        while q:
+            val, i = heapq.heappop(q)
+            cur.next = ListNode(val)
+            cur = cur.next
+            if lists[i]:
+                heapq.heappush(q, (lists[i].val, i))
+                lists[i] = lists[i].next
         return dummy.next
+
+
 if __name__ == "__main__":
-    numbers = [[1,4,5],[1,3,4],[2,6]]
+    numbers = [[1, 4, 5], [1, 3, 4], [2, 6]]
     result = Solution().mergeKLists(numbers)
     print(result)
