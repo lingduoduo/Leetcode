23e19c72609ff219e9265c3870c41ddfddefc638
Ling Huang <linghypshen@gmail.com>
2022-07-10 08:52:56 -0400

    add code

--- Leetcode/0048-Rotate-Image.py
+++ Leetcode/0048-Rotate-Image.py
@@ -1,16 +1,17 @@
-# class Solution:
-#     def rotate(self, matrix) -> None:
-#         """
-#         Do not return anything, modify matrix in-place instead.
-#         """
-#         res = []
-        
-#         for col in zip(*matrix):
-#             res.append(col[::-1])
+class Solution:
+    def rotate(self, matrix) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        res = []
 
-#         for i in range(len(matrix)):
-#             for j in range(len(matrix)):
-#                 matrix[i][j] = res[i][j]#
+        for col in zip(*matrix):
+            res.append(col[::-1])
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix)):
+                matrix[i][j] = res[i][j]  #
+
 
 class Solution(object):
     def rotate(self, matrix):
@@ -21,16 +22,19 @@ class Solution(object):
         for i in range(len(matrix)):
             matrix[i] = reversed(matrix[i])
 
-if __name__ == '__main__':
-   input = [
-       [1, 2, 3],
-       [4, 5, 6],
-       [7, 8, 9]
-   ]
-   results = Solution().rotate(input)
-   # print(results)
-   # for col in zip(*input):
-   #     print(col[::-1])
 
-   #  result = Solution().rotate(input)
-   #  print(result)
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        matrix.reverse()
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+if __name__ == '__main__':
+    input = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    results = Solution().rotate(input)
--- Leetcode/0054-Spiral-Matrix.py
+++ Leetcode/0054-Spiral-Matrix.py
@@ -1,44 +1,44 @@
-class Solution(object):
-    def spiralOrder(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
-        """
-
-        res = []
-
-        if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
-           return res
-
-        rowstart = 0
-        rowend = len(matrix) - 1
-        colstart = 0
-        colend = len(matrix[0]) - 1
-
-        while rowstart < rowend and colstart < colend:
-           for j in range(colstart, colend):
-               res.append(matrix[rowstart][j])
-           for i in range(rowstart, rowend):
-               res.append(matrix[i][colend])
-           for j in range(colend, colstart, -1):
-               res.append(matrix[rowend][j])
-           for i in range(rowend, rowstart, -1):
-               res.append(matrix[i][colstart])
-           rowstart += 1
-           rowend -= 1
-           colstart += 1
-           colend -= 1
-           
-        if rowstart == rowend and colstart == colend:
-           res.append(matrix[rowend][colend])
-        elif rowstart == rowend:
-           for j in range(colstart, colend + 1):
-               res.append(matrix[rowend][j])
-        elif colstart == colend:
-           for i in range(rowstart, rowend + 1):
-               res.append(matrix[i][colend])
-        return res
-
+# class Solution(object):
+#     def spiralOrder(self, matrix):
+#         """
+#         :type matrix: List[List[int]]
+#         :rtype: List[int]
+#         """
+#
+#         res = []
+#         if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
+#             return res
+#
+#         rowstart = 0
+#         rowend = len(matrix) - 1
+#         colstart = 0
+#         colend = len(matrix[0]) - 1
+#
+#         while rowstart < rowend and colstart < colend:
+#             for j in range(colstart, colend):
+#                 res.append(matrix[rowstart][j])
+#             for i in range(rowstart, rowend):
+#                 res.append(matrix[i][colend])
+#             for j in range(colend, colstart, -1):
+#                 res.append(matrix[rowend][j])
+#             for i in range(rowend, rowstart, -1):
+#                 res.append(matrix[i][colstart])
+#             rowstart += 1
+#             rowend -= 1
+#             colstart += 1
+#             colend -= 1
+#
+#         if rowstart == rowend and colstart == colend:
+#             res.append(matrix[rowend][colend])
+#         elif rowstart == rowend:
+#             for j in range(colstart, colend + 1):
+#                 res.append(matrix[rowend][j])
+#         elif colstart == colend:
+#             for i in range(rowstart, rowend + 1):
+#                 res.append(matrix[i][colend])
+#         return res
+#
+#
 # class Solution(object):
 #     def spiralOrder(self, matrix):
 #         row = len(matrix)
@@ -47,7 +47,7 @@ class Solution(object):
 #         col = len(matrix[0])
 #         if col ==0:
 #             return []
-
+#
 #         res = matrix[0] ###1,2,3
 #         if row>1:
 #             for i in range(1, row):
@@ -63,6 +63,30 @@ class Solution(object):
 #             M.append(t)
 #         return res + self.spiralOrder(M)
 
+class Solution():
+    def spiralOrder(self, matrix):
+        ret = []
+        r1 = 0
+        r2 = len(matrix) - 1
+        c1 = 0
+        c2 = len(matrix[0]) - 1
+        while r1 <= r2 and c1 <= c2:
+            for i in range(c1, c2 + 1):
+                ret.append(matrix[r1][i])
+            for i in range(r1 + 1, r2 + 1):
+                ret.append(matrix[i][c2])
+            if r1 != r2:
+                for i in reversed(range(c1, c2)):
+                    ret.append(matrix[r2][i])
+            if c1 != c2:
+                for i in reversed(range(r1 + 1, r2)):
+                    ret.append(matrix[i][c1])
+            r1 += 1
+            r2 -= 1
+            c1 += 1
+            c2 -= 1
+        return ret
+
 
 if __name__ == '__main__':
     matrix = [
@@ -73,9 +97,11 @@ if __name__ == '__main__':
     result = Solution().spiralOrder(matrix)
     print(result)
 
-    # matrix = [
-    # [1, 2, 3, 4], 
-    # [5, 6, 7, 8], 
-    # [9, 10, 11, 12]]
-    # result = Solution().spiralOrder(matrix)
-    # print(result)
+    matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]]
+    result = Solution().spiralOrder(matrix)
+    print(result)
+[1,2,3,4,8,12,11,10,9,5,6,7]
+[1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
