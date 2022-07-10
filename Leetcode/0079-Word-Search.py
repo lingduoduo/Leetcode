15f733dc7b58ce95e78ff1e03d5c178b0f90cbc7
Ling Huang <linghypshen@gmail.com>
2022-07-10 09:32:08 -0400

    add code

--- Leetcode/0079-Word-Search.py
+++ Leetcode/0079-Word-Search.py
@@ -5,88 +5,84 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-    ###    for x in range(len(board)):
-    ###        for y in range(len(board[0])):
-    ###            if self.search(board, word, x, y, 0):
-    ###                return True
-        
-    ###    return False
-    
-    ###def search(self, board, word, i, j, d):
-    ###    if d == len(word):
-    ###        return True
-        
-    ###    if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
-    ###        return False
-        
-    ###    if board[i][j] != word[d]:
-    ###        return False
-        
-    ###    board[i][j] = board[i][j].swapcase()
-    ###    result = self.search(board, word, i + 1, j, d + 1) or \
-    ###             self.search(board, word, i - 1, j, d + 1) or \
-    ###             self.search(board, word, i, j + 1, d + 1) or \
-    ###             self.search(board, word, i, j - 1, d + 1)
-    ###    board[i][j] = board[i][j].swapcase()
-        
-    ###    return result
-
-class Solution(object):
-    def exist(self, board, word):
-            for i in range(len(board)):
+        for i in range(len(board)):
             for j in range(len(board[0])):
                 if self.helper(board, i, j, word, 0):
                     return True
+
         return False
 
     def helper(board, i, j, word, wordinx):
         if wordinx == len(word):
             return True
 
-        if i<0 or i>len(board)-1 or j<0 or j>len(board[0])-1:
+        if i < 0 or i > len(board) - 1 or j < 0 or j > len(board[0]) - 1 or word[wordinx] != board[i][j]:
             return False
 
-        if word[wordinx] != board[i][j]:
-            return False
-
-        board[i][j] = "#"    
-        found = self.helper(board, i+1, j, word, wordinx+1) \
-        or self.helper(board, i-1, j, word, wordinx+1) \
-        or self.helper(board, i, j-1, word, wordinx+1) \
-        or self.helper(board, i, j+1, word, wordinx+1) 
+        board[i][j] = "#"
+        found = self.helper(board, i + 1, j, word, wordinx + 1) \
+                or self.helper(board, i - 1, j, word, wordinx + 1) \
+                or self.helper(board, i, j - 1, word, wordinx + 1) \
+                or self.helper(board, i, j + 1, word, wordinx + 1)
         board[i][j] = word[wordinx]
-
         return found
 
 
 from typing import List
+
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
+        def dfs(x, y, p):
+            if p == l:
+                return True
+            for dirx, diry in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                curx, cury = x + dirx, y + diry
+                if 0 <= curx < m and 0 <= cury < n and flag[curx][cury] and board[curx][cury] == word[p]:
+                    flag[curx][cury] = False
+                    if dfs(curx, cury, p + 1):
+                        flag[curx][cury] = True
+                        return True
+                    flag[curx][cury] = True
+            return False
+
+        m, n = len(board), len(board[0])
+        l = len(word)
+        flag = [[True] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    flag[i][j] = False
+                    if dfs(i, j, 1):
+                        return True
+                    flag[i][j] = True
+        return False
+
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
         self.visited = [[False] * len(board[0]) for _ in range(len(board))]
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if self.backtracking(board, word, 0, i, j):
+                if self.backtracking(board, word, i, j, 0):
                     return True
         return False
 
-    def backtracking(self, board, word, idx, r, c):
-        if idx == len(word):
+    def backtracking(self, board, word, row, col, idx):
+        if len(word) == idx:
             return True
 
-        if r < 0 or r >= len(board) or c < 0 or c >= len(board[0]) or board[r][c] != word[idx] or self.visited[r][c]:
+        if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]) or board[row][col] != word[size] or self.visited[row][col]:
             return False
-        
-        self.visited[r][c] = True
-        paths = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        for p in paths:
-            if self.backtracking(board, word, idx+1, r + p[0], c + p[1]):
-                return True
-        self.visited[r][c] = False
 
+        self.visited[row][col] = True
+        paths = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        for path in paths:
+            if self.backtracking(board, word, row + path[0], col + path[1], idx + 1):
+                return True
+        self.visited[row][col] = False
         return False
 
 if __name__ == '__main__':
-    res = Solution().exist(board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word="ABCCED")
+    res = Solution().exist(board=[["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], word="ABCCED")
     print(res)
-
