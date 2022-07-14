d6ebea0247563a6b2ae8854e2ca6ab4ea6af0b05
Ling Huang <linghypshen@gmail.com>
2022-07-14 09:23:01 -0400

    add code

--- Leetcode/0200-Number-of-Islands.py
+++ Leetcode/0200-Number-of-Islands.py
@@ -4,51 +4,57 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        ###First try
-        ###n = len(grid)
-        ###if n == 0: return 0
-        ###m = len(grid[0])
-        ###result = 0
-        
-        ###def dfs(grid, x, y):
-        ###	if x<0 or y<0 or x>=n or y>=m or grid[x][y]=='0':
-        ###		return
-        ###	grid[x][y]='0'
-        ###	dfs(grid, x+1, y)
-        ###	dfs(grid, x-1, y)
-        ###	dfs(grid, x, y+1)
-        ###	dfs(grid, x, y-1)
-        
-        ###for i in range(n):
-        ###	for j in range(m):
-        ###		if grid[i][j] == '1':
-        ### 		dfs(grid, i, j)
-        ### 		result += 1
-        ###return result
-        
-        ###Second Try
-    #     if not grid:
-    #         return 0
-    #     n = len(grid)
-    #     m = len(grid[0])
-    #     res = 0
-    #     for i in range(n):
-    #         for j in range(m):
-    #             if grid[i][j] == '1':
-    #                 res += 1
-    #                 self.dfs(grid, i, j, n, m)
-    #     return res
-    
-    # def dfs(self, grid, i, j, n, m):
-    #     if i < 0 or j < 0 or i >= n or j >= m or grid[i][j] == '0':
-    #         return
-    #     grid[i][j] = '0'
-    #     self.dfs(grid, i + 1, j, n, m)
-    #     self.dfs(grid, i - 1, j, n, m)
-    #     self.dfs(grid, i, j + 1, n, m)
-    #     self.dfs(grid, i, j - 1, n, m)
+        First
+        try
+            n = len(grid)
+        if n == 0: return 0
+        m = len(grid[0])
+        result = 0
 
-    res = 0
+        def dfs(grid, x, y):
+            if x < 0 or y < 0 or x >= n or y >= m or grid[x][y] == '0':
+                return
+            grid[x][y] = '0'
+            dfs(grid, x + 1, y)
+            dfs(grid, x - 1, y)
+            dfs(grid, x, y + 1)
+            dfs(grid, x, y - 1)
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == '1':
+                    dfs(grid, i, j)
+                result += 1
+        return result
+
+
+class Solution(object):
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+        n = len(grid)
+        m = len(grid[0])
+        res = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == '1':
+                    res += 1
+                    self.dfs(grid, i, j, n, m)
+        return res
+
+    def dfs(self, grid, i, j, n, m):
+        if i < 0 or j < 0 or i >= n or j >= m or grid[i][j] == '0':
+            return
+        grid[i][j] = '0'
+        self.dfs(grid, i + 1, j, n, m)
+        self.dfs(grid, i - 1, j, n, m)
+        self.dfs(grid, i, j + 1, n, m)
+        self.dfs(grid, i, j - 1, n, m)
+
+
+class Solution(object):
+    def numIslands(self, grid):
+        res = 0
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if grid[row][col] == "1":
@@ -60,7 +66,7 @@ class Solution(object):
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         grid[row][col] = "0"
         for direction in directions:
-            dx, dy = row+direction[0], col+direction[1]
-            if dx<0 or dx>=len(grid) or dy<0 or dy>=len(grid[0]) or grid[dx][dy]=="0":
+            dx, dy = row + direction[0], col + direction[1]
+            if dx < 0 or dx >= len(grid) or dy < 0 or dy >= len(grid[0]) or grid[dx][dy] == "0":
                 continue
             self.dfs(grid, dx, dy)
--- Leetcode/0417-Pacific-Atlantic-Water-Flow.py
+++ Leetcode/0417-Pacific-Atlantic-Water-Flow.py
@@ -3,14 +3,14 @@ class Solution:
         if not matrix or not matrix[0]:
             return []
         m, n = len(matrix), len(matrix[0])
-        p_visited = [[False]*n for _ in range(m)]
-        q_visited = [[False]*n for _ in range(m)]
+        p_visited = [[False] * n for _ in range(m)]
+        q_visited = [[False] * n for _ in range(m)]
         for i in range(m):
             self.dfs(matrix, p_visited, m, n, i, 0)
-            self.dfs(matrix, q_visited, m, n, i, n-1)
+            self.dfs(matrix, q_visited, m, n, i, n - 1)
         for j in range(n):
             self.dfs(matrix, p_visited, m, n, 0, j)
-            self.dfs(matrix, q_visited, m, n, m-1, j)
+            self.dfs(matrix, q_visited, m, n, m - 1, j)
         res = []
         for i in range(m):
             for j in range(n):
@@ -23,6 +23,48 @@ class Solution:
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         for direction in directions:
             x, y = i + direction[0], j + direction[1]
-            if x<0 or x>=m or y<0 or y>=n or visited[x][y] or matrix[x][y]<matrix[i][j]:
+            if x < 0 or x >= m or y < 0 or y >= n or visited[x][y] or matrix[x][y] < matrix[i][j]:
                 continue
             self.dfs(matrix, visited, m, n, x, y)
+
+
+class Solution:
+    def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
+        if matix is None:
+            return []
+
+        m = len(matrix)
+        n = len(matrix[0])
+        stack = []
+        for i in range(m):
+            stack.append((i, 0))
+        for i in range(n):
+            stack.append((0, i))
+        pac = self.dfs(matrix, stack)
+
+        stack = []
+        for i in range(m):
+            stack.append((i, n - 1))
+        for i in range(n):
+            stack.append((m - 1, i))
+        atl = self.dfs(matrix, stack)
+
+        res = pac.intersection(atl)
+        return [list(x) for x in res]
+
+    def dfs(self, matrix, q):
+        m = len(matrix)
+        n = len(matrix[0])
+        res = set([])
+        directions = {(-1, 0), (1, 0), (0, -1), (0, 1)}
+        while len(q) > 0:
+            curx, cury = q.pop()
+            ret.add((curx, cury))
+            height = matrix[curx][cury]
+            for dirx, diry in directions:
+                tmpx, tmpy = dirx + curx, diry + cury
+                if 0 <= tmpx < m and 0 <= tmpy < n and matrix[tmpx][tmpy] >= height:
+                    if (tmpx, tmpy) not in ret:
+                        q.append((tmpx, tmpy))
+                        ret.add((tmpx, tmpy))
+        return res
