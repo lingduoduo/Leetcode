b5724ad4704b84a1a1ce4a2a44fa8a23767f1ca8
Ling Huang <linghypshen@gmail.com>
2022-07-17 18:49:16 -0400

    add code

--- Leetcode/0300-Longest-Increasing-Subsequence.py
+++ Leetcode/0300-Longest-Increasing-Subsequence.py
@@ -1,16 +1,19 @@
-# class Solution:
-#     def lengthOfLIS(self, nums) -> int:
-#         if not nums:
-#             return 0
+class Solution:
+    def lengthOfLIS(self, nums) -> int:
+        if not nums:
+            return 0
 
-#         dp = [1]*len(nums)
-#         for i in range(1, len(nums)):
-#             for j in range(i):
-#                 if nums[j] < nums[i]:
-#                     dp[i] = max(dp[i], dp[j]+1)
-#         return max(dp)
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
 
 import bisect
+
+
 class Solution:
     def lengthOfLIS(self, nums) -> int:
         if not nums:
@@ -26,12 +29,25 @@ class Solution:
                 res[index] = nums[i]
         return len(res)
 
-if __name__=='__main__':
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        dp = [1] * len(nums)
+        dp[0] = 1
+        for i in range(1, len(nums)):
+            for j in range(0, i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
+
+if __name__ == '__main__':
     # nums = [10,9,2,5,3,7,101,18]
     # result = Solution().lengthOfLIS(nums)
     # print(result)
 
-    nums = [0,1,0,3,2,3]
+    nums = [0, 1, 0, 3, 2, 3]
     result = Solution().lengthOfLIS(nums)
     print(result)
-
