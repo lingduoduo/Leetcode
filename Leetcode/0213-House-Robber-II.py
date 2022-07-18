a4a72331128beeac2ccb03d1355342337a1d6b3d
Ling Huang <linghypshen@gmail.com>
2022-07-18 08:34:27 -0400

    add code

--- Leetcode/0213-House-Robber-II.py
+++ Leetcode/0213-House-Robber-II.py
@@ -7,7 +7,7 @@ class Solution:
         if len(nums) == 2:
             return max(nums[0], nums[1])
         n = len(nums)
-        return max(self.rob_sub(nums[0:n-1]), self.rob_sub(nums[1:n]))
+        return max(self.rob_sub(nums[0:n - 1]), self.rob_sub(nums[1:n]))
 
     def rob_sub(self, nums):
         if not nums:
@@ -15,11 +15,25 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         if len(nums) == 2:
-            retunr max(nums[0], nums[1])
+            retunr
+            max(nums[0], nums[1])
         n = len(nums)
-        dp = [0]*n
+        dp = [0] * n
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
         for i in range(2, n):
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
         return dp[-1]
+
+
+class Solution:
+    def _rob(self, nums: List[int]) -> int:
+        dp0 = dp1 = 0
+        for i in range(len(nums)):
+            dp0, dp1 = dp1, max(dp0 + nums[i], dp1)
+        return dp1
+
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        return max(self._rob(nums[:-1]), self._rob(nums[1:]))
