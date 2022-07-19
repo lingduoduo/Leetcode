7d832ce740353ab6cb0b0820f0265e4f3a515c3f
Ling Huang <linghypshen@gmail.com>
2022-07-19 08:58:24 -0400

    add code

--- Leetcode/0062-Unique-Paths.py
+++ Leetcode/0062-Unique-Paths.py
@@ -6,7 +6,7 @@ class Solution(object):
         :rtype: int
         """
         d = [[0] * (n + 1) for _ in range(m + 1)]
-        
+
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if i == 1 and j == 1:
@@ -14,6 +14,15 @@ class Solution(object):
                 else:
                     d[i][j] = d[i - 1][j] + d[i][j - 1]
         return d[m][n]
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j - 1]
+        return dp[-1]
 
 
 if __name__ == "__main__":
