2a4e3ece8d757657afaa1ec0a9fb3279efca2ed0
Ling Huang <linghypshen@gmail.com>
2022-07-23 12:52:57 -0400

    add code

--- Leetcode/0078-Subsets.py
+++ Leetcode/0078-Subsets.py
@@ -1,52 +1,72 @@
-# class Solution(object):
-#     def subsets(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: List[List[int]]
-#         """
-#         results = []
-#         self.dfs(nums, 0, results, [])
-#         return results
-    
-#     def dfs(self, nums, index, results, path):
-#         results.append(path)
-#         for i in range(index, len(nums)):
-#             self.dfs(nums, i + 1, results, path + [nums[i]])
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        results = []
+        self.dfs(nums, 0, results, [])
+        return results
 
-# class Solution:
-#     def subsets(self, nums: List[int]) -> List[List[int]]:
-#         self.res = []
-#         self.dfs(nums, 0, [])
-#         return self.res
-    
-#     def dfs(self, nums, index, path):
-#         self.res.append(path)
-#         for i in range(index, len(nums)):
-#             self.dfs(nums, i+1, path+[nums[i]])
+    def dfs(self, nums, index, results, path):
+        results.append(path)
+        for i in range(index, len(nums)):
+            self.dfs(nums, i + 1, results, path + [nums[i]])
 
-# class Solution:
-#     def subsets(self, nums):
-#         self.res = []
-#         for i in range(1+len(nums)):
-#             self.dfs(nums, i, 0, [])
-#         return self.res
 
-#     def dfs(self, nums, n, start, curr):
-#         if  n==len(curr):
-#             self.res.append(curr.copy())
-#             return
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        self.dfs(nums, 0, [])
+        return self.res
 
-#         for i in range(start, len(nums)):
-#             curr.append(nums[i])
-#             self.dfs(nums, n, i+1, curr)
-#             curr.pop()
+    def dfs(self, nums, index, path):
+        self.res.append(path)
+        for i in range(index, len(nums)):
+            self.dfs(nums, i + 1, path + [nums[i]])
+
+
+class Solution:
+    def subsets(self, nums):
+        self.res = []
+        for i in range(1 + len(nums)):
+            self.dfs(nums, i, 0, [])
+        return self.res
+
+    def dfs(self, nums, n, idx, path):
+        if n == len(path):
+            self.res.append(path.copy())
+            return
+
+        for i in range(idx, len(nums)):
+            path.append(nums[i])
+            self.dfs(nums, n, i + 1, path)
+            path.pop()
+
 
 class Solution(object):
     def subsets(self, nums):
-         n = len(nums)
-         return [[nums[i] for i in range(n) if s & 1 << i > 0] for s in range(1 << n)]
-         # for s in range(1 << n):
-         #    print(bin(s))
+        n = len(nums)
+        return [[nums[i] for i in range(n) if s & 1 << i > 0] for s in range(1 << n)]
+        # for s in range(1 << n):
+        #    print(bin(s))
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        for i in range(1 + len(nums)):
+            self.dfs(nums, i, 0, [])
+        return self.res
+
+    def dfs(self, nums, n, idx, path):
+        if n == len(path):
+            self.res.append(path)
+            return
+
+        for i in range(idx, len(nums)):
+            self.dfs(nums, n, i + 1, path + [nums[i]])
+
 
 if __name__ == "__main__":
     numbers = [1, 2, 3, 4, 5]
--- Leetcode/0090-Subsets-II.py
+++ Leetcode/0090-Subsets-II.py
@@ -1,15 +1,18 @@
-# class Solution:
-#     def subsetsWithDup(self, nums):
-#         res = []
-#         nums.sort()
-#         self.dfs(nums, 0, res, [])
-#         return res
-        
-#     def dfs(self, nums, index, res, path):
-#         if path not in res:
-#             res.append(path)
-#         for i in range(index, len(nums)):
-#             self.dfs(nums, i + 1, res, path + [nums[i]])
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums):
+        res = []
+        nums.sort()
+        self.dfs(nums, 0, res, [])
+        return res
+
+    def dfs(self, nums, index, res, path):
+        if path not in res:
+            res.append(path)
+        for i in range(index, len(nums)):
+            self.dfs(nums, i + 1, res, path + [nums[i]])
 
 
 class Solution:
@@ -19,7 +22,7 @@ class Solution:
         for i in range(len(nums)):
             self.dfs(nums, i, [])
         return self.res
-        
+
     def dfs(self, nums, index, path):
         if path not in self.res:
             self.res.append(path)
@@ -27,7 +30,21 @@ class Solution:
             self.dfs(nums, i + 1, path + [nums[i]])
 
 
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = [[], [nums[0]]]
+        cur = [[nums[0]]]
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                cur = [t + [nums[i]] for t in cur]
+            else:
+                cur = [t + [nums[i]] for t in res]
+            res += cur
+        return res
+
+
 if __name__ == '__main__':
-    Input = [1,2,2]
+    Input = [1, 2, 2]
     result = Solution().subsetsWithDup(Input)
     print(result)
