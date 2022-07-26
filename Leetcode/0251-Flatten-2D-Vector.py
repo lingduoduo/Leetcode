b7036b2e13cd2ee08b0fe8a0af98ff538c9af7fe
Ling Huang <linghypshen@gmail.com>
2022-07-26 10:00:55 -0400

    add code

--- Leetcode/0251-Flatten-2D-Vector.py
+++ Leetcode/0251-Flatten-2D-Vector.py
@@ -1,15 +1,15 @@
-###Implement an iterator to flatten a 2d vector.
+# Implement an iterator to flatten a 2d vector.
 
-###For example,
-###Given 2d vector =
+# For example,
+# Given 2d vector =
 
-###[
-###  [1,2],
-###  [3],
-###  [4,5,6]
-###]
-###By calling next repeatedly until hasNext returns false, the order of
-###elements returned by next should be: [1,2,3,4,5,6].
+# [
+#  [1,2],
+#  [3],
+#  [4,5,6]
+# ]
+# By calling next repeatedly until hasNext returns false, the order of
+# elements returned by next should be: [1,2,3,4,5,6].
 
 
 class Vector2D(object):
