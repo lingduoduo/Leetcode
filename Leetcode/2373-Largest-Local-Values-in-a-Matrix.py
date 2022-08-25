afbfceddcb543056e9eb648e0b0d37b1a61859cc
Ling Huang <linghypshen@gmail.com>
2022-08-25 09:16:22 -0400

    add code

---
+++ Leetcode/2373-Largest-Local-Values-in-a-Matrix.py
@@ -0,0 +1,19 @@
+from typing import List
+class Solution:
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+        res = [[0] * (n-2) for _ in range(n-2)]
+        for i in range(n-2):
+            for j in range(n-2):
+                dirs = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
+                tmp = [grid[i+dx][j+dy] for dx, dy in dirs]
+                res[i][j] = max(tmp)
+        return res
+
+if __name__ == "__main__":
+    res = Solution().largestLocal(grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]])
+    print(res)
+
+    res = Solution().largestLocal(grid = [[1,1,1,1,1],[1,1,1,1,1],[1,1,2,1,1],[1,1,1,1,1],[1,1,1,1,1]])
+    print(res)
+
