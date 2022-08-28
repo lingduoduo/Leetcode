75d6d8fd046565731b56a6148e27c5a42b9d771f
Ling Huang <linghypshen@gmail.com>
2022-08-28 00:27:22 -0400

    add code

--- Leetcode/0744-Find-Smallest-Letter-Greater-Than-Target.py
+++ Leetcode/0744-Find-Smallest-Letter-Greater-Than-Target.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution(object):
     def nextGreatestLetter(self, letters, target):
         """
@@ -9,3 +11,21 @@ class Solution(object):
             if letters[i] > target and i < len(letters) - 1:
                 return letters[i]
         return letters[0]
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        left, right = 0, len(letters)
+        while left < right:
+            mid = left + (right - left) // 2
+            if letters[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid
+        return letters[left % len(letters)]
+
+if __name__ == "__main__":
+    res = Solution().nextGreatestLetter(letters = ["c","f","j"], target = "a")
+    print(res)
+
+    res = Solution().nextGreatestLetter(letters = ["c","f","j"], target = "c")
+    print(res)
