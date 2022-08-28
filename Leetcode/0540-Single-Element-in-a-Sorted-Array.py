17d70cdeb329a641f8486ed8ce4a7bbcbe4a24fe
Ling Huang <linghypshen@gmail.com>
2022-08-28 09:11:05 -0400

    add code

--- Leetcode/0540-Single-Element-in-a-Sorted-Array.py
+++ Leetcode/0540-Single-Element-in-a-Sorted-Array.py
@@ -1,4 +1,35 @@
+from typing import List
+
+
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         s = set(nums)
-        return 2*sum(s) - sum(nums)
+        return 2 * sum(s) - sum(nums)
+
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        res = nums[0]
+        for num in nums[1:]:
+            res ^= num
+        return res
+
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l = 0
+        h = len(nums) - 1
+        while l < h:
+            m = l + (h - l) // 2
+            if m % 2 == 1:
+                m -= 1
+            if nums[m] == nums[m + 1]:
+                l = m + 2
+            else:
+                h = m
+        return nums[l]
+
+
+if __name__ == "__main__":
+    res = Solution().singleNonDuplicate(nums=[1, 1, 2, 3, 3, 4, 4, 8, 8])
+    print(res)
