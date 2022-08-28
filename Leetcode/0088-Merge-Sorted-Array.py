1c06611bd9f004f3213db52cc5fab1547a4f3bab
Ling Huang <linghypshen@gmail.com>
2022-08-27 22:05:57 -0400

    add code

--- Leetcode/0088-Merge-Sorted-Array.py
+++ Leetcode/0088-Merge-Sorted-Array.py
@@ -7,22 +7,6 @@ class Solution(object):
         :type n: int
         :rtype: void Do not return anything, modify nums1 in-place instead.
         """
-        
-        ###curr1, curr2 = m - 1, n - 1
-        ###k = m + n - 1
-        ###while curr1 >= 0 and curr2 >= 0:
-        ###    if nums1[curr1] > nums2[curr2]:
-        ###        nums1[k] = nums1[curr1]
-        ###        curr1 -= 1
-        ###    else:
-        ###        nums1[k] = nums2[curr2]
-        ###        curr2 -= 1
-        ###    k -= 1
-        ###while curr2 >= 0:
-        ###    nums1[k] = nums2[curr2]
-        ###    curr2 -= 1
-        ###    k -= 1
-        
         while m>0 and n>0:
             if nums1[m-1] < nums2[n-1]:
                 nums1[n+m-1] = nums2[n-1]
@@ -34,4 +18,26 @@ class Solution(object):
             nums1[:n] = nums2[:n]
         return nums1
 
+from typing import List
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m - 1
+        p2 = n - 1
+        for i in reversed(range(m + n)):
+            if p1 >= 0 and p2 >= 0:
+                if nums1[p1] < nums2[p2]:
+                    nums1[i] = nums2[p2]
+                    p2 -= 1
+                else:
+                    nums1[i] = nums1[p1]
+                    p1 -= 1
+            elif p2 >= 0:
+                nums1[i] = nums2[p2]
+                p2 -= 1
 
+if __name__ == "__main__":
+    res = Solution().merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
+    print(res)
