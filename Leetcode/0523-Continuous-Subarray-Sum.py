b48a620a90d4dedc86a9d612bddf5cbf7fe4e28e
Ling Huang <linghypshen@gmail.com>
2022-10-02 21:51:46 -0400

    add code

--- Leetcode/0523-Continuous-Subarray-Sum.py
+++ Leetcode/0523-Continuous-Subarray-Sum.py
@@ -1,35 +1,39 @@
 import collections
-# class Solution:
-#     def checkSubarraySum(self, nums, k) -> bool:
-#         dp = collections.defaultdict()
-#         dp[0] = -1
-#         tot = 0
-#         m = -1
-#         for i, num in enumerate(nums):
-#             print(dp)
-#             tot += num
-#             if k==0: 
-#                 m = tot
-#             else: 
-#                 m = tot % k
-#             if dp.get(m)==None: 
-#                 dp[m] = i
-#             elif i - dp[m] > 1: 
-#                 return True
-#         return False
 
-# class Solution:
-#     def checkSubarraySum(self, nums, k: int) -> bool:
-#         d = collections.defaultdict(int)
-#         tot = 0
-#         for idx, num in enumerate(nums):
-#             print(d)
-#             tot += num
-#             m = tot if k == 0 else tot%k
-#             if m in d and idx - d[m] >= 1:
-#                 return True
-#             d[m] = idx
-#         return False
+
+class Solution:
+    def checkSubarraySum(self, nums, k) -> bool:
+        dp = collections.defaultdict()
+        dp[0] = -1
+        tot = 0
+        m = -1
+        for i, num in enumerate(nums):
+            print(dp)
+            tot += num
+            if k == 0:
+                m = tot
+            else:
+                m = tot % k
+            if dp.get(m) == None:
+                dp[m] = i
+            elif i - dp[m] > 1:
+                return True
+        return False
+
+
+class Solution:
+    def checkSubarraySum(self, nums, k: int) -> bool:
+        d = collections.defaultdict(int)
+        tot = 0
+        for idx, num in enumerate(nums):
+            print(d)
+            tot += num
+            m = tot if k == 0 else tot % k
+            if m in d and idx - d[m] >= 1:
+                return True
+            d[m] = idx
+        return False
+
 
 class Solution:
     def checkSubarraySum(self, nums, k) -> bool:
@@ -40,32 +44,48 @@ class Solution:
             print(d)
             tot += num
             m = tot if k == 0 else tot % k
-            if not m in d: 
+            if not m in d:
                 d[m] = i
-            elif i - d[m] > 1: 
+            elif i - d[m] > 1:
                 return True
         return False
 
+
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        hash_map = {0: 0}
+        s = 0
+        for i in range(len(nums)):
+            s += nums[i]
+            # if the remainder s % k occurs for the first time
+            if s % k not in hash_map:
+                hash_map[s % k] = i + 1
+            # if the subarray size is at least two
+            elif hash_map[s % k] < i:
+                return True
+        return False
+
+
 if __name__ == '__main__':
-    # nums = [23, 2, 4, 6, 7]
-    # k=6
-    # results = Solution().checkSubarraySum(nums, k)
-    # print(results)
+    nums = [23, 2, 4, 6, 7]
+    k=6
+    results = Solution().checkSubarraySum(nums, k)
+    print(results)
 
-    # nums = [23,2,6,4,7]
-    # k = 0
-    # results = Solution().checkSubarraySum(nums, k)
-    # print(results)
+    nums = [23,2,6,4,7]
+    k = 0
+    results = Solution().checkSubarraySum(nums, k)
+    print(results)
 
-    # nums = [0]
-    # k = 0
-    # results = Solution().checkSubarraySum(nums, k)
-    # print(results)
+    nums = [0]
+    k = 0
+    results = Solution().checkSubarraySum(nums, k)
+    print(results)
 
-    # nums = [0, 1, 0]
-    # k = 0
-    # results = Solution().checkSubarraySum(nums, k)
-    # print(results)
+    nums = [0, 1, 0]
+    k = 0
+    results = Solution().checkSubarraySum(nums, k)
+    print(results)
 
     nums = [2, 2]
     k = 2
--- Leetcode/0987-Vertical-Order-Traversal-of-a-Binary-Tree.py
+++ Leetcode/0987-Vertical-Order-Traversal-of-a-Binary-Tree.py
@@ -12,19 +12,45 @@ class Solution:
 
         res = []
         for k in sorted(self.d.keys()):
-            level = [val for y, val in sorted(self.d[k], key = lambda x:(-x[0], x[1]))]
+            level = [val for y, val in sorted(self.d[k], key=lambda x: (-x[0], x[1]))]
             res.append(level)
         return res
 
     def dfs(self, root, x, y):
         if not root:
             return
-        
+
         self.d[x].append((y, root.val))
-            
+
         if root.left:
-            self.dfs(root.left, x-1, y-1)
+            self.dfs(root.left, x - 1, y - 1)
         if root.right:
-            self.dfs(root.right, x+1, y-1)
+            self.dfs(root.right, x + 1, y - 1)
 
 
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        node_list = []
+
+        def BFS(root):
+            queue = deque([(root, 0, 0)])
+            while queue:
+                node, row, column = queue.popleft()
+                if node is not None:
+                    node_list.append((column, row, node.val))
+                    queue.append((node.left, row + 1, column - 1))
+                    queue.append((node.right, row + 1, column + 1))
+
+        # step 1). construct the global node list, with the coordinates
+        BFS(root)
+
+        # step 2). sort the global node list, according to the coordinates
+        node_list.sort()
+
+        # step 3). retrieve the sorted results partitioned by the column index
+        ret = OrderedDict()
+        for column, row, value in node_list:
+            if column in ret:
+                ret[column].append(value)
+            else:
+                ret[column] = [value]
