fe84052054506ddf6c24a4eef3cc4141610f44db
Ling Huang <linghypshen@gmail.com>
2022-10-03 08:48:30 -0400

    add code

--- Leetcode/0215-Kth-Largest-Element-in-an-Array.py
+++ Leetcode/0215-Kth-Largest-Element-in-an-Array.py
@@ -65,10 +65,4 @@ class Solution:
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums = [-num for num in nums]
-        heapq.heapify(nums)
-        i = 0
-        while i < k:
-            res = heapq.heappop(nums)
-            i += 1
-        return -res
+        return heapq.nlargest(k, nums)[-1]
---
+++ Leetcode/0426-Convert-Binary-Search-Tree-to-Sorted-Doubly-Linked-List.py
@@ -0,0 +1,45 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+
+class Solution:
+    def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        def helper(node):
+            """
+            Performs standard inorder traversal:
+            left -> node -> right
+            and links all nodes into DLL
+            """
+            nonlocal last, first
+            if node:
+                # left
+                helper(node.left)
+                # node
+                if last:
+                    # link the previous node (last)
+                    # with the current one (node)
+                    last.right = node
+                    node.left = last
+                else:
+                    # keep the smallest node
+                    # to close DLL later on
+                    first = node
+                last = node
+                # right
+                helper(node.right)
+
+        if not root:
+            return None
+
+        # the smallest (first) and the largest (last) nodes
+        first, last = None, None
+        helper(root)
+        # close DLL
+        last.right = first
+        first.left = last
+        return first
