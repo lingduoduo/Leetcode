4227d8c0fd00ff559dfa6edf692af6fadbdcc997
Ling Huang <linghypshen@gmail.com>
2022-10-07 09:22:28 -0400

    add code

--- Leetcode/0543-Diameter-of-Binary-Tree.py
+++ Leetcode/0543-Diameter-of-Binary-Tree.py
@@ -1,40 +1,3 @@
-###Definition for a binary tree node.
-###class TreeNode(object):
-###    def __init__(self, x):
-###        self.val = x
-###        self.left = None
-###        self.right = None
-
-# class Solution(object):
-#     def diameterOfBinaryTree(self, root):
-#         """
-#         :type root: TreeNode
-#         :rtype: int
-#         """
-#         if root is None: return 0
-#         result = 0
-#
-#         def depth(root):
-#             if root is None:
-#                 return 0
-#             if root.left is None and root.right is None:
-#                 return 1
-#             l = depth(root.left)
-#             r = depth(root.right)
-#             result = max(result, l + r)
-#             return max(l, r) + 1
-#
-#         depth(root)
-#         return result
-
-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 from typing import Optional
 
 # Definition for a binary tree node.
@@ -61,20 +24,25 @@ class Solution:
 
         return 1 + max(l, r)
 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        if not root: return 0
-        self.res = 0
-        self.dfs(root)
-        return self.res
-    
-    def dfs(self, root):
-        if not root:
-            return 0
-        
-        left = self.dfs(root.left)
-        right = self.dfs(root.right)
-        
-        self.res = max(self.res,  left + right)
-        return max(left+1, right+1)
-    
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = 0
+
+        def depth(root):
+            if not root:
+                return 0
+            nonlocal res
+            l = depth(root.left)
+            r = depth(root.right)
+            res = max(res, l + r)
+            return 1 + max(l, r)
+
+        depth(root)
+        return res
