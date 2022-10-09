31b29705ee5e0434bbe0434bd9d144f14402df4d
Ling Huang <linghypshen@gmail.com>
2022-10-08 22:54:37 -0400

    add code

--- Leetcode/0088-Merge-Sorted-Array.py
+++ Leetcode/0088-Merge-Sorted-Array.py
@@ -38,6 +38,28 @@ class Solution:
                 nums1[i] = nums2[p2]
                 p2 -= 1
 
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Set p1 and p2 to point to the end of their respective arrays.
+        p1 = m - 1
+        p2 = n - 1
+
+        # And move p backwards through the array, each time writing
+        # the smallest value pointed at by p1 or p2.
+        for p in range(n + m - 1, -1, -1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+
 if __name__ == "__main__":
     res = Solution().merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
     print(res)
