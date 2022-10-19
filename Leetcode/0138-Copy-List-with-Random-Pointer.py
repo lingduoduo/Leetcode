3bd424d6a51d9abaa06bbffbd7700d2f2d2f8334
Ling Huang <linghypshen@gmail.com>
2022-10-19 09:43:31 -0400

    add code

--- Leetcode/0138-Copy-List-with-Random-Pointer.py
+++ Leetcode/0138-Copy-List-with-Random-Pointer.py
@@ -15,7 +15,7 @@ class Solution(object):
         nodedict = dict()
         dummy = Node(0, None, None)
         nodedict[head] = dummy
-        
+
         newCurr, curr = dummy, head
         while curr:
             node = Node(curr.val, curr.next, None)
@@ -30,31 +30,74 @@ class Solution(object):
             curr = curr.next
         return dummy.next
 
-   def copyRandomList(self, head):
-        """
-        :type head: RandomListNode
-        :rtype: RandomListNode
-        """
-        if not head:
+
+def copyRandomList(self, head):
+    """
+    :type head: RandomListNode
+    :rtype: RandomListNode
+    """
+    if not head:
+        return None
+
+    # 使用一个字典类型进行拷贝
+    res = dict()
+
+    # 数值拷贝
+    node = head
+    while node:
+        res[node] = RandomListNode(node.label)
+        node = node.next
+
+    # 设置尾结点
+    res[None] = None
+
+    # 进行两个指针的拷贝
+    node = head
+    while node:
+        res[node].next = res[node.next]
+        res[node].random = res[node.random]
+        node = node.next
+
+    return res[head]
+
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if head is None:
             return None
-        
-        # 使用一个字典类型进行拷贝
-        res = dict()
-        
-        # 数值拷贝
-        node = head
-        while node:
-            res[node] = RandomListNode(node.label)
-            node = node.next
-        
-        # 设置尾结点
-        res[None] = None
-        
-        # 进行两个指针的拷贝
-        node = head
-        while node:
-            res[node].next = res[node.next]
-            res[node].random = res[node.random]
-            node = node.next
-        
-        return res[head]
+
+        p = head
+        while p:
+            clone = Node(p.val)
+            clone.next = p.next
+            p.next = clone
+            p = clone.next
+
+        p1 = head
+        while p1:
+            p2 = p1.next
+            if p1.random:
+                p2.random = p1.random.next
+            p1 = p2.next
+
+        p1 = head
+        newhead = p1.next
+        while p1.next:
+            p2 = p1.next
+            p1.next = p2.next
+            p1 = p2
+        return newhead
+
+
+class Solution:
+    def __init__(self):
+        self.visit = {None: None}
+
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if head in self.visit:
+            return self.visit[head]
+        node = Node(head.val, None, None)
+        self.visit[head] = node
+        node.next = self.copyRandomList(head.next)
+        node.random = self.copyRandomList(head.random)
+        return node
