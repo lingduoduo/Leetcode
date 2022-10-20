a5baef80e1302b24f7600b94870bf15f03bfad6a
Ling Huang <linghypshen@gmail.com>
2022-10-20 08:39:12 -0400

    add code

--- Leetcode/1004-Max-Consecutive-Ones-III.py
+++ Leetcode/1004-Max-Consecutive-Ones-III.py
@@ -15,23 +15,7 @@ class Solution:
                     zero -= 1
                 left += 1
         return res
-        
-class Solution:
-    def longestOnes(self, A: List[int], K: int) -> int:
-        A.append(2)
-        left,res,right=0,0,1
-        c=A[left:right].count(0)
-        while right<len(A):
-            if c<=K:
-                res=max(res, right-left)
-                if A[right]==0:
-                    c+=1
-                right+=1
-            else:
-                if A[left]==0:
-                    c-=1
-                left+=1
-        return res
+
 
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
