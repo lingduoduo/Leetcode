1898a1bd320d45e74f049623c00c179512ee03d0
Ling Huang <linghypshen@gmail.com>
2022-11-04 09:26:34 -0400

    add code

--- Leetcode/0252-Meeting-Rooms.py
+++ Leetcode/0252-Meeting-Rooms.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort(key=lambda x: x[0])
@@ -10,9 +11,22 @@ class Solution:
         return True
 
 
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if len(intervals) <= 1:
+            return True
+        intervals = sorted(intervals, key=lambda x: x[0])
+        prev_start, prev_end = intervals[0]
+        for start, end in intervals[1:]:
+            if prev_end > start:
+                return False
+            prev_start, prev_end = start, end
+        return True
+
+
 if __name__ == "__main__":
-    res = Solution().canAttendMeetings(intervals=[[0,30],[5,10],[15,20]])
+    res = Solution().canAttendMeetings(intervals=[[0, 30], [5, 10], [15, 20]])
     print(res)
 
-    res = Solution().canAttendMeetings(intervals=[[7,10],[2,4]])
+    res = Solution().canAttendMeetings(intervals=[[7, 10], [2, 4]])
     print(res)
