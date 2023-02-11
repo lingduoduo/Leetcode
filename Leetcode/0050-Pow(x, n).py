b6f72fe0133ba651875941760f3170f70b0eddcf
Ling Huang <linghypshen@gmail.com>
2023-02-11 14:48:45 -0500

    add code

--- Leetcode/0050-Pow(x, n).py
+++ Leetcode/0050-Pow(x, n).py
@@ -6,15 +6,16 @@ class Solution(object):
         :rtype: float
         """
         ####二分求幂
-        if n==0:
-        	return 1
+        if n == 0:
+            return 1
 
-        if n<0:
-        	x = 1/x
-        	n = -n
-        if n%2:
-        	return x*self.myPow(x, n-1)
-        retrun self.myPow(x*x, n/2)
+        if n < 0:
+            x = 1 / x
+            n = -n
+        if n % 2:
+            return x * self.myPow(x, n - 1)
+        return self.myPow(x * x, n / 2)
+
 
 class Solution(object):
     def myPow(self, x, n):
