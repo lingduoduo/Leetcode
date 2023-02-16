0c07faf4e6cd3e0a287360c4e79c6c43f2dcc94b
Ling Huang <linghypshen@gmail.com>
2023-02-16 14:17:46 -0500

    add code

--- Leetcode/0678-Valid-Parenthesis-String.py
+++ Leetcode/0678-Valid-Parenthesis-String.py
@@ -2,24 +2,25 @@ class Solution:
     def checkValidString(self, s: str) -> bool:
         min_op = 0
         max_op = 0
-        
+
         for c in s:
             if c == '(':
                 min_op += 1
             else:
                 min_op -= 1
-            
+
             if c == '(' or c == '*':
                 max_op += 1
             else:
                 max_op -= 1
-            
+
             if max_op < 0:
                 return False
-            
+
             min_op = max(0, min_op)
-        
+
         return min_op == 0
+
 
 class Solution:
     def checkValidString(self, s: str) -> bool:
@@ -41,3 +42,39 @@ class Solution:
                         new_set.add(t - 1)
             old_set = new_set
         return 0 in old_set
+
+
+class Solution(object):
+    def checkValidString(self, s):
+        lo = hi = 0
+        for c in s:
+            lo += 1 if c == '(' else -1
+            hi += 1 if c != ')' else -1
+            if hi < 0: break
+            lo = max(lo, 0)
+
+        return lo == 0
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        s1 = []
+        s2 = []
+        for i, c in enumerate(s):
+            if c == '(':
+                s1.append(i)
+            elif c == '*':
+                s2.append(i)
+            else:
+                if s1:
+                    s1.pop()
+                elif s2:
+                    s2.pop()
+                else:
+                    return False
+
+        while s1 and s2:
+            if s1[-1] > s2[-1]: # ( is closer to the end than *
+                return False
+            s1.pop()
+            s2.pop()
+        return s1 == []
