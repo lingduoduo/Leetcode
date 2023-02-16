c785697cc298c35dc1edcb71a82e1158a9273156
Ling Huang <linghypshen@gmail.com>
2023-02-16 12:48:44 -0500

    add code

--- Leetcode/0018-4Sum.py
+++ Leetcode/0018-4Sum.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution(object):
     def fourSum(self, nums, target):
         """
@@ -5,30 +6,6 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        ###first try
-        ###nums = sorted(nums)
-        ###d = dict()
-        ###for i in range((len(nums)) - 1):
-        ###    for j in range(i + 1, len(nums)):
-        ###        if nums[i] + nums[j] in d:
-        ###            d[nums[i] + nums[j]].append([i, j])
-        ###        else:
-        ###            d[nums[i] + nums[j]] = [[i, j]]
-        ###res = []
-        ###for i in d:
-        ###    if target - i in d:
-        ###        list1 = d[i]
-        ###        list2 = d[target - i]
-        ###        for k1 in range(len(list1)):
-        ###            for k2 in range(len(list2)):
-        ###                item = [nums[list1[k1][0]], nums[list1[k1][1]],
-        ###                        nums[list2[k2][0]], nums[list2[k2][1]]]
-        ###                item = sorted(item)
-        ###                if (list1[k1][0] not in list2[k2]) and (
-        ###                        list1[k1][1] not in list2[k2]) and item not in res:
-        ###                    res.append(item)
-        ###return res
-        ###second try
         if len(nums) < 4:
             return []
         nums.sort()
@@ -55,7 +32,22 @@ class Solution(object):
                     else:
                         right -= 1
         return res
-        
+
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        n = len(nums)
+        seen = set()
+        ans = set()
+        for i in range(n):
+            for j in range(i+1, n):
+                for k in range(j+1, n):
+                    lastNumber = target - nums[i] - nums[j] - nums[k]
+                    if lastNumber in seen:
+                        arr = sorted([nums[i], nums[j], nums[k], lastNumber])
+                        ans.add((arr[0], arr[1], arr[2], arr[3]))
+            seen.add(nums[i])
+        return ans
 
 if __name__ == "__main__":
     nums = [1, 0, -1, 0, -2, 2]
