4b22a571e47e42d0abded0d20df300cb26f66a33
Ling Huang <linghypshen@gmail.com>
2023-02-18 09:45:47 -0500

    add code

--- Leetcode/0642-Design-Search-Autocomplete-System.py
+++ Leetcode/0642-Design-Search-Autocomplete-System.py
@@ -1,46 +1,65 @@
-class TrieNode:
+class TrieNode():
     def __init__(self):
-        self.children = dict()
-        self.sentences = set()
+        self.isEnd = False
+        self.children = {}
+        self.hot = 0
 
 class AutocompleteSystem(object):
-
     def __init__(self, sentences, times):
-        """
-        :type sentences: List[str]
-        :type times: List[int]
-        """
-        self.buffer = ''
-        self.stimes = collections.defaultdict(int)
-        self.trie = TrieNode()
-        for s, t in zip(sentences, times):
-            self.stimes[s] = t
-            self.addSentence(s)
-        self.tnode = self.trie
+        self.root = TrieNode()
+        self.searchTerm = ""
+        # 1. add historical data
+        for i, sentence in enumerate(sentences):
+            self.add(sentence, times[i])
+    def add(self,sentence, hot):
+        node = self.root
+        #2. for each character in sentence
+        for c in sentence:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        #3. when last character is added,
+        #   make node.isEnd = True indicate that the current node is end of the sentence
+        node.isEnd = True
+        #4. do -= because by negating, we can sort as ascending order later
+        node.hot-= hot
+
+    def search(self):
+        node = self.root
+        res = []
+        path = ""
+
+        for c in self.searchTerm:
+            if c not in node.children:
+                return res
+            # 6. add each character to path variable, path will added to res when we found node.isEnd ==True
+            path +=c
+            node = node.children[c]
+        # 7. at this point, node is at the given searchTerm.
+        # for ex. if search term is "i_a", we are at "a" node.
+        # from this point, we need to search all the possible sentence by using DFS
+        self.dfs(node, path,res)
+        # 11. variable res has result of all the relevant sentences
+        # we just need to do sort and return [1] element of first 3
+        return [item[1] for item in sorted(res)[:3]]
+
+    def dfs(self, node, path, res):
+        # 8. Check if node is end of the sentence
+        # if so, add path to res
+        if node.isEnd:
+            # 9. when add to res, we also want to add hot for sorting
+            res.append((node.hot, path))
+        # 10. keep going if the node has child
+        # until there is no more child (reached to bottom)
+        for c in node.children:
+            self.dfs(node.children[c], path + c, res)
 
     def input(self, c):
-        """
-        :type c: str
-        :rtype: List[str]
-        """
-        ans = []
-        if c != '#':
-            self.buffer += c
-            if self.tnode: self.tnode = self.tnode.children.get(c)
-            if self.tnode: ans = sorted(self.tnode.sentences, key=lambda x: (-self.stimes[x], x))[:3]
-        else:
-            self.stimes[self.buffer] += 1
-            self.addSentence(self.buffer)
-            self.buffer = ''
-            self.tnode = self.trie
-        return ans
+        if c != "#":
+            # 5. if input is not "#" add c to self.searchTerm and do self.search each time
+            self.searchTerm += c
+            return self.search()
 
-    def addSentence(self, sentence):
-        node = self.trie
-        for letter in sentence:
-            child = node.children.get(letter)
-            if child is None:
-                child = TrieNode()
-                node.children[letter] = child
-            node = child
-            child.sentences.add(sentence)
+        else:
+            self.add(self.searchTerm, 1)
+            self.searchTerm = ""
