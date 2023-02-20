a9dc5a4b8b8e718aace9c6cd75290431bd7a6ff9
Ling Huang <linghypshen@gmail.com>
2023-02-20 18:35:44 -0500

    add code

--- Leetcode/0202-Happy-Number.py
+++ Leetcode/0202-Happy-Number.py
@@ -17,6 +17,17 @@ class Solution(object):
             elif result < 10:
                 return False
 
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            res = 0
+            for c in list(str(n)):
+                res += int(c) ** 2
+            n = res
+        return n == 1
+
 
 if __name__ == "__main__":
     n = 1111111
