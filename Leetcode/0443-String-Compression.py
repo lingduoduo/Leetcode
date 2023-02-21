939845b75882d8e16d0dec9848b1c03e2a5911df
Ling Huang <linghypshen@gmail.com>
2023-02-21 13:31:35 -0500

    add code

--- Leetcode/0443-String-Compression.py
+++ Leetcode/0443-String-Compression.py
@@ -1,28 +1,13 @@
 class Solution:
     def compress(self, chars) -> int:
-        ###import collections
-        #
-        ###c = collections.Counter(chars)
-        ###res = []
-        #
-        ###for k, v in c.items():
-        ###    if v == 1:
-        ###        res.append(k)
-        ###    else:
-        ###        res.append(k)
-        ###        for cha in str(v):
-        ###            res.append(cha)
-        #
-        ###return res
-        
         n = len(chars)
         i, count = 0, 1
-        
-        for j in range(1, n-1):
-            if j < n and chars[j-1] == chars[j]:
+
+        for j in range(1, n - 1):
+            if j < n and chars[j - 1] == chars[j]:
                 count += 1
             else:
-                chars[i] = chars[j-1]
+                chars[i] = chars[j - 1]
                 i += 1
                 if count > 1:
                     for cha in str(count):
@@ -30,13 +15,30 @@ class Solution:
                         i += 1
                 count = 1
         return i
-                
-        
-        
-        
+
+from typing import List
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        i = 0
+        res = 0
+        while i < len(chars):
+            group_length = 1
+            while (i + group_length < len(chars)
+                   and chars[i + group_length] == chars[i]):
+                group_length += 1
+            chars[res] = chars[i]
+            res += 1
+            if group_length > 1:
+                str_repr = str(group_length)
+                chars[res:res + len(str_repr)] = list(str_repr)
+                res += len(str_repr)
+            i += group_length
+        return res
+
+
 if __name__ == "__main__":
-    numbers =["a","a","b","b","c","c","c"]
+    numbers = ["a", "a", "b", "b", "c", "c", "c"]
     numbers = ["c"]
-    numbers = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+    numbers = ["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"]
     result = Solution().compress(numbers)
     print(result)
--- Leetcode/1094-Car-Pooling.py
+++ Leetcode/1094-Car-Pooling.py
@@ -10,3 +10,24 @@ class Solution:
             if cur > capacity:
                 return False
         return True
+
+
+from typing import List
+
+
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        timestamp = []
+        for trip in trips:
+            timestamp.append([trip[1], trip[0]])
+            timestamp.append([trip[2], -trip[0]])
+
+        timestamp.sort()
+
+        used_capacity = 0
+        for time, passenger_change in timestamp:
+            used_capacity += passenger_change
+            if used_capacity > capacity:
+                return False
+
+        return True
