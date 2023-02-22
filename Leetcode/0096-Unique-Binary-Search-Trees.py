1f1004aa104e8bab389acf0e5996454a0b9dc60e
Ling Huang <linghypshen@gmail.com>
2023-02-22 16:11:01 -0500

    add code

--- Leetcode/0096-Unique-Binary-Search-Trees.py
+++ Leetcode/0096-Unique-Binary-Search-Trees.py
@@ -19,15 +19,12 @@ class Solution:
 class Solution:
     def numTrees(self, n: int) -> int:
         res = [0] * (n + 1)
-        
         res[0] = res[1] = 1
         
         for i in range(2, n + 1):
-            tmp = 0
             for j in range(0, i):
-                tmp += res[j] * res[i - 1 - j]
-            res[i] = tmp
-            
+                res[i] += res[j] * res[i - j - 1]
+
         return res[n]
 
         
