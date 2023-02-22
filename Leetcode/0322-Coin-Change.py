c5cf92098f3a6dc6e2afd479a0cb73e004acb8a8
Ling Huang <linghypshen@gmail.com>
2023-02-22 18:08:39 -0500

    add code

--- Leetcode/0322-Coin-Change.py
+++ Leetcode/0322-Coin-Change.py
@@ -39,3 +39,24 @@ class Solution:
             for i in range(coin, amount + 1):
                 dp[i] = min(dp[i], dp[i - coin] + 1)
         return dp[amount] if dp[amount] != float('inf') else -1
+
+from functools import lru_cache
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        @lru_cache(None)
+        def dfs(rem):
+            if rem < 0:
+                return -1
+            if rem == 0:
+                return 0
+            min_cost = float('inf')
+            for coin in coins:
+                res = dfs(rem - coin)
+                if res != -1:
+                    min_cost = min(min_cost, res + 1)
+            return min_cost if min_cost != float('inf') else -1
+
+        return dfs(amount)
--- Leetcode/0779-K-th-Symbol-in-Grammar.py
+++ Leetcode/0779-K-th-Symbol-in-Grammar.py
@@ -18,6 +18,16 @@ class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
         return bin(k - 1).count('1') % 2
 
+class Solution:
+   def kthGrammar(self, n: int, k: int) -> int:
+       def f(n, k):
+           if n == 1:
+               return 0
+           return f(n - 1, k // 2) if k % 2 == 0 else 1 - f(n - 1, k // 2)
+       k -= 1
+       return f(n, k)
+
+
 if __name__ == '__main__':
     res = Solution().kthGrammar(N=6, K=5)
     print(res)
