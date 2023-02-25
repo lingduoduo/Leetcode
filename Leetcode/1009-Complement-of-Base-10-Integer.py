6a2b503e425d0b126ee6641abb765848f80d3fec
Ling Huang <linghypshen@gmail.com>
2023-02-24 21:41:03 -0500

    add code

--- Leetcode/1009-Complement-of-Base-10-Integer.py
+++ Leetcode/1009-Complement-of-Base-10-Integer.py
@@ -12,6 +12,25 @@ class Solution:
             output = output*2 + res[i]
         return output
 
+
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        nums = list(bin(n)[2:][::-1])
+        res = 1
+        for i in range(len(nums)):
+            res <<= 1
+        res -= 1
+        res ^= n
+        return res
+
+
+import numpy as np
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        res = (2 << int(np.log2(n))) - 1
+        res ^= n
+        return res
+
 if __name__ == '__main__':
     results = Solution().bitwiseComplement(10)
     print(results)
