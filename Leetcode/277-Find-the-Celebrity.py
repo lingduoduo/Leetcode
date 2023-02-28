754ee5c5a56c6b3c7feaf112557500269a3f091c
Ling Huang <linghypshen@gmail.com>
2023-02-28 14:11:57 -0500

    add code

---
+++ Leetcode/277-Find-the-Celebrity.py
@@ -0,0 +1,17 @@
+# The knows API is already defined for you.
+# return a bool, whether a knows b
+# def knows(a: int, b: int) -> bool:
+
+class Solution:
+    def findCelebrity(self, n: int) -> int:
+        for i in range(n):
+            isCelebrity = True
+            for j in range(n):
+                if i == j:
+                    continue
+                if knows(i, j) or not knows(j, i):
+                    isCelebrity = False
+                    break
+            if isCelebrity:
+                return i
+        return -1
