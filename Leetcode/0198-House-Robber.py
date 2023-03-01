5e776a4b69026f43c2c9b6be12c4a5f74698436a
Ling Huang <linghypshen@gmail.com>
2023-03-01 13:41:20 -0500

    add code

--- Leetcode/0198-House-Robber.py
+++ Leetcode/0198-House-Robber.py
@@ -37,3 +37,46 @@ class Solution:
         for num in nums:
             dp0, dp1 = dp1, max(dp1, dp0 + num)
         return dp1
+
+
+class Solution:
+
+    def __init__(self):
+        self.memo = {}
+
+    def rob(self, nums: List[int]) -> int:
+
+        self.memo = {}
+
+        return self.robFrom(0, nums)
+
+    def robFrom(self, i, nums):
+
+        # No more houses left to examine.
+        if i >= len(nums):
+            return 0
+
+        # Return cached value.
+        if i in self.memo:
+            return self.memo[i]
+
+        # Recursive relation evaluation to get the optimal answer.
+        ans = max(self.robFrom(i + 1, nums), self.robFrom(i + 2, nums) + nums[i])
+
+        # Cache for future use.
+        self.memo[i] = ans
+        return ans
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @lru_cache(maxsize=None)
+        def robFrom(i):
+            # No more houses left to examine.
+            if i >= len(nums):
+                return 0
+
+            # Recursive relation evaluation to get the optimal answer.
+            return max(robFrom(i + 1), robFrom(i + 2) + nums[i])
+
+        return robFrom(0)
