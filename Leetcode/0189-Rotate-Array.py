a47dde061d93e28ba9c3b8a08c707c55a2e4d580
Ling Huang <linghypshen@gmail.com>
2023-03-03 15:35:45 -0500

    add code

--- Leetcode/0189-Rotate-Array.py
+++ Leetcode/0189-Rotate-Array.py
@@ -5,13 +5,23 @@ class Solution(object):
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        ###changed = list()
-        ###while k > len(nums):
-        ###    k -= len(nums)
-        ###changed = nums[-k:] + nums[:-k]
-        ###for i in range(len(nums)):
-        ###    nums[i] = changed[i]
         k = k % len(nums)
-        
         nums[:k], nums[k:] = nums[len(nums)-k:], nums[:len(nums)-k]
+
+class Solution:
+    def reverse(self, nums: list, start: int, end: int) -> None:
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start, end = start + 1, end - 1
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
        
