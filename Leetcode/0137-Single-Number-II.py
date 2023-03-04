00a121051995d3f839917e071f6b84c35d246f99
Ling Huang <linghypshen@gmail.com>
2023-03-03 21:04:41 -0500

    add code

--- Leetcode/0137-Single-Number-II.py
+++ Leetcode/0137-Single-Number-II.py
@@ -10,9 +10,20 @@ class Solution:
             for num in nums:
                 ct += (num >> i) & 1
             res |= (ct % 3) << i
-        return res - 2**32 if res >> 31 & 1 else res
+        return res - 2 ** 32 if res >> 31 & 1 else res
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen_once = seen_twice = 0
+
+        for num in nums:
+            seen_once = ~seen_twice & (seen_once ^ num)
+            seen_twice = ~seen_once & (seen_twice ^ num)
+
+        return seen_once
 
 if __name__ == '__main__':
-	nums = [2,2,3,2]
-	results = Solution().singleNumber(nums)
-	print(results)
+    nums = [2, 2, 3, 2]
+    results = Solution().singleNumber(nums)
+    print(results)
