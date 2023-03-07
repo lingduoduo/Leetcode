a9af44337251e20c773d75ee468c8e6b18cc0d0d
Ling Huang <linghypshen@gmail.com>
2023-03-07 18:40:16 -0500

    add code

--- Leetcode/0038-Count-and-Say.py
+++ Leetcode/0038-Count-and-Say.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(n-1):
             seq = self.getNext(seq)
         return seq
-    
+
     def getNext(self, seq):
         i, next_seq = 0, ""
         while i < len(seq):
@@ -15,9 +15,24 @@ class Solution:
             next_seq += str(count) + seq[i]
             i += 1
         return next_seq
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        current_string = '1'
+        for _ in range(n - 1):
+            next_string = ''
+            j = 0
+            k = 0
+            while j < len(current_string):
+                while (k < len(current_string) and current_string[k] == current_string[j]):
+                    k += 1
+                next_string += str(k - j) + current_string[j]
+                j = k
+            current_string = next_string
             
-        return next_seq
+        return current_string
+
 if __name__ == '__main__':
-    n = 5
+    n =
     res = Solution().countAndSay(n)
     print(res)
