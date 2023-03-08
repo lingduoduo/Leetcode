f450b9466e3411eac7125a29d50b8db21a3241bf
Ling Huang <linghypshen@gmail.com>
2023-03-08 10:37:11 -0500

    add code

--- Leetcode/0463-Island-Perimeter.py
+++ Leetcode/0463-Island-Perimeter.py
@@ -24,3 +24,25 @@ class Solution(object):
                     if j < m - 1 and grid[i][j + 1] == 1:
                         conn += 1
         return area * 4 - conn
+
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        result = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    result += 4
+
+                    if r > 0 and grid[r - 1][c] == 1:
+                        result -= 2
+
+                    if c > 0 and grid[r][c - 1] == 1:
+                        result -= 2
+
+        return result
+    
