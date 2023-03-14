a3cfdaca5405ec9d3a2cb62b57ad9abea2bd932f
Ling Huang <linghypshen@gmail.com>
2023-03-13 21:46:28 -0400

    add code

--- Leetcode/1592-Rearrange-Spaces-Between-Words.py
+++ Leetcode/1592-Rearrange-Spaces-Between-Words.py
@@ -19,7 +19,18 @@ class Solution:
             res += s_spaces[i]
         res += words[-1]
         res += " "*m
-        return res   
+        return res
+
+class Solution:
+    def reorderSpaces(self, text: str) -> str:
+        words = text.split() # split(sep=None) will discard empty strings.
+        cnt = len(words)
+        spaces = text.count(' ')
+        gap = 0 if cnt == 1 else spaces // (cnt - 1)
+      # trailing_spaces = spaces if gap == 0 else spaces % (cnt - 1)
+        trailing_spaces = spaces - gap * (cnt - 1) 
+        return (' ' * gap).join(words) + ' ' * trailing_spaces
+
 
 if __name__ == '__main__':
     # text = " practice   makes   perfect"
