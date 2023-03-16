6d92f3f5c7ea951376bb660f46aad941d920ab39
Ling Huang <linghypshen@gmail.com>
2023-03-15 23:35:45 -0400

    add code

--- Leetcode/0412-Fizz-Buzz.py
+++ Leetcode/0412-Fizz-Buzz.py
@@ -4,23 +4,6 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        ###result = []
-        ###for i in range(1, n + 1):
-        ###    if i % 15 == 0:
-        ###        result.append("FizzBuzz")
-        ###    elif i % 3 == 0:
-        ###        result.append("Fizz")
-        ###    elif i % 5 == 0:
-        ###        result.append("Buzz")
-        ###    else:
-        ###        result.append(str(i))
-        ###return result
-        #
-        ###return [str(i) * (i %
-        ###                  3 != 0 and i %
-        ###                  5 != 0) + "Fizz" * (i %
-        ###                                      3 == 0) + "Buzz" * (i %
-        ###                                                          5 == 0) for i in range(1, n + 1)]
         res = []
         for i in range(1, n+1):
             if i % 15 == 0:
@@ -32,4 +15,8 @@ class Solution(object):
             else:
                 res.append(str(i))
         return res
-    
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        return ["Fizz"*(d%3==0)+"Buzz"*(d%5==0) or f"{d}" for d in range(1,n+1)]
