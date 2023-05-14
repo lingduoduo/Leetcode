bbb42d0a2d13a90a8f03fdb4f2e8a3ea48bf54ab
Ling Huang <linghypshen@gmail.com>
2023-05-14 09:55:39 -0400

    add code

--- Leetcode/0745-Prefix and Suffix Search.py
+++ Leetcode/0745-Prefix and Suffix Search.py
@@ -24,6 +24,57 @@ class WordFilter:
             cur = cur[letter]
         return cur[WEIGHT]
 
+
+Trie = lambda: collections.defaultdict(Trie)
+WEIGHT = False
+
+
+class WordFilter:
+
+    def __init__(self, words):
+        from collections import defaultdict
+        self.prefixes = defaultdict(set)
+        self.suffixes = defaultdict(set)
+        self.weights = {}
+        for index, word in enumerate(words):
+            prefix, suffix = '', ''
+            for char in [''] + list(word):
+                prefix += char
+                self.prefixes[prefix].add(word)
+            for char in [''] + list(word[::-1]):
+                suffix += char
+                self.suffixes[suffix[::-1]].add(word)
+            self.weights[word] = index
+
+    def f(self, prefix, suffix):
+        weight = -1
+        for word in self.prefixes[prefix] & self.suffixes[suffix]:
+            if self.weights[word] > weight:
+                weight = self.weights[word]
+        return weight
+
+
+class WordFilter:
+
+    def __init__(self, words):
+        self.inputs = {}
+        for index, word in enumerate(words):
+            prefix = ''
+            for char in [''] + list(word):
+                prefix += char
+                suffix = ''
+                for char in [''] + list(word[::-1]):
+                    suffix += char
+                    self.inputs[prefix + '.' + suffix[::-1]] = index
+
+    def f(self, prefix, suffix):
+        return self.inputs.get(prefix + '.' + suffix, -1)
+
+
+# Your WordFilter object will be instantiated and called as such:
+# obj = WordFilter(words)
+# param_1 = obj.f(pref,suff)
+
 # Your WordFilter object will be instantiated and called as such:
 # obj = WordFilter(words)
 # param_1 = obj.f(pref,suff)
