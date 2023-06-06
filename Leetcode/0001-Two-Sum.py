dd9cc44d1d752af31a2c81fb3d712e76a5ecb3c9
Ling Huang <linghypshen@gmail.com>
2023-06-06 09:40:12 -0400

    add code

--- Leetcode/0001-Two-Sum.py
+++ Leetcode/0001-Two-Sum.py
@@ -1,42 +1,13 @@
 from typing import List
-
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for k, v in enumerate(nums):
-            i = k + 1
-            while i < len(nums):
-                if v + nums[i] == target:
-                    return ([k, i])
-                else:
-                    i = i + 1
-
-
-class Solution(object):
-    def twoSum(self, nums, target):
-        d = {}
-        for i, v in enumerate(nums):
-            d[v] = i
-
-        for i in range(len(nums)):
-            if (target - nums[i] in d) and d[target - nums[i]] != i:
-                return i, d[target - nums[i]]
-        return None
-
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pos = dict()
-        for i, num in enumerate(nums):
-            if target - num in pos:
-                return [pos[target - num], i]
+        d = dict()
+        d[nums[0]] = 0
+        for i in range(1, len(nums)):
+            if target - nums[i] in d:
+                return [i, d[target - nums[i]]]
             else:
-                pos[num] = i
-        return [0, 0]
+                d[nums[i]] = i
 
 if __name__ == "__main__":
     numbers = [2, 7, 11, 15]
