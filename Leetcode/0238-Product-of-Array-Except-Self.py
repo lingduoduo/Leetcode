0bf24a97b892424ecb07fcc9db63d6de5f1acbf3
Ling Huang <linghypshen@gmail.com>
2023-06-06 11:27:06 -0400

    add code

--- Leetcode/0238-Product-of-Array-Except-Self.py
+++ Leetcode/0238-Product-of-Array-Except-Self.py
@@ -1,44 +1,23 @@
-# class Solution(object):
-#     def productExceptSelf(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: List[int]
-#         """
-#         result = [1] * len(nums)
-#         prod = 1
-#         for i in range(1, len(nums)):
-#             prod *= nums[i - 1]
-#             result[i] *= prod
-        
-#         prod = 1
-#         for i in range(len(nums) - 2, -1, -1):
-#             prod *= nums[i + 1]
-#             result[i] *= prod
-        
-#         return result
-
-
 class Solution(object):
     def productExceptSelf(self, nums):
-        fromLeft = []
-        cum = 1
-        for num in nums:
-            cum *= num
-            fromLeft.append(cum)
-        cum = 1
-        for i in reversed(range(len(nums))):
-            if i == len(nums) - 1:
-                fromLeft[i] = fromLeft[i-1]
-                cum *= nums[i]
-                continue
-            elif i == 0:
-                fromLeft[i] = cum
-            else:
-                fromLeft[i] = fromLeft[i-1]*cum
-                cum *= nums[i]
-        return fromLeft
+        left = [nums[0]]
+        right = [nums[-1]]
+        for i in range(1, len(nums)):
+            left.append(left[-1] * nums[i])
+            right.append(right[-1] * nums[-i - 1])
+        right = right[::-1]
+        res = [right[1]]
+        for i in range(1, len(nums) - 1):
+            res.append(left[i - 1] * right[i + 1])
+        res.append(left[-2])
+        return res
+
 
 if __name__ == '__main__':
-    nums = [1,2,3,4]
+    nums = [1, 2, 3, 4]
+    results = Solution().productExceptSelf(nums)
+    print(results)
+
+    nums = [-1, 1, 0, -3, 3]
     results = Solution().productExceptSelf(nums)
     print(results)
