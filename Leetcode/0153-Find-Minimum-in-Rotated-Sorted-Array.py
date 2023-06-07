0616fe4b1f7c8eeafeed5cdd37a72b5ccee80a79
Ling Huang <linghypshen@gmail.com>
2023-06-06 20:23:31 -0400

    add code

--- Leetcode/0153-Find-Minimum-in-Rotated-Sorted-Array.py
+++ Leetcode/0153-Find-Minimum-in-Rotated-Sorted-Array.py
@@ -7,15 +7,16 @@ class Solution(object):
         if not nums:
             return None
         left = 0
-        right = len(nums)-1
-        res = nums[0]
-        
-        while left <= right:
-            mid = left + (right-left)//2
-            if nums[left] <= nums[mid]:
-                res = min(res, nums[left])
-                left = mid + 1
+        right = len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] < nums[right]:
+                right = mid
             else:
-                res = min(res, nums[mid])
-                right = mid -1
-        return res
+                left = mid + 1
+        return nums[left]
+
+
+if __name__ == "__main__":
+    res = Solution().findMin(nums=[3, 4, 5, 1, 2])
+    print(res)
