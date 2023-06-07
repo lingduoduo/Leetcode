71501fc0344506cb3f9eec23bf3b718c378d2405
Ling Huang <linghypshen@gmail.com>
2023-06-06 22:22:19 -0400

    add code

--- Leetcode/0011-Container-With-Most-Water.py
+++ Leetcode/0011-Container-With-Most-Water.py
@@ -4,30 +4,19 @@ class Solution:
         :type height: List[int]
         :rtype: int
         """
-        ###first try
-        ###i=0
-        ###j=len(height)-1
-        ###result = 0
-        ###while i<j:
-        ###    result = max(result, (j-i)*min(height[i], height[j]))
-        ###    if height[i]<height[j]:
-        ###        i+=1
-        ###    else:
-        ###        j-=1
-        ###return result
-        
         res = 0
         left = 0
-        right = len(height)-1
-        while left<right:
+        right = len(height) - 1
+        while left < right:
             h = min(height[left], height[right])
-            res = max(res, h*(right-left))
-            if height[left]<height[right]:
+            res = max(res, h * (right - left))
+            if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
         return res
 
-if __name__=="__main__":
-    height = [1,8,6,2,5,4,8,3,7]
+
+if __name__ == "__main__":
+    height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
     print(Solution().maxArea(height))
