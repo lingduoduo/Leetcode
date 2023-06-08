f823965e2c6679bbd3c3d80d6d588971299488e3
Ling Huang <linghypshen@gmail.com>
2023-06-08 12:57:50 -0400

    add code

--- Leetcode/0169-Majority-Element.py
+++ Leetcode/0169-Majority-Element.py
@@ -1,16 +1,15 @@
 import collections
 class Solution(object):
     def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        ###"""
-        ###nums = sorted(nums)
-        ###return nums[len(nums) // 2]
-
         d = collections.Counter(nums)
         f = d.most_common()
         return f[0][0]
+
+
+class Solution(object):
+    def majorityElement(self, nums):
+        d = collections.Counter(nums)
+        return sorted(d.keys(), key=d.get)[-1]
 
 if __name__ == '__main__':
     nums = [2,2,1,1,1,2,2]
--- Leetcode/0190-Reverse-Bits.py
+++ Leetcode/0190-Reverse-Bits.py
@@ -1,30 +1,12 @@
-###class Solution:
-    ###@param n, an integer
-    ###@return an integer
-    ###def reverseBits(self, n):
-    ###    if n == 0:
-    ###        return 0
-    ###    bits = self.intToBit(n)
-    ###    return self.bitToInt(bits[::-1])
-    ###    ###return bits
-    #
-    ###def intToBit(self, n):
-    ###    bits = ''
-    ###    while (n > 0):
-    ###        bits = str(n % 2) + bits
-    ###        n = n // 2
-    ###    while len(bits) < 32:
-    ###        bits = '0' + bits
-    ###    return bits
-    #
-    ###def bitToInt(self, bits):
-    ###    res = 0
-    ###    for i in range(len(bits)):
-    ###        if bits[i] == '1':
-    ###            res = res * 2 + 1
-    ###        else:
-    ###            res = res * 2
-    ###    return res
+class Solution:
+    def bitToInt(self, bits):
+       res = 0
+       for i in range(len(bits)):
+           if bits[i] == '1':
+               res = res * 2 + 1
+           else:
+               res = res * 2
+       return res
 
 class Solution:
     def reverseBits(self, n: int) -> int:
@@ -33,7 +15,15 @@ class Solution:
             res = (res << 1) + (n & 1)
             n = n >> 1
         return res
-        
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res, power = 0, 31
+        while n:
+            res += (n & 1) << power
+            n >>= 1
+            power -= 1
+        return res
 
 if __name__ == '__main__':
     n = 43261596
