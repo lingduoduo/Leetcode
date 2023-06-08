314aacb0a002e8efa5a41c7707504970ddda9947
Ling Huang <linghypshen@gmail.com>
2023-06-08 08:16:54 -0400

    add code

--- Leetcode/0026-Remove-Duplicates-from-Sorted-Array.py
+++ Leetcode/0026-Remove-Duplicates-from-Sorted-Array.py
@@ -1,24 +1,25 @@
+from typing import List
+
+
 class Solution(object):
     def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        ###if len(nums) == 0:
-        ###    return 0
-        ###i = 0
-        ###for j in range(1, len(nums)):
-        ###    if nums[i] != nums[j]:
-        ###        i += 1
-        ###        nums[i] = nums[j]
-        ###return i + 1
-        
-        if not nums:
-            return 0
-        
         count = 0
         for i in range(len(nums)):
             if nums[count] != nums[i]:
                 count += 1
                 nums[count] = nums[i]
-        return count
+        return count + 1
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        j = 1
+        while j < len(nums):
+            if nums[j] == nums[i]:
+                j += 1
+            else:
+                i += 1
+                nums[i] = nums[j]
+                j += 1
+        return i + 1
