b65a83a61774a7ab21283cd471bd86dcab3cfded
Ling Huang <linghypshen@gmail.com>
2023-06-11 11:54:00 -0400

    add code

--- Leetcode/0328-Odd-Even-Linked-List.py
+++ Leetcode/0328-Odd-Even-Linked-List.py
@@ -1,63 +1,29 @@
-###Definition for singly-linked list.
-###class ListNode(object):
-###    def __init__(self, x):
-###        self.val = x
-###        self.next = None
-
-# class Solution(object):
-#     def oddEvenList(self, head):
-#         """
-#         :type head: ListNode
-#         :rtype: ListNode
-#         """
-#         odd = ListNode(0)
-#         even = ListNode(0)
-#
-#         oddHead = odd
-#         evenHead = even
-#         index = 1
-#         while head:
-#             if index % 2 == 1:
-#                 odd.next = head
-#                 odd = odd.next
-#             else:
-#                 even.next = head
-#                 even = even.next
-#             index += 1
-#             head = head.next
-#         even.next = None
-#         odd.next = evenHead.next
-#         return oddHead.next
 
 # Definition for singly-linked list.
-from typing import List, Optional
+from typing import Optional
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return head
-
         odd = ListNode(-1)
-        pt_odd = odd
+        p1 = odd
         even = ListNode(-1)
-        pt_even = even
+        p2 = even
 
         p = head
         cnt = 1
         while p:
             if cnt % 2 == 1:
-                pt_odd.next = p
-                pt_odd = pt_odd.next
+                p1.next = p
+                p1 = p1.next
             else:
-                pt_even.next = p
-                pt_even = pt_even.next
-            p = p.next
+                p2.next = p
+                p2 = p2.next
             cnt += 1
-        pt_even.next = None
-        pt_odd.next = even.next
+            p = p.next
+        p2.next = None
+        p1.next = even.next
         return odd.next
-
