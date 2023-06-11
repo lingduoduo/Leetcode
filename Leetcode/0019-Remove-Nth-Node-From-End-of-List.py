0d61bde0d524c62faca6152e63245fc1d938841d
Ling Huang <linghypshen@gmail.com>
2023-06-11 10:12:38 -0400

    add code

--- Leetcode/0019-Remove-Nth-Node-From-End-of-List.py
+++ Leetcode/0019-Remove-Nth-Node-From-End-of-List.py
@@ -1,74 +1,24 @@
 #Definition for singly-linked list.
+from typing import Optional
+s
 class ListNode(object):
    def __init__(self, x):
        self.val = x
        self.next = None
 
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
-        dummy = ListNode(0)
-        dummy.next = head
-        curr = dummy
-
-        d = dict()
-        cnt = 1
-        while curr:
-           d[cnt] = curr
-           cnt+=1
-           curr = curr.next
-
-        pos = len(d.keys()) - n
-
-        d[pos].next = d[pos].next.next
-        return dummy.next
-
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        dummy = ListNode(0)
-        slow = fast = dummy
-        dummy.next = head
-
-        for i in range(n):
-            fast = fast.next
-
-        while fast.next != None:
-            fast = fast.next
-            slow = slow.next
-
-        slow.next = slow.next.next
-        return dummy.next
-
-# Definition for singly-linked list.
-from typing import Optional
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        fast = head
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        slow = fast = dummy
+
         for i in range(n):
             fast = fast.next
 
-        if fast is None:
-            return head.next
-
-        slow = head
         while fast.next:
-            fast = fast.next
             slow = slow.next
+            fast = fast.next
+
         slow.next = slow.next.next
-        return head
-
-
-
-
-
-
+        return dummy.next
--- Leetcode/0203-Remove-Linked-List-Elements.py
+++ Leetcode/0203-Remove-Linked-List-Elements.py
@@ -1,8 +1,8 @@
 ###Definition for singly-linked list.
-###class ListNode(object):
-###    def __init__(self, x):
-###        self.val = x
-###        self.next = None
+class ListNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.next = None
 
 class Solution(object):
     def removeElements(self, head, val):
@@ -11,20 +11,6 @@ class Solution(object):
         :type val: int
         :rtype: ListNode
         """
-        ###dummy = ListNode(0)
-        ###dummy.next = head
-        #
-        ###prev = dummy
-        ###curr = head
-        #
-        ###while curr:
-        ###    if curr.val == val:
-        ###        prev.next = curr.next
-        ###    else:
-        ###        prev = prev.next
-        ###    curr = curr.next
-        #
-        ###return dummy.next
         dummy = ListNode(float("-inf"))
         dummy.next = head
         prev, curr = dummy, dummy.next
