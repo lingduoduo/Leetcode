4134ce1bf1be996191321b850d64f7889b6d3fb7
Ling Huang <linghypshen@gmail.com>
2023-06-13 14:25:52 -0400

    add code

--- Leetcode/0092-Reverse-Linked-List-II.py
+++ Leetcode/0092-Reverse-Linked-List-II.py
@@ -3,7 +3,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         if left == right:
--- Leetcode/0150-Evaluate-Reverse-Polish-Notation.py
+++ Leetcode/0150-Evaluate-Reverse-Polish-Notation.py
@@ -1,23 +1,18 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        res = []
-
+        stack = []
         for token in tokens:
-            if token not in ['+', '-', '*', '/']:
-                res.append(int(token))
+            if token in list("+-*/"):
+                num1 = stack.pop()
+                num2 = stack.pop()
+                if token == "+":
+                    stack.append(num1 + num2)
+                elif token == "-":
+                    stack.append(num2 - num1)
+                elif token == "*":
+                    stack.append(num1 * num2)
+                elif token == "/":
+                    stack.append(int(float(num2)/num1))
             else:
-                n2 = res.pop()
-                n1 = res.pop()
-                res.append(int(eval(f'{n1}{token}{n2}')))
-                
-            # if token == '+':
-            #     res.append(n1+n2)
-            # elif token == '-':
-            #     res.append(n1-n2)
-            # elif token == '*':
-            #     res.append(n1*n2)
-            # elif token == '/':
-            #     res.append(int(n1/n2))
-
-
-        return res[0]
+                stack.append(int(token))
+        return stack.pop()
