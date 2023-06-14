ec29ea85cd919cc7c92efa9ba97168926b3fb4ca
Ling Huang <linghypshen@gmail.com>
2023-06-14 18:07:01 -0400

    add code

--- Leetcode/0205-Isomorphic-Strings.py
+++ Leetcode/0205-Isomorphic-Strings.py
@@ -1,47 +1,16 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        ###d1 = {}
-        ###d2 = {}
-        #
-        ###for i in range(min(len(s), len(t))):
-        ###    if s[i] in d1:
-        ###        d1[s[i]] += [i]
-        ###    else:
-        ###        d1[s[i]] = [i]
-        #
-        ###    if t[i] in d2:
-        ###        d2[t[i]] += [i]
-        ###    else:
-        ###        d2[t[i]] = [i]
-        #
-        ###    if s[i] != t[i]:
-        ###        return False
-        #
-        ###pos1 = sorted([v for v in d1.values()])
-        ###pos2 = sorted([v for v in d2.values()])
-        #
-        ###return pos1 == pos2
-        
-        if len(s) != len(t):
-            return False
-        
-        return self.half(s, t) and self.half(t, s)
-    
-    def half(self, s: str, t: str):
-        lookup = {}
-        for i in range(len(s)):
-            if s[i] not in lookup:
-                lookup[s[i]] = t[i]
-            elif lookup[s[i]] != t[i]
-                return False
-        return True
-        
-    
+        return self.helper(s) == self.helper(t)
+
+    def helper(self, word):
+        res, d = [], {}
+        for i, w in enumerate(word):
+            if w not in d:
+                d[w] = i
+            res.append(d[w])
+        return res
+
+
 if __name__ == "__main__":
     s = "ab"
     t = "ca"
