d86777f72984cdee22ddff908a24e244a2efcec1
Ling Huang <linghypshen@gmail.com>
2023-06-14 13:26:36 -0400

    add code

--- Leetcode/1475-Final-Prices-With-a-Special-Discount-in-a-Shop.py
+++ Leetcode/1475-Final-Prices-With-a-Special-Discount-in-a-Shop.py
@@ -6,3 +6,19 @@ class Solution:
                     prices[i] -= prices[j]
                     break
         return prices
+
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = [0]
+        res = []
+        for p in prices[::-1]:
+            if p > stack[-1]:
+                res.append(p - stack[-1])
+                stack.append(p)
+            else:
+                while p < stack[-1]:
+                    stack.pop()
+                res.append(p - stack[-1])
+                stack.append(p)
+        return res[::-1]
