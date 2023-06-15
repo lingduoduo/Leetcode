b93e91b2b64f0c05a102098e005914faf44f9b4f
Ling Huang <linghypshen@gmail.com>
2023-06-15 09:01:40 -0400

    add code

--- Leetcode/0042-Trapping-Rain-Water.py
+++ Leetcode/0042-Trapping-Rain-Water.py
@@ -1,19 +1,19 @@
-# class Solution:
-#     def trap(self, height: List[int]) -> int:
-        # leftmosthigh = [0 for i in range(len(height))]
-        # leftmax = 0
-        # for i in range(len(height)):
-        #     if height[i] > leftmax: 
-        #         leftmax = height[i]
-        #     leftmosthigh[i] = leftmax
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        leftmosthigh = [0 for i in range(len(height))]
+        leftmax = 0
+        for i in range(len(height)):
+            if height[i] > leftmax:
+                leftmax = height[i]
+            leftmosthigh[i] = leftmax
             
-        # res = 0
-        # rightmax = 0
-        # for i in reversed(range(len(height))):
-        #     if height[i] > rightmax: rightmax = height[i]
-        #     if min(rightmax, leftmosthigh[i]) > height[i]:
-        #         res += min(rightmax, leftmosthigh[i]) - height[i]
-        # return res
+        res = 0
+        rightmax = 0
+        for i in reversed(range(len(height))):
+            if height[i] > rightmax: rightmax = height[i]
+            if min(rightmax, leftmosthigh[i]) > height[i]:
+                res += min(rightmax, leftmosthigh[i]) - height[i]
+        return res
 
 
 class Solution:
@@ -22,10 +22,10 @@ class Solution:
         rightmax = [0]*len(height)
         for i in range(1, len(height)):
             leftmax[i] = max(leftmax[i-1], height[i-1])
-        print(leftmax)
+
         for i in range(len(height)-2, -1, -1):
             rightmax[i] = max(rightmax[i+1], height[i+1])
-        print(rightmax)
+
         res = [0]*len(height)
         for i in range(len(height)):
             res[i] = max(0, min(leftmax[i], rightmax[i]) - height[i])
