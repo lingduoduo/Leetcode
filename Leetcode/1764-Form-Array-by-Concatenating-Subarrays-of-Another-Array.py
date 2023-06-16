8fcb729986d2fa4d2e1b8a2845fc6f1c8439b323
Ling Huang <linghypshen@gmail.com>
2023-06-16 18:14:27 -0400

    add code

--- Leetcode/1764-Form-Array-by-Concatenating-Subarrays-of-Another-Array.py
+++ Leetcode/1764-Form-Array-by-Concatenating-Subarrays-of-Another-Array.py
@@ -1,20 +1,13 @@
-import collections
 from typing import List
+
 
 class Solution:
     def canChoose(self, groups: List[List[int]], nums: List[int]) -> bool:
-        n = len(nums)
-        s = 0
-        for group in groups:
-            found = False
-            for i in range(n - len(group)):
-                if s
-                s == i + g
+        pass
 
 
-
-if __name__ == '__main__':
-    groups = [[1,-1,-1],[3,-2,0]]
-    nums = [1,-1,0,1,-1,-1,3,-2,0]
+if __name__ == "__main__":
+    groups = [[1, -1, -1], [3, -2, 0]]
+    nums = [1, -1, 0, 1, -1, -1, 3, -2, 0]
     res = Solution().canChoose(groups, nums)
     print(res)
