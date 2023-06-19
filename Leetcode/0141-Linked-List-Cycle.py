16df287b87dc5060b9766fd471a54fd6aea131f0
Ling Huang <linghypshen@gmail.com>
2023-06-19 14:59:59 -0400

    add code

--- Leetcode/0141-Linked-List-Cycle.py
+++ Leetcode/0141-Linked-List-Cycle.py
@@ -1,9 +1,8 @@
-###Definition for singly-linked list.
-###class ListNode(object):
-###    def __init__(self, x):
-###        self.val = x
-###        self.next = None
 
+class ListNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.next = None
 
 class Solution(object):
     def hasCycle(self, head):
@@ -11,25 +10,6 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        ###visited = set()
-        ###while head is not None:
-        ###	if head in visited:
-        ###		return True
-        ###	visited.add(head)
-        ###	head = head.next
-        ###return False
-        ###slow = fast = head
-        ###if fast is None:
-        ###    return False
-        ###while fast is not None:
-        ###    if fast.next is None:
-        ###        return False
-        ###    fast = fast.next.next
-        ###    slow = slow.next
-        ###    if fast == slow:
-        ###        return True
-        ###return False
-
         fast, slow = head, head
         while fast and fast.next:
             fast = fast.next.nest
