6e26f9b16f4a9c4bd2a4123ef04513662c143e9e
Ling Huang <linghypshen@gmail.com>
2023-06-19 16:12:08 -0400

    add code

--- Leetcode/0142-Linked-List-Cycle-II.py
+++ Leetcode/0142-Linked-List-Cycle-II.py
@@ -18,3 +18,19 @@ class Solution:
                 visited.add(head)
                 head = head.next
         return None
+
+class Solution(object):
+    def detectCycle(self, head):
+        slow, fast = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                break
+        if not fast or not fast.next:
+            return None
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return fast
--- Leetcode/0881-Boats-to-Save-People.py
+++ Leetcode/0881-Boats-to-Save-People.py
@@ -1,15 +1,17 @@
-class Solution:
-    def numRescueBoats(self, people, limit) -> int:
-        people.sort()
-        res = 0
-        left, right = 0, len(people) - 1
-        while left <= right:
-            if people[left] + people[right] <= limit:
-                left += 1
-            right -= 1
-            res += 1
-        return res
+from typing import List
 
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        l = 0
+        r = len(people) -1
+        res = 0
+        while l <= r:
+            if people[l] + people[r] <= limit:
+                l += 1
+            res += 1
+            r -= 1
+        return res
 
 if __name__ == "__main__":
     # people = [1,2]
