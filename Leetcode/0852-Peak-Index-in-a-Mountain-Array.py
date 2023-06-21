a0dc1c91058fb4fee4db43df41d9849238807239
Ling Huang <linghypshen@gmail.com>
2023-06-20 21:57:53 -0400

    add code

--- Leetcode/0852-Peak-Index-in-a-Mountain-Array.py
+++ Leetcode/0852-Peak-Index-in-a-Mountain-Array.py
@@ -11,6 +11,18 @@ class Solution:
         return res
 
 
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l = 0
+        r = len(arr) - 1
+        while l < r:
+            m = l + (r - l)//2
+            if arr[m] < arr[m + 1]:
+                l = m + 1
+            else:
+                r = m
+        return l
+
 if __name__ == "__main__":
     arr = [0, 1, 0]
     res = Solution().peakIndexInMountainArray(arr)
