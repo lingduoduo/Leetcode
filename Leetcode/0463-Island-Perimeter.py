a899b8e45d411157687d1e7a001fbc88e80ab106
Ling Huang <linghypshen@gmail.com>
2023-06-21 22:58:02 -0400

    add code

--- Leetcode/0463-Island-Perimeter.py
+++ Leetcode/0463-Island-Perimeter.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution(object):
     def islandPerimeter(self, grid):
         """
@@ -32,16 +34,24 @@ class Solution:
         cols = len(grid[0])
 
         result = 0
-
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    result += 4
-
-                    if r > 0 and grid[r - 1][c] == 1:
-                        result -= 2
-
-                    if c > 0 and grid[r][c - 1] == 1:
-                        result -= 2
-
+                    if r == 0:
+                        up = 0
+                    else:
+                        up = grid[r - 1][c]
+                    if r == rows - 1:
+                        down = 0
+                    else:
+                        down = grid[r + 1][c]
+                    if c == 0:
+                        left = 0
+                    else:
+                        left = grid[r][c - 1]
+                    if c == cols - 1:
+                        right = 0
+                    else:
+                        right = grid[r][c + 1]
+                    result += 4 - (up + down + left + right)
         return result
