833170c60a2df957e8a916fdb00e32f207cb97cd
Ling Huang <linghypshen@gmail.com>
2023-06-21 22:24:52 -0400

    add code

--- Leetcode/0695-Max–Area–of-Island.py
+++ Leetcode/0695-Max–Area–of-Island.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         res = 0
@@ -18,3 +19,23 @@ class Solution:
                 if grid[i][j] == 1:
                     res = max(res, dfs(i, j))
         return res
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        res = 0
+        self.visited = set()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    res = max(res, self.dfs(grid, i, j))
+        return res
+
+    def dfs(self, grid, i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0 or (i, j) in self.visited:
+            return 0
+
+        grid[i][j] == 0
+        self.visited.add((i, j))
+        return 1 + self.dfs(grid, i + 1, j) + self.dfs(grid, i - 1, j) + self.dfs(grid, i, j + 1) + self.dfs(grid, i,
+                                                                                                             j - 1)
