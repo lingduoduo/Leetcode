8f885eb6878ddcc7e731103b2673de9cf7a1ffc2
Ling Huang <linghypshen@gmail.com>
2023-06-23 13:53:43 -0400

    add code

--- Leetcode/0017-Letter-Combinations-of-a-Phone-Number.py
+++ Leetcode/0017-Letter-Combinations-of-a-Phone-Number.py
@@ -66,6 +66,42 @@ class Solution:
         return res
 
 
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+
+        d = dict()
+        d['0'] = ''
+        d['1'] = ''
+        d['2'] = 'abc'
+        d['3'] = 'def'
+        d['4'] = 'ghi'
+        d['5'] = 'jkl'
+        d['6'] = 'mno'
+        d['7'] = 'pqrs'
+        d['8'] = 'tuv'
+        d['9'] = 'wxyz'
+
+        res = []
+
+        def dfs(idx, path):
+            nonlocal res
+            if len(path) == len(digits):
+                res.append(''.join(path))
+
+            if idx >= len(digits):
+                return
+
+            for cha in d[digits[idx]]:
+                path.append(cha)
+                dfs(idx + 1, path)
+                path.pop()
+
+        dfs(0, [])
+        return res
+
+
 if __name__ == "__main__":
     digits = "23"
     result = Solution().letterCombinations(digits)
