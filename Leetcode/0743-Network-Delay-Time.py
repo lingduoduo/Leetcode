9e0d90f6aa4f3e4ffe35e30b65c0c13aabb9144b
Ling Huang <linghypshen@gmail.com>
2023-06-25 10:40:52 -0400

    add code

--- Leetcode/0743-Network-Delay-Time.py
+++ Leetcode/0743-Network-Delay-Time.py
@@ -1,3 +1,8 @@
+import collections
+from typing import List
+import heapq
+
+
 class Solution:
     def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
         K -= 1
@@ -18,3 +23,24 @@ class Solution:
             done.add(smallest)
 
         return -1 if float("inf") in dist else max(dist)
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        d = collections.defaultdict(list)
+        for u, v, w in times:
+            d[u].append([w, v])
+
+        stack = [(0, k)]
+        seen = set()
+        while stack:
+            time, node = heapq.heappop(stack)
+            seen.add(node)
+
+            if len(seen) == n:
+                return time
+
+            for next_time, next_node in d[node]:
+                if next_node not in seen:
+                    heapq.heappush(stack, [time + next_time, next_node])
+        return -1
