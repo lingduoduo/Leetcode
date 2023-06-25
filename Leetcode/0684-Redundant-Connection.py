ebf4fedee9f54574ca09b9c90b8624b89242f48b
Ling Huang <linghypshen@gmail.com>
2023-06-25 12:13:44 -0400

    add code

--- Leetcode/0684-Redundant-Connection.py
+++ Leetcode/0684-Redundant-Connection.py
@@ -1,4 +1,7 @@
-#######Union Find Method
+import collections
+
+
+# Union Find Method
 class UnionFindSet:
     def __init__(self, n):
         self._parents = [i for i in range(n + 1)]
@@ -41,53 +44,6 @@ class Solution:
         return None
 
 
-####DFS
-"""
-Input: [[1,2], [1,3], [2,3]]
-Output: [2,3]
-"""
-
-
-# class Solution:
-
-#     def findRedundantConnection(self, edges):
-#         d = dict()
-
-#         for edge in edges:
-#             u = edge[0]
-#             v = edge[1]
-
-#             if self.haspath(u, v, d, []):
-#                 return edge
-
-#             d[u] = d.get(u, []) + [v]
-#             d[v] = d.get(v, []) + [u]
-#         return []
-
-#     def haspath(self, start, target, d, visited):
-#         if start == target:
-#             return True
-#         visited.append(start)
-
-#         if start not in d or target not in d:
-#             return False
-
-#         if start in d and target in d[start]:
-#             return True
-
-#         while visited:
-#             startnode = visited.pop(0)
-#             visited += d[startnode]
-
-#             for node in d[startnode]:
-#                 d[startnode].remove(node)
-#                 d[node].remove(startnode)
-
-#                 if self.haspath(node, target, d, visited):
-#                     return True
-#             return False
-
-
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         graph = collections.defaultdict(set)
@@ -111,11 +67,3 @@ if __name__ == "__main__":
     edges = [[1, 2], [1, 3], [2, 3]]
     result = Solution().findRedundantConnection(edges)
     print(result)
-    #
-    ###edges = [[1,2], [2,3], [3,4], [1,4], [1,5]]
-    ###result = Solution().findRedundantConnection(edges)
-    ###print(result)
-
-    ###edges = [[1,2],[2,3],[2,4],[4,5],[1,5]]
-    ###result = Solution().findRedundantConnection(edges)
-    ###print(result)
