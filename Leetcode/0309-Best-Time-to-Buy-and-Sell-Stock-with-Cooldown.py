59c19a86897ca06725b21bd9f899be6477f84bfc
Ling Huang <linghypshen@gmail.com>
2023-06-28 18:46:23 -0400

    add code

--- Leetcode/0309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.py
+++ Leetcode/0309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if not prices: return 0
@@ -9,24 +12,27 @@ class Solution:
         for i in range(1, len(prices)):
             tmp = curr
             curr = max(curr, hold + prices[i])
-            hold = max(hold, (prev if i>=2 else 0) - prices[i])
+            hold = max(hold, (prev if i >= 2 else 0) - prices[i])
             prev = tmp
         return curr
 
+class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices: return 0 
-        profit = 0                ## before buy status
-        cooldown = 0            ## after sell status
-        hold = -price[0]        ## before sell status
+        if not prices: return 0
+        profit = 0  ## before buy status
+        cooldown = 0  ## after sell status
+        hold = -prices[0]  ## before sell status
 
         for i in range(1, len(prices)):
             prev_profit = profit
-            profit = hold+prices[i]              ## sell
-            hold = max(hold, cooldown-prices[i]) ## buy
+            profit = hold + prices[i]  ## sell
+            hold = max(hold, cooldown - prices[i])  ## buy
             cooldown = max(cooldown, prev_profit)
-            
+
         return max(cooldown, profit)
 
+
+class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         buy = float("-inf")
         pre_buy = 0
@@ -35,21 +41,7 @@ class Solution:
 
         for i in range(len(prices)):
             pre_buy = buy
-            buy = max(pre_buy, pre_sell-prices[i])
+            buy = max(pre_buy, pre_sell - prices[i])
             pre_sell = sell
-            sell = max(pre_sell, pre_buy+prices[i])
+            sell = max(pre_sell, pre_buy + prices[i])
         return sell
-
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int buy = INT_MIN, pre_buy = 0, sell = 0, pre_sell = 0;
-        for (int price : prices) {
-             = buy;
-            buy = max(pre_sell - price, pre_buy);
-            pre_sell = sell;
-            sell = max(pre_buy + price, pre_sell);
-        }
-        return sell;
-    }
-};
