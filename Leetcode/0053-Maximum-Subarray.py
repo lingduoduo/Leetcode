0bf6b09f7d817c96663df7ea65af8376e315b5a1
Ling Huang <linghypshen@gmail.com>
2023-06-29 11:36:30 -0400

    add code

--- Leetcode/0053-Maximum-Subarray.py
+++ Leetcode/0053-Maximum-Subarray.py
@@ -29,6 +29,18 @@ class Solution:
         return res
 
 
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum = nums[0]
+        max_sum = nums[0]
+
+        for i in range(1, len(nums)):
+            current_sum = max(nums[i], current_sum + nums[i])
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum
+
+
 if __name__ == "__main__":
     # numbers = [-2,1,-3,4,-1,2,1,-5,4]
     numbers = [-2, 1]
