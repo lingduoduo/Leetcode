94a9543baf29f28b0fd8910e4e2a17b147082d83
Ling Huang <linghypshen@gmail.com>
2023-06-28 21:58:17 -0400

    add code

--- Leetcode/0005-Longest-Palindromic-Substring.py
+++ Leetcode/0005-Longest-Palindromic-Substring.py
@@ -1,73 +1,51 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        ###print(s)
-        ###if len(s) <= 1:
-        ###    return s
-        #
-        ###result = ""
-        ###size = 0
-        #
-        ###for i in range(len(s)):
-        ###    indices = [k for k, x in enumerate(s) if x == s[i] and k > i]
-        #
-        ###    for j in indices:
-        ###        if s[i:(j + 1)] == s[i:(j + 1)][::-1] and (j - i + 1) > size:
-        ###            size = j - i + 1
-        ###            result = s[i:(j + 1)]
-        #
-        ###if size < 2:
-        ###    return s[1]
-        ###else:
-        ###    return result
-
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
         n = len(s)
-
-        def getlen(l, r):
-            while l >= 0 and r < n and s[l] == s[r]:
-                l -= 1
-                r += 1
-            return r - l - 1
-
-        start = 0
-        length = 0
-        for i in range(n):
-            cur = max(getlen(i, i), getlen(i, i + 1))
-            if cur <= length:
-                continue
-            length = cur
-            start = i - (cur - 1) // 2
-        return s[start : start + length]
-
-
-class Solution(object):
-    def longestPalindrome(self, s):
-        # 1.先判斷該字串是否為一個字母，或是本身是迴文
-        if (len(s) <= 1) or s == s[::-1]:
+        if n < 2:
             return s
-        else:
-            # 紀錄開始的位置
-            startIndex = 1
-            # 紀錄目前最大迴文的長度
-            maxLen = 1
-            # 開始跑迴圈
-            for i in range(1, len(s)):
-                # 跑第一圈時，odd要找 s[-1:2]，even要找 s[0:2]
-                # 跑第二圈時，odd要找 s[0:3]，even要找 s[1:3]
-                # 跑第三圈時，odd要找 s[1:4]，even要找 s[2:4]
-                odd = s[i - maxLen - 1 : i + 1]
-                even = s[i - maxLen : i + 1]
-                # 判斷odd是否為迴文
-                if i - maxLen - 1 >= 0 and odd == odd[::-1]:
-                    startIndex = i - maxLen - 1
-                    maxLen += 2
-                elif i - maxLen >= 0 and even == even[::-1]:
-                    startIndex = i - maxLen
-                    maxLen += 1
-        return s[startIndex : startIndex + maxLen]
+        i = 0
+        start = 0
+        size = 0
+        while i < n:
+            if n - i <= size // 2:
+                break
+            left = right = i
+            while right < n - 1 and s[right] == s[right+1]:
+                right += 1
+            i = right + 1
+            while right < n - 1 and left > 0 and s[right + 1] == s[left - 1]:
+                right += 1
+                left -= 1
+            cur = right - left + 1
+            if cur > size:
+                start = left
+                size = cur
+        return s[start: start + size]
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        res = [0, 0]
+
+        for i in range(n):
+            dp[i][i] = True
+
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                res = [i, i + 1]
+
+        for diff in range(2, n):
+            for i in range(n - diff):
+                j = i + diff
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    res = [i, j]
+
+        i, j = res
+        return s[i:j + 1]
 
 
 if __name__ == "__main__":
