ef59d7bb0f096002ec9575c7903aebd0c36b09b6
Ling Huang <linghypshen@gmail.com>
2023-06-30 14:44:52 -0400

    add code

--- Leetcode/0213-House-Robber-II.py
+++ Leetcode/0213-House-Robber-II.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if not nums:
@@ -7,7 +10,7 @@ class Solution:
         if len(nums) == 2:
             return max(nums[0], nums[1])
         n = len(nums)
-        return max(self.rob_sub(nums[0 : n - 1]), self.rob_sub(nums[1:n]))
+        return max(self.rob_sub(nums[0: n - 1]), self.rob_sub(nums[1:n]))
 
     def rob_sub(self, nums):
         if not nums:
@@ -15,7 +18,7 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         if len(nums) == 2:
-            retunr
+            return
             max(nums[0], nums[1])
         n = len(nums)
         dp = [0] * n
@@ -37,3 +40,26 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         return max(self._rob(nums[:-1]), self._rob(nums[1:]))
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        odds = self.helper(nums[:-1])
+        evens = self.helper(nums[1:])
+        return max(odds, evens)
+
+    def helper(self, vals: List[int]) -> int:
+        dp = [0] * (len(vals) + 1)
+        for i, num in enumerate(vals):
+            if i == 0:
+                dp[i + 1] = vals[i]
+            else:
+                dp[i + 1] = max(dp[i], dp[i - 1] + vals[i])
+        return dp[-1]
+
+
+if __name__ == "__main__":
+    res = Solution().rob([1, 2, 3])
+    print(res)
