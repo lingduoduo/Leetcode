1ecb62005ea783d7c6810a8f5387c825cbe87f5e
Ling Huang <linghypshen@gmail.com>
2023-07-01 11:39:09 -0400

    add code

--- Leetcode/0676-Implement-Magic-Dictionary.py
+++ Leetcode/0676-Implement-Magic-Dictionary.py
@@ -9,40 +9,6 @@ class MagicDictionary:
     Input: search("hell"), Output: False
     Input: search("leetcoded"), Output: False
     """
-
-    ###def __init__(self):
-    ###    """
-    ###    Initialize your data structure here.
-    ###    """
-    ###    self.d = list()
-
-    ###def buildDict(self, dict) -> None:
-    ###    """
-    ###    Build a dictionary through a list of words
-    ###    """
-
-    ###    for i in range(len(dict)):
-    ###        if dict[i] not in self.d:
-    ###            self.d.append(dict[i])
-
-    ###def search(self, word) -> bool:
-    ###    """
-    ###    Returns if there is any word in the trie that equals to the given word after modifying exactly one character
-    ###    """
-
-    ###    for i in range(len(self.d)):
-    ###        if len(self.d[i]) != len(word):
-    ###            continue
-    ###        match = 0
-    ###        j = 0
-    ###        while j < len(word):
-    ###            if word[j] == self.d[i][j]:
-    ###                match += 1
-    ###            j += 1
-    ###        if match == len(word) - 1:
-    ###            return True
-    ###    return False
-
     def __init__(self):
         """
         Initialize your data structure here.
@@ -66,6 +32,39 @@ class MagicDictionary:
             self.near[cand] > 1 or self.near[cand] == 1 and word not in self.words
             for cand in self._candidate(word)
         )
+
+
+class MagicDictionary(object):
+    def __init__(self):
+        self.buckets = collections.defaultdict(list)
+
+    def buildDict(self, words):
+        for word in words:
+            self.buckets[len(word)].append(word)
+
+    def search(self, word):
+        return any(sum(a!=b for a,b in zip(word, candidate)) == 1
+                   for candidate in self.buckets[len(word)])
+
+
+class MagicDictionary:
+
+    def __init__(self):
+        pass
+
+    def _genneighbors(self, word):
+        for i in range(len(word)):
+            yield word[:i] + '*' + word[i + 1:]
+
+    def buildDict(self, dictionary: List[str]) -> None:
+        self.words = set(dictionary)
+        self.d = collections.Counter(nei for word in dictionary
+                                     for nei in self._genneighbors(word))
+
+    def search(self, searchWord: str) -> bool:
+        return any(self.d[nei] > 1 or
+                   self.d[nei] == 1 and searchWord not in self.words
+                   for nei in self._genneighbors(searchWord))
 
 
 if __name__ == "__main__":
