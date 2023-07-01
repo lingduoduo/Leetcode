2e7b9dccc07daa6ab5c76d7e1a19611f6661bb23
Ling Huang <linghypshen@gmail.com>
2023-07-01 10:34:27 -0400

    add code

--- Leetcode/0648-Replace-Words.py
+++ Leetcode/0648-Replace-Words.py
@@ -2,6 +2,7 @@ from functools import reduce
 from typing import List
 import collections
 
+
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
         rootset = set(dictionary)
@@ -32,3 +33,43 @@ class Solution:
             return cur.get(END, word)
 
         return " ".join(map(replace, sentence.split()))
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.isWord = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for w in word:
+            node = node.children[w]
+        node.isWord = True
+
+    def search(self, word):
+        node = self.root
+        osf = ''
+        for c in word:
+            if c not in node.children: break
+            node = node.children[c]
+            osf += c
+            if node.isWord: return osf
+        return word
+
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = Trie()
+        for words in dictionary:
+            trie.insert(words)
+        res = ''
+        for word in sentence.split():
+            if res:
+                res += ' '
+            res += trie.search(word)
+        return res
