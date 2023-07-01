bf3f29c9169101947a145c5d32afac6745fdde2e
Ling Huang <linghypshen@gmail.com>
2023-07-01 10:23:11 -0400

    add code

--- Leetcode/0648-Replace-Words.py
+++ Leetcode/0648-Replace-Words.py
@@ -1,3 +1,7 @@
+from functools import reduce
+from typing import List
+import collections
+
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
         rootset = set(dictionary)
@@ -9,3 +13,22 @@ class Solution:
             return word
 
         return "".join(map(replace, sentence.split()))
+
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        Trie = lambda: collections.defaultdict(Trie)
+        trie = Trie()
+        END = True
+
+        for root in dictionary:
+            reduce(dict.__getitem__, root, trie)[END] = root
+
+        def replace(word):
+            cur = trie
+            for letter in word:
+                if letter not in cur or END in cur: break
+                cur = cur[letter]
+            return cur.get(END, word)
+
+        return " ".join(map(replace, sentence.split()))
