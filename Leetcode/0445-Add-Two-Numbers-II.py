9629cc9cadbe8e880edf4aa2c411033886a04ea2
Ling Huang <linghypshen@gmail.com>
2023-07-03 00:18:41 -0400

    add code

--- Leetcode/0445-Add-Two-Numbers-II.py
+++ Leetcode/0445-Add-Two-Numbers-II.py
@@ -1,101 +1,72 @@
 ###Definition for singly-linked list.
-###class ListNode(object):
-###    def __init__(self, x):
-###        self.val = x
-###        self.next = None
+class ListNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.next = None
 
 
-# class Solution(object):
-#     def addTwoNumbers(self, l1, l2):
-#         """
-#         :type l1: ListNode
-#         :type l2: ListNode
-#         :rtype: ListNode
-#         """
-#         stack1 = []
-#         curr = l1
-#         while curr:
-#             stack1.append(curr)
-#             curr = curr.next
-#
-#         stack2 = []
-#         curr = l2
-#         while curr:
-#             stack2.append(curr)
-#             curr = curr.next
-#
-#         dummy = ListNode(-1)
-#         carry = 0
-#         curr = dummy
-#         while stack1 and stack2:
-#             node1 = stack1.pop()
-#             node2 = stack2.pop()
-#             par = carry + node1.val + node2.val
-#             if par >= 10:
-#                 carry = 1
-#                 par = par - 10
-#             else:
-#                 carry = 0
-#             node = ListNode(par)
-#             node.next = dummy.next
-#             dummy.next = node
-#         while stack1:
-#             node1 = stack1.pop()
-#             par = carry + node1.val
-#             if par >= 10:
-#                 carry = 1
-#                 par = par - 10
-#             else:
-#                 carry = 0
-#             node = ListNode(par)
-#             node.next = dummy.next
-#             dummy.next = node
-#         while stack2:
-#             node2 = stack2.pop()
-#             par = carry + node2.val
-#             if par >= 10:
-#                 carry = 1
-#                 par = par - 10
-#             else:
-#                 carry = 0
-#             node = ListNode(par)
-#             node.next = dummy.next
-#             dummy.next = node
-#         if carry == 1:
-#             node = ListNode(1)
-#             node.next = dummy.next
-#             dummy.next = node
-#
-#         return dummy.next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        stack1 = []
+        curr = l1
+        while curr:
+            stack1.append(curr)
+            curr = curr.next
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-# class Solution:
-#     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-#         s1 = []
-#         s2 = []
-#         while l1:
-#             s1.append(l1.val)
-#             l1 = l1.next
-#         while l2:
-#             s2.append(l2.val)
-#             l2 = l2.next
-#         tmp = 0
-#         res = ListNode(0)
-#         while s1 or s2:
-#             if s1:
-#                 tmp += s1.pop()
-#             if s2:
-#                 tmp += s2.pop()
-#             res.val = tmp % 10
-#             head = ListNode(tmp // 10)
-#             head.next = res
-#             res = head
-#             tmp = tmp // 10
-#         return res.next if res.val == 0 else res
+        stack2 = []
+        curr = l2
+        while curr:
+            stack2.append(curr)
+            curr = curr.next
+
+        dummy = ListNode(-1)
+        carry = 0
+        curr = dummy
+        while stack1 and stack2:
+            node1 = stack1.pop()
+            node2 = stack2.pop()
+            par = carry + node1.val + node2.val
+            if par >= 10:
+                carry = 1
+                par = par - 10
+            else:
+                carry = 0
+            node = ListNode(par)
+            node.next = dummy.next
+            dummy.next = node
+        while stack1:
+            node1 = stack1.pop()
+            par = carry + node1.val
+            if par >= 10:
+                carry = 1
+                par = par - 10
+            else:
+                carry = 0
+            node = ListNode(par)
+            node.next = dummy.next
+            dummy.next = node
+        while stack2:
+            node2 = stack2.pop()
+            par = carry + node2.val
+            if par >= 10:
+                carry = 1
+                par = par - 10
+            else:
+                carry = 0
+            node = ListNode(par)
+            node.next = dummy.next
+            dummy.next = node
+        if carry == 1:
+            node = ListNode(1)
+            node.next = dummy.next
+            dummy.next = node
+
+        return dummy.next
 
 from typing import Optional
 
@@ -124,14 +95,15 @@ class Solution:
             p = p.next
 
         head = ListNode(-1)
+        p = head
         carry = 0
         while l1Stack or l2Stack or carry:
             x = l1Stack.pop() if l1Stack else 0
             y = l2Stack.pop() if l2Stack else 0
             v = x + y + carry
-            p = head.next
-            head.next = ListNode(v % 10)
-            head.next.next = p
+            newnode = ListNode(v % 10)
+            newnode.next = p.next
+            p.next = newnode
             carry = v // 10
         return head.next
 
