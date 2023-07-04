12d5b952db426cbb50652790ce5c8a6c79e59e2e
Ling Huang <linghypshen@gmail.com>
2023-07-04 16:33:32 -0400

    add code

--- Leetcode/0295-Find-Median-from-Data-Stream.py
+++ Leetcode/0295-Find-Median-from-Data-Stream.py
@@ -39,6 +39,38 @@ class MedianFinder(object):
             return (-self.small[0] + self.large[0]) / 2
 
 
+class MedianFinder:
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        ## heap from small to large
+        self.lo = []
+        self.hi = []
+
+    def addNum(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        heappush(self.lo, -num)  # lo is maxheap, so -1 * num
+        heappush(self.hi, -self.lo[0])  # hi is minheap
+        heappop(self.lo)
+
+        if len(self.lo) < len(self.hi):
+            heappush(self.lo, -self.hi[0])
+            heappop(self.hi)
+
+    def findMedian(self):
+        """
+        :rtype: float
+        """
+        if len(self.lo) > len(self.hi):
+            return -self.lo[0]
+        else:
+            return (self.hi[0] - self.lo[0]) / 2  # - as low has -ve values
+
+
 if __name__ == "__main__":
     obj = MedianFinder()
     obj.addNum(1)
