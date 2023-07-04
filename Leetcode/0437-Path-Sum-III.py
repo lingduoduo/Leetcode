81a0660a2ba03bf621d0bb51ee4f343185d41d6e
Ling Huang <linghypshen@gmail.com>
2023-07-04 17:00:25 -0400

    add code

--- Leetcode/0437-Path-Sum-III.py
+++ Leetcode/0437-Path-Sum-III.py
@@ -1,40 +1,6 @@
-###Definition for a binary tree node.
-###class TreeNode(object):
-###    def __init__(self, x):
-###        self.val = x
-###        self.left = None
-###        self.right = None
-
-
-# class Solution(object):
-#     def pathSum(self, root, sum):
-#         """
-#         :type root: TreeNode
-#         :type sum: int
-#         :rtype: int
-#         """
-        if not root:
-            return 0
-
-        res = self.dfs(root, sum)
-        res += self.pathSum(root.left, sum)
-        res += self.pathSum(root.right, sum)
-        return res
-
-
-    def dfs(self, root, targetSum):
-        if not root: return 0
-
-        partial = 0
-        if root.val == targetSum:
-            partial += 1
-
-        partial += self.dfs(root.left, targetSum - root.val)
-        partial += self.dfs(root.right, targetSum - root.val)
-
-        return partial
-
 from typing import Optional
+
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -42,11 +8,16 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         if not root:
             return 0
-        return self.parts(root, targetSum) + self.pathSum(root.left, targetSum) + self.pathSum(root.right, targetSum)
+        return (
+            self.parts(root, targetSum)
+            + self.pathSum(root.left, targetSum)
+            + self.pathSum(root.right, targetSum)
+        )
 
     def parts(self, root, target):
         if not root:
@@ -60,5 +31,3 @@ class Solution:
         res += self.parts(root.right, target - root.val)
 
         return res
-
-
--- Leetcode/0535-Encode-and-Decode-TinyURL.py
+++ Leetcode/0535-Encode-and-Decode-TinyURL.py
@@ -6,18 +6,18 @@ full_tiny = {}
 tiny_full = {}
 letters = string.ascii_letters + string.digits
 
+
 class Codec:
-    
     def encode(self, longUrl: str) -> str:
-        """Encodes a URL to a shortened URL.
-        """
-        def short_addr:
+        """Encodes a URL to a shortened URL."""
+
+        def short_addr():
             res = ""
             for i in range(6):
-                tmp = letters[random.randint(0,1000) % 62]
+                tmp = letters[random.randint(0, 1000) % 62]
                 res += tmp
             return res
-        
+
         if longUrl in full_tiny:
             return "http://tinyurl.com/" + full_tiny[longUrl]
         else:
@@ -25,17 +25,16 @@ class Codec:
             full_tiny[longUrl] = suffix
             tiny_full[suffix] = longUrl
         return "http://tinyurl.com/" + suffix
-    
+
     def decode(self, shortUrl: str) -> str:
-        """Decodes a shortened URL to its original URL.
-        """
+        """Decodes a shortened URL to its original URL."""
         shortUrl = shortUrl.split("/")[-1]
         if shortUrl in tiny_full:
             return tiny_full[shortUrl]
         else:
             return None
-        
-        
+
+
 ###Your Codec object will be instantiated and called as such:
 ###codec = Codec()
 ###codec.decode(codec.encode(url))
