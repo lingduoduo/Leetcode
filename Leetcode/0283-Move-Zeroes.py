6339914bd9a7e814ec1b41c1d513dafb8eff7091
Ling Huang <linghypshen@gmail.com>
2023-07-06 11:59:09 -0400

    add code

--- Leetcode/0283-Move-Zeroes.py
+++ Leetcode/0283-Move-Zeroes.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution(object):
     def moveZeroes(self, nums):
         i = 0
@@ -35,6 +37,31 @@ class Solution:
             l += 1
 
 
+import pysnooper
+
+@pysnooper.snoop()
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1: return
+        i = 0
+        j = 0
+        while j < len(nums):
+            while j < len(nums) and nums[i] != 0:
+                i += 1
+                j += 1
+            while j < len(nums) and nums[j] == 0:
+                j += 1
+            if j < len(nums):
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j += 1
+
 if __name__ == "__main__":
-    res = Solution().moveZeroes(nums=[0, 1, 0, 3, 12])
+    # res = Solution().moveZeroes(nums=[0, 1, 0, 3, 12])
+    # print(res)
+
+    res = Solution().moveZeroes(nums=[0, 0])
     print(res)
