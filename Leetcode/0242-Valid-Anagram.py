6ea53d6f695e70da8d3619ef5e1bc19ed5fdf9cd
Ling Huang <linghypshen@gmail.com>
2023-07-09 16:16:54 -0400

    add code

--- Leetcode/0242-Valid-Anagram.py
+++ Leetcode/0242-Valid-Anagram.py
@@ -8,13 +8,6 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        ###c1 = collections.Counter(s)
-        ###c2 = collections.Counter(t)
-        ###if c1 == c2:
-        ###    return True
-        ###else:
-        ###    return False
-
         d = dict()
 
         for i in range(len(s)):
@@ -33,8 +26,38 @@ class Solution(object):
         return val == [0] * len(val)
 
 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        record = [0] * 26
+        for i in s:
+            # 并不需要记住字符a的ASCII，只要求出一个相对数值就可以了
+            record[ord(i) - ord("a")] += 1
+        for i in t:
+            record[ord(i) - ord("a")] -= 1
+        for i in range(26):
+            if record[i] != 0:
+                # record数组如果有的元素不为零0，说明字符串s和t 一定是谁多了字符或者谁少了字符。
+                return False
+        return True
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        h = [0] * 26
+        for i in s:
+            h[ord(i) - ord("a")] += 1
+        for i in t:
+            h[ord(i) - ord("a")] -= 1
+        return all(i == 0 for i in h)
+
+
 if __name__ == "__main__":
     s = "anagram"
     t = "nagaram"
+    results = Solution().isAnagram(s, t)
+    print(results)
+
+    s = "rat"
+    t = "car"
     results = Solution().isAnagram(s, t)
     print(results)
