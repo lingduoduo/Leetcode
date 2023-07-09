378a80fe612a9b73a8c0b1df58b4dd3c31e1af20
Ling Huang <linghypshen@gmail.com>
2023-07-09 15:14:41 -0400

    add code

--- Leetcode/0328-Odd-Even-Linked-List.py
+++ Leetcode/0328-Odd-Even-Linked-List.py
@@ -30,3 +30,20 @@ class Solution:
         p2.next = None
         p1.next = even.next
         return odd.next
+
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        odd = head
+        evenhead = even = odd.next
+
+        while even and even.next:
+            odd.next = odd.next.next
+            odd = odd.next
+            even.next = even.next.next
+            even = even.next
+        odd.next = evenhead
+        return head
