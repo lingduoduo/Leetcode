61f153c9b6f2e361fb45a87fb798c25c79b9e367
Ling Huang <linghypshen@gmail.com>
2023-07-09 19:17:03 -0400

    add code

--- Leetcode/0383-Ransom-Note.py
+++ Leetcode/0383-Ransom-Note.py
@@ -1,9 +1,25 @@
+import collections
+
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         d_ransom = collections.Counter(ransomNote)
         d_magazine = collections.Counter(magazine)
+        return not d_ransom - d_magazine
 
-        for k, v in d_ransom:
-            if d_magazine[k] < v:
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        d = collections.Counter(magazine)
+        for cha in ransomNote:
+            if cha not in d:
                 return False
+            d[cha] -= 1
+            if d[cha] == 0:
+                del d[cha]
         return True
+
+
+if __name__ == "__main__":
+    res = Solution().canConstruct(ransomNote="aa", magazine="ab")
+    print(res)
