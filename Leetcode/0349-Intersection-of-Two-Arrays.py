a5fc7709c8d69ab43979b5d953603456ff94936b
Ling Huang <linghypshen@gmail.com>
2023-07-09 16:32:43 -0400

    add code

--- Leetcode/0349-Intersection-of-Two-Arrays.py
+++ Leetcode/0349-Intersection-of-Two-Arrays.py
@@ -1,27 +1,5 @@
 class Solution(object):
     def intersection(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        ###result = set()
-        #
-        ###l1 = len(nums1)
-        ###l2 = len(nums2)
-        #
-        ###if l1 < l2:
-        ###    for i in range(l1):
-        ###        if nums1[i] in nums2:
-        ###            result.add(nums1[i])
-        ###else:
-        ###    for i in range(l2):
-        ###        if nums2[i] in nums1:
-        ###            result.add(nums2[i])
-        ###return list(result)
-        #
-        ###return list(set(nums1) & set(nums2))
-
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
 
@@ -31,3 +9,23 @@ class Solution(object):
             if nums1[i] in ref:
                 res.add(nums1[i])
         return res
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        count1 = [0] * 1001
+        count2 = [0] * 1001
+        result = []
+        for i in range(len(nums1)):
+            count1[nums1[i]] += 1
+        for j in range(len(nums2)):
+            count2[nums2[j]] += 1
+        for k in range(1001):
+            if count1[k] * count2[k] > 0:
+                result.append(k)
+        return result
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
