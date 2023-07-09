878887726af2b6cbe0714236dd4cef84d5fc8763
Ling Huang <linghypshen@gmail.com>
2023-07-09 19:03:08 -0400

    add code

--- Leetcode/0454-4Sum-II.py
+++ Leetcode/0454-4Sum-II.py
@@ -1,3 +1,7 @@
+from typing import List
+import collections
+
+
 class Solution:
     def fourSumCount(
         self, A: List[int], B: List[int], C: List[int], D: List[int]
@@ -13,3 +17,24 @@ class Solution:
                 if -(num3 + num4) in d:
                     res += d[-(num3 + num4)]
         return res
+
+
+class Solution(object):
+    def fourSumCount(self, nums1, nums2, nums3, nums4):
+        # 使用字典存储nums1和nums2中的元素及其和
+        hashmap = dict()
+        for n1 in nums1:
+            for n2 in nums2:
+                if n1 + n2 in hashmap:
+                    hashmap[n1 + n2] += 1
+                else:
+                    hashmap[n1 + n2] = 1
+
+        # 如果 -(n1+n2) 存在于nums3和nums4, 存入结果
+        count = 0
+        for n3 in nums3:
+            for n4 in nums4:
+                key = -n3 - n4
+                if key in hashmap:
+                    count += hashmap[key]
+        return count
