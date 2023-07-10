912b0e91790ad6eb57c56e98d27ff5eb743d1a03
Ling Huang <linghypshen@gmail.com>
2023-07-10 12:53:28 -0400

    add code

--- Leetcode/0344-Reverse-String.py
+++ Leetcode/0344-Reverse-String.py
@@ -1,19 +1,24 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+
+        # 该方法已经不需要判断奇偶数，经测试后时间空间复杂度比用 for i in range(len(s)//2)更低
+        # 因为while每次循环需要进行条件判断，而range函数不需要，直接生成数字，因此时间复杂度更低。推荐使用range
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+
 class Solution(object):
     def reverseString(self, s):
         """
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        ###n = len(s)
-        ###mid = n // 2
-        #
-        ###if n % 2 == 0:
-        ###    for i in range(n // 2):
-        ###        s[mid + i], s[mid - (i + 1)] = s[mid - (i + 1)], s[mid + i]
-        ###else:
-        ###    for i in range(1, n // 2 + 1):
-        ###        s[mid + i], s[mid - i] = s[mid - i], s[mid + i]
-
         n = len(s)
 
         for i in range(n // 2):
