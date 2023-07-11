7eec0d6a8257f078c5cb476443bc46f9f6ec45e6
Ling Huang <linghypshen@gmail.com>
2023-07-11 18:32:56 -0400

    add code

--- Leetcode/0347-Top-K-Frequent-Elements.py
+++ Leetcode/0347-Top-K-Frequent-Elements.py
@@ -1,3 +1,7 @@
+import collections
+from typing import List
+
+
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
@@ -5,17 +9,6 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        import collections
-
-        ###d = collections.Counter(nums)
-        ###l = sorted(d.values())[-k:]
-
-        ###res = list()
-        ###for k, v in d.items():
-        ###    if v in l:
-        ###        res.append(k)
-        ###return res
-
         counter = collections.Counter(nums).most_common()
         print(counter)
         return [counter[i][0] for i in range(k)]
@@ -33,6 +26,46 @@ class Solution(object):
         while ksmallest:
             val, key = heapq.heappop(ksmallest)
             res.append(key)
+        return res
+
+
+import heapq
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 要统计元素出现频率
+        map_ = {}  # nums[i]:对应出现的次数
+        for i in range(len(nums)):
+            map_[nums[i]] = map_.get(nums[i], 0) + 1
+
+        # 对频率排序
+        # 定义一个小顶堆，大小为k
+        pri_que = []  # 小顶堆
+
+        # 用固定大小为k的小顶堆，扫描所有频率的数值
+        for key, freq in map_.items():
+            heapq.heappush(pri_que, (freq, key))
+            if len(pri_que) > k:  # 如果堆的大小大于了K，则队列弹出，保证堆的大小一直为k
+                heapq.heappop(pri_que)
+
+        # 找出前K个高频元素，因为小顶堆先弹出的是最小的，所以倒序来输出到数组
+        result = [0] * k
+        for i in range(k - 1, -1, -1):
+            result[i] = heapq.heappop(pri_que)[1]
+        return result
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        cnt = collections.Counter(nums)
+        stack = []
+        for i, v in cnt.items():
+            heapq.heappush(stack, (v, i))
+            if len(stack) > k:
+                heapq.heappop(stack)
+        print(stack)
+        res = [i for v, i in stack]
         return res
 
 
