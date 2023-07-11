44252e31d81fb4f319863decce8ad025c98757be
Ling Huang <linghypshen@gmail.com>
2023-07-10 23:45:32 -0400

    add code

--- Leetcode/0459-Repeated-Substring-Pattern.py
+++ Leetcode/0459-Repeated-Substring-Pattern.py
@@ -7,3 +7,20 @@ class Solution:
                 if sub * times == s:
                     return True
         return False
+
+
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        next = [0] * len(s)
+        j = -1
+        next[0] = -1
+        for i in range(1, len(s)):
+            while j >= 0 and s[i] != s[j + 1]:
+                j = next[j]
+            if s[i] == s[j + 1]:
+                j += 1
+            next[i] = j
+
+        if next[-1] != -1 and len(s) % (len(s) - (next[-1] + 1)) == 0:
+            return True
+        return False
