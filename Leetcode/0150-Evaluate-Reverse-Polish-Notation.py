6be9dfe17838f1a420063f6e01b522c87fd4f3fd
Ling Huang <linghypshen@gmail.com>
2023-07-11 13:30:19 -0400

    add code

--- Leetcode/0150-Evaluate-Reverse-Polish-Notation.py
+++ Leetcode/0150-Evaluate-Reverse-Polish-Notation.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
@@ -15,4 +18,22 @@ class Solution:
                     stack.append(int(float(num2) / num1))
             else:
                 stack.append(int(token))
+        return stack.pop()
+
+
+from operator import add, sub, mul
+
+
+class Solution:
+    op_map = {"+": add, "-": sub, "*": mul, "/": lambda x, y: int(x / y)}
+
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token not in {"+", "-", "*", "/"}:
+                stack.append(int(token))
+            else:
+                op2 = stack.pop()
+                op1 = stack.pop()
+                stack.append(self.op_map[token](op1, op2))  # 第一个出来的在运算符后面
         return stack.pop()
