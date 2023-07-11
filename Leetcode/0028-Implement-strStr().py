5fd343043ae31f15e2f34d6ecf6bfc48c2df8814
Ling Huang <linghypshen@gmail.com>
2023-07-10 21:43:38 -0400

    add code

--- Leetcode/0028-Implement-strStr().py
+++ Leetcode/0028-Implement-strStr().py
@@ -5,16 +5,33 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        # return haystack.find(needle)
-
-        ###h=len(haystack)
-        ###n=len(needle)
-        ###for i in range(h-n+1):
-        ###    if haystack[i:i+n]==needle:
-        ###        return i
-        ###return -1
-
         for i in range(len(haystack) - len(needle) + 1):
             if haystack[i : (i + len(needle))] == needle:
                 return i
+        return -1
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+
+        next = [0] * len(needle)
+        j = -1
+        next[0] = j
+        for i in range(1, len(needle)):
+            while j >= 0 and needle[i] != needle[j + 1]:
+                j = next[j]
+            if needle[i] == needle[j + 1]:
+                j += 1
+            next[i] = j
+
+        j = -1
+        for i in range(len(haystack)):
+            while j >= 0 and haystack[i] != needle[j + 1]:
+                j = next[j]
+            if haystack[i] == needle[j + 1]:
+                j += 1
+            if j == len(needle) - 1:
+                return i - len(needle) + 1
         return -1
