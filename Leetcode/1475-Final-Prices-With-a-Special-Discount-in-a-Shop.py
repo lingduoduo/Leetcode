bb09929b3cc17151909f33fe1dfc71edaa958560
Ling Huang <linghypshen@gmail.com>
2023-07-13 12:29:57 -0400

    add code

--- Leetcode/1475-Final-Prices-With-a-Special-Discount-in-a-Shop.py
+++ Leetcode/1475-Final-Prices-With-a-Special-Discount-in-a-Shop.py
@@ -25,3 +25,15 @@ class Solution:
                 res.append(p - stack[-1])
                 stack.append(p)
         return res[::-1]
+
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = [0]
+        res = prices.copy()
+        for i in range(1, len(prices)):
+            while stack and prices[stack[-1]] >= prices[i]:
+                prev = stack.pop()
+                res[prev] = prices[prev] - prices[i]
+            stack.append(i)
+        return res
