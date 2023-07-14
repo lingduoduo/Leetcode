f328a493a0c73a5db9e88ceda1c7b568788d2ccf
Ling Huang <linghypshen@gmail.com>
2023-07-14 12:20:16 -0400

    add code

--- Leetcode/0113-Path-Sum-II.py
+++ Leetcode/0113-Path-Sum-II.py
@@ -65,3 +65,19 @@ class Solution:
         self.traversal(node.left, count, path, result)
         self.traversal(node.right, count, path, result)
         path.pop()
+
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        if not root:
+            return []
+        stack = [(root, [root.val])]
+        res = []
+        while stack:
+            node, path = stack.pop()
+            if not node.left and not node.right and sum(path) == targetSum:
+                res.append(path)
+            if node.right:
+                stack.append((node.right, path + [node.right.val]))
+            if node.left:
+                stack.append((node.left, path + [node.left.val]))
+        return res
