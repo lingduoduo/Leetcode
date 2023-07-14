89bc8ec4643ec6e00bf8be2a0cc27b589d5f49ef
Ling Huang <linghypshen@gmail.com>
2023-07-14 13:36:49 -0400

    add code

--- Leetcode/0104-Maximum-Depth-of-Binary-Tree.py
+++ Leetcode/0104-Maximum-Depth-of-Binary-Tree.py
@@ -12,34 +12,6 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        ####First Try
-        ###if root is None:
-        ###	return 0
-        ###leftdepth = self.maxDepth(root.left)
-        ###rightdepth = self.maxDepth(root.right)
-        ###return max(leftdepth, rightdepth)+1
-
-        ####Second Try
-        ###if not root:
-        ###    return 0
-        ###left = 0
-        ###right = 0
-        ###if root.left:
-        ###    left = self.maxDepth(root.left)
-        ###if root.right:
-        ###    right = self.maxDepth(root.right)
-        ###return max(left, right) + 1
-
-        ###if not root:
-        ###    return 0
-        ###if not root.left and not root.right:
-        ###    return 1
-        ###if not root.left:
-        ###    return 1+self.maxDepth(root.right)
-        ###if not root.right:
-        ###    return 1+self.maxDepth(root.left)
-        ###return 1+max(self.maxDepth(root.left), self.maxDepth(root.right))
-
         if not root:
             return 0
 
