163ced6c85aa75ccd921cb713afe865fb5f508a6
Ling Huang <linghypshen@gmail.com>
2023-07-14 13:45:30 -0400

    add code

--- Leetcode/0654-Maximum-Binary-Tree.py
+++ Leetcode/0654-Maximum-Binary-Tree.py
@@ -47,6 +47,30 @@ class Solution:
         return root
 
 
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        if len(nums) == 1:
+            return TreeNode(nums[0])
+        node = TreeNode(0)
+        # 找到数组中最大的值和对应的下标
+        maxValue = 0
+        maxValueIndex = 0
+        for i in range(len(nums)):
+            if nums[i] > maxValue:
+                maxValue = nums[i]
+                maxValueIndex = i
+        node.val = maxValue
+        # 最大值所在的下标左区间 构造左子树
+        if maxValueIndex > 0:
+            new_list = nums[:maxValueIndex]
+            node.left = self.constructMaximumBinaryTree(new_list)
+        # 最大值所在的下标右区间 构造右子树
+        if maxValueIndex < len(nums) - 1:
+            new_list = nums[maxValueIndex + 1 :]
+            node.right = self.constructMaximumBinaryTree(new_list)
+        return node
+
+
 if __name__ == "__main__":
     nums = [3, 2, 1, 6, 0, 5]
     result = Solution().constructMaximumBinaryTree(nums)
