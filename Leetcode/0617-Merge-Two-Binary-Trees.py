51936054fb22663524cb78bbf2c7cf95dc11fdb9
Ling Huang <linghypshen@gmail.com>
2023-07-16 16:52:27 -0400

    add code

--- Leetcode/0617-Merge-Two-Binary-Trees.py
+++ Leetcode/0617-Merge-Two-Binary-Trees.py
@@ -47,3 +47,19 @@ class Solution:
         newT.left = self.mergeTrees(root1.left, root2.left)
         newT.right = self.mergeTrees(root1.right, root2.right)
         return newT
+
+
+class Solution:
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
+        # 递归终止条件:
+        #  但凡有一个节点为空, 就立刻返回另外一个. 如果另外一个也为None就直接返回None.
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+        # 上面的递归终止条件保证了代码执行到这里root1, root2都非空.
+        root1.val += root2.val  # 中
+        root1.left = self.mergeTrees(root1.left, root2.left)  # 左
+        root1.right = self.mergeTrees(root1.right, root2.right)  # 右
+
+        return root1
