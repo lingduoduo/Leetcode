5e4e704d05443a94449ba23ebd8bf95890915c04
Ling Huang <linghypshen@gmail.com>
2023-07-17 12:11:34 -0400

    add code

--- Leetcode/0669-Trim-a-Binary-Search-Tree.py
+++ Leetcode/0669-Trim-a-Binary-Search-Tree.py
@@ -1,9 +1,9 @@
-###Definition for a binary tree node.
-###class TreeNode(object):
-###    def __init__(self, x):
-###        self.val = x
-###        self.left = None
-###        self.right = None
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 
 class Solution(object):
@@ -23,4 +23,17 @@ class Solution(object):
 
         root.left = self.trimBST(root.left, L, R)
         root.right = self.trimBST(root.right, L, R)
+        return root
+
+
+class Solution:
+    def trimBST(self, root: TreeNode, low: int, high: int) -> TreeNode:
+        if root is None:
+            return None
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+        if root.val > high:
+            return self.trimBST(root.left, low, high)
+        root.left = self.trimBST(root.left, low, high)
+        root.right = self.trimBST(root.right, low, high)
         return root
