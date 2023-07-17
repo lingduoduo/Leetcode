5f95c7cd250d89cbb1b51bbfa31b9b0f91fc06ba
Ling Huang <linghypshen@gmail.com>
2023-07-16 23:51:41 -0400

    add code

--- Leetcode/0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
+++ Leetcode/0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
@@ -18,3 +18,25 @@ class Solution:
                 pointer = pointer.left
             else:
                 return pointer
+
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
+        return None
