51247ac4e40cd9362e5ad2936825d2b791febe55
Ling Huang <linghypshen@gmail.com>
2023-07-17 12:57:43 -0400

    add code

--- Leetcode/0108-Convert-Sorted-Array-to-Binary-Search-Tree.py
+++ Leetcode/0108-Convert-Sorted-Array-to-Binary-Search-Tree.py
@@ -1,50 +1,18 @@
-###Definition for a binary tree node.
-###class TreeNode(object):
-###    def __init__(self, x):
-###        self.val = x
-###        self.left = None
-###        self.right = None
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 
-class Solution(object):
-    def sortedArrayToBST(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: TreeNode
-        """
-        ###if not nums: return None
-        #
-        ######if len(nums)==1: return TreeNode(nums[0])
-        ###mid = len(nums) // 2
-        ###root = TreeNode(nums[mid])
-        ###left = nums[:mid]
-        ###right = nums[mid + 1:]
-        ###if len(left) >= 1:
-        ###    root.left = self.sortedArrayToBST(left)
-        ###if len(right) >= 1:
-        ###    root.right = self.sortedArrayToBST(right)
-        ###return root
-
-        ###    if not nums:
-        ###        return None
-
-        ###    return self.build(nums, 0, len(nums)-1)
-
-        ###def build(self, arr, left, right):
-        ###    if left>right:
-        ###        return None
-        ###    mid = left + (right - left)//2
-        ###    print(mid)
-        ###    root = TreeNode(arr[mid])
-        ###    root.left = self.build(arr, left, mid-1)
-        ###    root.right = self.build(arr, mid+1, right)
-        ###    return root
-        if not nums:
-            return
-
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if len(nums) == 0:
+            return None
         left = 0
         right = len(nums) - 1
-        mid = left + (right - mid) // 2
+        mid = left + (right - left) // 2
         root = TreeNode(nums[mid])
         root.left = self.sortedArrayToBST(nums[:mid])
         root.right = self.sortedArrayToBST(nums[mid + 1 :])
--- Leetcode/0538-Convert-BST-to-Greater-Tree.py
+++ Leetcode/0538-Convert-BST-to-Greater-Tree.py
@@ -1,3 +1,11 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def convertBST(self, root: TreeNode) -> TreeNode:
         self.res = []
@@ -25,3 +33,44 @@ class Solution:
             self.inOrder(root.left, True)
             root.val = self.res.pop(0)
             self.inOrder(root.right, True)
+
+
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        stack = []
+
+        def traverse(root):
+            nonlocal stack
+            if not root:
+                return
+            if root.left:
+                traverse(root.left)
+            stack.append(root)
+            if root.right:
+                traverse(root.right)
+
+        traverse(root)
+        prev = 0
+        while stack:
+            node = stack.pop()
+            prev += node.val
+            node.val = prev
+        return root
+
+
+class Solution:
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        self.pre = 0  # 记录前一个节点的数值
+        self.traversal(root)
+        return root
+
+    def traversal(self, cur):
+        if cur is None:
+            return
+        self.traversal(cur.right)
+        cur.val += self.pre
+        self.pre = cur.val
+        self.traversal(cur.left)
