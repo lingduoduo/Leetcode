7e4e00176ee6a42c438b62e7c7344558887aac37
Ling Huang <linghypshen@gmail.com>
2023-07-16 23:32:40 -0400

    add code

--- Leetcode/0236-Lowest-Common-Ancestor-of-a-Binary-Tree.py
+++ Leetcode/0236-Lowest-Common-Ancestor-of-a-Binary-Tree.py
@@ -17,3 +17,29 @@ class Solution:
         if left and right:
             return root
         return left if left else right
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        parents = {root.val: None}
+        stack = [root]
+
+        while stack:
+            node = stack.pop(0)
+            if node.left:
+                parents[node.left.val] = node
+                stack.append(node.left)
+            if node.right:
+                parents[node.right.val] = node
+                stack.append(node.right)
+        ancestors = set()
+        if p.val not in parents or q.val not in parents:
+            return None
+        while p:
+            ancestors.add(p.val)
+            p = parents[p.val]
+        while q and q.val not in ancestors:
+            q = parents[q.val]
+        return q
