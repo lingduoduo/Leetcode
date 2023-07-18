ea07529c48e732b85a44dd9dec709a98a37eea35
Ling Huang <linghypshen@gmail.com>
2023-07-18 10:18:42 -0400

    add code

--- Leetcode/0017-Letter-Combinations-of-a-Phone-Number.py
+++ Leetcode/0017-Letter-Combinations-of-a-Phone-Number.py
@@ -102,6 +102,68 @@ class Solution:
         return res
 
 
+class Solution(object):
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+
+        d = dict()
+        d[0] = "0"
+        d[1] = "1"
+        d[2] = "abc"
+        d[3] = "def"
+        d[4] = "ghi"
+        d[5] = "jkl"
+        d[6] = "mno"
+        d[7] = "pqrs"
+        d[8] = "tuv"
+        d[9] = "wxyz"
+
+        res = [""]
+        if not digits:
+            return res
+
+        for cha in digits:
+            cur = res
+            part = []
+            for strs in cur:
+                part += [strs + cha for cha in d[int(cha)]]
+            res = part
+        return res
+
+
+class Solution:
+    def letterCombinations(digits):
+        letterMap = [
+            "",  # 0
+            "",  # 1
+            "abc",  # 2
+            "def",  # 3
+            "ghi",  # 4
+            "jkl",  # 5
+            "mno",  # 6
+            "pqrs",  # 7
+            "tuv",  # 8
+            "wxyz",  # 9
+        ]
+
+        def getCombinations(self, digits, index, s):
+            if index == len(digits):
+                result.append(s)
+                return
+            digit = int(digits[index])
+            letters = letterMap[digit]
+            for letter in letters:
+                self.getCombinations(digits, index + 1, s + letter)
+
+        result = []
+        if len(digits) == 0:
+            return result
+
+        getCombinations(digits, 0, "")
+        return result
+
+
 if __name__ == "__main__":
     digits = "23"
     result = Solution().letterCombinations(digits)
