7ce4713b9af55db8d49d207a5a17077612e4cbbe
Ling Huang <linghypshen@gmail.com>
2023-07-17 22:39:44 -0400

    add code

--- Leetcode/0216-Combination-Sum-III.py
+++ Leetcode/0216-Combination-Sum-III.py
@@ -24,6 +24,22 @@ class Solution(object):
         return
 
 
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        def backtracking(idx, cnt, tot, path):
+            nonlocal res
+            if cnt == k and tot == n:
+                res.append(path)
+                return
+
+            for i in range(idx, 10):
+                backtracking(i + 1, cnt + 1, tot + i, path + [i])
+
+        res = []
+        backtracking(1, 0, 0, [])
+        return res
+
+
 if __name__ == "__main__":
     k = 3
     n = 7
