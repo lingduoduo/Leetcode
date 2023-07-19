e26b9b75b54881c111df8768c77ece8f3de40608
Ling Huang <linghypshen@gmail.com>
2023-07-19 17:54:53 -0400

    add code

--- Leetcode/0376-Wiggle-Subsequence.py
+++ Leetcode/0376-Wiggle-Subsequence.py
@@ -35,6 +35,22 @@ class Solution:
         return res
 
 
+class Solution:
+    def wiggleMaxLength(self, nums):
+        if len(nums) <= 1:
+            return len(nums)  # 如果数组长度为0或1，则返回数组长度
+        curDiff = 0  # 当前一对元素的差值
+        preDiff = 0  # 前一对元素的差值
+        result = 1  # 记录峰值的个数，初始为1（默认最右边的元素被视为峰值）
+        for i in range(len(nums) - 1):
+            curDiff = nums[i + 1] - nums[i]  # 计算下一个元素与当前元素的差值
+            # 如果遇到一个峰值
+            if (preDiff <= 0 and curDiff > 0) or (preDiff >= 0 and curDiff < 0):
+                result += 1  # 峰值个数加1
+                preDiff = curDiff  # 注意这里，只在摆动变化的时候更新preDiff
+        return result  # 返回最长摆动子序列的长度
+
+
 if __name__ == "__main__":
     nums = [1, 17, 5, 10, 13, 15, 10, 5, 16, 8]
     res = Solution().wiggleMaxLength(nums)
