98974a40e4e57d4eec5b06d1105ea241ae909e53
Ling Huang <linghypshen@gmail.com>
2023-07-19 17:31:41 -0400

    add code

--- Leetcode/0455-Assign-Cookies.py
+++ Leetcode/0455-Assign-Cookies.py
@@ -13,6 +13,19 @@ class Solution:
         return result
 
 
+class Solution:
+    def findContentChildren(self, g, s):
+        g.sort()  # 将孩子的贪心因子排序
+        s.sort()  # 将饼干的尺寸排序
+        index = len(s) - 1  # 饼干数组的下标，从最后一个饼干开始
+        result = 0  # 满足孩子的数量
+        for i in range(len(g) - 1, -1, -1):  # 遍历胃口，从最后一个孩子开始
+            if index >= 0 and s[index] >= g[i]:  # 遍历饼干
+                result += 1
+                index -= 1
+        return result
+
+
 if __name__ == "__main__":
     # g = [1, 2, 3]
     # s = [1, 1]
