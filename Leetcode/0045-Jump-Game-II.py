71ea05bdb2b6b4042ef0d361f72d5e180b98008f
Ling Huang <linghypshen@gmail.com>
2023-07-20 16:52:21 -0400

    add code

--- Leetcode/0045-Jump-Game-II.py
+++ Leetcode/0045-Jump-Game-II.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def jump(self, nums) -> int:
         if not nums or len(nums) < 2:
@@ -12,6 +15,19 @@ class Solution:
                 res += 1
                 cur = maxreach
         return res
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        result = [float("inf")] * len(nums)  # 初始化结果数组，初始值为一个较大的数
+        result[0] = 0  # 起始位置的步数为0
+
+        for i in range(len(nums)):  # 遍历数组
+            for j in range(nums[i] + 1):  # 在当前位置能够跳跃的范围内遍历
+                if i + j < len(nums):  # 确保下一跳的位置不超过数组范围
+                    result[i + j] = min(result[i + j], result[i] + 1)  # 更新到达下一跳位置的最小步数
+
+        return result[-1]  # 返回到达最后一个位置的最小步数
 
 
 if __name__ == "__main__":
