564b16cdf45db02394ecac01be9062c3c8f391d3
Ling Huang <linghypshen@gmail.com>
2023-07-19 22:33:24 -0400

    add code

--- Leetcode/0122-Best-Time-to-Buy-and-Sell-Stock-II.py
+++ Leetcode/0122-Best-Time-to-Buy-and-Sell-Stock-II.py
@@ -13,3 +13,11 @@ class Solution:
                 total += prices[i] - prices[i - 1]
 
         return total
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        result = 0
+        for i in range(1, len(prices)):
+            result += max(prices[i] - prices[i - 1], 0)
+        return result
