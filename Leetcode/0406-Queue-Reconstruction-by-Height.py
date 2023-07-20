9a2e290c8763167ee2a76d336ccef451eef61ec8
Ling Huang <linghypshen@gmail.com>
2023-07-20 19:22:33 -0400

    add code

--- Leetcode/0406-Queue-Reconstruction-by-Height.py
+++ Leetcode/0406-Queue-Reconstruction-by-Height.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         d = {}
@@ -22,4 +25,15 @@ class Solution:
         res = []
         for p in people:
             res.insert(p[1], p)
+        return res
+
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key=lambda x: (x[0], -x[1]), reverse=True)
+
+        res = []
+        for p in people:
+            h, idx = p
+            res = res[:idx] + [p] + res[idx:]
         return res
--- Leetcode/0860-Lemonade-Change.py
+++ Leetcode/0860-Lemonade-Change.py
@@ -23,6 +23,38 @@ class Solution:
                     return False
         return True
 
+        five = 0
+        ten = 0
+        twenty = 0
+
+        for bill in bills:
+            # 情况一：收到5美元
+            if bill == 5:
+                five += 1
+
+            # 情况二：收到10美元
+            if bill == 10:
+                if five <= 0:
+                    return False
+                ten += 1
+                five -= 1
+
+            # 情况三：收到20美元
+            if bill == 20:
+                # 先尝试使用10美元和5美元找零
+                if five > 0 and ten > 0:
+                    five -= 1
+                    ten -= 1
+                    # twenty += 1
+                # 如果无法使用10美元找零，则尝试使用三张5美元找零
+                elif five >= 3:
+                    five -= 3
+                    # twenty += 1
+                else:
+                    return False
+
+        return True
+
 
 if __name__ == "__main__":
     bills = [5, 5, 5, 10, 20]
