53ce9276f51ed9a61026d9ee8ff09fda1ec7bf06
Ling Huang <linghypshen@gmail.com>
2023-07-21 09:14:46 -0400

    add code

--- Leetcode/0738-Monotone-Increasing-Digits.py
+++ Leetcode/0738-Monotone-Increasing-Digits.py
@@ -22,21 +22,15 @@ class Solution:
 
 
 class Solution:
-    def monotoneIncreasingDigits(self, N: int) -> int:
-        # 将整数转换为字符串
-        strNum = list(str(N))
+    def monotoneIncreasingDigits(self, n: int) -> int:
+        strs = list(str(n))
 
-        # 从右往左遍历字符串
-        for i in range(len(strNum) - 1, 0, -1):
-            # 如果当前字符比前一个字符小，说明需要修改前一个字符
-            if strNum[i - 1] > strNum[i]:
-                strNum[i - 1] = str(int(strNum[i - 1]) - 1)  # 将前一个字符减1
-                # 将修改位置后面的字符都设置为9，因为修改前一个字符可能破坏了递增性质
-                for j in range(i, len(strNum)):
-                    strNum[j] = "9"
-
-        # 将列表转换为字符串，并将字符串转换为整数并返回
-        return int("".join(strNum))
+        for i in range(len(strs) - 1, 0, -1):
+            if strs[i - 1] > strs[i]:
+                strs[i - 1] = str(int(strs[i - 1]) - 1)
+                for j in range(i, len(strs)):
+                    strs[j] = "9"
+        return int("".join(strs))
 
 
 if __name__ == "__main__":
