3ae7d2e3f64c442b4bfc03dca0a6195513b3156d
Ling Huang <linghypshen@gmail.com>
2023-07-21 07:37:51 -0400

    add code

--- Leetcode/0402-Remove-K-Digits.py
+++ Leetcode/0402-Remove-K-Digits.py
@@ -15,6 +15,18 @@ class Solution:
         return "".join(stack).lstrip("0") or "0"
 
 
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = [num[0]]
+        for cha in num[1:]:
+            while stack and stack[-1] > cha and k >= 1:
+                stack.pop()
+                k -= 1
+            stack.append(cha)
+        res = "".join(stack[:-k] if k else stack)
+        return res.lstrip("0") or "0"
+
+
 if __name__ == "__main__":
     num = "1432219"
     k = 3
