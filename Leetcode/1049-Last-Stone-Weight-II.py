93a2dd1a383f818de5f8ca08bfe5d147d2dece91
Ling Huang <linghypshen@gmail.com>
2023-07-24 14:18:49 -0400

    add code

--- Leetcode/1049-Last-Stone-Weight-II.py
+++ Leetcode/1049-Last-Stone-Weight-II.py
@@ -18,3 +18,14 @@ class Solution:
             if dp[psum] and 2 * psum - s >= 0:
                 res = min(res, 2 * psum - s)
         return res
+
+
+class Solution:
+    def lastStoneWeightII(self, stones):
+        s = sum(stones)
+        target = s // 2
+        dp = [0] * (target + 1)
+        for stone in stones:
+            for j in range(target, stone - 1, -1):
+                dp[j] = max(dp[j], dp[j - stone] + stone)
+        return s - 2 * dp[-1]
