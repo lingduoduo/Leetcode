631c6a02cc6f0a95f7486004e877df643ff8325b
Ling Huang <linghypshen@gmail.com>
2023-07-24 16:27:58 -0400

    add code

--- Leetcode/0518-Coin-Change-2.py
+++ Leetcode/0518-Coin-Change-2.py
@@ -35,6 +35,18 @@ class Solution:
         return dp[0][amount]
 
 
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        # 遍历物品
+        for i in range(len(coins)):
+            # 遍历背包
+            for j in range(coins[i], amount + 1):
+                dp[j] += dp[j - coins[i]]
+        return dp[-1]
+
+
 if __name__ == "__main__":
     # amount = 5
     # coins = [1, 2, 5]
