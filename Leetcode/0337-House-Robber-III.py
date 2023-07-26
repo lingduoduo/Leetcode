d5a1846e0f63ae5342744acd7c9a00ebef1f6df4
Ling Huang <linghypshen@gmail.com>
2023-07-25 23:47:16 -0400

    add code

--- Leetcode/0337-House-Robber-III.py
+++ Leetcode/0337-House-Robber-III.py
@@ -72,3 +72,30 @@ class Solution:
             return [rob, not_rob]
 
         return max(helper(root))
+
+
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        # dp数组（dp table）以及下标的含义：
+        # 1. 下标为 0 记录 **不偷该节点** 所得到的的最大金钱
+        # 2. 下标为 1 记录 **偷该节点** 所得到的的最大金钱
+        def traversal(node):
+            # 递归终止条件，就是遇到了空节点，那肯定是不偷的
+            if not node:
+                return (0, 0)
+
+            left = traversal(node.left)
+            right = traversal(node.right)
+
+            # 不偷当前节点, 偷子节点
+            val_0 = max(left) + max(right)
+
+            # 偷当前节点, 不偷子节点
+            val_1 = node.val + left[0] + right[0]
+
+            return (val_0, val_1)
+
+        dp = traversal(root)
+        return max(dp)
+
+    # 要用后序遍历, 因为要通过递归函数的返回值来做下一步计算
