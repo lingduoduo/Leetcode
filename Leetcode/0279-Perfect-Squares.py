e8a71895f4f52e1085caf191977bab8103908c7e
Ling Huang <linghypshen@gmail.com>
2023-07-25 21:11:53 -0400

    add code

--- Leetcode/0279-Perfect-Squares.py
+++ Leetcode/0279-Perfect-Squares.py
@@ -39,3 +39,16 @@ class Solution:
                     i += 1
             count += 1
             q = deque(_q)
+
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [float("inf")] * (n + 1)
+        dp[0] = 0
+
+        for i in range(1, n + 1):  # 遍历背包
+            for j in range(1, int(i**0.5) + 1):  # 遍历物品
+                # 更新凑成数字 i 所需的最少完全平方数数量
+                dp[i] = min(dp[i], dp[i - j * j] + 1)
+
+        return dp[n]
