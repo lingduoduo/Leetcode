726398a6cb8b4b4091a0b8463fecfe6b0df62ce9
Ling Huang <linghypshen@gmail.com>
2023-07-27 12:57:05 -0400

    add code

--- Leetcode/0123-Best-Time-to-Buy-and-Sell-Stock-III.py
+++ Leetcode/0123-Best-Time-to-Buy-and-Sell-Stock-III.py
@@ -21,11 +21,6 @@ class Solution:
         if not prices:
             return 0
 
-        # buy1[i] = max(buy1[i - 1], -prices[i])
-        # sell1[i] = max(sell1[i - 1], buy1[i] + prices[i])
-        # buy2[i] = max(buy2[i - 1], sell1[i - 1] - prices[i])
-        # sell2[i] = max(sell2[i - 1], buy2[i] + prices[i])
-
         sell1 = sell2 = 0
         buy1 = buy2 = -float("inf")
         for p in prices:
@@ -34,3 +29,17 @@ class Solution:
             buy2 = max(buy2, sell1 - p)
             sell2 = max(sell2, buy2 + p)
         return sell2
+
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 0:
+            return 0
+        dp = [[0] * 5 for _ in range(len(prices))]
+        dp[0][1] = -prices[0]
+        dp[0][3] = -prices[0]
+        for i in range(1, len(prices)):
+            dp[i][0] = dp[i - 1][0]
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+            dp[i][2] = max(dp[i - 1][2], dp[i - 1][1] + prices[i])
+            dp[i][3] = max(dp[i - 1][3], dp[i - 1][2] - prices[i])
+            dp[i][4] = max(dp[i - 1][4], dp[i - 1][3] + prices[i])
+        return dp[-1][4]
--- Leetcode/0188-Best-Time-to-Buy-and-Sell-Stock-IV.py
+++ Leetcode/0188-Best-Time-to-Buy-and-Sell-Stock-IV.py
@@ -49,3 +49,17 @@ class Solution:
                 dp[i][0] = max(dp[i][0], dp[i - 1][1] - price)
                 dp[i][1] = max(dp[i][1], price + dp[i][0])
         return dp[-1][1]
+
+
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        if len(prices) == 0:
+            return 0
+        dp = [[0] * (2 * k + 1) for _ in range(len(prices))]
+        for j in range(1, 2 * k, 2):
+            dp[0][j] = -prices[0]
+        for i in range(1, len(prices)):
+            for j in range(0, 2 * k - 1, 2):
+                dp[i][j + 1] = max(dp[i - 1][j + 1], dp[i - 1][j] - prices[i])
+                dp[i][j + 2] = max(dp[i - 1][j + 2], dp[i - 1][j + 1] + prices[i])
+        return dp[-1][2 * k]
