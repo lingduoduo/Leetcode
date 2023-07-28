d8cc8add1a459b2427fdadf2b73199072544a298
Ling Huang <linghypshen@gmail.com>
2023-07-28 19:43:11 -0400

    add code

--- Leetcode/0005-Longest-Palindromic-Substring.py
+++ Leetcode/0005-Longest-Palindromic-Substring.py
@@ -25,27 +25,20 @@ class Solution:
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        res = [0, 0]
-
-        for i in range(n):
-            dp[i][i] = True
-
-        for i in range(n - 1):
-            if s[i] == s[i + 1]:
-                dp[i][i + 1] = True
-                res = [i, i + 1]
-
-        for diff in range(2, n):
-            for i in range(n - diff):
-                j = i + diff
-                if s[i] == s[j] and dp[i + 1][j - 1]:
-                    dp[i][j] = True
-                    res = [i, j]
-
-        i, j = res
-        return s[i : j + 1]
+        dp = [[False] * len(s) for _ in range(len(s))]
+        maxlenth = 0
+        left = 0
+        right = 0
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i, len(s)):
+                if s[j] == s[i]:
+                    if j - i <= 1 or dp[i + 1][j - 1]:
+                        dp[i][j] = True
+                if dp[i][j] and j - i + 1 > maxlenth:
+                    maxlenth = j - i + 1
+                    left = i
+                    right = j
+        return s[left : right + 1]
 
 
 if __name__ == "__main__":
