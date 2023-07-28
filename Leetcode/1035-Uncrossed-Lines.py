920261e16e6af6a6f7ebe48e0ca6de368f6464ff
Ling Huang <linghypshen@gmail.com>
2023-07-28 17:29:13 -0400

    add code

--- Leetcode/1035-Uncrossed-Lines.py
+++ Leetcode/1035-Uncrossed-Lines.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def maxUncrossedLines(self, A: List[int], B: List[int]) -> int:
         dp = [[0 for _ in range(len(B))] for _ in range(len(A))]
@@ -9,4 +12,16 @@ class Solution:
                         dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1)
                     else:
                         dp[i][j] = 1
+        return dp[-1][-1]
+
+
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
+        for i in range(1, len(nums1) + 1):
+            for j in range(1, len(nums2) + 1):
+                if nums1[i - 1] == nums2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
         return dp[-1][-1]
