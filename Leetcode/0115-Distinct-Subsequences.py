54a36deb1665fadee9c4d9bcbb7888fe69e509a5
Ling Huang <linghypshen@gmail.com>
2023-07-31 14:20:48 -0400

    add code

--- Leetcode/0115-Distinct-Subsequences.py
+++ Leetcode/0115-Distinct-Subsequences.py
@@ -27,31 +27,6 @@ class Solution:
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         m, n = len(s), len(t)
-        # Dynamic Programming table
-        dp = [[0 for i in range(n + 1)] for j in range(m + 1)]
-        # Base case initialization
-        for j in range(n + 1):
-            dp[m][j] = 0
-        # Base case initialization
-        for i in range(m + 1):
-            dp[i][n] = 1
-        # Iterate over the strings in reverse so as to
-        # satisfy the way we've modeled our recursive solution
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                # Remember, we always need this result
-                dp[i][j] = dp[i + 1][j]
-                # If the characters match, we add the
-                # result of the next recursion call (in this
-                # case, the value of a cell in the dp table
-                if s[i] == t[j]:
-                    dp[i][j] += dp[i + 1][j + 1]
-        return dp[0][0]
-
-
-class Solution:
-    def numDistinct(self, s: str, t: str) -> int:
-        m, n = len(s), len(t)
         dp = [[0] * (m + 1) for _ in range(n + 1)]
         for j in range(m + 1):
             dp[0][j] = 1
