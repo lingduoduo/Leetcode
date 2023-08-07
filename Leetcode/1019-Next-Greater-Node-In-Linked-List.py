8b28d034f26e62684c07817e8b4f072c8183a6c2
Ling Huang <linghypshen@gmail.com>
2023-08-07 17:26:07 -0400

    add code

--- Leetcode/1019-Next-Greater-Node-In-Linked-List.py
+++ Leetcode/1019-Next-Greater-Node-In-Linked-List.py
@@ -1,8 +1,13 @@
 ###Definition for singly-linked list.
-###class ListNode:
-###    def __init__(self, val=0, next=None):
-###        self.val = val
-###        self.next = next
+from typing import List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def nextLargerNodes(self, head: ListNode) -> List[int]:
         nums = []
@@ -16,3 +21,24 @@ class Solution:
                 res[stack.pop()] = n
             stack.append(i)
         return res
+
+
+class Solution:
+    def nextLargerNodes(self, head: ListNode) -> List[int]:
+        answer, stack = [], []
+        # We use an integer 'cnt' to represent the index.
+        cnt = 0
+
+        while head:
+            # Set the next greate value of the current value 'head.val' as 0 by default.
+            answer.append(0)
+            while stack and head.val > stack[-1][1]:
+                curr_id, val = stack.pop()
+                answer[curr_id] = head.val
+
+            # Add both the index and the value to stack.
+            stack.append([cnt, head.val])
+            cnt += 1
+            head = head.next
+
+        return answer
---
+++ Leetcode/1425-Constrained-Subsequence-Sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
+        deque = []
+        for i, num in enumerate(nums):
+            while (
+                deque and deque[0] < i - k
+            ):  # delete that didn't end with a number in A[i-k:i]
+                deque.pop(0)
+
+            if deque:  # compute the max sum we can get at index i
+                nums[i] = nums[deque[0]] + num
+
+            while deque and nums[deque[-1]] < nums[i]:
+                # delet all the sequence that smaller than current sum, becaus there will never be
+                # considers ==> smaller than current sequence, and end before current sequence
+                deque.pop()
+
+            if nums[i] > 0:  # if nums[i] < 0, it can't be a useful prefix sum
+                deque.append(i)
+
+        return max(nums)
