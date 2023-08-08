619df6cb5f660ec2a2906e473fcafd2a09ea7a74
Ling Huang <linghypshen@gmail.com>
2023-08-08 16:03:07 -0400

    add code

--- Leetcode/1695-Maximum-Erasure-Value.py
+++ Leetcode/1695-Maximum-Erasure-Value.py
@@ -17,3 +17,23 @@ class Solution:
             seen.add(x)
             res = max(res, tot)
         return res
+
+
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        res = 0
+        q = deque()
+        seen = set()  # Using a set for O(1) lookups
+        current_sum = 0
+
+        for num in nums:
+            while num in seen:
+                left_num = q.popleft()
+                current_sum -= left_num
+                seen.remove(left_num)
+            q.append(num)
+            seen.add(num)
+            current_sum += num
+            res = max(res, current_sum)
+
+        return res
