f3d8b3e02ac7c8796fff112d31900ee438a49f4a
Ling Huang <linghypshen@gmail.com>
2023-08-07 21:40:25 -0400

    add code

--- Leetcode/1696-Jump-Game-VI.py
+++ Leetcode/1696-Jump-Game-VI.py
@@ -1,28 +1,14 @@
 from typing import List
 import heapq
-
-
-class Solution:
-    def maxResult(self, nums: List[int], k: int) -> int:
-        if not nums:
-            return 0
-
-        dp = [float(-inf)] * len(nums)
-        dp[0] = nums[0]
-
-        for i in range(len(nums)):
-            for j in range(max(i - k, 0), i):
-                dp[i] = max(dp[j] + nums[i], dp[i])
-
-        return dp[-1]
-
+import collections
+from collections import deque
 
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
         if not nums:
             return 0
 
-        dp = [float(-inf)] * len(nums)
+        dp = [float("-inf")] * len(nums)
         dp[0] = nums[0]
         max_heap = []
         heapq.heappush(max_heap, (-nums[0], 0))
@@ -53,3 +39,20 @@ class Solution:
             dq.append(i)
 
         return dp[-1]
+
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        score = nums[0]
+        dq = deque()
+        dq.append((0, score))
+        for i in range(1, n):
+            # pop the old index
+            while dq and dq[0][0] < i-k:
+                dq.popleft()
+            score = dq[0][1] + nums[i]
+            # pop the smaller value
+            while dq and score >= dq[-1][1]:
+                dq.pop()
+            dq.append((i, score))
+        return score
