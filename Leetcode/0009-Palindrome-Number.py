82052b786344fdc09dd1dc4a65e0f978879866f8
Ling Huang <linghypshen@gmail.com>
2023-08-09 14:10:43 -0400

    add code

--- Leetcode/0009-Palindrome-Number.py
+++ Leetcode/0009-Palindrome-Number.py
@@ -4,32 +4,6 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        ####First try
-        ###if x<0 or (x%10 == 0 and x!=0):
-        ###    return False
-        ###if x in range(10):
-        ###    return True
-
-        ###revx = 0
-        ###i = x
-
-        ###while i>0:
-        ###    revx = revx*10 + i%10
-        ###    i=int(i/10)
-
-        ###if revx == x:
-        ###    return True
-        ###else:
-        ###    return False
-
-        ####Second try
-        ###if x<0:
-        ###    return False
-        ###else:
-        ###    num = str(x)
-        ###    return num[::-1]==num
-
-        ###third try
         input = x
         if x < 0:
             return False
