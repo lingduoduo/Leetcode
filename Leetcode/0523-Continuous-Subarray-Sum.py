d3afa49dd163d975d3fac52f0ca6b6edaf8646c4
Ling Huang <linghypshen@gmail.com>
2023-08-10 12:50:17 -0400

    add code

--- Leetcode/0523-Continuous-Subarray-Sum.py
+++ Leetcode/0523-Continuous-Subarray-Sum.py
@@ -1,4 +1,5 @@
 import collections
+from typing import List
 
 
 class Solution:
@@ -26,7 +27,6 @@ class Solution:
         d = collections.defaultdict(int)
         tot = 0
         for idx, num in enumerate(nums):
-            print(d)
             tot += num
             m = tot if k == 0 else tot % k
             if m in d and idx - d[m] >= 1:
@@ -36,32 +36,28 @@ class Solution:
 
 
 class Solution:
-    def checkSubarraySum(self, nums, k) -> bool:
-        d = collections.defaultdict()
-        d[0] = -1
-        tot = 0
-        for i, num in enumerate(nums):
-            print(d)
-            tot += num
-            m = tot if k == 0 else tot % k
-            if not m in d:
-                d[m] = i
-            elif i - d[m] > 1:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        hash_map = {0: 0}
+        s = 0
+        for i in range(len(nums)):
+            s += nums[i]
+            if s % k not in hash_map:
+                hash_map[s % k] = i + 1
+            elif hash_map[s % k] < i:
                 return True
         return False
 
 
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        hash_map = {0: 0}
-        s = 0
-        for i in range(len(nums)):
-            s += nums[i]
-            # if the remainder s % k occurs for the first time
-            if s % k not in hash_map:
-                hash_map[s % k] = i + 1
-            # if the subarray size is at least two
-            elif hash_map[s % k] < i:
+        presum = 0
+        d = collections.Counter()
+        d[0] = 0
+        for i, num in enumerate(nums):
+            presum += num
+            if presum % k not in d:
+                d[presum % k] = i + 1
+            elif d[presum % k] < i:
                 return True
         return False
 
