1177f8b02a2d78ed7eaa95416ddb575d53aa95a9
Ling Huang <linghypshen@gmail.com>
2023-08-20 10:27:40 -0400

    add code

--- Leetcode/1650-Lowest-Common-Ancestor-of-a-Binary-Tree-III.py
+++ Leetcode/1650-Lowest-Common-Ancestor-of-a-Binary-Tree-III.py
@@ -9,3 +9,12 @@ class Solution:
                 return q
             else:
                 q = q.parent
+
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        p1, p2 = p, q
+        while p1 != p2:
+            p1 = p1.parent if p1.parent else q
+            p2 = p2.parent if p2.parent else p
+        return p1
