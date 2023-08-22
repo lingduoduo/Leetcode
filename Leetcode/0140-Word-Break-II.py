7d9aca19afaf64ff2e022587d68908da163cb5d0
Ling Huang <linghypshen@gmail.com>
2023-08-21 22:46:18 -0400

    add code

--- Leetcode/0140-Word-Break-II.py
+++ Leetcode/0140-Word-Break-II.py
@@ -52,25 +52,14 @@ class Solution:
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        # quick check on the characters,
-        #   otherwise it would exceed the time limit for certain test cases.
-        if set(Counter(s).keys()) > set(Counter("".join(wordDict)).keys()):
-            return []
-
         wordSet = set(wordDict)
-
         dp = [[]] * (len(s) + 1)
         dp[0] = [""]
-
-        for endIndex in range(1, len(s) + 1):
+        for j in range(1, len(s) + 1):
             sublist = []
-            # fill up the values in the dp array.
-            for startIndex in range(0, endIndex):
-                word = s[startIndex:endIndex]
-                if word in wordSet:
-                    for subsentence in dp[startIndex]:
-                        sublist.append((subsentence + " " + word).strip())
-
-            dp[endIndex] = sublist
-
-        return dp[len(s)]
+            for i in range(j):
+                if s[i:j] in wordSet:
+                    for words in dp[i]:
+                        sublist.append((words + " " + s[i:j]).strip())
+            dp[j] = sublist
+        return dp[-1]
