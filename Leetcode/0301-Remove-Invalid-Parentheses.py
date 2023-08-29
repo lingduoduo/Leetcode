a3e997f2e3b42d41bcaf356e3da6925b21319ef8
Ling Huang <linghypshen@gmail.com>
2023-08-29 18:42:07 -0400

    add code

--- Leetcode/0301-Remove-Invalid-Parentheses.py
+++ Leetcode/0301-Remove-Invalid-Parentheses.py
@@ -85,3 +85,101 @@ class Solution:
         # right parentheses and that greatly helps pruning the recursion.
         recurse(s, 0, 0, 0, left, right, [])
         return list(result.keys())
+
+
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        res = []
+        lefts_to_remove, rights_to_remove = 0, 0
+        lefts, rights = 0, 0
+        for i in range(len(s)):
+            if s[i] == "(":
+                lefts += 1
+            elif s[i] == ")":
+                if lefts > 0:
+                    lefts -= 1
+                else:
+                    rights_to_remove += (
+                        1  # if right doesn't have a matching left, it should be removed
+                    )
+        lefts_to_remove = (
+            lefts  # if we have more lefts than rights, extra lefts should be removed
+        )
+
+        self.backtracking(0, 0, s, 0, "", res, lefts_to_remove, rights_to_remove)
+        if not res:
+            res.append("")
+
+        return res
+
+    def backtracking(
+        self, lefts, rights, s, ind, cur_str, res, lefts_to_remove, rights_to_remove
+    ):
+        if ind == len(s):
+            if (
+                lefts == rights
+                and lefts_to_remove == 0
+                and rights_to_remove == 0
+                and cur_str not in res
+            ):
+                res.append(cur_str)
+            return
+
+        if s[ind] == "(":
+            if lefts_to_remove > 0:
+                self.backtracking(
+                    lefts,
+                    rights,
+                    s,
+                    ind + 1,
+                    cur_str,
+                    res,
+                    lefts_to_remove - 1,
+                    rights_to_remove,
+                )
+            self.backtracking(
+                lefts + 1,
+                rights,
+                s,
+                ind + 1,
+                cur_str + "(",
+                res,
+                lefts_to_remove,
+                rights_to_remove,
+            )
+
+        elif s[ind] == ")":
+            if (lefts == 0 or lefts >= rights) and rights_to_remove > 0:
+                self.backtracking(
+                    lefts,
+                    rights,
+                    s,
+                    ind + 1,
+                    cur_str,
+                    res,
+                    lefts_to_remove,
+                    rights_to_remove - 1,
+                )
+            if lefts > rights:
+                self.backtracking(
+                    lefts,
+                    rights + 1,
+                    s,
+                    ind + 1,
+                    cur_str + ")",
+                    res,
+                    lefts_to_remove,
+                    rights_to_remove,
+                )
+
+        else:
+            self.backtracking(
+                lefts,
+                rights,
+                s,
+                ind + 1,
+                cur_str + s[ind],
+                res,
+                lefts_to_remove,
+                rights_to_remove,
+            )
