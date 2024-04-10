da5c43937490c249e38ab6f48daf8dd3f7d52bd5
Ling <linghypshen@gmail.com>
2024-04-10 09:01:35 -0400

    update

--- Leetcode/0704-Binary-Search.py
+++ Leetcode/0704-Binary-Search.py
@@ -24,6 +24,7 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums)
+        
         while left <= right:
             mid = left + (right - left)//2
             if nums[mid] == target:
