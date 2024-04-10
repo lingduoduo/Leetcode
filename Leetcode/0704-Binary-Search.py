d3733db441e0b9278f3613e501a78cbf7f091af5
Ling <linghypshen@gmail.com>
2024-04-10 08:55:37 -0400

    update code

--- Leetcode/0704-Binary-Search.py
+++ Leetcode/0704-Binary-Search.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution(object):
     def search(self, nums, target):
         """
@@ -5,9 +7,6 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        if not nums:
-            return -1
-
         left = 0
         right = len(nums)
 
@@ -21,7 +20,20 @@ class Solution(object):
                 right = mid
         return -1
 
-
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)
+        while left <= right:
+            mid = left + (right - left)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+    
 if __name__ == "__main__":
     nums = [-1, 0, 3, 5, 9, 12]
     ###nums = [5]
