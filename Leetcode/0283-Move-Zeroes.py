03e8b5301a5a1376da35db7c99d829b86d425399
Ling <linghypshen@gmail.com>
2024-04-12 12:23:48 -0400

    add notebooks

--- Leetcode/0283-Move-Zeroes.py
+++ Leetcode/0283-Move-Zeroes.py
@@ -21,46 +21,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if len(nums) == 1:
-            return
-
-        l = 0
-        while l < len(nums):
-            if nums[l] != 0:
-                l += 1
-                continue
-            r = l + 1
-            while r < len(nums) and nums[r] == 0:
-                r += 1
-            if r < len(nums) and nums[r] != 0:
-                nums[l] = nums[r]
-                nums[r] = 0
-            l += 1
-
-
-import pysnooper
-
-
-@pysnooper.snoop()
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        if len(nums) <= 1:
-            return
-        i = 0
-        j = 0
-        while j < len(nums):
-            while j < len(nums) and nums[i] != 0:
-                i += 1
-                j += 1
-            while j < len(nums) and nums[j] == 0:
-                j += 1
-            if j < len(nums):
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-                j += 1
+        idx = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[idx], nums[i] = nums[i], nums[idx]
+                idx += 1
+        return nums
 
 
 if __name__ == "__main__":
