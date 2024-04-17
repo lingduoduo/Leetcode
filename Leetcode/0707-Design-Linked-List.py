e19a22c0b2223ba20bff3f8b24d311f53f93fbe5
Ling <linghypshen@gmail.com>
2024-04-17 14:42:24 -0400

    Update code

--- Leetcode/0707-Design-Linked-List.py
+++ Leetcode/0707-Design-Linked-List.py
@@ -6,132 +6,6 @@ class Node:
 
 class MyLinkedList:
     def __init__(self):
-        self.head = self.tail = None
-        self.size = 0
-
-    def getNode(self, index):
-        n = Node(0, self.head)
-        for i in range(index + 1):
-            n = n.next
-        return n
-
-    def get(self, index):
-        if index < 0 or index >= self.size:
-            return -1
-        return self.getNode(index).val
-
-    def addAtHead(self, val):
-        n = Node(val, self.head)
-        self.head = n
-        if self.size == 0:
-            self.tail = n
-        self.size += 1
-
-    def addAtTail(self, val):
-        n = Node(val)
-        if self.size == 0:
-            self.head = self.tail = n
-        else:
-            self.tail.next = n
-            self.tail = n
-        self.size += 1
-
-    def addAtIndex(self, index, val):
-        if index < 0 or index > self.size:
-            return
-        if index == 0:
-            return self.addAtHead(val)
-        if index == self.size:
-            return self.addAtTail(val)
-        prev = self.getNode(index - 1)
-        n = Node(val, prev.next)
-        prev.next = n
-        self.size += 1
-
-    def deleteAtIndex(self, index):
-        if index < 0 or index >= self.size:
-            return
-        prev = self.getNode(index - 1)
-        prev.next = prev.next.next
-        if index == 0:
-            self.head = prev.next
-        if index == self.size - 1:
-            self.tail = prev
-        self.size -= 1
-
-
-class Node:
-    def __init__(self, val=-1, next=None):
-        self.val = val
-        self.next = next
-
-
-import pysnooper
-
-
-class MyLinkedList:
-    def __init__(self):
-        self.head = Node()
-        self.size = 0
-
-    @pysnooper.snoop()
-    def get(self, index: int) -> int:
-        if index < 0 or index >= self.size:
-            return -1
-        p = self.head
-        steps = -1
-        while steps < index:
-            p = p.next
-            steps += 1
-        return p.val
-
-    @pysnooper.snoop()
-    def addAtHead(self, val: int) -> None:
-        self.head.next = Node(val, self.head.next)
-        self.size += 1
-
-    @pysnooper.snoop()
-    def addAtTail(self, val: int) -> None:
-        p = self.head
-        while p.next:
-            p = p.next
-        p.next = Node(val)
-        self.size += 1
-
-    @pysnooper.snoop()
-    def addAtIndex(self, index: int, val: int) -> None:
-        if index < 0 or index > self.size:
-            return
-        p = self.head
-        steps = -1
-        while steps < index - 1:
-            p = p.next
-            steps += 1
-        p.next = Node(val, p.next)
-        self.size += 1
-
-    @pysnooper.snoop()
-    def deleteAtIndex(self, index: int) -> None:
-        if index < 0 or index >= self.size:
-            return
-
-        p = self.head
-        steps = -1
-        while steps < index - 1:
-            p = p.next
-            steps += 1
-        p.next = p.next.next
-        self.size -= 1
-
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
-class MyLinkedList:
-    def __init__(self):
         self.dummy_head = ListNode()
         self.size = 0
 
@@ -178,85 +52,88 @@ class MyLinkedList:
 
 
 class ListNode:
-    def __init__(self, val):
+    def __init__(self, val, prev=None, next=None):
         self.val = val
-        self.next = None
+        self.prev = prev
+        self.next = next
 
-
-class MyLinkedList(object):
+class MyLinkedList:
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.head = None
+        self.tail = None
         self.size = 0
 
+    def getNode(self, index: int) -> ListNode:
+        if index < 0 or index >= self.size:
+            return None
+        
+        if index < self.size // 2:
+            current = self.head
+            for i in range(index):
+                current = current.next
+        else:
+            current = self.tail
+            for i in range(self.size - index - 1):
+                current = current.prev
+        return current
+
     def get(self, index: int) -> int:
-        """
-        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
-        """
         if index < 0 or index >= self.size:
             return -1
-
-        current = self.head
-
-        for _ in range(0, index):
-            current = current.next
-
-        return current.val
+        node = self.getNode(index)
+        return node.val if node else -1
 
     def addAtHead(self, val: int) -> None:
-        """
-        Add a node of value val before the first element of the linked list. After the insertion, the new node will be
-        the first node of the linked list.
-        """
-        self.addAtIndex(0, val)
-
-    def addAtTail(self, val: int) -> None:
-        """
-        Append a node of value val to the last element of the linked list.
-        """
-        self.addAtIndex(self.size, val)
-
-    def addAtIndex(self, index: int, val: int) -> None:
-        """
-        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked
-        list, the node will be appended to the end of linked list. If index is greater than the length, the node will not
-        be inserted.
-        """
-        if index > self.size:
-            return
-
-        current = self.head
-        new_node = ListNode(val)
-
-        if index <= 0:
-            new_node.next = current
-            self.head = new_node
-        else:
-            for _ in range(index - 1):
-                current = current.next
-            new_node.next = current.next
-            current.next = new_node
-
+        new_node = ListNode(val, None, self.head)
+        if self.head:
+            self.head.prev = new_node
+        self.head = new_node
+        if self.size == 0:
+            self.tail = new_node
         self.size += 1
 
+    def addAtTail(self, val: int) -> None:
+        new_node = ListNode(val, self.tail, None)
+        if self.tail:
+            self.tail.next = new_node
+        self.tail = new_node
+        if self.size == 0:
+            self.head = new_node
+        self.size += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.size:
+            return
+        if index == 0:
+            self.addAtHead(val)
+        elif index == self.size:
+            self.addAtTail(val)
+        else:
+            pred = self.getNode(index - 1)
+            new_node = ListNode(val, pred, pred.next)
+            pred.next.prev = new_node
+            pred.next = new_node
+            self.size += 1
+
     def deleteAtIndex(self, index: int) -> None:
-        """
-        Delete the index-th node in the linked list, if the index is valid.
-        """
         if index < 0 or index >= self.size:
             return
-
-        current = self.head
-
         if index == 0:
-            self.head = self.head.next
+            if self.head.next:
+                self.head = self.head.next
+                self.head.prev = None
+            else:
+                self.head = self.tail = None
+        elif index == self.size - 1:
+            if self.tail.prev:
+                self.tail = self.tail.prev
+                self.tail.next = None
+            else:
+                self.head = self.tail = None
         else:
-            for _ in range(0, index - 1):
-                current = current.next
-            current.next = current.next.next
-
+            node = self.getNode(index)
+            node.prev.next = node.next
+            node.next.prev = node.prev
         self.size -= 1
 
 
--- Leetcode/tmp.py
+++ Leetcode/tmp.py
@@ -1,9 +1,55 @@
-from typing import List
-class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list(set(nums1) & set(nums2))
+class ListNode:
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
 
+
+class MyLinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+    
+    def get(self, index: int) -> int:
+        dummy = ListNode(0)
+        dummy.next = self.head
+
+        p = dummy.next
+        for i in range(index+1):
+            if p is None:
+                return -1
+            p = p.next
+        return p.val
+    
+    def addAtHead(self, val: int) -> None:
+        p = ListNode(val, None, self.head)
+        if self.head:
+            p.prev = self.head.prev
+        else:
+            self.tail = p
+        self.head = p
+        self.size += 1
+    
+    def addAtTail(self, val: int) -> None:
+        p = ListNode(val, self.tail, None)
+        if self.tail:
+            self.tail.next = p
+        else:
+            self.head = p
+        self.tail = p
+        self.size += 1
+    
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        
+
+            
 if __name__ == '__main__':
-    nums1 = [1,2,2,1]
-    nums2 = [2,2]
-    print(Solution().intersection(nums1, nums2))  # Output is [2]
+    myLinkedList = MyLinkedList();
+    myLinkedList.addAtHead(1);
+    myLinkedList.addAtTail(3);
+    myLinkedList.addAtIndex(1, 2);    
+    myLinkedList.get(1);              
+    myLinkedList.deleteAtIndex(1);   
+    myLinkedList.get(1);             
