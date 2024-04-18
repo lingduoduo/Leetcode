e682eaf251b57227ad53009927c3b7bfb043aa83
Ling <linghypshen@gmail.com>
2024-04-18 13:13:58 -0400

    Update code

--- Leetcode/0151-Reverse-Words-in-a-String.py
+++ Leetcode/0151-Reverse-Words-in-a-String.py
@@ -89,28 +89,20 @@ class Solution:
 
 class Solution(object):
     def reverseWords(self, s: str) -> str:
-        # 删除前后空白
         s = s.strip()
-        # 反转整个字符串
         s = s[::-1]
-        # 将字符串拆分为单词，并反转每个单词
         s = " ".join(word[::-1] for word in s.split())
         return s
 
 
 class Solution:
     def reverseWords(self, s: str) -> str:
-        # 将字符串拆分为单词，即转换成列表类型
         words = s.split()
-
-        # 反转单词
         left, right = 0, len(words) - 1
         while left < right:
             words[left], words[right] = words[right], words[left]
             left += 1
             right -= 1
-
-        # 将列表转换成字符串
         return " ".join(words)
 
 
--- Leetcode/tmp.py
+++ Leetcode/tmp.py
@@ -1,16 +1,9 @@
 from typing import List
 
-class Solution:
-    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
-        if len(sentence1) != len(sentence2):
-            return False  
-        
-        similarPairsSet = set(tuple(pair) for pair in similarPairs)
-        for s1, s2 in zip(sentence1, sentence2):
-            if s1 != s2 and (s1, s2) not in similarPairsSet and (s2, s1) not in similarPairsSet:
-                return False 
-        return True 
-    
+class Solution(object):           
+    def reverseWords(self, s: str, i: int) -> str:
+        return s[-i:] + s[:-i]
+
 if __name__ == "__main__":
     s = Solution()
-    print(s.areSentencesSimilar(sentence1 = ["great","acting","skills"], sentence2 = ["fine","drama","talent"], similarPairs = [["great","fine"],["drama","acting"],["skills","talent"]]))
+    print(s.reverseWords("abcdefg", 2))
