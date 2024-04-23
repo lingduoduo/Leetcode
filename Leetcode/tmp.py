ab7663a387ede02fdadade481c5633fd89ce9eff
Ling <linghypshen@gmail.com>
2024-04-23 13:18:42 -0400

    Add code

--- Leetcode/tmp.py
+++ Leetcode/tmp.py
@@ -2,27 +2,22 @@ import collections
 from typing import List
 from collections import defaultdict
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder:
-            return None
-        val = preorder[0]
-        root = TreeNode(val)
-        idx = inorder.index(val)
-        root.left = self.buildTree(preorder[1: 1+idx], inorder[:idx])
-        root.right = self.buildTree(preorder[idx+1:], inorder[idx+1:])
-        return root
-
+    def removeDuplicates(self, nums: List[int]) -> int:
+        cnt = 1
+        idx = 0
+        for i in range(1, len(nums)):
+            if nums[i] == nums[idx]:
+                cnt += 1
+                if cnt == 2:
+                    idx += 1
+            else:
+                cnt = 1
+                idx += 1
+                nums[idx] = nums[i]
+        return idx + 1
         
-
-
 if __name__ == "__main__":
-    res = Solution().productExceptSelf(nums = [-1,1,0,-3,3])
+    res = Solution().removeDuplicates(nums = [1,1,1,2,2,3])
     print(res)  
 
