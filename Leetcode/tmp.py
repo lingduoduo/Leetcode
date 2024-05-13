f28617b4bb75bd017258924239bbe51c4caa2450
Ling <linghypshen@gmail.com>
2024-05-13 17:19:31 -0400

    add code

--- Leetcode/tmp.py
+++ Leetcode/tmp.py
@@ -1,22 +1,64 @@
 from typing import List, Optional
 import heapq
 
-from collections import OrderedDict
+import collections
+
 
 class Solution:
-    def customSortString(self, order: str, s: str) -> str:
-        d = {chr: idx for idx, chr in enumerate(order)}
+
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        dsu = UnionFind()
+        d = {}
+
+        for i in range(len(accounts)):
+            for j in range(1, len(accounts[i])):
+                email = accounts[i][j]
+                if email not in d:
+                    d[email] = i
+                else:
+                    dsu.union(i, d[email])
+
+        components = {}
+        for email, group in d.items():
+            group_rep = dsu.find(group)
+            if group_rep not in components:
+                components[group_rep] = []
+            components[group_rep].append(email)
 
         res = []
-        for chr in s:
-            if chr in d:
-                res.append((d[chr], chr))
+        for group, emails in components.items():
+            emails.sort()
+            emails = [accounts[group][0]] + emails
+            res.append(emails)
+
+        return res
+
+
+class UnionFind:
+    def __init__(self):
+        self.parent = [i for i in range(1001)]
+        self.rank = [1] * 1001
+
+    def find(self, x):
+        while self.parent[x] != x:
+            x = self.parent[x]
+        return x
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x == root_y:
+            return False
+        else:
+            if self.rank[x] >= self.rank[y]:
+                self.rank[x] += self.rank[y]
+                self.parent[y] = x
             else:
-                res.append((float('inf'), chr))
-        res.sort(key=lambda x: x[0])
-        return "".join([x[1] for x in res])
+                self.rank[y] += self.rank[x]
+                self.parent[x] = y
+            return True
 
 # Test the code        
 if __name__ == '__main__':
-    res = Solution().customSortString("cba", "abcd")
+    res = Solution().accountsMerge(accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]])
     print(res)
