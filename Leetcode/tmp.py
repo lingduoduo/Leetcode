78410d84ed88e7909a228e7aca872b0c34908286
Ling <linghypshen@gmail.com>
2024-05-20 13:15:00 -0400

    add code

--- Leetcode/tmp.py
+++ Leetcode/tmp.py
@@ -2,31 +2,17 @@ import functools
 import heapq
 from multiprocessing import heap
 from typing import List
+from itertools import accumulate
 
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
-        n = len(num)
-        res = set()
-        @functools.lru_cache(maxsize=None)
-        def dfs(start, tot, prev, expr):
-            if start == n:
-                if tot == target:
-                    res.add(expr)
-                return
+        nums = [1,2,3,4, 5]
+        return list(accumulate(nums))
 
-            for end in range(start, n):
-                cur = int(num[start: end + 1])
-                if start == 0:
-                    dfs(end + 1, cur, cur, str(cur))
-                else:
-                    dfs(end + 1, tot + cur, cur, expr + "+" + str(cur))
-                    dfs(end + 1, tot - cur, -cur, expr + "-" + str(cur))
-                    dfs(end + 1, tot - prev + cur * prev, cur * prev, expr + "*" + str(cur))
-        dfs(0, 0, 0, "")
-        return res
+
 
 if __name__ == "__main__":
-    res = Solution().findClosestElements(arr = [1,2,3,4,5], k = 4, x = -1)
+    res = Solution().addOperators("12345", 5)
     print(res)
 
 
