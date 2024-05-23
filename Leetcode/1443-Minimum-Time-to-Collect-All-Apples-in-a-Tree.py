0294df0a60f060a0352fe966a6c231dc8ba94c84
Ling <linghypshen@gmail.com>
2024-05-23 17:55:01 -0400

    add code

--- Leetcode/1443-Minimum-Time-to-Collect-All-Apples-in-a-Tree.py
+++ Leetcode/1443-Minimum-Time-to-Collect-All-Apples-in-a-Tree.py
@@ -1,42 +1,30 @@
 import collections
 import heapq
 
-
+from typing import List
 class Solution:
-    def minTime(self, n: int, edges, hasApple) -> int:
-        d = collections.defaultdict(list)
-        for i in range(len(edges)):
-            fromNode, toNode = edges[i]
-            d[fromNode].append((toNode, hasApple[toNode]))
-            d[toNode].append((fromNode, hasApple[fromNode]))
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        def dfs(node):
+            visited[node] = 1
+            res = 0
+            for i in grid[node]:
+                if visited[i] == 1:
+                    continue
+                x = dfs(i)
+                if x != -1:
+                    res += x
+                    res += 2
+            return res if res > 0 or hasApple[node] == True else -1
 
-        stack = [(0, 0)]
+        grid = [[] for i in range(n)]
+        for i, j in edges:
+            grid[i].append(j)
+            grid[j].append(i)
         visited = [0] * n
-
-        res = set()
-        tot = hasApple.count(True)
-
-        # while any(hasApple):
-        while stack:
-            print(stack)
-            dist, fromNode = heapq.heappop(stack)
-            visited[fromNode] = 1
-            for node, flag in d[fromNode]:
-                if visited[node] == 0:
-                    visited[node] = 1
-                    dist += 1
-                    heapq.heappush(stack, (dist, node))
-
-                    if flag:
-                        res.add((fromNode, node))
-        return res
-        # if res == tot:
-        #   return
-
-
-# Output: 8
-# Explanation: The figure above represents the given tree where red vertices have an apple.
-# One optimal path to collect all apples is shown by the green arrows.
+        x = dfs(0)
+        if x == -1:
+            return 0
+        return x
 
 
 if __name__ == "__main__":
