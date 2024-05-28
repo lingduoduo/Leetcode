961436203defa2d7f0caad64763a8a0f549e8cea
Ling <linghypshen@gmail.com>
2024-05-28 12:23:39 -0400

    add code

--- Leetcode/0536-Construct-Binary-Tree-from-String.py
+++ Leetcode/0536-Construct-Binary-Tree-from-String.py
@@ -35,3 +35,29 @@ class Solution:
                     else:
                         stack[-1].right = node
         return stack[-1] if stack else TreeNode(num) if s else None
+
+class Solution:
+    def str2tree(self, s: str) -> Optional[TreeNode]:
+        if not s:
+            return None
+        stack, number = [], ''
+        for c in s:
+            if c in '()':
+                if c == '(' and number:
+                    stack.append(TreeNode(number))
+                    number = ''
+                elif c== ')':
+                    if number:
+                        node, parent = TreeNode(number), stack[-1]
+                        number = ''
+                    else:
+                        node, parent = stack.pop(), stack[-1]
+                    if parent.left:
+                        parent.right = node
+                    else:
+                        parent.left = node
+            else:
+                number += c
+        if number:
+            stack = [TreeNode(number)]
+        return stack[0]
