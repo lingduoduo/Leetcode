babc3f776add31dc3f2e37f5deced14a2c4925d5
Ling <linghypshen@gmail.com>
2024-06-03 13:18:54 -0400

    add code

--- Leetcode/tmp.py
+++ Leetcode/tmp.py
@@ -1,26 +1,17 @@
 from  collections import Counter
 from typing import List
+import collections
 
 class Solution:
-    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        l = 0
-        d = Counter()
-        curr = 0
-        res = 0
+    def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+        edges = collections.defaultdict()
 
-        for r in range(len(nums)):
-            d[nums[r]] += 1
-            curr += nums[r]
-            while d[nums[r]] > 1 or (r - l + 1) > k:
-                curr -= nums[l]
-                d[nums[l]] -= 1
-                l += 1
-            if (r - l + 1) == k:
-                res = max(res, curr)
-        return res
+        for x, y in pairs:
+            edges[x] = y
+
 
 if __name__ == "__main__":
-    res = Solution().maximumSubarraySum(nums = [1,5,4,2,9,9,9], k = 3)
+    res = Solution().validArrangement(pairs = [[5,1],[4,5],[11,9],[9,4]])
     print(res)
 
 
