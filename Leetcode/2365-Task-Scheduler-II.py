2f4e39a3de87e4eae81c8ee56760f766d8ae5366
Ling <linghypshen@gmail.com>
2024-06-04 13:45:04 -0400

    add code

--- Leetcode/2365-Task-Scheduler-II.py
+++ Leetcode/2365-Task-Scheduler-II.py
@@ -1,8 +1,4 @@
-from  collections import Counter
 from typing import List
-import collections
-
-
 class Solution:
     def taskSchedulerII(self, tasks: List[int], space: int) -> int:
         res = 0
@@ -13,11 +9,3 @@ class Solution:
                 res = max(res, d[task] + space + 1)
             d[task] = res
         return res
-
-
-if __name__ == "__main__":
-    res = Solution().taskSchedulerII(tasks = [5,8,8,5], space = 2)
-    print(res)
-
-
-
