118d3cd6aa6d65a60134670b401c9981679344da
Ling <linghypshen@gmail.com>
2024-06-05 21:38:33 -0400

    add code

--- Leetcode/tmp.py
+++ Leetcode/tmp.py
@@ -1,32 +1,17 @@
 from  collections import Counter
 from typing import List
 import collections
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
-    def balanceBST(self, root: TreeNode) -> TreeNode:
-        def inorder_bts(root):
-            if not root:
-                return []
-            return inorder_bts(root.left) + [root] + inorder_bts(root.right)
-
-        def construct_bst(left, right, path):
-            if left > right:
-                return None
-            middle = (left + right) // 2
-            path[middle].left = construct_bst(left, middle - 1, path)
-            path[middle].right = construct_bst(middle + 1, right, path)
-            return path[middle]
-
-
-        path = inorder_bts(root)
-        return construct_bst(0, len(path) - 1, path)
-
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        res = ""
+        for i, c in enumerate(s):
+            if spaces and i == spaces[0]:
+                res += " "
+                spaces.pop(0)
+            res += c
+        return res
 
 if __name__ == "__main__":
-    res = Solution().taskSchedulerII(tasks = [5,8,8,5], space = 2)
+    res = Solution().addSpaces(s = "LeetcodeHelpsMeLearn", spaces = [8,13,15])
     print(res)
+
