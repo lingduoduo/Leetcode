d4af95ffd850c85096cd3fc7349546c9c43d13c6
Ling <linghypshen@gmail.com>
2024-06-17 21:51:55 -0400

    adde code

--- Leetcode/0079-Word-Search.py
+++ Leetcode/0079-Word-Search.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution(object):
     def exist(self, board, word):
         """
@@ -34,42 +36,6 @@ class Solution(object):
         )
         board[i][j] = word[wordinx]
         return found
-
-
-from typing import List
-
-
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfs(x, y, p):
-            if p == l:
-                return True
-            for dirx, diry in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                curx, cury = x + dirx, y + diry
-                if (
-                    0 <= curx < m
-                    and 0 <= cury < n
-                    and flag[curx][cury]
-                    and board[curx][cury] == word[p]
-                ):
-                    flag[curx][cury] = False
-                    if dfs(curx, cury, p + 1):
-                        flag[curx][cury] = True
-                        return True
-                    flag[curx][cury] = True
-            return False
-
-        m, n = len(board), len(board[0])
-        l = len(word)
-        flag = [[True] * n for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == word[0]:
-                    flag[i][j] = False
-                    if dfs(i, j, 1):
-                        return True
-                    flag[i][j] = True
-        return False
 
 
 class Solution:
--- Leetcode/tmp.py
+++ Leetcode/tmp.py
@@ -1,52 +1,10 @@
-from  collections import Counter
-from typing import List, Optional
-import math
-import collections
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from typing import List
 
 
 class Solution:
-    def addOneRow(self, root: TreeNode, v: int, d: int) -> TreeNode:
-        if d == 1:
-            return TreeNode(v, root, None)
-
-        nodes = []
-        def travese(r, d):
-            if not r:
-                return
-            if d == 0:
-                nodes.append(r)
-                return
-            travese(r.left, d - 1)
-            travese(r.right, d - 1)
-
-        travese(root, d - 2)
-        for node in nodes:
-            node.left = TreeNode(v, node.left, None)
-            node.right = TreeNode(v, None, node.right)
-        return root
-
-class Solution:
-    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
-        def dfs(root, val, depth):
-            if root==None: return
-            if depth>2:
-                dfs(root.left, val, depth-1)
-                dfs(root.right, val, depth-1)
-            else:
-                root.left=TreeNode(val, root.left, None)
-                root.right=TreeNode(val, None, root.right)
-        if depth == 1:
-            return TreeNode(val, root, None)
-        dfs(root, val, depth)
-        return root
+    def isMonotonic(self, nums: List[int]) -> bool:
+        return all([(x - y) <= 0 for x, y in zip(nums, nums[1:])]) or all([(x - y) >= 0 for x, y in zip(nums, nums[1:])])
 
 if __name__ == "__main__":
-    res = Solution().evenOddBit(n = 5)
+    res = Solution().isMonotonic(nums = [1,2,2,3])
     print(res)
-
