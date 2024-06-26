5da6caf6badba8fdde63863ad3b63cd1d0d6cf38
Ling <linghypshen@gmail.com>
2024-06-26 14:29:13 -0400

    add code

--- Leetcode/tmp.py
+++ Leetcode/tmp.py
@@ -1,19 +1,15 @@
 import collections
 from typing import List
 
-
+import numpy as np
 class Solution:
+    def maxOperations(self, nums: List[int]) -> int:
 
-    def rankTeams(self, votes: List[str]) -> str:
-        d = collections.defaultdict(int)
-        for vote in votes:
-            for i, c in enumerate(vote):
-                d[c] += i
-        voted_names = sorted(d.keys())
-        return "".join(sorted(voted_names, key=lambda x: d[x], reverse=True))
+
+
 
 
 if __name__ == "__main__":
-    res = Solution().rankTeams(votes = ["BCA","CAB","CBA","ABC","ACB","BAC"])
+    res = Solution().maxOperations(nums = [3,2,1,2,3,4])
     print(res)
 
