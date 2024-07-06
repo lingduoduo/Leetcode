ca22a00b5901c27d29630665b3c7d9de1319e9ad
Ling <linghypshen@gmail.com>
2024-07-06 12:38:53 -0400

    Add code

--- Leetcode/0274-H-Index.py
+++ Leetcode/0274-H-Index.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         n = len(citations)
@@ -6,3 +7,13 @@ class Solution:
         for k, v in enumerate(citations):
             h = max(h, min(n - k, v))
         return h
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        citations.sort()
+
+        for i,v in enumerate(citations):
+            if n - i <= v:
+                return n - i
+        return 0
