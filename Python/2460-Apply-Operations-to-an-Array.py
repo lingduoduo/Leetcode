e4a8d8e11e0631287246b9240ed2341627cb001a
linghypshen@gmail.com <linghypshen@gmail.com>
2025-09-27 19:39:21 -0400

    add code

---
+++ Python/2460-Apply-Operations-to-an-Array.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i] *= 2
+                nums[i + 1] = 0
+        idx = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[idx], nums[i] = nums[i], nums[idx]
+                idx += 1
+        return nums
+
+if __name__ == '__main__':
+    res = Solution().applyOperations(nums = [1,2,2,1,1,0])
+    print(res)
+    res = Solution().applyOperations(nums = [0,1])
+    print(res)
+
---
+++ TypeScript/2460.ts
@@ -0,0 +1,28 @@
+class Solution {
+  applyOperations(nums: number[]): number[] {
+    const n = nums.length;
+
+    // Phase 1: apply operations left-to-right
+    for (let i = 0; i < n - 1; i++) {
+      if (nums[i] === nums[i + 1]) {
+        nums[i] *= 2;
+        nums[i + 1] = 0;
+      }
+    }
+
+    // Phase 2: move zeros to the end (stable)
+    let idx = 0; // next position for a non-zero
+    for (let i = 0; i < n; i++) {
+      if (nums[i] !== 0) {
+        if (i !== idx) {
+          [nums[idx], nums[i]] = [nums[i], nums[idx]];
+        }
+        idx++;
+      }
+    }
+    return nums;
+  }
+}
+
+const sol = new Solution();
+console.log(sol.applyOperations(nums = [1,2,2,1,1,0]))
