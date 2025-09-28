8ddaadafe485a510bf5139e0b4e1c8d82ce4f9b2
linghypshen@gmail.com <linghypshen@gmail.com>
2025-09-27 20:36:08 -0400

    add code

---
+++ TypeScript/0203.ts
@@ -0,0 +1,28 @@
+
+// Definition for singly-linked list.
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+
+class Solution {
+  removeElements(head: ListNode | null, val: number): ListNode | null {
+    const dummy = new ListNode(0, head);
+    let p: ListNode = dummy;
+
+    while (p.next) {
+      if (p.next.val === val) {
+        p.next = p.next.next; // skip the node
+      } else {
+        p = p.next;           // move forward
+      }
+    }
+    return dummy.next;
+  }
+}
+
