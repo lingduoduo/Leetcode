af45cde4be5874b4cd779dbf747a3016741568b0
linghypshen@gmail.com <linghypshen@gmail.com>
2025-10-14 20:55:07 -0400

    add code

--- TypeScript/0077.ts
+++ TypeScript/0077.ts
@@ -1,7 +1,7 @@
 function combine(n: number, k: number): number[][] {
     let res: number[][] = [];
 
-    function backtrack(idx: number, path: number[]): void {
+    function dfs(idx: number, path: number[]): void {
         if (path.length === k) {
             res.push([...path]);   // push a copy
             return;                   // <-- keep return inside the if
@@ -9,11 +9,11 @@ function combine(n: number, k: number): number[][] {
 
         for (let i = idx; i <= n; i++) { // iterate 1..n
             path.push(i);                // push element
-            backtrack(i + 1, path); // next start is i + 1
+            dfs(i + 1, path); // next start is i + 1
             path.pop();                  // undo
         }
     }
 
-    backtrack(1, []);  // start from 1 with empty path
+    dfs(1, []);  // start from 1 with empty path
     return res;
 }
