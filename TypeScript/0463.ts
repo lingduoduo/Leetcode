31618bfa9330a4a7a4e57a7cb7350933d8d7319a
linghypshen@gmail.com <linghypshen@gmail.com>
2025-10-28 11:22:17 -0400

    add code

--- TypeScript/0463.ts
+++ TypeScript/0463.ts
@@ -2,9 +2,11 @@ function islandPerimeter(grid: number[][]): number {
     let res = 0;
     for (let i = 0; i < grid.length; i++){
         for (let j = 0; j < grid[0].length; j++){
-            if (grid[i][j] === 1) res += 4;
-            if (i > 0 && grid[i-1][j] === 1) res -= 2;
-            if (j > 0 && grid[i][j-1] === 1) res -= 2;
+            if (grid[i][j] === 1){
+                res += 4;
+                if (i > 0 && grid[i-1][j] === 1) res -= 2;
+                if (j > 0 && grid[i][j-1] === 1) res -= 2;
+            }
         }
     }
     return res;
