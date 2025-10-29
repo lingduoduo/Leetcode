5ce867640581059b4d95c7e58cd12e4fde8f7507
linghypshen@gmail.com <linghypshen@gmail.com>
2025-10-29 19:58:16 -0400

    add code

--- Python/0287-Find-the-Duplicate-Number.py
+++ Python/0287-Find-the-Duplicate-Number.py
@@ -1,25 +1,5 @@
 from typing import List
 
-
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        n = len(nums)
-        left = 0
-        right = n - 1
-
-        while left <= right:
-            mid = left + (right - left) // 2
-            cnt = 0
-            for i in range(len(nums)):
-                if nums[i] <= mid:
-                    cnt += 1
-            if cnt <= mid:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return left
-
-
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         for i in range(len(nums)):
@@ -33,21 +13,6 @@ class Solution:
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        slow = nums[0]
-        fast = nums[nums[0]]
-        while slow != fast:
-            fast = nums[nums[fast]]
-            slow = nums[slow]
-        fast = 0
-        while slow != fast:
-            fast = nums[fast]
-            slow = nums[slow]
-        return fast
-
-
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        n = len(nums)
         l = 0
         r = len(nums)
         while l <= r:
