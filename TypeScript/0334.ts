0f86120d09b9cc418221a80b8ba6cb2b0c198278
linghypshen@gmail.com <linghypshen@gmail.com>
2025-11-12 13:46:20 -0500

    add code

---
+++ TypeScript/0334.ts
@@ -0,0 +1,15 @@
+function increasingTriplet(nums: number[]): boolean {
+    let first = Infinity;
+    let second = Infinity;
+
+    for (const num of nums) {
+        if (num <= first) {
+            first = num;
+        } else if (num <= second) {
+            second = num;
+        } else {
+            return true;
+        }
+    }
+    return false;
+};
