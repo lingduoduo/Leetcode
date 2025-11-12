409330710b7a52c67e9f2b4bd6307fe90ced3bc5
linghypshen@gmail.com <linghypshen@gmail.com>
2025-11-12 12:28:12 -0500

    add code

---
+++ TypeScript/0300.ts
@@ -0,0 +1,17 @@
+function lengthOfLIS(nums: number[]): number {
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    const dp: number[] = new Array(nums.length).fill(1);
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    return Math.max(...dp);
+};
