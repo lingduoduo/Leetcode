3b98cf8c67370c8e80d0fa2228fb16931bf54039
linghypshen@gmail.com <linghypshen@gmail.com>
2025-11-19 17:13:47 -0500

    add code

--- Python/0012-interger-to-Roman.py
+++ Python/0012-interger-to-Roman.py
@@ -1,18 +1,30 @@
-class Solution(object):
+class Solution:
     def intToRoman(self, num: int) -> str:
-        """
-        :type s: str
-        :rtype: int
-        """
-        k = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-        v = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        # (value, symbol) pairs in descending order
+        vals = [
+            (1000, "M"),
+            (900,  "CM"),
+            (500,  "D"),
+            (400,  "CD"),
+            (100,  "C"),
+            (90,   "XC"),
+            (50,   "L"),
+            (40,   "XL"),
+            (10,   "X"),
+            (9,    "IX"),
+            (5,    "V"),
+            (4,    "IV"),
+            (1,    "I"),
+        ]
 
-        res = ""
-        for i in range(len(v)):
-            while v[i] <= num:
-                res += k[i]
-                num -= v[i]
-        return res
+        res = []
+        for val, sym in vals:
+            if num == 0:
+                break
+            count, num = divmod(num, val)
+            res.append(sym * count)
+
+        return "".join(res)
 
 
 if __name__ == "__main__":
