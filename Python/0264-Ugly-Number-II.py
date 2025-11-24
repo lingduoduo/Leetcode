01e741dbb175097c304d4a68014af23cacfed450
linghypshen@gmail.com <linghypshen@gmail.com>
2025-11-24 13:47:49 -0500

    add code

--- Python/0264-Ugly-Number-II.py
+++ Python/0264-Ugly-Number-II.py
@@ -18,6 +18,26 @@ class Solution:
         return dp[-1]
 
 
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        dp = [0] * n
+        dp[0] = 1
+
+        p2 = p3 = p5 = 0
+
+        for i in range(1, n):
+            next2 = dp[p2] * 2
+            next3 = dp[p3] * 3
+            next5 = dp[p5] * 5
+
+            dp[i] = min(next2, next3, next5)
+
+            if dp[i] == next2: p2 += 1
+            if dp[i] == next3: p3 += 1
+            if dp[i] == next5: p5 += 1
+
+        return dp[-1]
+
 import heapq
 
 
