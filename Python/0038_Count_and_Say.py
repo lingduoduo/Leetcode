bc0ca333a9cd3107f555720e975957793735a1a0
linghypshen@gmail.com <linghypshen@gmail.com>
2025-12-02 14:17:47 -0500

    add code

--- Python/0038_Count_and_Say.py
+++ Python/0038_Count_and_Say.py
@@ -19,21 +19,20 @@ class Solution:
 
 class Solution:
     def countAndSay(self, n: int) -> str:
-        current_string = "1"
+        res = "1"
         for _ in range(n - 1):
-            next_string = ""
-            j = 0
-            k = 0
-            while j < len(current_string):
-                while (
-                    k < len(current_string) and current_string[k] == current_string[j]
-                ):
-                    k += 1
-                next_string += str(k - j) + current_string[j]
-                j = k
-            current_string = next_string
-
-        return current_string
+            prev = res[0]
+            cnt = 1
+            cur = ""
+            for chr in res[1:]:
+                if prev == chr:
+                    cnt += 1
+                else:
+                    cur += str(cnt) + prev
+                    prev = chr
+                    cnt = 1
+            res = cur + str(cnt) + prev
+        return res
 
 
 if __name__ == "__main__":
