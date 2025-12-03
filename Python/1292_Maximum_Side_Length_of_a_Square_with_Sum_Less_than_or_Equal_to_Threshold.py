9b6992a994342016c5964c67341385689b197ab3
linghypshen@gmail.com <linghypshen@gmail.com>
2025-12-03 11:52:33 -0500

    add code

--- Python/1292_Maximum_Side_Length_of_a_Square_with_Sum_Less_than_or_Equal_to_Threshold.py
+++ Python/1292_Maximum_Side_Length_of_a_Square_with_Sum_Less_than_or_Equal_to_Threshold.py
@@ -1,29 +1,48 @@
+from typing import List
+
 class Solution:
     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
-        res = 0
         m = len(mat)
+        if m == 0:
+            return 0
         n = len(mat[0])
+
+        # build 2D prefix-sum
         presum = [[0] * (n + 1) for _ in range(m + 1)]
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 presum[i][j] = (
-                    presum[i][j - 1]
-                    + presum[i - 1][j]
+                    presum[i - 1][j]
+                    + presum[i][j - 1]
                     - presum[i - 1][j - 1]
                     + mat[i - 1][j - 1]
                 )
-                lo, hi = 1, min(i, j) + 1
-                while lo < hi:
-                    mid = (lo + hi) // 2
-                    cursum = (
-                        presum[i][j]
-                        - presum[i - mid][j]
-                        - presum[i][j - mid]
-                        + presum[i - mid][j - mid]
-                    )
-                    if cursum > threshold:
-                        hi = mid
-                    else:
-                        lo = mid + 1
-                res = max(res, lo - 1)
+
+        left, right = 0, min(m, n) + 1  # search in [0, min(m,n)]
+        res = 0
+        while left < right:
+            mid = left + (right - left) // 2
+            valid = False
+            if mid > 0:
+                for i in range(mid, m + 1):
+                    for j in range(mid, n + 1):
+                        cursum = (
+                            presum[i][j]
+                            - presum[i - mid][j]
+                            - presum[i][j - mid]
+                            + presum[i - mid][j - mid]
+                        )
+                        if cursum <= threshold:
+                            valid = True
+                            break
+                    if valid:
+                        break
+
+            if valid:
+                res = mid
+                left = mid + 1
+            else:
+                right = mid
+
         return res
+
