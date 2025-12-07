1ccdc47c5e0417211c103eb1a0f823a521ff795a
linghypshen@gmail.com <linghypshen@gmail.com>
2025-12-07 12:43:37 -0500

    add code

--- Python/0128_Longest_Consecutive_Sequence.py
+++ Python/0128_Longest_Consecutive_Sequence.py
@@ -26,3 +26,33 @@ class Solution:
                 cur += 1
             res = max(res, current_length)
         return res
+
+class UnionFind:
+    def __init__(self, n):
+        self.par = list(range(n))
+        self.size = [1] * n
+    
+    def find(self, x):
+        if x != self.par[x]:
+            self.par[x] = self.find(self.par[x])
+        return self.par[x]
+
+    def union(self, x, y):
+        rx, ry = self.find(x), self.find(y)
+        self.par[rx] = ry
+        # union by size
+        if self.size[rx] < self.size[ry]:
+            rx, ry = ry, rx
+        self.par[ry] = rx
+        self.size[rx] += self.size[ry]
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums = list(set(nums))
+        uf = UnionFind(len(nums))
+        index = {v: i for i, v in enumerate(nums)}
+        for v in nums:
+            if v + 1 in index:
+                uf.union(index[v], index[v + 1])
+        return max(uf.size)
