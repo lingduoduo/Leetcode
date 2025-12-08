bd7ca360d6279acbb2470607aa35a6069a92cc1e
linghypshen@gmail.com <linghypshen@gmail.com>
2025-12-08 11:24:50 -0500

    add code

--- Python/3542-Minimum-Operations-to-Convert-All-Elements-to-Zero.py
+++ Python/3542-Minimum-Operations-to-Convert-All-Elements-to-Zero.py
@@ -3,10 +3,10 @@ class Solution:
         stack = []
         res = 0
         for num in nums:
-            if num == 0:
-                continue
             while stack and stack[-1] > num:
                 stack.pop()
+            if num == 0:
+                continue
             if not stack or stack[-1] < num:
                 res += 1
                 stack.append(num)
