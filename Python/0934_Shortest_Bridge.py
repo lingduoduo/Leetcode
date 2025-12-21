5e96f6016e57537ec0fc2d2301f2cec0cf786a99
linghypshen@gmail.com <linghypshen@gmail.com>
2025-12-21 13:47:38 -0500

    add code

--- Python/0934_Shortest_Bridge.py
+++ Python/0934_Shortest_Bridge.py
@@ -1,29 +1,48 @@
-from typing import List
+from typing import List, Tuple
+from collections import deque
+
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        start_i, start_j = next((i, j) for i in range(m) for j in range(n) if grid[i][j])
 
-        stack = [(start_i, start_j)]
-        visited = set(stack)
+        # 1) find any cell of the first island
+        si = sj = -1
+        found = False
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    si, sj = i, j
+                    found = True
+                    break
+            if found:
+                break
+
+        # 2) DFS to collect all cells in the first island
+        stack: List[Tuple[int, int]] = [(si, sj)]
+        visited = {(si, sj)}  # cells already in island or already expanded in BFS
+
         while stack:
             i, j = stack.pop()
-            visited.add((i, j))
-            for ii, jj in (i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j):
-                if 0 <= ii < m and 0 <= jj < n and grid[ii][jj] and (ii, jj) not in visited:
-                    stack.append((ii, jj))
-                    visited.add((ii, jj))
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                x, y = i + dx, j + dy
+                if 0 <= x < m and 0 <= y < n and grid[x][y] == 1 and (x, y) not in visited:
+                    stack.append((x, y))      # <-- tuple, not list
+                    visited.add((x, y))
 
-        res = 0
-        queue = list(visited)
-        while queue:
-            new_queue = []
-            for i, j in queue:
-                for ii, jj in (i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j):
-                    if 0 <= ii < m and 0 <= jj < n and (ii, jj) not in visited:
-                        if grid[ii][jj] == 1:
-                            return res
-                        new_queue.append((ii, jj))
-                        visited.add((ii, jj))
-            queue = new_queue
-            res += 1
+        # 3) BFS expansion layer by layer from the first island until hitting the second
+        q = deque(visited)
+        steps = 0
+
+        while q:
+            for _ in range(len(q)):
+                i, j = q.popleft()
+                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    x, y = i + dx, j + dy
+                    if 0 <= x < m and 0 <= y < n and (x, y) not in visited:
+                        if grid[x][y] == 1:
+                            return steps
+                        visited.add((x, y))
+                        q.append((x, y))
+            steps += 1
+
+        return -1
