11be6e17cd0051d1cb3199d6e3b84ac41a8e2231
linghypshen@gmail.com <linghypshen@gmail.com>
2025-12-25 14:36:58 -0500

    Add code

--- Python/0000.py
+++ Python/0000.py
@@ -8,25 +8,32 @@ import random
 import bisect
 
 class Solution:
-    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        # sort jobs by start time
-        jobs = sorted(zip(startTime, endTime, profit))  # (s, e, p)
-        n = len(jobs)
-        starts = [s for s, _, _ in jobs]
+    def threeSumClosest(self, nums: List[int], target: int) -> int:        
+        nums.sort()
+        n = len(nums)
+        res = nums[0] + nums[1] + nums[2]
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            j = i + 1
+            k = n - 1
+            while j < k:
+                tot = nums[i] + nums[j] + nums[k]
+                if abs(tot - target) < abs(res - target):
+                    res = tot
+                if tot == target:
+                    return tot
+                elif tot < target:
+                    j += 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                else:
+                    k -= 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+        return res
 
-        # nxt[i] = first index j > i such that jobs[j].start >= jobs[i].end
-        nxt = [n] * n
-        for i in range(n):
-            e = jobs[i][1]
-            nxt[i] = bisect.bisect_left(starts, e)
-
-        dp = [0] * (n + 1)  # dp[i] = max profit from jobs[i:]
-        for i in range(n - 1, -1, -1):
-            dp[i] = max(dp[i + 1], jobs[i][2] + dp[nxt[i]])
-
-        return dp[0]
-        
 if __name__ == "__main__":
-    res = Solution().jobScheduling(startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70])
+    res = Solution().threeSumClosest(nums = [-1,2,1,-4], target = 1)
     print(res)
 
--- Python/0016_3Sum_Closest.py
+++ Python/0016_3Sum_Closest.py
@@ -1,25 +1,31 @@
 from typing import List
 
-
 class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:        
         nums.sort()
         n = len(nums)
-        res = float("inf")
+        res = nums[0] + nums[1] + nums[2]
         for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
             j = i + 1
             k = n - 1
             while j < k:
-                val = nums[i] + nums[j] + nums[k]
-                if val == target:
-                    return val
-                elif abs(val - target) < abs(res - target):
-                    res = val
-                if val < target:
+                tot = nums[i] + nums[j] + nums[k]
+                if abs(tot - target) < abs(res - target):
+                    res = tot
+                if tot == target:
+                    return tot
+                elif tot < target:
                     j += 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
                 else:
                     k -= 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
         return res
+
 
 
 if __name__ == "__main__":
