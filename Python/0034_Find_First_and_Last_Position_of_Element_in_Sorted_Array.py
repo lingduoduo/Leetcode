fdd0a192536904dab4f312b82108956a7c0b81dc
linghypshen@gmail.com <linghypshen@gmail.com>
2025-12-25 17:23:31 -0500

    Add code

--- Python/0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ Python/0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -1,124 +1,36 @@
 from typing import List
 
 
-class Solution(object):
-    def searchRange(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        if not nums:
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        if n == 0:
             return [-1, -1]
 
-        lower = self.lowerBound(nums, target)
-        upper = self.upperBound(nums, target)
-
-        if lower == upper:
-            return [-1, -1]
-        return [lower, upper - 1]
-
-    def lowerBound(self, nums, target):
-        """
-        return index such that target<=nums[index]
-        """
-        if not nums:
-            return -1
-
-        left = 0
-        right = len(nums)
-
+        # find first position >= target
+        left, right = 0, n
         while left < right:
             mid = left + (right - left) // 2
             if nums[mid] < target:
                 left = mid + 1
             else:
                 right = mid
-        return left
-
-    def upperBound(self, nums, target):
-        """
-        return index such that target<nums[index]
-        """
-        if not nums:
-            return -1
-
-        left = 0
-        right = len(nums)
-
-        while left <= right:
-            mid = left + (right - left) // 2
-            if nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid
-        return left
-
-        ###Second Try
-        if not nums:
+                
+        first = left
+        if first == n or nums[first] != target:
             return [-1, -1]
 
-        if target not in nums:
-            return [-1, -1]
-
-
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return [-1, -1]
-
-        if target not in nums:
-            return [-1, -1]
-
-        left1, right1 = 0, len(nums)
-        while left1 < right1:
-            mid1 = left1 + (right1 - left1) // 2
-            if nums[mid1] < target:
-                left1 = mid1 + 1
-            else:
-                right1 = mid1
-
-        left2, right2 = 0, len(nums)
-        while left2 < right2:
-            mid2 = left2 + (right2 - left2) // 2
-            if nums[mid2] <= target:
-                left2 = mid2 + 1
-            else:
-                right2 = mid2
-
-        if left1 == left2:
-            return [-1, -1]
-        return [left1, left2 - 1]
-
-
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = self.lower_bound(nums, target)
-        right = self.higher_bound(nums, target)
-        if left == right:
-            return [-1, -1]
-        return [left, right - 1]
-
-    def lower_bound(self, nums, target):
-        left, right = 0, len(nums)
-        while left < right:
-            mid = left + (right - left) // 2
-            if nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid
-        return left
-
-    def higher_bound(self, nums, target):
-        left, right = 0, len(nums)
+        # find first position > target
+        left, right = 0, n
         while left < right:
             mid = left + (right - left) // 2
             if nums[mid] <= target:
                 left = mid + 1
             else:
                 right = mid
-        return left
-
+        last = left - 1
+        return [first, last]
+    
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
