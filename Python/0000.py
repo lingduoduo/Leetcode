4460d8ac68ce5adeed695058a59911dea5df26b0
linghypshen@gmail.com <linghypshen@gmail.com>
2025-12-30 11:38:43 -0500

    add code

--- Python/0000.py
+++ Python/0000.py
@@ -6,34 +6,22 @@ from collections import deque, defaultdict
 from typing import List
 import random
 import bisect
+import math
 
 class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:        
-        nums.sort()
-        n = len(nums)
-        res = nums[0] + nums[1] + nums[2]
-        for i in range(n - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            j = i + 1
-            k = n - 1
-            while j < k:
-                tot = nums[i] + nums[j] + nums[k]
-                if abs(tot - target) < abs(res - target):
-                    res = tot
-                if tot == target:
-                    return tot
-                elif tot < target:
-                    j += 1
-                    while j < k and nums[j] == nums[j - 1]:
-                        j += 1
-                else:
-                    k -= 1
-                    while j < k and nums[k] == nums[k + 1]:
-                        k -= 1
-        return res
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        tot, cur, start = 0, 0, 0
+        n = len(gas)
+        for i in range(n):
+            print(i, cur, tot)
+            tot += gas[(i + 1) % n] - cost[i]
+            cur += gas[(i + 1) % n] - cost[i]
+            if cur < 0:
+                start = i + 1
+                cur = 0
+        return -1 if tot < 0 else start
 
 if __name__ == "__main__":
-    res = Solution().threeSumClosest(nums = [-1,2,1,-4], target = 1)
+    res = Solution().canCompleteCircuit(gas = [5,1,2,3,4], cost = [4,4,1,5,1])
     print(res)
 
