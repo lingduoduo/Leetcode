662a5142260f49d9cbe9c375e64422e964cd3766
linghypshen@gmail.com <linghypshen@gmail.com>
2025-12-30 10:36:12 -0500

    add code

--- Python/0670_Maximum_Swap.py
+++ Python/0670_Maximum_Swap.py
@@ -15,3 +15,20 @@ class Solution:
                 right = max_idx
         nums[left], nums[right] = nums[right], nums[left]
         return int("".join(nums))
+
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        strs = list(str(num))
+        last = [-1] * 10
+
+        for i, v in enumerate(strs):
+            last[ord(v) - ord('0')] = i
+        
+        for i, ch in enumerate(strs):
+            cur = ord(ch) - ord('0')
+            for d in range(9, cur, -1):
+                if last[d] > i:
+                    j = last[d]
+                    strs[i], strs[j] = strs[j], strs[i]
+                    return int("".join(strs))
+        return num
