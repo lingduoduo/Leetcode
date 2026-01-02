e58900989f0b132e293df8eb1b03d33d5d746dba
linghypshen@gmail.com <linghypshen@gmail.com>
2026-01-02 12:39:40 -0500

    add code

--- Python/0031_Next_Permutation.py
+++ Python/0031_Next_Permutation.py
@@ -17,6 +17,32 @@ class Solution(object):
         return nums
 
 
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+
+        first = -1
+        for i in range(n - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                first = i
+                break
+
+        if first == -1:
+            nums.reverse()
+            return
+
+        second = -1
+        for i in range(n - 1, first, -1):
+            if nums[i] > nums[first]:
+                second = i
+                break
+
+        # swap pivot and successor
+        nums[first], nums[second] = nums[second], nums[first]
+
+        # reverse suffix (in-place slice assignment)
+        nums[first + 1:] = nums[first + 1:][::-1]
+
 if __name__ == "__main__":
     nums = [1, 4, 3, 2]
     # nums = [1, 2, 3]
