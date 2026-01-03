72a3bc3a58505f12e58306090417794001090906
linghypshen@gmail.com <linghypshen@gmail.com>
2026-01-03 15:27:00 -0500

    Add code

--- Python/0282_Expression_Add_Operators.py
+++ Python/0282_Expression_Add_Operators.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def addOperators(self, num: str, target: int):
         """
@@ -21,42 +22,33 @@ class Solution:
             i += 1
         return result
 
-    def addOperatorsDFS(self, num, target, pos, operand1, operand2, expr, result):
-        if pos == len(num) and operand1 + operand2 == target:
-            result.append("".join(expr))
-        else:
-            val, i = 0, pos
-            val_str = ""
-            while i < len(num):
-                # print(expr)
-                val = val * 10 + ord(num[i]) - ord("0")
-                val_str += num[i]
-                # Avoid "00...".
-                if str(val) != val_str:
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+        n = len(num)
+        res: List[str] = []
+
+        def dfs(idx: int, path: List[str], value: int, last: int) -> None:
+            if idx == n:
+                if value == target:
+                    res.append("".join(path))
+                return
+
+            cur = 0
+            for i in range(idx, n):
+                if i > idx and num[idx] == "0":
                     break
 
-                # Case '+':
-                expr.append("+" + val_str)
-                self.addOperatorsDFS(
-                    num, target, i + 1, operand1 + operand2, val, expr, result
-                )
-                expr.pop()
+                cur = cur * 10 + (ord(num[i]) - ord("0"))
+                if idx == 0:
+                    dfs(i + 1, path + [num[idx : i + 1]], cur, cur)
+                else:
+                    dfs(i + 1, path + ["+", num[idx : i + 1]], value + cur, cur)
+                    dfs(i + 1, path + ["-", num[idx : i + 1]], value - cur, -cur)
+                    dfs(i + 1, path + ["*", num[idx : i + 1]], value - last + last * cur, last * cur)
 
-                # Case '-':
-                expr.append("-" + val_str)
-                self.addOperatorsDFS(
-                    num, target, i + 1, operand1 + operand2, -val, expr, result
-                )
-                expr.pop()
+        dfs(0, [], 0, 0)
+        return res
 
-                # Case '*':
-                expr.append("*" + val_str)
-                self.addOperatorsDFS(
-                    num, target, i + 1, operand1, operand2 * val, expr, result
-                )
-                expr.pop()
-
-                i += 1
 
 
 if __name__ == "__main__":
