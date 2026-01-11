16abc3313a756fa06b2dbd4a2e3f0f609a82352b
linghypshen@gmail.com <linghypshen@gmail.com>
2026-01-11 13:09:43 -0500

    add code

--- Python/0723_Candy_Crush.py
+++ Python/0723_Candy_Crush.py
@@ -44,3 +44,41 @@ class Solution:
                 # now that all items have been copied to their right spots, place zero's appropriately at the top of the board
                 for x in range(offset):
                     board[x][y] = 0
+
+class Solution:
+    def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
+        m, n = len(board), len(board[0])
+
+        while True:
+            mark = [[False] * n for _ in range(m)]
+        
+            for r in range(m):
+                for c in range(n - 2):
+                    v = board[r][c]
+                    if v != 0 and v == board[r][c + 1] == board[r][c + 2]:
+                        mark[r][c] = mark[r][c + 1] = mark[r][c + 2] = True
+            
+            for r in range(m - 2):
+                    for c in range(n):
+                        v = board[r][c]
+                        if v != 0 and v == board[r + 1][c] == board[r + 2][c]:
+                            mark[r][c] = mark[r + 1][c] = mark[r + 2][c] = True
+            
+            if not any(any(row) for row in mark):
+                return board
+            
+            for r in range(m):
+                for c in range(n):
+                    if mark[r][c]:
+                        board[r][c] = 0
+        
+            for c in range(n):
+                write = m - 1
+                for r in range(m - 1, -1, -1):
+                    if board[r][c] != 0:
+                        board[write][c] = board[r][c]
+                        write -= 1
+                for r in range(write, -1, -1):
+                    board[r][c] = 0
+
+
