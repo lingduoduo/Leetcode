7a11ecff0152a752fb6a241bf12c1cbab27b28f0
linghypshen@gmail.com <linghypshen@gmail.com>
2026-01-11 14:45:23 -0500

    add code

--- Python/1146_Snapshot_Array.py
+++ Python/1146_Snapshot_Array.py
@@ -1,3 +1,5 @@
+from bisect import bisect_right
+
 class SnapshotArray:
     def __init__(self, length: int):
         self.dif = {}
@@ -16,7 +18,28 @@ class SnapshotArray:
         else:
             return 0
 
+class SnapshotArray:
+    def __init__(self, length: int):
+        self.snap_id = 0
+        self.snapshots = [[(0, 0)] for _ in range(length)]
 
+    def set(self, index: int, val: int) -> None:
+        h = self.snapshots[index]
+        if h[-1][0] == self.snap_id:
+            h[-1] = (self.snap_id, val)
+        else:
+            h.append((self.snap_id, val))
+
+    def snap(self) -> int:
+        sid = self.snap_id
+        self.snap_id += 1
+        return sid
+
+    def get(self, index: int, snap_id: int) -> int:
+        h = self.snapshots[index]
+        i = bisect_right(h, (snap_id, 10**18)) - 1
+        return h[i][1]
+    
 # Your SnapshotArray object will be instantiated and called as such:
 # obj = SnapshotArray(length)
 # obj.set(index,val)
