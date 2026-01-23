489ed36a46c2f68b53496b7eacbfbcd986aa2ed3
linghypshen@gmail.com <linghypshen@gmail.com>
2026-01-23 16:43:51 -0500

    Add code

--- Python/0322_Coin_Change.py
+++ Python/0322_Coin_Change.py
@@ -1,49 +1,16 @@
-class Solution(object):
-    def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
-        dp = [float("inf") * (amount + 1)]
-
-        dp[0] = 0
-
-        for i in range(len(coins)):
-            for j in range(coins[i], amount + 1):
-                dp[j] = min(dp[j], dp[j - coins[i]] + 1)
-        return -1 if dp[-1] == float(-1) else dp[-1]
-
-
-class Solution(object):
-    def coinChange(self, coins, amount):
-        dp = [0] + [-1] * len(amount)
-
-        for x in range(len(amount)):
-            if dp[x] < 0:
-                continue
-
-            for c in coins:
-                if x + c > amount:
-                    continue
-                if dp[x + c] < 0 or dp[x + c] > dp[x] + 1:
-                    dp[x + c] = dp[x] + 1
-        return dp[amount]
-
+from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float("inf")] * (amount + 1)
+        dp = [float('inf')] * (amount + 1)
         dp[0] = 0
         for coin in coins:
             for i in range(coin, amount + 1):
                 dp[i] = min(dp[i], dp[i - coin] + 1)
-        return dp[amount] if dp[amount] != float("inf") else -1
+        return dp[-1] if dp[-1] != float('inf') else -1    
 
 
 from functools import lru_cache
-
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         @lru_cache(None)
