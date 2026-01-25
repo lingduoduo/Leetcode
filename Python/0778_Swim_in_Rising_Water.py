9f5a24d8f8b75da5ba834123b3e9b70eb3172fae
linghypshen@gmail.com <linghypshen@gmail.com>
2026-01-25 13:28:57 -0500

    Add code

--- Python/0778_Swim_in_Rising_Water.py
+++ Python/0778_Swim_in_Rising_Water.py
@@ -1,20 +1,27 @@
-from typing import List
 import heapq
 
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
-        pq = [(grid[0][0], 0, 0)]     # (time_so_far, x, y)
-        visited = {(0, 0)}
+        res = float("-inf")
+        nodes = []
+        heapq.heappush(nodes, (grid[0][0], 0, 0))
+        visited = set()
 
-        while pq:
-            t, x, y = heapq.heappop(pq)
-            if (x, y) == (n - 1, n - 1):
-                return t
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
+        while nodes:
+            val, i, j = heapq.heappop(nodes)
+
+            if (i, j) in visited:
+                continue
+            visited.add((i, j))
+
+            res = max(res, val)
+            if i == n - 1 and j == n - 1:
+                return res
+
+            for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+                nx, ny = i + dx, j + dy
                 if 0 <= nx < n and 0 <= ny < n and (nx, ny) not in visited:
-                    visited.add((nx, ny))
-                    nt = max(t, grid[nx][ny])  # time needed to enter neighbor
-                    heapq.heappush(pq, (nt, nx, ny))
+                    heapq.heappush(nodes, (grid[nx][ny], nx, ny))
+
+        return res
