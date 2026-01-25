fbb7834476ae0d196508ef645dc1cc8e710a73ea
linghypshen@gmail.com <linghypshen@gmail.com>
2026-01-24 20:54:29 -0500

    add code

--- Python/0865_Smallest_Subtree_with_all_the_Deepest_Nodes.py
+++ Python/0865_Smallest_Subtree_with_all_the_Deepest_Nodes.py
@@ -1,9 +1,12 @@
+from collections import deque
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+
+        self.right = right
 class Solution:
     def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
         def dfs(root):
@@ -20,3 +23,27 @@ class Solution:
                 return (root, depth)
 
         return dfs(root)[0]
+
+
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        if not root: return None
+        parent = {root: None}
+        q = deque([root])
+
+        while q:
+            deepest = list(q)
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    parent[node.left] = node
+                    q.append(node.left)
+                if node.right:
+                    parent[node.right] = node
+                    q.append(node.right)
+
+        cur = set(deepest)
+        while len(cur) > 1:
+            cur = {parent[node] for node in cur}
+            
+        return next(iter(cur))
