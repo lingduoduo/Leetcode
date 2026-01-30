9cb7c89dd29cde88df921a3528516b1a95fb0ab2
linghypshen@gmail.com <linghypshen@gmail.com>
2026-01-30 15:36:40 -0500

    Add code

--- Python/0371_Sum_of_Two_Integers.py
+++ Python/0371_Sum_of_Two_Integers.py
@@ -19,3 +19,23 @@ class Solution:
         while b != 0:
             a, b = (a ^ b) & mask, ((a & b) << 1) & mask
         return a if a <= MAX else ~(a ^ mask)
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        res = 0
+        carry = 0
+        bit = 1
+
+        while a or b or carry:
+            a_bit = a & 1
+            b_bit = b & 1
+
+            s = a_bit ^ b_bit ^ carry
+            carry = (a_bit & b_bit) | (a_bit & carry) | (b_bit & carry)
+
+            res |= s * bit
+            bit <<= 1
+            a >>= 1
+            b >>= 1
+
+        return res  
