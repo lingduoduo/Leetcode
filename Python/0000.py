e9fe3c848328a87819aeba41128ad22395836f06
linghypshen@gmail.com <linghypshen@gmail.com>
2026-02-10 14:48:04 -0500

    add code

--- Python/0000.py
+++ Python/0000.py
@@ -16,29 +16,46 @@ import math
     #             res = max(res, len(set(arr[i] + arr[j])))
     #     return res
 
+from typing import List
 
 class SparseVector:
     def __init__(self, nums: List[int]):
         self.d = {i: v for i, v in enumerate(nums) if v != 0}
-    
-    def prod(self, vec: "SparseVector"):
-        res = 0
-        if len(vec.d.keys) <= len(self.d):
-            small = vec.d
-            large = self.d
-        else:
-            small = self.d
-            large = vec.d
-        for i in small:
-            if i in large:
-                res += small[i] * large[i]
-        return res
-        
 
+    def dotProd(self, vec: "SparseVector") -> int:
+        small, large = (self.d, vec.d) if len(self.d) <= len(vec.d) else (vec.d, self.d)
+        res = 0
+        for k, v in small.items():
+            res += v * large.get(k, 0)
+        return res
+
+
+class Solution:
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+        m1, n1 = len(mat1), len(mat1[0])
+        m2, n2 = len(mat2), len(mat2[0])
+
+        rows = [SparseVector(mat1[i]) for i in range(m1)]
+
+        cols = []
+        for j in range(n2):
+            col_j = [mat2[i][j] for i in range(m2)]
+            cols.append(SparseVector(col_j))
+
+        res = [[0] * n2 for _ in range(m1)]
+        for i in range(m1):
+            rvec = rows[i]
+            if not rvec.d:
+                continue
+            for j in range(n2):
+                val = rvec.dotProd(cols[j])
+                if val:
+                    res[i][j] = val
+        return res
 
 
 if __name__ == "__main__":
-    res = Solution().subarraySum(nums = [1,2,3], k = 3)
+    res = Solution().generateParenthesis(n=3)
     print(res)
 
 
--- Python/0311_Sparse_Matrix_Multiplication.py
+++ Python/0311_Sparse_Matrix_Multiplication.py
@@ -1,24 +1,29 @@
 from typing import List
 
 
+from typing import List
+
 class Solution:
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
         m1, n1 = len(mat1), len(mat1[0])
         m2, n2 = len(mat2), len(mat2[0])
-        res = [[0 for j in range(n2)] for i in range(m1)]
-        x = [dict() for i in range(m1)]
-        y = [dict() for i in range(n2)]
+        if n1 != m2:
+            return []
+
+        res = [[0] * n2 for _ in range(m1)]
+        row2 = [[] for _ in range(m2)]
+        for r in range(m2):
+            for c in range(n2):
+                v = mat2[r][c]
+                if v != 0:
+                    row2[r].append((c, v))
+
         for i in range(m1):
-            for j in range(n1):
-                if mat1[i][j] != 0:
-                    x[i][j] = mat1[i][j]
-        for i in range(m2):
-            for j in range(n2):
-                if mat2[i][j] != 0:
-                    y[j][i] = mat2[i][j]
-        for i in range(m1):
-            for j in range(n2):
-                for r in x[i]:
-                    if r in y[j]:
-                        res[i][j] += x[i][r] * y[j][r]
+            for k in range(n1):          # shared dimension
+                a = mat1[i][k]
+                if a == 0:
+                    continue
+                for j, b in row2[k]:     # mat2 row k -> columns j
+                    res[i][j] += a * b
+
         return res
