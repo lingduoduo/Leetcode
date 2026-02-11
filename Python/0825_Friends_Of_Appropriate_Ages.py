88dab66050854875b666bd5cb48e2e2c0e515dc6
linghypshen@gmail.com <linghypshen@gmail.com>
2026-02-11 10:25:49 -0500

    add code

--- Python/0825_Friends_Of_Appropriate_Ages.py
+++ Python/0825_Friends_Of_Appropriate_Ages.py
@@ -1,5 +1,5 @@
 import collections
-
+from typing import List
 
 class Solution:
     def numFriendRequests(self, ages: List[int]) -> int:
@@ -12,4 +12,30 @@ class Solution:
                     res += count[x] * (count[x] - 1)
                 else:
                     res += count[x] * count[y]
+        return res
+
+
+class Solution:
+    def numFriendRequests(self, ages: List[int]) -> int:
+        MAX = 120
+        d = [0] * (MAX + 1)
+        for a in ages:
+            d[a] += 1
+
+        # prefix[i] = number of people with age <= i
+        prefix = [0] * (MAX + 1)
+        for a in range(1, MAX + 1):
+            prefix[a] = prefix[a - 1] + d[a]
+
+        res = 0
+        for a in range(15, MAX + 1):  # ages < 15 can never send valid requests
+            if d[a] == 0:
+                continue
+
+            # valid b must satisfy: b > 0.5*a + 7 and b <= a
+            low = math.floor(0.5 * a + 7)
+            valid_b = prefix[a] - prefix[low]  # count of ages in (low, a]
+            # each of the cnt[a] people can send to (valid_b - 1) people (exclude self when b == a)
+            res += d[a] * (valid_b - 1)
+
         return res
