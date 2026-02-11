c6d3ca0d78f24c260bdc78f83df241fb6395f32f
linghypshen@gmail.com <linghypshen@gmail.com>
2026-02-11 11:41:46 -0500

    add code

--- Python/0735_Asteroid_Collision.py
+++ Python/0735_Asteroid_Collision.py
@@ -12,3 +12,23 @@ class Solution:
             if asteroid != None:
                 stack.append(asteroid)
         return stack
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for num in asteroids:
+            alive = True
+
+            # Only possible collision: right-moving on stack, left-moving incoming
+            while alive and stack and stack[-1] > 0 and num < 0:
+                if stack[-1] < -num:          # top explodes
+                    stack.pop()
+                    continue
+                elif stack[-1] == -num:       # both explode
+                    stack.pop()
+                alive = False               # incoming explodes (or tied case handled)
+            
+            if alive:
+                stack.append(num)
+
+        return stack
