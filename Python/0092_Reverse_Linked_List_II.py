a91e8a2a17fccc31449eec48ca4f40f5159ab3cc
linghypshen@gmail.com <linghypshen@gmail.com>
2026-02-23 09:46:03 -0500

    add code

--- Python/0092_Reverse_Linked_List_II.py
+++ Python/0092_Reverse_Linked_List_II.py
@@ -35,3 +35,26 @@ class Solution:
         p1.next = pre
 
         return dummyNode.next
+
+
+class Solution:
+    def reverseBetween(
+        self, head: Optional[ListNode], left: int, right: int
+    ) -> Optional[ListNode]:
+        if not head or left == right:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        prev = dummy
+        for _ in range(left - 1):
+            prev = prev.next
+
+        cur = prev.next
+
+        for _ in range(right - left):
+            cur.next.next, prev.next, cur.next = prev.next, cur.next, cur.next.next
+
+        return dummy.next
+
