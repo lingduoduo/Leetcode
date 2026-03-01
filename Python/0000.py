6c211671b0a7010c74a0f97eb734d63f1965424f
linghypshen@gmail.com <linghypshen@gmail.com>
2026-02-28 22:17:31 -0500

    add code

--- Python/0000.py
+++ Python/0000.py
@@ -2,24 +2,44 @@ import heapq
 from typing import List
 from collections import defaultdict, deque, Counter
 from typing import List, Tuple, Optional
+import math
+
+from typing import Optional
 
 
 class Solution:
-    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow, fast = head, head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
-                break
-        if not fast or not fast.next:
-            return None
-        slow = head
-        while slow != fast:
-            slow = slow.next
-            fast = fast.next
-        return slow
+    def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, cnt):
+            if not node: return 0
+
+            if node.left:
+                if node.left.val == node.val:
+                    left = dfs(node.left, cnt + 1)
+                else:
+                    left = dfs(node.left, cnt)
+            
+            if node.right:
+                if node.right.val == node.val:
+                    right = dfs(node.right, cnt + 1)
+                else:
+                    right = dfs(node.right, cnt)
+            
+            if node.left and node.right:
+                if node.left.val == node.right.val:
+                    return left + right
+                else:
+                    return max(left, right)
+            else:
+                return left or right
+        
+        dfs(root, 0)
+            
+
+
+                
+
+
 
 if __name__ == "__main__":
-    res = Solution().largestRectangleArea(heights = [2,1,5,6,2,3])
+    res = Solution().concatenatedBinary(n=3)
     print(res)
--- Python/0687_Longest_Univalue_Path.py
+++ Python/0687_Longest_Univalue_Path.py
@@ -1,55 +1,33 @@
-###Definition for a binary tree node.
-###class TreeNode(object):
-###    def __init__(self, x):
-###        self.val = x
-###        self.left = None
-###        self.right = None
+# Definition for a binary tree node.
+class TreeNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.left = None
+       self.right = None
 
+class Solution:
+    def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, cnt):
+            if not node: return 0
 
-class Solution(object):
-    def longestUnivaluePath(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        self.result = 0
-
-        def univalue(root):
-            if root is None:
-                return 0
-
-            right = univalue(root.right)
-            left = univalue(root.left)
-            right_result = 0
-            left_result = 0
-
-            if root.right and root.val == root.right.val:
-                right_result = right + 1
-
-            if root.left and root.val == root.left.val:
-                left_result = left + 1
-
-            self.result = max(self.result, left_result + right_result)
-            return max(left_result, right_result)
-
-        univalue(root)
-        return self.result
-
-        if not root:
-            return 0
-        self.res = 0
-        self.getPath(root)
-        return self.res
-
-    def getPath(self, root):
-        if not root:
-            return 0
-        left = self.getPath(root.left)
-        right = self.getPath(root.right)
-        pl, pr = 0, 0
-        if root.left and root.left.val == root.val:
-            pl = 1 + left
-        if root.right and root.right.val == root.val:
-            pr = 1 + right
-        self.res = max(self.res, pl + pr)
-        return max(pl, pr)
+            if node.left:
+                if node.left.val == node.val:
+                    left = dfs(node.left, cnt + 1)
+                else:
+                    left = dfs(node.left, cnt)
+            
+            if node.right:
+                if node.right.val == node.val:
+                    right = dfs(node.right, cnt + 1)
+                else:
+                    right = dfs(node.right, cnt)
+            
+            if node.left and node.right:
+                if node.left.val == node.right.val:
+                    return left + right
+                else:
+                    return max(left, right)
+            else:
+                return left or right
+        
+        dfs(root, 0)
