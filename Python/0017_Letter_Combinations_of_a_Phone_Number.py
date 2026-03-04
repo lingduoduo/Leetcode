f348a57fa294b66321e35c8ce2c06ac34daec2fc
linghypshen@gmail.com <linghypshen@gmail.com>
2026-03-04 16:04:01 -0500

    add code

--- Python/0017_Letter_Combinations_of_a_Phone_Number.py
+++ Python/0017_Letter_Combinations_of_a_Phone_Number.py
@@ -1,36 +1,5 @@
 from typing import List
 
-
-class Solution(object):
-    def letterCombinations(self, digits: str) -> List[str]:
-        if digits == "":
-            return []
-
-        d = dict()
-        d[0] = "0"
-        d[1] = "1"
-        d[2] = "abc"
-        d[3] = "def"
-        d[4] = "ghi"
-        d[5] = "jkl"
-        d[6] = "mno"
-        d[7] = "pqrs"
-        d[8] = "tuv"
-        d[9] = "wxyz"
-
-        res = [""]
-        if not digits:
-            return res
-
-        for digit in digits:
-            tmp = []
-            for ch in d[int(digit)]:
-                for str in res:
-                    tmp.append(str + ch)
-            res = tmp
-        return res
-
-
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if digits == "":
@@ -59,36 +28,6 @@ class Solution:
                 path.append(cha)
                 dfs(idx + 1, path)
         dfs(0, [])
-        return res
-
-
-class Solution(object):
-    def letterCombinations(self, digits: str) -> List[str]:
-        if digits == "":
-            return []
-
-        d = dict()
-        d[0] = "0"
-        d[1] = "1"
-        d[2] = "abc"
-        d[3] = "def"
-        d[4] = "ghi"
-        d[5] = "jkl"
-        d[6] = "mno"
-        d[7] = "pqrs"
-        d[8] = "tuv"
-        d[9] = "wxyz"
-
-        res = [""]
-        if not digits:
-            return res
-
-        for cha in digits:
-            cur = res
-            part = []
-            for strs in cur:
-                part += [strs + cha for cha in d[int(cha)]]
-            res = part
         return res
 
 
@@ -123,6 +62,31 @@ class Solution:
         getCombinations(digits, 0, "")
         return result
 
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return None
+            
+        letterMap = [
+            "abc",  # 2
+            "def",  # 3
+            "ghi",  # 4
+            "jkl",  # 5
+            "mno",  # 6
+            "pqrs",  # 7
+            "tuv",  # 8
+            "wxyz"  # 9
+        ]
+
+        res = [""]
+        for digit in digits: #"23"
+            cur = []
+            for pre in res:
+                for ch in letterMap[ord(digit) - ord("2")]:
+                    cur.append(pre + ch)
+            res = cur[:]
+        return res
 
 if __name__ == "__main__":
     digits = "23"
--- Python/0542_01_Matrix.py
+++ Python/0542_01_Matrix.py
@@ -1,28 +1,29 @@
 from typing import List
-import collections
+from collections import deque
 
 
 class Solution:
-    def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
-        m, n = len(matrix), len(matrix[0])
-        dist = [[float("inf")] * n for _ in range(m)]
-        q = collections.deque()
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+
+        que = deque([])
         for i in range(m):
             for j in range(n):
-                if matrix[i][j] == 0:
-                    dist[i][j] = 0
-                    q.append((i, j))
-        dirs = [[0, 1], [0, -1], [-1, 0], [1, 0]]
-        while q:
-            curr = q.popleft()
-            for d in dirs:
-                x = curr[0] + d[0]
-                y = curr[1] + d[1]
-                if x >= 0 and x < m and y >= 0 and y < n and matrix[x][y] == 1:
-                    if dist[x][y] > dist[curr[0]][curr[1]] + 1:
-                        dist[x][y] = dist[curr[0]][curr[1]] + 1
-                        q.append((x, y))
-        return dist
+                if mat[i][j] == 0:
+                    que.append((i, j))
+                else:
+                    mat[i][j] = float("inf")
+                    
+        while que:
+            for _ in range(len(que)):
+                i, j = que.popleft()
+                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    nx, ny = i + dx, j + dy
+                    if 0 <= nx < m and 0 <= ny < n and mat[nx][ny] > mat[i][j] + 1:
+                        mat[nx][ny] = mat[i][j] + 1
+                        que.append((nx, ny))
+        return mat
 
 
 if __name__ == "__main__":
